4fc30c63b5eba3781a710dc40a796f833566b21a
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-23 11:18:34 +0000

    blank

--- html/test/kakinaka/flush.php
+++ html/test/kakinaka/flush.php
@@ -3,18 +3,16 @@
 print("test");
   MyFlush();
 
-
-sleep(2);
-
 for($i = 0; $i < 100; $i++){
 	print("aa");
 	MyFlush();
 }
 
 function MyFlush() {
-  flush();
-  ob_end_flush();
-  ob_start();
+	flush();
+	ob_end_flush();
+	ob_start();
+	sleep(1);
 }
 
 
