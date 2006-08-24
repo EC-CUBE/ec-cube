ccd21121505783fb2ac55aa97338d2abe551404e
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-24 12:08:53 +0000

    blank

--- html/admin/customer/index.php
+++ html/admin/customer/index.php
@@ -152,7 +152,6 @@ if ($_POST['mode'] == "search" || $_POST['mode'] == "csv"  || $_POST['mode'] == 
 		}else{
 			$searchSql = $objSelect->getList();
 		}
-		$searchSql .= ",";
 		$objPage->search_data = $objConn->getAll($searchSql, $objSelect->arrVal);
 
 		switch($_POST['mode']) {
