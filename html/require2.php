602a4f56d39443749f28f68fd6b9521bb21d44b0
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-24 03:51:14 +0000

    *** empty log message ***

---
+++ html/require2.php
@@ -0,0 +1,28 @@
+<?php
+$include_dir = realpath(dirname( __FILE__));
+require_once($include_dir . "/../data/conf/conf.php");	
+require_once($include_dir . "/../data/lib/glib.php");
+require_once($include_dir . "/../data/lib/slib.php");
+require_once($include_dir . "/../data/class/SC_View.php");
+require_once($include_dir . "/../data/class/SC_DbConn.php");
+require_once($include_dir . "/../data/class/SC_Session.php");
+require_once($include_dir . "/../data/class/SC_Query.php");
+require_once($include_dir . "/../data/class/SC_SelectSql.php");
+require_once($include_dir . "/../data/class/SC_CheckError.php");
+require_once($include_dir . "/../data/class/SC_PageNavi.php");
+require_once($include_dir . "/../data/class/SC_Date.php");
+require_once($include_dir . "/../data/class/SC_Image.php");
+require_once($include_dir . "/../data/class/SC_UploadFile.php");
+require_once($include_dir . "/../data/class/SC_JpGraph.php");
+require_once($include_dir . "/../data/class/SC_SiteInfo.php");
+require_once($include_dir . "/../data/class/GC_SendMail.php");
+require_once($include_dir . "/../data/class/SC_FormParam.php");
+require_once($include_dir . "/../data/class/SC_CartSession.php");
+require_once($include_dir . "/../data/class/SC_SiteSession.php");
+require_once($include_dir . "/../data/class/SC_Customer.php");
+require_once($include_dir . "/../data/class/SC_Cookie.php");
+require_once($include_dir . "/../data/class/SC_Page.php");
+require_once($include_dir . "/../data/class/SC_Pdf.php");
+require_once(ROOT_DIR."data/include/page_layout.inc");
+
+?>
