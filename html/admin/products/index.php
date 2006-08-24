38e41c0f12dfc8b8c364db053436a9c31903e49e
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-24 01:01:07 +0000

    blank

--- html/admin/products/index.php
+++ html/admin/products/index.php
@@ -250,7 +250,7 @@ if ($_POST['mode'] == "search" || $_POST['mode'] == "csv"  || $_POST['mode'] == 
 			// 検索結果の取得
 			$objPage->arrProducts = $objQuery->select($col, $from, $where, $arrval);
 			
-			sfprintr($objQuery->select($col, $from, $where, $arrval););
+			sfprintr($objQuery->select($col, $from, $where, $arrval));
 			
 //			sfprintr($objPage->arrProducts);
 
