cc24bbd05d150b65fe449bb15cbc78394897ac3a
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-24 03:40:02 +0000

    blank

--- html/admin/products/index.php
+++ html/admin/products/index.php
@@ -260,150 +260,149 @@ if ($_POST['mode'] == "search" || $_POST['mode'] == "csv"  || $_POST['mode'] == 
 	}
 }
 */
-Array
+$arrProducts = Array
 (
-    '0' => Array
+    '0' => 'Array
         (
-            'product_id' => 18,
-            'name' => test,
-            'category_id' => 11,
-            'main_list_image' => 08172054_44e458f942afc.gif,
-            'status' => 1,
-            'product_code' => cd 01,
-            'price01' => 500,
-            'price02' => 500,
-            'stock' => 43,
-            'stock_unlimited' => ""
+            'product_id' => '18,'
+            'name' => 'test,'
+            'category_id' => '11,'
+            'main_list_image' => '08172054_44e458f942afc.gif,'
+            'status' => '1,'
+            'product_code' => 'cd 01,'
+            'price01' => '500,'
+            'price02' => '500,'
+            'stock' => '43,'
+            'stock_unlimited' => '""
         ),
 
-    '1' => Array
+    '1' => 'Array
         (
-            'product_id' => 14,
-            'name' => LPOエビス,
-            'category_id' => 10,
-            'main_list_image' => 08171740_44e42b7f67953.gif,
-            'status' => 1,
-            'product_code' => LPO,
-            'price01' => 15000,
-            'price02' => 15000,
-            'stock' => 14999,
-            'stock_unlimited' => ""
+            'product_id' => '14,'
+            'name' => 'LPOエビス,'
+            'category_id' => '10,'
+            'main_list_image' => '08171740_44e42b7f67953.gif,'
+            'status' => '1,'
+            'product_code' => 'LPO,'
+            'price01' => '15000,'
+            'price02' => '15000,'
+            'stock' => '14999,'
+            'stock_unlimited' => '""
         ),
 
-    '2' => Array
+    '2' => 'Array
         (
-            'product_id' => 16,
-            'name' => LPOエビス,
-            'category_id' => 10,
-            'main_list_image' => 08181941_44e59975c535d.gif,
-            'status' => 1,
-            'product_code' => LPO,
-            'price01' => 15000,
-            'price02' => 15000,
-            'stock' => 14927,
-            'stock_unlimited' => ""
+            'product_id' => '16,'
+            'name' => 'LPOエビス,'
+            'category_id' => '10,'
+            'main_list_image' => '08181941_44e59975c535d.gif,'
+            'status' => '1,'
+            'product_code' => 'LPO,'
+            'price01' => '15000,'
+            'price02' => '15000,'
+            'stock' => '14927,'
+            'stock_unlimited' => '""
         ),
 
-    '3' => Array
+    '3' => 'Array
         (
-            'product_id' => 15,
-            'name' => LPOエビス,
-            'category_id' => 10,
-            'main_list_image' => 08171740_44e42b7f67953.gif,
-            'status' => 1,
-            'product_code' => LPO,
-            'price01' => 15000,
-            'price02' => 15000,
-            'stock' => 14998,
-            'stock_unlimited' => ""
+            'product_id' => '15,'
+            'name' => 'LPOエビス,'
+            'category_id' => '10,'
+            'main_list_image' => '08171740_44e42b7f67953.gif,'
+            'status' => '1,'
+            'product_code' => 'LPO,'
+            'price01' => '15000,'
+            'price02' => '15000,'
+            'stock' => '14998,'
+            'stock_unlimited' => '""
+        ),
+    '4' => 'Array
+        (
+            'product_id' => '17,'
+            'name' => 'LPOエビス,'
+            'category_id' => '15,'
+            'main_list_image' => '08171740_44e42b7f67953.gif,'
+            'status' => '1,'
+            'product_code' => 'LPO,'
+            'price01' => '15000,'
+            'price02' => '15000,'
+            'stock' => '0,'
+            'stock_unlimited' => '""
         ),
 
-    '4' => Array
+    '5' => 'Array
         (
-            'product_id' => 17,
-            'name' => LPOエビス,
-            'category_id' => 15,
-            'main_list_image' => 08171740_44e42b7f67953.gif,
-            'status' => 1,
-            'product_code' => LPO,
-            'price01' => 15000,
-            'price02' => 15000,
-            'stock' => 0,
-            'stock_unlimited' => ""
+            'product_id' => '13,'
+            'name' => 'LPOエビス,'
+            'category_id' => '10,'
+            'main_list_image' => '08171740_44e42b7f67953.gif,'
+            'status' => '1,'
+            'product_code' => 'LPO,'
+            'price01' => '15000,'
+            'price02' => '15000,'
+            'stock' => '15000,'
+            'stock_unlimited' => '""
         ),
 
-    '5' => Array
+    '6' => 'Array
         (
-            'product_id' => 13,
-            'name' => LPOエビス,
-            'category_id' => 10,
-            'main_list_image' => 08171740_44e42b7f67953.gif,
-            'status' => 1,
-            'product_code' => LPO,
-            'price01' => 15000,
-            'price02' => 15000,
-            'stock' => 15000,
-            'stock_unlimited' => ""
+            'product_id' => '12,'
+            'name' => 'LPOエビス,'
+            'category_id' => '10,'
+            'main_list_image' => '08171740_44e42b7f67953.gif,'
+            'status' => '1,'
+            'product_code' => 'LPO,'
+            'price01' => '15000,'
+            'price02' => '15000,'
+            'stock' => '15000,'
+            'stock_unlimited' => '""
         ),
 
-    '6' => Array
+    '7' => 'Array
         (
-            'product_id' => 12,
-            'name' => LPOエビス,
-            'category_id' => 10,
-            'main_list_image' => 08171740_44e42b7f67953.gif,
-            'status' => 1,
-            'product_code' => LPO,
-            'price01' => 15000,
-            'price02' => 15000,
-            'stock' => 15000,
-            'stock_unlimited' => ""
+            'product_id' => '11,'
+            'name' => 'LPOエビス,'
+            'category_id' => '10,'
+            'main_list_image' => '08171740_44e42b7f67953.gif,'
+            'status' => '1,'
+            'product_code' => 'LPO,'
+            'price01' => '15000,'
+            'price02' => '15000,'
+            'stock' => '15000,'
+            'stock_unlimited' => '""
+        ),
+    '8' => 'Array
+        (
+            'product_id' => '10,'
+            'name' => 'LPOエビス,'
+            'category_id' => '10,'
+            'main_list_image' => '08171740_44e42b7f67953.gif,'
+            'status' => '1,'
+            'product_code' => 'LPO,'
+            'price01' => '15000,'
+            'price02' => '15000,'
+            'stock' => '15000,'
+            'stock_unlimited' => '""
         ),
 
-    '7' => Array
+    '9' => 'Array
         (
-            'product_id' => 11,
-            'name' => LPOエビス,
-            'category_id' => 10,
-            'main_list_image' => 08171740_44e42b7f67953.gif,
-            'status' => 1,
-            'product_code' => LPO,
-            'price01' => 15000,
-            'price02' => 15000,
-            'stock' => 15000,
-            'stock_unlimited' => ""
-        ),
-
-    '8' => Array
-        (
-            'product_id' => 10,
-            'name' => LPOエビス,
-            'category_id' => 10,
-            'main_list_image' => 08171740_44e42b7f67953.gif,
-            'status' => 1,
-            'product_code' => LPO,
-            'price01' => 15000,
-            'price02' => 15000,
-            'stock' => 15000,
-            'stock_unlimited' => ""
-        ),
-
-    '9' => Array
-        (
-            'product_id' => 9,
-            'name' => LPOエビス,
-            'category_id' => 10,
-            'main_list_image' => 08171740_44e42b7f67953.gif,
-            'status' => 1,
-            'product_code' => LPO,
-            'price01' => 15000,
-            'price02' => 15000,
-            'stock' => 15000,
-            'stock_unlimited' => ""
+            'product_id' => '9,'
+            'name' => 'LPOエビス,'
+            'category_id' => '10,'
+            'main_list_image' => '08171740_44e42b7f67953.gif,'
+            'status' => '1,'
+            'product_code' => 'LPO,'
+            'price01' => '15000,'
+            'price02' => '15000,'
+            'stock' => '15000,'
+            'stock_unlimited' => '""
         )
 
-)
+);
 
+$objPage->arrProducts
 
 
 // カテゴリの読込
