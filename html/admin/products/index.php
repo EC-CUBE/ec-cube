0d52ca749bcd56372ad773d89ebf0d9fdafc714a
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-24 03:33:04 +0000

    blank

--- html/admin/products/index.php
+++ html/admin/products/index.php
@@ -10,11 +10,9 @@ class LC_Page {
 	var $arrPageMax;
 	function LC_Page() {
 		$this->tpl_mainpage = 'products/index.tpl';
-//		$this->tpl_mainpage = 'order/index.tpl';
 		$this->tpl_mainno = 'products';
 		$this->tpl_subnavi = 'products/subnavi.tpl';
 		$this->tpl_subno = 'index';
-		$this->tpl_pager = ROOT_DIR . 'data/Smarty/templates/admin/pager.tpl';
 		$this->tpl_subtitle = '商品マスタ';
 		
 		global $arrPageMax;
