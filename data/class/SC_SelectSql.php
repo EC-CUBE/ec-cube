ba9a11256216d5fb6d87ed3511ff63c25e869622
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-25 04:11:58 +0000

    blank

--- data/class/SC_SelectSql.php
+++ data/class/SC_SelectSql.php
@@ -71,7 +71,7 @@ class SC_SelectSql {
 		
 		// FROM
 //		$date1 = date("Y/m/d", mktime(0,0,0,$from_month,$from_day,$from_year));	
-		$date1 = $from_year . "/" . $from_month . "/" . $from_day;
+		$date1 = $from_year . "a/" . $from_month . "/" . $from_day;
 		
 		sfprintr($date1);
 		
