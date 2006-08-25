19085c7e07135024957a6e4be63fc03d3c9b9531
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-25 04:13:12 +0000

    blank

--- data/class/SC_SelectSql.php
+++ data/class/SC_SelectSql.php
@@ -81,7 +81,7 @@ class SC_SelectSql {
 		// 開始期間だけ指定の場合
 		if( ( $from_year != "" ) && ( $from_month != "" ) && ( $from_day != "" ) &&	( $to_year == "" ) && ( $to_month == "" ) && ( $to_day == "" ) ) {
 			$date1 = date("Y/m/d", mktime(0,0,0,$from_month,$from_day,$from_year));
-			$this->setWhere( $column ." >= ?" );
+			$this->setWhere( $column ." >= " . $date1 );
 			$return = array($date1);
 		}
 
