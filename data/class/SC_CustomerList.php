02e3aa5ea4eb9f09b48a0c6b23a78330cf601875
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-26 08:35:55 +0000

    blank

--- data/class/SC_CustomerList.php
+++ data/class/SC_CustomerList.php
@@ -246,7 +246,7 @@ class SC_CustomerList extends SC_SelectSql {
 		
 		sfprintr($array);
 		$limit = $array['page_rows'];
-		$offset = $array['page_rows'] * $array['search_pageno'];
+//		$offset = $array['page_rows'] * $array['search_pageno'];
 		$this->setLimitOffset($limit, $offset);
 
 		$this->setOrder( "customer_id DESC" );
