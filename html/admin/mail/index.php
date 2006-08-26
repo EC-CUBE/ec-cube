1f24a2ba6f55e409dde50587acbe141e1a4dd844
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-26 09:35:59 +0000

    blank

--- html/admin/mail/index.php
+++ html/admin/mail/index.php
@@ -217,7 +217,7 @@ case 'regist_complete':
 			$objPage->tpl_mainpage = 'mail/input_confirm.tpl';
 		} else if( $_POST['mode'] == 'regist_complete' ){
 			lfRegistData($objPage->list_data);
-			header("Location: /admin/mail/sendmail.php");
+			header("Location: /admin/mail/sendmail.php?mode=now");
 			exit;			
 		}
 	}
