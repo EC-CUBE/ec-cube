822ac852d61bf03fa3ccc920d49daa4219ad4cf0
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-28 10:24:26 +0000

    blank

--- html/install/index.php
+++ html/install/index.php
@@ -277,7 +277,7 @@ function lfDispStep1($objPage) {
 	global $objDBParam;
 	// hiddenに入力値を保持
 	$objPage->arrHidden = $objDBParam->getHashArray();
-	$objPage->tpl_mainpage = 'step2.tpl';
+	$objPage->tpl_mainpage = 'step1_dummy.tpl';
 	$objPage->tpl_mode = 'step1';
 	return $objPage;
 }
