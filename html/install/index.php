58bf15a6674768de658981c34e49cc794b080216
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-28 10:07:49 +0000

    blank

--- html/install/index.php
+++ html/install/index.php
@@ -276,8 +276,8 @@ function lfGetFileMode($path) {
 function lfDispStep1($objPage) {
 	global $objDBParam;
 	// hiddenに入力値を保持
-	//$objPage->arrHidden = $objDBParam->getHashArray();
-	$objPage->tpl_mainpage = 'step1.tpl';
+	$objPage->arrHidden = $objDBParam->getHashArray();
+	$objPage->tpl_mainpage = 'step2.tpl';
 	$objPage->tpl_mode = 'step1';
 	return $objPage;
 }
