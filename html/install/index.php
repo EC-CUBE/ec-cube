155f734fd7e2a02ef7473a38d59d12c7abce2b41
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-28 05:35:58 +0000

    *** empty log message ***

--- html/install/index.php
+++ html/install/index.php
@@ -369,7 +369,6 @@ function lfCheckDBError($objFormParam) {
 		// 接続エラー
 		if(PEAR::isError($objDB)) {
 			$objErr->arrErr['all'] = ">> " . $objDB->message . "<br>";
-			$objErr->arrErr['all'] = ">> " . "インストールの前にDBを作成しておく必要があります。";			
 			gfPrintLog($objDB->userinfo, "./temp/install.log");
 		}
 	}		
