8ae8ada83d8dbfc47c7f4e1dde961847ae23a147
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-28 13:08:05 +0000

    *** empty log message ***

--- html/js/navi.js
+++ html/js/navi.js
@@ -31,7 +31,7 @@
 		document.getElementById(ids).style.backgroundColor = color;
 	}	
 
-		// サブナビの表示切替
+	// サブナビの表示切替
 	function naviStyleChange2(ids, file){
 		document.getElementById(ids).style.backgroundImage = "url(" + file + ")";
 
