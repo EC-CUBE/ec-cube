5121c9c8f8f7d03c5dc46089d66cadc7d62e8cfb
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-28 04:49:28 +0000

    *** empty log message ***

--- html/js/navi.js
+++ html/js/navi.js
@@ -25,4 +25,7 @@
 	function chgImgImageSubmit(fileName,imgObj){
 	imgObj.src = fileName;
 	
-}
+	// サブナビの表示切替
+	function naviStyleChange(ids, color){
+		document.getElementById(ids).style.backgroundColor = color;
+	}	
