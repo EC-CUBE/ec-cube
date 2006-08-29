4d187e99ae1a9216511cbeb0a6e922054af6e603
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-29 01:18:54 +0000

    blank

--- html/admin/design/bloc.php
+++ html/admin/design/bloc.php
@@ -137,7 +137,6 @@ function lfgetBlocData($where = '', $arrVal = ''){
 	// SQLÀ¸À®
 	$sql = " SELECT ";
 	$sql .= "	bloc_id";
-	$sql .= "	,bloc_code";
 	$sql .= "	,bloc_name";
 	$sql .= "	,tpl_path";
 	$sql .= "	,filename";
