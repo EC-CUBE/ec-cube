51a54ae7439328202df6b423fdb2743f9d23ecbf
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-29 03:52:33 +0000

    blank

--- html/admin/order/edit.php
+++ html/admin/order/edit.php
@@ -284,7 +284,7 @@ function lfRegistData($order_id) {
 	$sql .= "     dtb_order";
 	$sql .= " SET";
 	$sql .= "     payment_method = (SELECT payment_method FROM dtb_payment WHERE payment_id = ?)";
-	$sql .= "     ,deliv_date = (SELECT time FROM dtb_delivtime WHERE time_id = ? AND deliv_id = (SELECT deliv_id FROM dtb_order WHERE order_id = ?))";
+	$sql .= "     ,deliv_time = (SELECT time FROM dtb_delivtime WHERE time_id = ? AND deliv_id = (SELECT deliv_id FROM dtb_order WHERE order_id = ?))";
 	$sql .= " WHERE order_id = ?";
 	
 	if ($arrRet['deliv_time_id'] == "") {
