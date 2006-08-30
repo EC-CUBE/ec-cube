70cc27dcb0135e7dffff68a929a8ba82068d8794
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-30 16:11:23 +0000

    blank

--- html/admin/design/template.php
+++ html/admin/design/template.php
@@ -98,7 +98,7 @@ function lfgetTemplate(){
 	
 	// テンプレートデータを取得する
 	$objQuery = new SC_Query();
-	$sql = "SELECT template_code,template_name,file_path FROM dtb_templates ORDER BY create_date DESC";
+	$sql = "SELECT template_code,template_name FROM dtb_templates ORDER BY create_date DESC";
 	$arrTemplate = $objQuery->getall($sql);
 	
 	switch($objPage->tpl_subno_template) {
