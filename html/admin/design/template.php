506a0c44c78c102b4337d1cc507c945675e27983
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-30 10:22:56 +0000

    blank

--- html/admin/design/template.php
+++ html/admin/design/template.php
@@ -68,7 +68,7 @@ $tpl_arrTemplate = array();
 $objPage->arrTemplate = lfgetTemplate();
 
 // デフォルトチェック取得
-$objPage->MainImage = $objPage->arrTemplate['check'];
+$objPage->MainImage = 1;// $objPage->arrTemplate['check'];
 $objPage->arrTemplate['check'] = array($objPage->arrTemplate['check']=>"check");
 
 // 画面の表示
