36e27a20d12ab6c59bfec902183f7759654ebfcf
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-30 08:17:41 +0000

    *** empty log message ***

--- html/admin/batch/daily.php
+++ html/admin/batch/daily.php
@@ -43,9 +43,7 @@ function lfStartDailyTotal($term, $start, $command = false) {
 	
 	$objQuery = new SC_Query();
 	$arrRet = $objQuery->select("create_date", "dtb_bat_order_daily", "create_date > ?", array($batch_pass));
-	
-	
-	
+		
 	if(count($arrRet) > 0) {
 		gfPrintLog("LAST BATCH " . $arrRet[0]['create_date'] . " > " . $batch_pass . " -> EXIT BATCH $batch_date");
 		return;
