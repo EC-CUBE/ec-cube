7f676731530e3267efb3fa13b0056298b17ee964
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-30 08:47:45 +0000

    blank

--- html/test/kakinaka/wordwrap.php
+++ html/test/kakinaka/wordwrap.php
@@ -17,13 +17,12 @@ $cut_len = 10;
 
 $len = mb_strlen($test);
 
-//while($len > $cut_len){
+while($len > $cut_len){
 	$str[] = mb_substr($test,0,10);
 	$tmp = mb_substr($test, 10, $len);
 	$len = mb_strlen($tmp);
-//}
+}
 $str[] = $tmp;
-print($len);
 print_r($str);
 
 ?>
