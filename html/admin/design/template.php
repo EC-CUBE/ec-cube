9303ef1085f3f638d4b036778aa60587362746c4
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-30 11:59:35 +0000

    blank

--- html/admin/design/template.php
+++ html/admin/design/template.php
@@ -130,11 +130,11 @@ function lfgetTemplate(){
 
 	// 画像表示配列作成
 	foreach($arrTemplate as $key => $val){
-//		$arrTemplateImage['image'][$key] = $val[$Image];
 		$arrTemplateImage['image'][$key] = $filepath . $val['template_code'] . "/" . $Image;
 		$arrTemplateImage['code'][$key] = $val['template_code'];
 	}
 	
+	sfprintr($arrDefcheck);
 	sfprintr($arrTemplateImage);
 	
 	// 初期チェック
