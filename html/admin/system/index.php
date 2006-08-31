98ad40a72e6cf168cc561678cdbfd7d52f464347
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-08-31 12:26:19 +0000

    blank

--- html/admin/system/index.php
+++ html/admin/system/index.php
@@ -42,7 +42,7 @@ $oquery = new SC_Query();
 $linemax = $oquery->count("dtb_member", "delete <> 1 AND member_id <>".ADMIN_ID);
 
 // 稼動中の件数を取得
-$workmax = $oquery->count("dtb_member", "move = 1 AND delete <> 1 AND member_id <>".ADMIN_ID);
+$workmax = $oquery->count("dtb_member", "work = 1 AND delete <> 1 AND member_id <>".ADMIN_ID);
 $objPage->workmax= $workmax;
 
 // ページ送りの処理
