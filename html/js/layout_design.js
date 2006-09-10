e659d567dc34a1c0117d5447ce41e1bfffb61f82
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-10 11:30:07 +0000

    blank

--- html/js/layout_design.js
+++ html/js/layout_design.js
@@ -394,7 +394,7 @@ function fnCreateArr( addEvt , top , left ){
 	//test = all_elms[i].attributes.getNamedItem('class');
 	//alert(test.nodeValue);
 
-var t0 = document.getElementById(i)
+var t0 = document.getElementById('test0')
 t0a = t0.attributes.item('id')
 
 alert( t0a.value )
