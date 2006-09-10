0cd04ba7c45c175a3d75af1aab00f5c2162dd5eb
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-10 08:05:44 +0000

    blank

--- html/admin/customer/edit.php
+++ html/admin/customer/edit.php
@@ -148,7 +148,7 @@ if ( $_POST["mode"] != "edit" && is_numeric($_POST["customer_id"])) {
 			$objPage->tpl_mainpage = 'customer/edit_confirm.tpl';
 			$passlen = strlen($objPage->arrForm['password']);
 			$objPage->passlen = lfPassLen($passlen);
-			print_r(objPage->arrForm);
+			sfprintr($objPage->arrForm);
 			
 		}
 		//--°° ‘Ω∏
