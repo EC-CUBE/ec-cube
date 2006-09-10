bad7156d7cecdd3626de63c5162332670cf248b6
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-10 11:58:07 +0000

    blank

--- html/js/layout_design.js
+++ html/js/layout_design.js
@@ -106,10 +106,32 @@ function setZindex(node, val) {
 
 // 値を取得
 function getAttrValue ( elm, attrname ) {
-//	if (typeof(elm.attributes[ attrname ]) != 'undefined') {
-		alert(elm.attributes[ attrname ].nodeValue);
+/*
+	if (typeof(elm.attributes[ attrname ]) != 'undefined') {
 	    return elm.attributes[ attrname ].nodeValue;
-//	}
+	}
+*/	
+
+	alert(typeof elm.attributes.getNamedItem(attrname));
+	val = "";
+	if((typeof ScriptEngineMajorVersion)=='function')
+	{
+		if( Math.floor(ScriptEngineMajorVersion()) == 5 &&
+			navigator.userAgent.indexOf("Win")!=-1) //win-e5対応
+			{
+			val = elm.attributes.item(attrname)
+			}
+		else
+		{
+			val = elm.attributes.getNamedItem(attrname)
+		}
+	} else {
+		val = elm.attributes.getNamedItem(attrname)
+	}
+	
+	alert(elm.attributes[ attrname ].nodeValue+"/"+val.value);
+	
+	return val.value;
 }
 
 // 値をセット
