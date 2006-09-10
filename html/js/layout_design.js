5c312bf05fd6cc1d2c6a18bd41c619cc358116c7
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-10 12:05:34 +0000

    blank

--- html/js/layout_design.js
+++ html/js/layout_design.js
@@ -111,6 +111,7 @@ function getAttrValue ( elm, attrname ) {
 	    return elm.attributes[ attrname ].nodeValue;
 	}
 */	
+alert(1111);
 alert("/////"+typeof(elm.attributes.getNamedItem(attrname).value));
 
 	if (typeof(elm.attributes.getNamedItem(attrname)) != 'object'){
