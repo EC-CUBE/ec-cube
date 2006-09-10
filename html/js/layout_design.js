fbb2490c353bd54dbff22c71177bf5d60c965cdd
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-10 10:55:34 +0000

    blank

--- html/js/layout_design.js
+++ html/js/layout_design.js
@@ -393,7 +393,8 @@ function fnCreateArr( addEvt , top , left ){
 
 	for ( var i = 0; i < all_elms.length; i++ ) {
 	
-alert(all_elms[i].getAttributeNode('class').nodeValue);
+	test = all_elms[i].getAttributeNode('class');
+alert(test.value);
 	
 		// classが dragged_elm の場合のみ処理を行う
 		if ( getAttrValue ( all_elms[i], 'class' ) == 'dragged_elm' ) {
