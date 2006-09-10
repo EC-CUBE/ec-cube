8ecaf0666894732ccc5ed23b028d0b1fcffec796
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-10 12:00:22 +0000

    blank

--- html/js/layout_design.js
+++ html/js/layout_design.js
@@ -111,6 +111,7 @@ function getAttrValue ( elm, attrname ) {
 	    return elm.attributes[ attrname ].nodeValue;
 	}
 */	
+alert(typeof(elm.attributes.getNamedItem(attrname)));
 
 	if (typeof(elm.attributes.getNamedItem(attrname)) != 'object'){
 	val = "";
