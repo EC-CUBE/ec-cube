884e7a9e077ae247edbbc14d3e0fc063dad50b25
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-10 10:42:30 +0000

    blank

--- html/js/layout_design.js
+++ html/js/layout_design.js
@@ -392,6 +392,8 @@ function fnCreateArr( addEvt , top , left ){
 	arrObjtmp['Unused'] = Array();
 
 	for ( var i = 0; i < all_elms.length; i++ ) {
+	
+	alert(123);
  
 		// classが dragged_elm の場合のみ処理を行う
 		if ( getAttrValue ( all_elms[i], 'class' ) == 'dragged_elm' ) {
