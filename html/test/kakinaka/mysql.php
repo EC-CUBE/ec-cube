66aa2458158e942fcf25785ab2188f62616924dc
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-11 03:28:02 +0000

    blank

--- html/test/kakinaka/mysql.php
+++ html/test/kakinaka/mysql.php
@@ -1,10 +1,15 @@
 <?php
 include("DB.php"); // PEAR の DB クラスを読み込む
 
+print("start");
+
 $db = DB::connect("mysql://eccube_db_user:password@test.ec-cube.net");
 $result = $db->query("select * from test");
 while($row = $result->fetchRow()){
     print_r($row);
 }
+
+
+print("end");
 
 ?> 
