4ce469284a72d7f40b77e525d82d629d226bc6ba
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-12 10:38:19 +0000

    *** empty log message ***

--- html/test/kakinaka/mysql.php
+++ html/test/kakinaka/mysql.php
@@ -1,14 +1,13 @@
 <?php
-include("DB.php"); // PEAR の DB クラスを読み込む
+include("./DB.php"); // PEAR の DB クラスを読み込む
 
 print("start");
 
-$db = DB::connect("mysql://eccube_db_user:password@naka.lockon.net/eccube_db");
+$db = DB::connect("mysql://eccube_db_user:password@210.188.212.163/eccube_db");
 $result = $db->query("select * from test");
 while($row = $result->fetchRow()){
     print_r($row);
 }
-
 
 print("end");
 
