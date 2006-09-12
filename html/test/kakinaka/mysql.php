d9a5e45a061e97febcc38a166164503957ae45f3
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-12 10:59:37 +0000

    blank

--- html/test/kakinaka/mysql.php
+++ html/test/kakinaka/mysql.php
@@ -1,11 +1,11 @@
 <?php
 require_once 'DB.php'; // PEAR の DB クラスを読み込む
 
-print("start");
+print("start<br>");
 
 $dsn = "mysql://eccube_db_user:password@210.188.212.163/eccube_db";
 
-print($dsn);
+print($dsn."<br>");
 
 if(($db = DB::connect($dsn)) == 0){
   print "おおっと！データベースに接続できません。";
