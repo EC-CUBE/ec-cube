004e1a5975076a3ac8960c4308b1c75225ad4921
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-13 04:50:49 +0000

    blank

--- data/class/SC_DbConn.php
+++ data/class/SC_DbConn.php
@@ -1,7 +1,7 @@
 <?php
 $current_dir = realpath(dirname(__FILE__));
 require_once($current_dir . "/../module/DB.php");
-//require_once($current_dir . "/../module/DB/common.php");
+require_once($current_dir . "/../module/DB/common.php");
 
 $objDbConn = "";
 
