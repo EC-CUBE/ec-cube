87020f51b414cc62f82f610ef36945f5d22fc5ec
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-16 10:24:26 +0000

    blank

--- data/lib/slib.php
+++ data/lib/slib.php
@@ -2301,8 +2301,6 @@ function sfGetCatTree($parent_category_id, $count_check = false) {
 	$objQuery->setoption("ORDER BY rank DESC");
 	$arrRet = $objQuery->select($col, $from, $where);
 	
-	$objQuery->getLastQuery();
-	
 	$arrParentID = sfGetParents($objQuery, 'dtb_category', 'parent_category_id', 'category_id', $parent_category_id);
 	
 	foreach($arrRet as $key => $array) {
