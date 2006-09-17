58fb9522eb42346bfe21df5cc22cdc58a46a8282
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-17 07:25:35 +0000

    blank

--- html/admin/contents/csv_sql.php
+++ html/admin/contents/csv_sql.php
@@ -150,6 +150,8 @@ $objPage->arrColList = sfarrCombine($arrColList['column_name'],$arrColList['desc
 $objPage->selectTable = $selectTable;															// 選択されているテーブル
 $objPage->sql_id = $sql_id;																		// 選択されているSQL
 
+sfprintr($objPage->arrTableList);
+
 // POSTされたデータをセットする
 if (count($_POST) > 0) {
 	$arrSqlData[0]['name'] = $_POST['name'];
