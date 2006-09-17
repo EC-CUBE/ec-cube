540ea2754176f547560f8beef5a49d2ceea46471
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-17 12:56:51 +0000

    blank

--- html/admin/home.php
+++ html/admin/home.php
@@ -48,7 +48,7 @@ $objPage->arrSoldout = lfGetSoldOut();
 $arrNewOrder = lfGetNewOrder();
 
 foreach ($arrNewOrder as $key => $val){
-	sfprintr($val['create_date'] . "////" . substr($val['create_date'], 0,19));
+	sfprintr($val['create_date'] . "////" . str_replace(substr("-", "/",$val['create_date'], 0,19)));
 	
 }
 $objPage->arrNewOrder = lfGetNewOrder();
