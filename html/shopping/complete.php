c5f2a9a0d8b7a5878d3c63bcc14297bfa31870fe
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-17 10:19:38 +0000

    blank

--- html/shopping/complete.php
+++ html/shopping/complete.php
@@ -330,8 +330,8 @@ function lfRegistOrderDetail($objQuery, $order_id, $objCartSess) {
 	$arrCart = $objCartSess->getCartList();
 	$max = count($arrCart);
 	
-	sfprintr($order_id);
 	
+	$order_id = '0';
 	// 既に存在する詳細レコードを消しておく。
 	$objQuery->delete("dtb_order_detail", "order_id = ?", $order_id);
 
