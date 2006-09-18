6b94ab38998e0c345659b5e5317f782bd6952f62
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-18 07:38:21 +0000

    blank

--- html/install/insert_data.sql
+++ html/install/insert_data.sql
@@ -304,7 +304,6 @@ VALUES (2, 0, 0, 'nabe-01', 100, NULL, NULL, 1700, 1650, NULL, 2, now(), NULL);
 
 INSERT INTO dtb_recommend_products (product_id,rank,comment,status,creator_id,create_date,update_date) VALUES (2, 4, 'お口直しに。', 0, 2, now(), now());
 
---規格
 INSERT INTO dtb_class (name,status,rank,creator_id,create_date,update_date,del_flg,product_id) VALUES ('味', NULL, 1, 2, now(), now(), 0, NULL);
 INSERT INTO dtb_class (name,status,rank,creator_id,create_date,update_date,del_flg,product_id) VALUES ('大きさ', NULL, 2, 2, now(), NULL, 0, NULL);
 
