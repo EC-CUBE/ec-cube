ab5f9bad4fca911bcc30994853d849d3be5720a7
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-18 06:59:15 +0000

    blank

--- html/install/index.php
+++ html/install/index.php
@@ -441,7 +441,10 @@ function lfExecuteSQL($filepath, $db_user, $db_password, $db_server, $db_name) {
 			$sql = fread($fp, filesize($filepath));
 			fclose($fp);
 		}
-		$dsn = "pgsql://".$db_user.":".$db_password."@".$db_server."/".$db_name;
+		
+//		$dsn = "pgsql://".$db_user.":".$db_password."@".$db_server."/".$db_name;
+		$dsn = "mysql://".$db_user.":".$db_password."@".$db_server.":3307/".$db_name;
+
 		$objDB = DB::connect($dsn);
 		// 接続エラー
 		if(!PEAR::isError($objDB)) {
