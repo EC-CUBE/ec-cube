1a58a4dffb172e074d834d9ce719cf4adc1c590a
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-18 09:25:30 +0000

    blank

--- html/install/index.php
+++ html/install/index.php
@@ -81,7 +81,7 @@ case 'step3':
 	// 入力データを渡す。
 	$arrRet =  $objDBParam->getHashArray();
 	// テーブルの作成
-	$objPage->arrErr = lfExecuteSQL("./create_table_mysql.sql", $arrRet['db_user'], $arrRet['db_password'], $arrRet['db_server'], $arrRet['db_name'], $arrRet['db_type'], $arrRet['db_port']); 
+	$objPage->arrErr = lfExecuteSQL("./create_table_".$arrRet['db_type']."sql", $arrRet['db_user'], $arrRet['db_password'], $arrRet['db_server'], $arrRet['db_name'], $arrRet['db_type'], $arrRet['db_port']); 
 	if(count($objPage->arrErr) == 0) {
 		$objPage->tpl_message.="○：テーブルの作成に成功しました。<br>";
 	} else {
