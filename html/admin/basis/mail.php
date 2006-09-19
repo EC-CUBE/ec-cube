cca71a2ab5bd2f39c7b6795393b9507278e33794
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-19 12:05:28 +0000

    blank

--- html/admin/basis/mail.php
+++ html/admin/basis/mail.php
@@ -75,10 +75,10 @@ function lfRegist( $conn, $data ){
 	$result = $conn->getAll($sql, array($_POST['template_id']) );
 	if ( $result ){
 		$sql_where = "template_id = ". addslashes($_POST['template_id']);
-		$conn->query("UPDATE dtb_mailtemplate SET template_id = ?, subject = ?,header = ?, footer = ?,creator_id = ?, update_date = ?", $data);
+		$conn->query("UPDATE dtb_mailtemplate SET template_id = ?, subject = ?,header = ?, footer = ?,creator_id = ?, update_date = now()", $data);
 	}else{
 		$data["create_date"] = "now()";
-		$conn->query("INSERT INTO dtb_mailtemplate (template_id,subject,header,footer,creator_id,update_date,create_date) values ( ?,?,?,?,?,?,? )", $data);
+		$conn->query("INSERT INTO dtb_mailtemplate (template_id,subject,header,footer,creator_id,update_date,create_date) values ( ?,?,?,?,?,now(),now() )", $data);
 	}
 
 //	$conn->autoExecute("dtb_mailtemplate", $data, $sql_where);	
