a8a216266b82b10c41d9e228e1d8aae7abd718b8
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-19 04:36:35 +0000

    blank

--- html/admin/design/index.php
+++ html/admin/design/index.php
@@ -383,6 +383,8 @@ function lfSetPreData($arrPageData){
 	// tplファイルのコピー
 	copy(ROOT_DIR . $arrPageData[0]['tpl_dir'].$arrPageData[0]['filename'].".tpl", ROOT_DIR . USER_DIR."templates/".$filename.".tpl");
 	
+	sfprintr($arrPageData);
+	
 	// 更新データの取得
 	$sql = "select page_name, header_chk, footer_chk from dtb_pagelayout where page_id = ?";
 	$ret = $objDBConn->getAll($sql, $arrPageData[0]['page_id']);
