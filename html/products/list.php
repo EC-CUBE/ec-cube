856ccf83d0444ec09db50fd82146592e823c4860
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-20 08:24:37 +0000

    blank

--- html/products/list.php
+++ html/products/list.php
@@ -206,7 +206,7 @@ function lfDispProductsList($category_id, $name, $disp_num, $orderby) {
 	$objNavi = new SC_PageNavi($_POST['pageno'], $linemax, $disp_num, "fnNaviPage", NAVI_PMAX);
 	
 	$strnavi = $objNavi->strnavi;
-	$strnavi = str_replace('onclick="fnNaviPage', '', $strnavi);
+	$strnavi = str_replace('onclick="fnNaviPage', 'onclick="form1.mode.value=""; fnNaviPage', $strnavi);
 	sfprintr( $strnavi);
 	$objPage->tpl_strnavi = $strnavi;		// 表示文字列
 	$startno = $objNavi->start_row;					// 開始行
