d3d3861e1e2774fcdd861dbfc35936dddef4ea3d
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-20 03:39:19 +0000

    blank

--- html/admin/products/classcategory.php
+++ html/admin/products/classcategory.php
@@ -120,6 +120,7 @@ function lfInsertClass() {
 		$sqlval['creator_id'] = $_SESSION['member_id'];
 		$sqlval['rank'] = $objQuery->max("dtb_classcategory", "rank", $where, array($_POST['class_id'])) + 1;
 		$sqlval['create_date'] = "now()";
+		$sqlval['update_date'] = "now()";
 		// INSERT¤Î¼Â¹Ô
 		$ret = $objQuery->insert("dtb_classcategory", $sqlval);
 	}
