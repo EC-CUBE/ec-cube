2c42ea489b06d549719de31d852fcc520ab5206f
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-20 09:53:01 +0000

    blank

--- html/install/index.php
+++ html/install/index.php
@@ -429,9 +429,8 @@ function lfCheckDBError($objFormParam) {
 	if(count($objErr->arrErr) == 0) {
 		// 接続確認
 		$dsn = $arrRet['db_type']."://".$arrRet['db_user'].":".$arrRet['db_password']."@".$arrRet['db_server'].$port."/".$arrRet['db_name'];
-	sfprintr($dsn);
 		// Debugモード指定
-		$options['debug'] = 3;
+//		$options['debug'] = 3;
 		$objDB = DB::connect($dsn, $options);
 		// 接続エラー
 		if(PEAR::isError($objDB)) {
