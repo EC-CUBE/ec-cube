b6612e725a4fa1e130c410ce5d54c808e7b245ba
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-22 06:11:11 +0000

    blank

--- html/install/index.php
+++ html/install/index.php
@@ -177,8 +177,12 @@ case 'drop':
 	break;
 // 完了画面
 case 'complete':
+
 	// ショップマスタ情報の書き込み
 	$arrRet =  $objDBParam->getHashArray();
+	
+	sfprintr($arrRet);
+	
 	$dsn = $arrRet['db_type']."://".$arrRet['db_user'].":".$arrRet['db_password']."@".$arrRet['db_server'].$port."/".$arrRet['db_name'];
 	$sqlval['shop_name'] = $objWebParam->getValue('shop_name');
 	$sqlval['email01'] = $objWebParam->getValue('admin_mail');
