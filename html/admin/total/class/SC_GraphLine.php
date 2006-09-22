36a6e4c2143af5c87f0a1f4d7d4eabb39422c92c
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-22 02:41:13 +0000

    blank

--- html/admin/total/class/SC_GraphLine.php
+++ html/admin/total/class/SC_GraphLine.php
@@ -111,7 +111,7 @@ class SC_GraphLine extends SC_GraphBase{
 				$text_width = $this->getTextWidth($this->arrXLabel[$i], FONT_SIZE);
 				$x_pos = $x;
 				
-				if ($bar) $bar_margin = -12;
+				if ($bar) $bar_margin = -15;
 
 				$this->setText(FONT_SIZE, $x_pos + $this->x_margin + $bar_margin, $pos + FONT_SIZE + $this->y_margin, $this->arrXLabel[$i], NULL, $this->XLabelAngle);
 			}
