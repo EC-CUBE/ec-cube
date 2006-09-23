43cd1d31ac42af872f1bdc2a1b5982e7d0eb955b
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-23 08:34:01 +0000

    blank

--- html/admin/system/bkup.php
+++ html/admin/system/bkup.php
@@ -98,11 +98,11 @@ function lfCreateBkupData(){
 
 		// CSVデータ生成
 		if (count($arrData) > 0) {
-			
-		}	
 		$csv_data .= $val . "\n";
 		
 		sfprintr($arrData);
+			
+		}	
 		
 		//sfprintr(array_keys($arrData[0]));
 		//sfprintr(sfGetCSVList($arrData));
