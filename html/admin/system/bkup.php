ad0386c8fad88e31a4c8cadf6fca6ce5dd0bbc1e
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-23 08:31:41 +0000

    blank

--- html/admin/system/bkup.php
+++ html/admin/system/bkup.php
@@ -97,7 +97,7 @@ function lfCreateBkupData(){
 		$arrData = $objQuery->getAll("SELECT * FROM $val");
 
 		// CSVデータ生成		
-		$csv_data .= sfGetCSVList(array($val));
+		$csv_data .= sfGetCommaList(array($val));
 		
 		//sfprintr(array_keys($arrData[0]));
 		//sfprintr(sfGetCSVList($arrData));
