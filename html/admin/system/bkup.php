425c14c08c8bbb06f8793adf4f639695d4197b84
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-25 04:17:47 +0000

    blank

--- html/admin/system/bkup.php
+++ html/admin/system/bkup.php
@@ -412,7 +412,7 @@ function lfCreateTable(){
 	// テーブルの存在チェック
 	$arrTableList = lfGetTableList();
 	
-	if(array_key_exists("dtb_bkup", $arrTableList)){
+	if(!array_key_exists("dtb_bkup", $arrTableList)){
 		// 存在していなければ作成
 		$cre_sql = "
 			create table dtb_bkup
