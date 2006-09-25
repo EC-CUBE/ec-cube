13d15904598268319594a877653105aaaa175ade
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-25 00:39:58 +0000

    blank

--- html/admin/system/bkup.php
+++ html/admin/system/bkup.php
@@ -193,10 +193,11 @@ function lfCreateBkupData($bkup_name){
 		$tar = new Archive_Tar($objPage->bkup_dir . $bkup_name.".tar.gz", TRUE);
 
 		//bkupフォルダに移動する
-		chdir($bkup_dir);
+//		chdir($bkup_dir);
 		
 		//圧縮をおこなう
-		$zip = $tar->create("./" . $bkup_name . "/");
+//		$zip = $tar->create("./" . $bkup_name . "/");
+		$zip = $tar->create($bkup_dir);
 		
 		sfprintr($tar);
 
