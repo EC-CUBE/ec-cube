90d803363bfad72f308061f674f32ab28acf0f67
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-09-25 03:05:03 +0000

    blank

--- html/admin/system/bkup.php
+++ html/admin/system/bkup.php
@@ -250,11 +250,9 @@ function lfGetTableList(){
 function lfGetAutoIncrement($table_name){
 	$arrColList = lfGetColumnList($table_name);
 	
-	sfprintr($arrColList['defval']);
-	
 	foreach($arrColList['defval'] as $key => $val){
-		sfprintr(substr($val,1,10));
-		if (substr($val,1,10) == 'nextval(\'') {
+		sfprintr(substr($val,0,10));
+		if (substr($val,0,10) == 'nextval(\'') {
 			sfprintr($val);
 		}		
 	}
