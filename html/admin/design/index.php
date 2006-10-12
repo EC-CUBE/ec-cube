845f7d41b9d3495b1626050bb6a8f4dffe23e50c
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-12 14:50:55 +0000

    *** empty log message ***

--- html/admin/design/index.php
+++ html/admin/design/index.php
@@ -151,7 +151,7 @@ if ($_POST['mode'] == 'confirm' or $_POST['mode'] == 'preview') {
 		lfSetPreData($arrPageData);
 		
 		$_SESSION['preview'] = "ON";
-		//header("location: ". URL_DIR . "preview/index.php");
+		header("location: ". URL_DIR . "preview/index.php");
 	}else{
 		header("location: ./index.php?page_id=$page_id&msg=on");
 	}
