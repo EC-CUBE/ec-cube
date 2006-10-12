c33cc1af930eeb92bb868941afb9f052825c782a
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-12 12:29:52 +0000

    *** empty log message ***

--- html/install/index.php
+++ html/install/index.php
@@ -520,8 +520,11 @@ function lfCheckDBError($objFormParam) {
 	if(count($objErr->arrErr) == 0) {
 		// 接続確認
 		$dsn = $arrRet['db_type']."://".$arrRet['db_user'].":".$arrRet['db_password']."@".$arrRet['db_server'].":".$arrRet['db_port']."/".$arrRet['db_name'];
+		
+		print($dsn);
+		
 		// Debugモード指定
-		$options['debug'] = 3;
+		$options['debug'] = 9;
 		$objDB = DB::connect($dsn, $options);
 		// 接続エラー
 		if(PEAR::isError($objDB)) {
