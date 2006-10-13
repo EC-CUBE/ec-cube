4b62c2b190d79c1e01908ac21023c85aefc0ca72
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-13 01:19:23 +0000

    *** empty log message ***

--- data/lib/slib.php
+++ data/lib/slib.php
@@ -23,6 +23,9 @@ function sfTabaleExists($table_name, $dsn = DEFAULT_DSN) {
 	$objQuery = new SC_Query($dsn);
 	// 正常に接続されている場合
 	if(!$objQuery->isError()) {
+		
+		print("ike");
+		
 		list($db_type) = split(":", $dsn);
 		// postgresqlとmysqlとで処理を分ける
 		if ($db_type == "pgsql") {
