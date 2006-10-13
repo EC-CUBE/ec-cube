9024659bb0c6ec467108aceebea8240e2b9485c5
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-13 04:00:13 +0000

    *** empty log message ***

--- html/install/index.php
+++ html/install/index.php
@@ -577,7 +577,13 @@ function lfExecuteSQL($filepath, $dsn, $disp_err = true) {
 			$sql_split = split(";",$sql);
 			foreach($sql_split as $key => $val){
 				if (trim($val) != "") {
+					
+					gfPrintLog("nn", "./temp/install.log");
+					
 					$ret = $objDB->query($val);
+					
+					gfPrintLog("nn", "./temp/install.log");
+					
 					if(PEAR::isError($ret) && $disp_err) {
 						$arrErr['all'] = ">> " . $ret->message . "<br>";
 						// エラー文を取得する
