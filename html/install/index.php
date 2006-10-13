f3c4a58e03a400175a11e7cfd0a98983a6695826
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-13 04:09:26 +0000

    *** empty log message ***

--- html/install/index.php
+++ html/install/index.php
@@ -7,8 +7,6 @@
 require_once("../require.php");
 $INSTALL_DIR = realpath(dirname( __FILE__));
 
-define('DB_DEBUG', 12);
-
 class LC_Page {
 	function LC_Page() {
 		$this->arrDB_TYPE = array(
@@ -532,7 +530,7 @@ function lfCheckDBError($objFormParam) {
 		// 接続確認
 		$dsn = $arrRet['db_type']."://".$arrRet['db_user'].":".$arrRet['db_password']."@".$arrRet['db_server'].":".$arrRet['db_port']."/".$arrRet['db_name'];
 		// Debugモード指定
-		$options['debug'] = DB_DEBUG;
+		$options['debug'] = PEAR_DB_DEBUG;
 		$objDB = DB::connect($dsn, $options);
 		// 接続エラー
 		if(PEAR::isError($objDB)) {
@@ -572,7 +570,7 @@ function lfExecuteSQL($filepath, $dsn, $disp_err = true) {
 			fclose($fp);
 		}
 		// Debugモード指定
-		$options['debug'] = DB_DEBUG;
+		$options['debug'] = PEAR_DB_DEBUG;
 		$objDB = DB::connect($dsn, $options);
 		// 接続エラー
 		if(!PEAR::isError($objDB)) {
