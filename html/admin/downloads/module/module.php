50b575f5a5384172b6da0b48dc0f9cf7d73b1aaf
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-17 12:51:45 +0000

    *** empty log message ***

--- html/admin/downloads/module/module.php
+++ html/admin/downloads/module/module.php
@@ -1,8 +1,30 @@
 <?php
-/*
- * Copyright(c) 2000-2006 LOCKON CO.,LTD. All Rights Reserved.
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * Database independent query interface
  *
- * http://www.lockon.co.jp/
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Database
+ * @package    DB
+ * @author     Stig Bakken <ssb@php.net>
+ * @author     Tomas V.V.Cox <cox@idecnet.com>
+ * @author     Daniel Convissor <danielc@php.net>
+ * @copyright  1997-2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/DB
+ */
+
+/**
+ * Obtain the PEAR class so it can be extended from
  */
 require_once("../../require.php");
 
