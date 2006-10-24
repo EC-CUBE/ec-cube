fce8d3cc019fa04d86b07c64241c8f107dc71a56
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-24 14:07:32 +0000

    *** empty log message ***

--- html/admin/total/class/config.php
+++ html/admin/total/class/config.php
@@ -82,7 +82,7 @@ $ARR_SHADE_COLOR = array(100,100,100);
 // 縁の色
 $ARR_FLAME_COLOR = array(0, 0, 0);
 // 文字色
-$ARR_TEXT_COLOR = array(255, 0, 0);
+$ARR_TEXT_COLOR = array(0, 0, 0);
 // 背景カラー
 $ARR_BG_COLOR = array(255,255,255);	
 // タイトル文字色
