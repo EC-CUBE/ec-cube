1a2f65fab00615b7fa0bb0ecb0d446ddf18e44ba
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-24 04:59:38 +0000

    *** empty log message ***

--- html/rss/product.php
+++ html/rss/product.php
@@ -12,7 +12,7 @@ class LC_Page{
 	function LC_Page(){
 		$this->tpl_mainpage = "rss/product.tpl";
 		$this->encode = "UTF-8";
-		($_GET['product_id'] == "") ? $this->description = "商品一覧情報" : $this->description = "商品詳細情報";
+		($_GET['product_id'] == "") ? $this->title = "商品一覧情報" : $this->title = "商品詳細情報";
 	}
 }
 
