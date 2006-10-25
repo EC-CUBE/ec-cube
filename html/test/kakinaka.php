cd6ab2948a4b61a1cbfea92d0f9fb8edccd1e449
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-25 01:44:23 +0000

    *** empty log message ***

---
+++ html/test/kakinaka.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * Copyright(c) 2000-2006 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ */
+require_once("../require.php");
+require_once(DATA_PATH. "module/Tar.php");
+
+define("DB_TYPE", "pgsql");	
+
+$objQuery = new SC_Query();
+
+
+//-------------------------------------------------------------------------------------------------------
+
+?>
