8fc76a17efc266298fbadffbfbf09c15abfeefa4
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-25 10:18:15 +0000

    *** empty log message ***

--- data/conf/conf.php
+++ data/conf/conf.php
@@ -79,9 +79,9 @@ define ("RECOMMEND_PRODUCT_MAX", 4);
 define("LIB_DIR", DATA_PATH . "lib/");						// ライブラリのパス
 define("TTF_DIR", DATA_PATH . "fonts/");					// フォントのパス
 define("GRAPH_DIR", HTML_PATH . "upload/graph_image/");		// グラフ格納ディレクトリ
-define("GRAPH_URL", URL_DIR . "upload/graph_image/");					// グラフURL
-define("GRAPH_PIE_MAX", 10);									// 円グラフ最大表示数
-define("GRAPH_LABEL_MAX", 40);									// グラフのラベルの文字数
+define("GRAPH_URL", URL_DIR . "upload/graph_image/");		// グラフURL
+define("GRAPH_PIE_MAX", 10);								// 円グラフ最大表示数
+define("GRAPH_LABEL_MAX", 40);								// グラフのラベルの文字数
 
 /** パス関連 **/
 
