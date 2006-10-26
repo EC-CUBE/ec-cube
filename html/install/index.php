54222b365c83a18da696d04afe6207726e601f5a
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-26 10:28:12 +0000

    *** empty log message ***

--- html/install/index.php
+++ html/install/index.php
@@ -234,7 +234,7 @@ case 'complete':
 		$arrSendData = array();
 		foreach($_POST as $key => $val){
 			if (ereg("^senddata_*", $key)){
-				$arrSendDataTmp = array(trim($key, "senddata") => $val);
+				$arrSendDataTmp = array(trim($key, "senddata_") => $val);
 				$arrSendData = array_merge($arrSendData, $arrSendDataTmp);
 			}
 		}
