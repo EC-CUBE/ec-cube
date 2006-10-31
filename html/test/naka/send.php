afc7e980d83788200f8bdc634d19403ac099ae22
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-31 10:28:42 +0000

    *** empty log message ***

---
+++ html/test/naka/send.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once("../../require.php");
+require_once("../../data/module/Request.php");
+
+$order_url = "http://beta.epsilon.jp/cgi-bin/order/receive_order3.cgi";
+
+$arrData = array(
+	'order_number' => '93963923',
+	'st_code' => '10100-0000-00000',
+	'memo1' => '試験用オーダー情報',
+	'user_mail_add' => 'naka@lockon.co.jp',
+	'item_name' => 'プリンタ',
+	'contract_code' => '13094800',
+	'user_name' => 'naka',
+	'process_code' => '2',
+	'mission_code' => '1',
+	'item_price' => '34800',
+	'xml' => '0',		
+	'item_code' => 'abc12345',
+	'memo2' => '',
+	'user_id' => 'test'
+);
+
+$req = new HTTP_Request($order_url);
+$req->setMethod(HTTP_REQUEST_METHOD_POST);
+		
+$arrSendData = array();
+$req->addPostDataArray($arrData;
+
+if (!PEAR::isError($req->sendRequest())) {
+	$response = $req->getResponseBody();
+} else {
+	$response = "";
+}
+
+sfPrintR($response);
+
+$req->clearPostData();
+
+?>
