551d39964a030df2cf5717ade76656ab8ad86a94
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-10-31 00:43:20 +0000

    *** empty log message ***

--- html/test/naka/mail.php
+++ html/test/naka/mail.php
@@ -4,8 +4,7 @@ $to = "naka@lockon.co.jp";
 //$body = "テストです。アイウロエ??????彅??";
 $body = "テストです。";
 
-
-$body = mb_convert_encoding($body, 'UTF-8', 'EUC-JP');
+//$body = mb_convert_encoding($body, 'UTF-8', 'EUC-JP');
 
 print("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>\n");
 print($body);
