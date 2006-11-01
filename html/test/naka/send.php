4495ba32a05d107bcfe2ec508a8fd822e11c02cf
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-01 00:34:48 +0000

    *** empty log message ***

--- html/test/naka/send.php
+++ html/test/naka/send.php
@@ -35,14 +35,14 @@ if (!PEAR::isError($req->sendRequest())) {
 }
 $req->clearPostData();
 
-print($response);
-
-/*
 $parser = xml_parser_create();
 xml_parser_set_option($parser,XML_OPTION_SKIP_WHITE,1);
 xml_parse_into_struct($parser,$response,$arrVal,$idx);
 xml_parser_free($parser);
 
+sfPrintR($arrVal);
+
+/*
 $decode = urldecode($arrVal[3]['attributes']['ERR_DETAIL']);
 
 print(mb_convert_encoding($decode, 'EUC-JP', 'Shift_JIS'));
