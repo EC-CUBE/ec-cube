007b8a914a44ef7d64f7c2259143f4efa40c9189
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-02 07:42:34 +0000

    *** empty log message ***

--- html/admin/basis/payment.php
+++ html/admin/basis/payment.php
@@ -60,7 +60,8 @@ function lfGetPaymentList($fix = 1) {
 //	$where .= " AND fix = ?";
 	$table = "dtb_payment";
 	$objQuery->setorder("rank DESC");
-	$arrRet = $objQuery->select($col, $table, $where, array($fix));
+//	$arrRet = $objQuery->select($col, $table, $where, array($fix));
+	$arrRet = $objQuery->select($col, $table, $where);
 	return $arrRet;
 }
 
