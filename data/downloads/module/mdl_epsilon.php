e4c45af5fb12d2b75efcb1189152860f4fe81934
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-05 07:29:20 +0000

    *** empty log message ***

--- data/downloads/module/mdl_epsilon.php
+++ data/downloads/module/mdl_epsilon.php
@@ -95,7 +95,7 @@ $objView->display($objPage->tpl_mainpage);		//テンプレートの出力
 //-------------------------------------------------------------------------------------------------------
 /* パラメータ情報の初期化 */
 function lfInitParam($objFormParam) {
-	$objFormParam->addParam("契約コード", "code", STEXT_LEN, "KVa", array("EXIST_CHECK", "MAX_LENGTH_CHECK"));
+	$objFormParam->addParam("契約コード", "code", INT_LEN, "KVa", array("EXIST_CHECK", "MAX_LENGTH_CHECK"));
 	$objFormParam->addParam("利用決済", "payment", "", "", array("EXIST_CHECK"));
 	$objFormParam->addParam("利用クレジット", "credit");	
 	$objFormParam->addParam("利用コンビニ", "convenience");	
