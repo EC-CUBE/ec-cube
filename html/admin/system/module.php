edbf3a12a97364aec3111644485e24119453d70b
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-06 05:48:40 +0000

    *** empty log message ***

--- html/admin/system/module.php
+++ html/admin/system/module.php
@@ -278,6 +278,9 @@ function lfUninstallModule() {
 	
 	// モジュール側に削除情報を送信する
 	$req = new HTTP_Request("http://test.ec-cube.net/ec-cube/admin/system/load_module.php");
+
+	$req->addCookie("PHPSESSID", $_COOKIE["PHPSESSID"]);
+
 	$req->setMethod(HTTP_REQUEST_METHOD_POST);
 	$req->addPostData("module_id", $arrRet[0]['module_id']);
 	$req->addPostData("mode", "module_del");
