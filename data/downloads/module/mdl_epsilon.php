669fdc6e122bf4f15b8a17e49ca6e1bbb4fa1f67
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-06 06:49:25 +0000

    blank

--- data/downloads/module/mdl_epsilon.php
+++ data/downloads/module/mdl_epsilon.php
@@ -205,7 +205,7 @@ function lfLoadData(){
 	$arrDisp = $arrRet[0];
 	foreach($arrRet as $key => $val){
 		// 利用決済を表示用に変換
-		$arrDisp["payment"][$key] .= $val["payment"] . "-";
+		$arrDisp["payment"][$key] .= $val["payment"];
 	}
 	
 	sfprintr($arrDisp);
