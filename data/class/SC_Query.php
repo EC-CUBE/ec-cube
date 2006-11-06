045e3e79c5af0c8e003cc73b45f8db72a8e885b3
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-06 03:06:41 +0000

    blank

--- data/class/SC_Query.php
+++ data/class/SC_Query.php
@@ -177,7 +177,7 @@ class SC_Query {
 		$strval = '';
 		$find = false;
 		
-		if(count($sqlval) <= 0 ) return false;
+		if(count($sqlval) <= 0 )
 		
 		foreach ($sqlval as $key => $val) {
 			$strcol .= $key . ',';
