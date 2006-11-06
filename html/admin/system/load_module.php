13706c3ff6787846760bd893ddb3dcd930a065bc
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-06 05:37:23 +0000

    *** empty log message ***

--- html/admin/system/load_module.php
+++ html/admin/system/load_module.php
@@ -18,18 +18,19 @@ if(is_numeric($_GET['module_id'])) {
 	if(file_exists($path)) {
 		
 		if($_GET['mode'] == "module_del"){
-			// モジュール側に削除情報を送信する
-			$req = new HTTP_Request($path);
+/*			// モジュール側に削除情報を送信する
+			$req = new HTTP_Request(SITE_URL . );
 			$req->setMethod(HTTP_REQUEST_METHOD_POST);
 			$req->addPostData("mode", "module_del");
 			$req->sendRequest();
 			
 			sfprintr($req);
 			$req->clearPostData();
-			
-		}else{
-			require_once($path);
+*/
+			$_POST["mode"] = "module_del";
 		}
+			
+		require_once($path);
 		exit;
 	} else {
 		print("モジュールの取得に失敗しました。:".$path);
--- html/admin/system/module.php
+++ html/admin/system/module.php
@@ -281,6 +281,7 @@ function lfUninstallModule() {
 	$req->setMethod(HTTP_REQUEST_METHOD_GET);
 //	$req->addPostData("module_id", $arrRet[0]['module_id']);
 //	$req->addPostData("mode", "module_del");
+	$req->setURL("http://test.ec-cube.net/ec-cube/admin/system/load_module.php?module_id=" .  $arrRet[0]['module_id'] . "&mode=module_del");
 	$req->sendRequest();
 //	$req->clearPostData();
 	exit();
