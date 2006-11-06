fb51b0369d25cd235bb331f299b55dba07491a2b
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-06 06:05:55 +0000

    blank

--- data/downloads/module/mdl_epsilon.php
+++ data/downloads/module/mdl_epsilon.php
@@ -151,7 +151,7 @@ default:
 
 $objPage->arrForm = $objFormParam->getFormParamList();
 
-$objView->assignobj($objPage);					//変数をテンプレートにアサインするaa
+$objView->assignobj($objPage);					//変数をテンプレートにアサインするaaaaa
 $objView->display($objPage->tpl_mainpage);		//テンプレートの出力
 //-------------------------------------------------------------------------------------------------------
 /* パラメータ情報の初期化 */
