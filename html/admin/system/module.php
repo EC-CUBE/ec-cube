e606e59b5cac35c7dd31823ab8a9250969807640
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-06 05:41:14 +0000

    blank

--- html/admin/system/module.php
+++ html/admin/system/module.php
@@ -281,7 +281,8 @@ function lfUninstallModule() {
 	$req->setMethod(HTTP_REQUEST_METHOD_GET);
 	$req->addPostData("module_id", $arrRet[0]['module_id']);
 	$req->addPostData("mode", "module_del");
-	$req->setURL("http://test.ec-cube.net/ec-cube/admin/system/load_module.php?module_id=" .  $arrRet[0]['module_id'] . "&mode=module_del");
+	//$req->setURL("http://test.ec-cube.net/ec-cube/admin/system/load_module.php?module_id=" .  $arrRet[0]['module_id'] . "&mode=module_del");
+	$req->setURL("http://test.ec-cube.net/ec-cube/admin/system/load_module.php");
 	$req->sendRequest();
 //	$req->clearPostData();
 	exit();
