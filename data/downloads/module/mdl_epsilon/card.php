5cfc04481d9b009bd0cf8961ae61620f66338c99
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-07 07:07:46 +0000

    blank

--- data/downloads/module/mdl_epsilon/card.php
+++ data/downloads/module/mdl_epsilon/card.php
@@ -105,7 +105,7 @@ $err_code = lfGetXMLValue($arrVal,'RESULT','ERR_CODE');
 
 if($err_code != "") {
 	$err_detail = lfGetXMLValue($arrVal,'RESULT','ERR_DETAIL');
-	sfDispSiteError(FREE_ERROR_MSG, "", true, "クレジットカード決済処理中に以下のエラーが発生しました。<br /><br /><br />" . $err_detail . "<br /><br /><br />この手続きは無効となりました。");
+	sfDispSiteError(FREE_ERROR_MSG, "", true, "クレジットカード決済処理中に以下のエラーが発生しました。<br /><br /><br />・" . $err_detail . "<br /><br /><br />この手続きは無効となりました。");
 } else {
 	$url = lfGetXMLValue($arrVal,'RESULT','REDIRECT');
 	header("Location: " . $url);	
