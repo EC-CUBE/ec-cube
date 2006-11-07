24e19893f66758d29381282924d6725ccb96081a
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-07 01:30:50 +0000

    blank

--- html/shopping/load_payment_module.php
+++ html/shopping/load_payment_module.php
@@ -13,6 +13,15 @@ $objCartSess = new SC_CartSession();
 // アクセスの正当性の判定
 $uniqid = sfCheckNormalAccess($objSiteSess, $objCartSess);
 
+$payment_id = $_SESSION["payment_id"];
+
+// 決済情報を取得する
+if(sfColumnExists("dtb_payment", "memo01")){
+	$sql = "SELECT memo01, memo02, memo03, memo04, memo05, memo06, memo07, memo08, memo09, memo10 FROM dtb_payment WHERE payment_id = ?";
+	$arrPayment = $objQuery->getall($sql, array($payment_id));
+}
+
+
 if(is_numeric($module_id)) {
 	$objQuery = new SC_Query();
 	$arrRet = $objQuery->select("main_php", "dtb_module", "module_id = ?", array($module_id));
@@ -24,7 +33,5 @@ if(is_numeric($module_id)) {
 		print("モジュールの取得に失敗しました。:".$path);
 	}
 }
-
-sfprintr($_SESSION);
 
 ?>
