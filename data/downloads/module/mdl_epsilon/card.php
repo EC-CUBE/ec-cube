92a28616aca43ac25374ddf5ca97f94eeac882ae
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-07 06:06:44 +0000

    blank

--- data/downloads/module/mdl_epsilon/card.php
+++ data/downloads/module/mdl_epsilon/card.php
@@ -34,7 +34,7 @@ $uniqid = sfCheckNormalAccess($objSiteSess, $objCartSess);
 $_SESSION['site']['pre_page'] = $_SERVER['PHP_SELF'];
 
 sfprintr($_SESSION["site"]);
-
+exit();
 // カート集計処理
 $objPage = sfTotalCart($objPage, $objCartSess, $arrInfo);
 // 一時受注テーブルの読込
