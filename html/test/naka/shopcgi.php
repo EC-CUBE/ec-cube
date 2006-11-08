40d5f53dbbae2a3e3d0338a8771c4f46d3c9ba44
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-08 04:17:06 +0000

    *** empty log message ***

--- html/test/naka/shopcgi.php
+++ html/test/naka/shopcgi.php
@@ -41,7 +41,7 @@ $order_url = "http://mod-i.ccsware.net/ohayou/EntryTran.php";
 
 // 店舗情報の送信
 $arrData = array(
-	'OrderId' => '2006-11-08-12-55-26-190',
+	'OrderId' => sfGetUniqRandomId(),
 	'TdTenantName' => '',
 	'TdFlag' => '',
 	'ShopId' => 'test',
@@ -69,6 +69,27 @@ $req->clearPostData();
 print($response);
 
 // 決済情報の送信
+
+/*
+    [AccessId] => 60f511479544541eb1ec9dc6f700b8c1
+    [AccessPass] => 925a413ce991e7b9230fbc575b9dc430
+    [OrderId] => 2006-11-08-13-08-07-732
+    [RetURL] => http://test.ec-cube.net/cgi-bin/ShopCGI/cgi-1/ShopBuy.pl
+    [CardType] => VISA,     11111, 111111111111111111111111111111111111, 1111111111
+    [Method] => 2
+    [PayTimes] => 4
+    [CardNo1] => 4111
+    [CardNo2] => 1111
+    [CardNo3] => 1111
+    [CardNo4] => 1111
+    [ExpireMM] => 06
+    [ExpireYY] => 07
+    [ClientFieldFlag] => 1
+    [ClientField1] => f1
+    [ClientField2] => f2
+    [ClientField3] => f3
+*/
+
 
 
 ?>
