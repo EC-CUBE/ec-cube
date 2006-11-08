fac32d395de9b7b20c871898a6405cf312de2658
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-08 03:01:49 +0000

    *** empty log message ***

--- html/test/naka/shopcgi.php
+++ html/test/naka/shopcgi.php
@@ -3,6 +3,23 @@
 require_once("../../require.php");
 require_once(DATA_PATH . "module/Request.php");
 
-sfPrintR($_POST);
+/*
+
+from index2.html
+
+Array
+(
+    [TenantNo] => 11111111
+    [OrderId] => 2006-11-08-11-39-53-966
+    [JobCd] => CHECK
+    [Amount] => 
+    [Tax] => 
+    [Currency] => 
+    [Memo] => 
+)
+
+*/
+
+
 
 ?>
