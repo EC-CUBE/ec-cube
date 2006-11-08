fb43402c4bf60bc4b772f2ac7e993bd9e70a6159
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-08 10:44:35 +0000

    blank

--- data/downloads/module/mdl_epsilon/convenience.php
+++ data/downloads/module/mdl_epsilon/convenience.php
@@ -104,10 +104,8 @@ switch($_POST["mode"]){
 			
 		);
 		
-		sfprintr($arrData);
-		
 		// データ送信
-		//sfPostPaymentData($order_url, $arrData);
+		sfPostPaymentData($order_url, $arrData);
 		break;
 		
 	//戻る
