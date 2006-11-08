ea0d5cd22534efccd698069b0da139c7a80f7bd1
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-08 08:34:14 +0000

    *** empty log message ***

--- html/test/naka/recv.php
+++ html/test/naka/recv.php
@@ -2,7 +2,6 @@
 require_once("../../require.php");
 require_once(DATA_PATH . "module/Request.php");
 
-$strmask = "/home/web/test.ec-cube.net/cgi-bin/ShopCGI/common/strmask/FreeBSD/strmask.exe";
 $cmd = $strmask . " -d " . $_GET['SendData'];
 
 $tmpResult = popen($cmd, "r");
