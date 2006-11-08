57ef5efeb21a11cc52cae7c9632640e3421610dc
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-08 07:12:36 +0000

    *** empty log message ***

--- html/test/naka/shopcgi.php
+++ html/test/naka/shopcgi.php
@@ -3,42 +3,9 @@
 require_once("../../require.php");
 require_once(DATA_PATH . "module/Request.php");
 
-/*
-
-from index2.html
-
-Array
-(
-    [TenantNo] => 11111111
-    [OrderId] => 2006-11-08-11-39-53-966
-    [JobCd] => CHECK
-    [Amount] => 
-    [Tax] => 
-    [Currency] => 
-    [Memo] => 
-)
-
-*/
-
-/*
-
-センタへの送信データ
-
-OrderId=>2006-11-08-12-43-21-935
-TdTenantName=>
-TdFlag=>
-ShopId=>test
-Amount=>0
-ShopPass=>test
-Currency=>
-Tax=>0
-JobCd=>CHECK
-TenantNo=>TenantNo
-
-*/
-
 $entry_url = "http://mod-i.ccsware.net/ohayou/EntryTran.php";
 $exec_url = "https://mod-i.ccsware.net/ohayou/ExecTran.php";
+// 受注番号の取得
 $order_id = sfGetUniqRandomId();
 
 // 店舗情報の送信
@@ -66,38 +33,9 @@ if (!PEAR::isError($req->sendRequest())) {
 	$response = "";
 }
 $req->clearPostData();
-
-if($response != "") {
-	$response = ereg_replace("[\n\r]", "", $response);
-	$arrTemp = split("&", $response);
-	foreach($arrTemp as $ret) {
-		list($key, $val) = split("=", $ret);
-		$arrRet[$key] = $val;
-	}
-}
-
-sfPrintR($arrRet);
+$arrRet = lfGetPostArray($response);
 
 // 決済情報の送信
-/*
-    [AccessId] => 60f511479544541eb1ec9dc6f700b8c1
-    [AccessPass] => 925a413ce991e7b9230fbc575b9dc430
-    [OrderId] => 2006-11-08-13-08-07-732
-    [RetURL] => http://test.ec-cube.net/cgi-bin/ShopCGI/cgi-1/ShopBuy.pl
-    [CardType] => VISA,     11111, 111111111111111111111111111111111111, 1111111111
-    [Method] => 2
-    [PayTimes] => 4
-    [CardNo1] => 4111
-    [CardNo2] => 1111
-    [CardNo3] => 1111
-    [CardNo4] => 1111
-    [ExpireMM] => 06
-    [ExpireYY] => 07
-    [ClientFieldFlag] => 1
-    [ClientField1] => f1
-    [ClientField2] => f2
-    [ClientField3] => f3
-*/
 $arrData = array(
 	'AccessId' => $arrRet['ACCESS_ID'],
 	'AccessPass' => $arrRet['ACCESS_PASS'],
@@ -119,8 +57,6 @@ $arrData = array(
 	'ModiFlag' => '1',	// リダイレクトページでの応答を受け取らない。
 );
 
-sfPrintR($arrData);
-
 $req = new HTTP_Request($exec_url);
 $req->setMethod(HTTP_REQUEST_METHOD_POST);
 
@@ -133,8 +69,20 @@ if (!PEAR::isError($req->sendRequest())) {
 }
 $req->clearPostData();
 
-print("<!--");
-sfPrintR($response);
-print("-->");
+$arrRet = lfGetPostArray($response);
 
+sfPrintR($arrRet);
+
+//---------------------------------------------------------------------------------------------------------------------------------
+function lfGetPostArray($text) {
+	if($text != "") {
+		$text = ereg_replace("[\n\r]", "", $text);
+		$arrTemp = split("&", $text);
+		foreach($arrTemp as $ret) {
+			list($key, $val) = split("=", $ret);
+			$arrRet[$key] = $val;
+		}
+	}
+	return $arrRet;
+}
 ?>
