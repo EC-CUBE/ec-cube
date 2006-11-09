38b2ce0bfafcce889a0afc9f0293090b1c835607
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-09 04:31:46 +0000

    blank

--- data/downloads/module/mdl_epsilon/card.php
+++ data/downloads/module/mdl_epsilon/card.php
@@ -94,6 +94,11 @@ $arrXML = sfPostPaymentData($order_url, $arrData);
 // エラーがあるかチェックする
 $err_code = sfGetXMLValue($arrXML,'RESULT','ERR_CODE');
 
+sfprintr("test");
+sfprintr($_SESSION);
+exit();
+
+
 if($err_code != "") {
 	$err_detail = sfGetXMLValue($arrXML,'RESULT','ERR_DETAIL');
 	sfDispSiteError(FREE_ERROR_MSG, "", true, "購入処理中に以下のエラーが発生しました。<br /><br /><br />・" . $err_detail . "<br /><br /><br />この手続きは無効となりました。");
