908586a777e5d94905e92ec9486fd976a31bf806
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-09 01:22:08 +0000

    blank

--- data/downloads/module/mdl_epsilon/mdl_epsilon.php
+++ data/downloads/module/mdl_epsilon/mdl_epsilon.php
@@ -132,7 +132,7 @@ case 'edit':
 		}
 	
 		// javascript実行
-		//$objPage->tpl_onload = 'alert("登録完了しました。\n基本情報＞支払方法設定より詳細設定をしてください。"); window.close();';
+		$objPage->tpl_onload = 'alert("登録完了しました。\n基本情報＞支払方法設定より詳細設定をしてください。"); window.close();';
 	}
 	break;
 case 'module_del':
