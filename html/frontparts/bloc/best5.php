beae1803dec0981ace0fbb3133e2efb4868956ac
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-16 08:07:19 +0000

    blank

--- html/frontparts/bloc/best5.php
+++ html/frontparts/bloc/best5.php
@@ -33,11 +33,10 @@ function lfGetRanking(){
 	$from = "dtb_best_products AS A INNER JOIN vw_products_allclass AS allcls using(product_id)";
 	$where = "status = 1";
 	$order = "rank";
+	$objQuery->setorder($order);
 	
 	$arrBestProducts = $objQuery->select($col, $from, $where);
 		
-	sfprintr($arrBestProducts);
-
 	return $arrBestProducts;
 }
 
