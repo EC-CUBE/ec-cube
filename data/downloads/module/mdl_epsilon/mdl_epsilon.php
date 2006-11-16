1f643a59a1e992479473886c6af93838a3580a8d
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-16 05:04:49 +0000

    blank

--- data/downloads/module/mdl_epsilon/mdl_epsilon.php
+++ data/downloads/module/mdl_epsilon/mdl_epsilon.php
@@ -115,7 +115,6 @@ case 'edit':
 				// セブンイレブンのみ選択した場合には利用上限を30万にする。
 				if(count($_POST["convenience"]) == 1 and $_POST["convenience"][0] == 11) {
 					$upper_rule_max = "300000";
-					
 					($arrPaymentData["upper_rule"] > $upper_rule_max) ? $upper_rule = $upper_rule_max : $upper_rule = $arrPaymentData["upper_rule"];
 				}else{
 					$upper_rule_max = "500000";
