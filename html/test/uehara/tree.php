a96fb20887da9bcd93150b4d5cc00f119bd2bbe3
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-17 12:01:34 +0000

    blank

--- html/test/uehara/tree.php
+++ html/test/uehara/tree.php
@@ -12,18 +12,34 @@ $dir = USER_PATH;
 $objView = new SC_UserView("./templates/");
 $objQuery = new SC_Query();
 
-if (is_dir($dir)) { 
-    if ($dh = opendir($dir)) { 
-        while (($file = readdir($dh)) !== false) { 
-            echo "filename: ". $file . " : filetype: " . filetype($dir . $file) . "<br>\n"; 
-        } 
-        closedir($dh); 
-    } 
-} 
+$arrTree = array();
 
+getDir($arrTree, $dir);
+
+sfprintr($arrTree);
 //$objView->assignobj($objPage);
-$objView->display("tree.tpl")
+$objView->display("tree.tpl");
 
 //-----------------------------------------------------------------------------------------------------------------------------------
+
+/* 
+ * 関数名：getDir()
+ * 説明　：指定パス配下のディレクトリ取得
+ * 引数1 ：ツリーを格納配列
+ * 引数2 ：取得するディレクトリパス
+ */
+function getDir(&$arrTree, $dir) {
+	if (is_dir($dir)) { 
+	    if ($dh = opendir($dir)) { 
+	        while (($file = readdir($dh)) !== false) { 
+				// ./ と ../を除くディレクトリのみを取得
+				if(filetype($dir . $file) == 'dir' && $file != "." && $file != "..") {
+					$arrTree[] = dir.file;
+				} 
+	        } 
+	        closedir($dh); 
+	    }
+	} 
+}
 
 ?>
