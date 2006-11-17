d34ea0b9cc76f9243ccde679e0a3d542ded9921d
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-17 11:08:09 +0000

    blank

--- html/test/uehara/tree.php
+++ html/test/uehara/tree.php
@@ -6,8 +6,20 @@
  */
 require_once("../../require.php");
 
+
+$dir = USER_PATH;
+
 $objView = new SC_UserView("./templates/");
 $objQuery = new SC_Query();
+
+if (is_dir($dir)) { 
+    if ($dh = opendir($dir)) { 
+        while (($file = readdir($dh)) !== false) { 
+            echo "filename: ". $file . " : filetype: " . filetype($dir . $file) . "<br>\n"; 
+        } 
+        closedir($dh); 
+    } 
+} 
 
 //$objView->assignobj($objPage);
 $objView->display("tree.tpl")
