ec89e540bcc888b839554ad004dafe4eeb2a0462
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-17 00:18:00 +0000

    blank

--- data/class/SC_View.php
+++ data/class/SC_View.php
@@ -143,10 +143,8 @@ class SC_SiteView extends SC_View{
 		$this->initpath();
 		
 		// PHP5ではsessionをスタートする前にヘッダー情報を送信していると警告が出るため、先にセッションをスタートするように変更
-		sfDomainSessionStart();
 		$objCartSess = new SC_CartSession();
 		$objCartSess->setPrevURL($_SERVER['REQUEST_URI']);
-
 		
 	}
 }
