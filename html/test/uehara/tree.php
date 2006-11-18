e7b6a5c1861f539d7d144b62ee1f4d169ded3d5c
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-18 07:21:00 +0000

    blank

--- html/test/uehara/tree.php
+++ html/test/uehara/tree.php
@@ -7,39 +7,63 @@
 require_once("../../require.php");
 
 
-$dir = USER_PATH;
+$top_dir = USER_PATH;
 
 $objView = new SC_UserView("./templates/");
 $objQuery = new SC_Query();
 
-$arrTree = array();
+switch($_POST['mode']) {
 
-getDir($arrTree, $dir);
+case 'view':
+case 'download':	
+case 'delete':
+	$now_dir = $_POST['view_dir'];
+	
+case 'view':
+	break;
 
-sfprintr($arrTree);
+case 'download':
+	break;
+	
+case 'delete':
+	break;
+	
+default :
+	$now_dir = $top_dir;
+	break;
+}
+// 現在のディレクトリ配下のファイル一覧を取得
+$arrFileList = getFileList($now_dir);
+
+sfprintr($arrFileList);
+
 //$objView->assignobj($objPage);
 $objView->display("tree.tpl");
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 
 /* 
- * 関数名：getDir()
+ * 関数名：getFileList()
  * 説明　：指定パス配下のディレクトリ取得
  * 引数1 ：ツリーを格納配列
  * 引数2 ：取得するディレクトリパス
  */
-function getDir(&$arrTree, $dir) {
+function getFileList($dir) {
+	$arrFileList = array();
 	if (is_dir($dir)) { 
 	    if ($dh = opendir($dir)) { 
 	        while (($file = readdir($dh)) !== false) { 
 				// ./ と ../を除くディレクトリのみを取得
-				if(filetype($dir . $file) == 'dir' && $file != "." && $file != "..") {
-					$arrTree[] = $dir.$file;
-				} 
+				//if(filetype($dir . $file) == 'dir' && $file != "." && $file != "..") {
+				if($file != "." && $file != "..") {
+					$arrAllFile[] = $dir.$file;
+				}
 	        } 
 	        closedir($dh); 
 	    }
 	} 
+	
+	return $arrFileList;
 }
 
 ?>
