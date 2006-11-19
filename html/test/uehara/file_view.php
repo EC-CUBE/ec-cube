c8f6c42595e9c8f77cbc4cc043deb3d8ce317e61
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-19 11:37:56 +0000

    blank

--- html/test/uehara/file_view.php
+++ html/test/uehara/file_view.php
@@ -9,7 +9,7 @@ require_once("../../require.php");
 
 // ファイル内容表示
 print("<pre>\n");
-lfReadFile(USER_PATH.$_GET['file']);
+print(lfReadFile(USER_PATH.$_GET['file']));
 print("</pre>\n");
 
 /* 
@@ -17,9 +17,24 @@ print("</pre>\n");
  * 引数1 ：ファイルパス
  * 説明　：ファイル読込
  */
+ /*
 function lfReadFile($file) {
 	$fp = fopen($file, "r");
 	$read_file = fpassthru($fp); 
 	fclose($fp); 
 }
+*/
+function lfReadFile($filename) { 
+    $str = ""; 
+    // バイナリモードでオープン 
+    $fp = @fopen($filename, "rb" ); 
+    //ファイル内容を全て変数に読み込む 
+    if($fp) { 
+        $str = @fread($fp, filesize($filename)+1); 
+    } 
+    @fclose($fp); 
+    // 改行コードの前に<br />を挿入 
+    $str = nl2br($str); 
+    return $str; 
+} 
 ?>
