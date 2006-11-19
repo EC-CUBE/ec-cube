d7eaef4d58dcfc1cf77e9095862aadacff08c100
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-19 11:13:57 +0000

    *** empty log message ***

---
+++ html/test/uehara/file_view.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright(c) 2000-2006 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ */
+ 
+// ファイル内容表示
+echo lfReadFile($_GET['file']);
+
+/* 
+ * 関数名：lfReadFile()
+ * 引数1 ：ファイルパス
+ * 説明　：ファイル読込
+ */
+function lfReadFile($file) {
+	$fp = fopen($file, "r");
+	$read_file = fpassthru($fp); 
+	fclose($fp); 
+	
+	return read_file;
+}
+?>
