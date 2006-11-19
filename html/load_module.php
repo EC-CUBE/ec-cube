438ac057aabb1dfcf63305c942d7851b3251e462
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-19 06:28:27 +0000

    blank

--- html/load_module.php
+++ html/load_module.php
@@ -7,7 +7,6 @@
 require_once("./require.php");
 require_once(DATA_PATH . "module/Request.php");
 
-
 if($_GET['module_id'] != ""){
 	$module_id = $_GET['module_id'];
 }elseif($_POST['module_id'] != ""){
