c9076adb164f14e004f2f18177300cd50bfb239a
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-19 05:41:27 +0000

    *** empty log message ***

--- html/test/kakinaka/epsilon_check.php
+++ html/test/kakinaka/epsilon_check.php
@@ -10,9 +10,8 @@ if($_POST["trans_code"] != "" and $_POST["paid"] == 1 and $_POST["order_number"]
 	$sql = "UPDATE dtb_order SET status = 6, update_date = now() WHERE order_id = ? AND memo04 = ? ";
 	$objQuery->query($sql, array($_POST["order_number"], $_POST["trans_code"]));
 	
-	$log_path = DATA_PATH . "logs/epsilon.log";
-	
 	// POSTの内容を全てログ保存
+	$log_path = DATA_PATH . "logs/epsilon.log";
 	gfPrintLog("epsilon conveni start---------------------------------------------------------", $log_path);
 	foreach($_POST as $key => $val){
 		gfPrintLog( "\t" . $key . " => " . $val, $log_path);
---
+++ html/test/send.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("../require.php");
+require_once(DATA_PATH . "module/Request.php");
+
+$order_url = "http://test.ec-cube.net/ec-cube/test/kakinaka/epsilon_check.php";
+
+$arrData = array(
+	'order_number' => '1',
+	'trans_code' => '1',
+	'paid' => 1	
+);
+
+$req = new HTTP_Request($order_url);
+$req->setMethod(HTTP_REQUEST_METHOD_POST);
+		
+$arrSendData = array();
+$req->addPostDataArray($arrData);
+if (!PEAR::isError($req->sendRequest())) {
+	$response = $req->getResponseBody();
+} else {
+	$response = "";
+}
+
+$req->clearPostData();
+
+?>
