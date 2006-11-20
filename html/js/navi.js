4266c5c2da6522502d4ecc9ec28e28f1ab181a9a
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-20 00:39:20 +0000

    *** empty log message ***

--- html/js/navi.js
+++ html/js/navi.js
@@ -9,7 +9,7 @@
 		arrImgList = new Array (
 			"/img/header/basis_on.jpg","/img/header/product_on.jpg","/img/header/customer_on.jpg","/img/header/order_on.jpg",
 			"/img/header/sales_on.jpg","/img/header/mail_on.jpg","/img/header/contents_on.jpg",
-			"/img/header/mainpage_on.gif","/img/header/seitecheck_on.gif","/img/header/logout_on.gif",
+			"/img/header/mainpage_on.gif","/img/header/sitecheck_on.gif","/img/header/logout.gif",
 			"/img/contents/btn_search_on.jpg","/img/contents/btn_regist_on.jpg",
 			"/img/contents/btn_csv_on.jpg","/img/contents/arrow_left.jpg","/img/contents/arrow_right.jpg"
 		);
