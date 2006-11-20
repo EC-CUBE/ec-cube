aad85cbb81fb3f6521d31e148d737b2529775841
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-20 04:07:32 +0000

    blank

--- html/test/uehara/file_view.php
+++ html/test/uehara/file_view.php
@@ -10,6 +10,6 @@ require_once("../../require.php");
 
 // ファイル内容表示
 header("Content-type: text/plain\n\n");
-print(nl2br(lfReadFile(USER_PATH.$_GET['file'])));
+print(lfReadFile(USER_PATH.$_GET['file']));
 
 ?>
