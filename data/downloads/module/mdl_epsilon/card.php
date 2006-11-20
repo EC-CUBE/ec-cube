e650367f2d2e3660aee4edcef5a3ee7f7ce1f75e
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-20 07:01:17 +0000

    blank

--- data/downloads/module/mdl_epsilon/card.php
+++ data/downloads/module/mdl_epsilon/card.php
@@ -81,11 +81,7 @@ function lfSendCredit($arrData, $arrPayment, $arrMainProduct, $again = true){
 	$order_url = $arrPayment[0]["memo02"];
 
 	// 非会員のときは user_id に not_memberと送る
-	($arrData["customer_id"] == "") ? $user_id = "not_member" : $user_id = $arrData["customer_id"];	
-	
-	
-	sfprintr($user_id);
-	exit();
+	($arrData["customer_id"] == 0) ? $user_id = "not_member" : $user_id = $arrData["customer_id"];	
 	
 	// 送信データ生成
 	$item_name = $arrMainProduct["name"] . "×" . $arrMainProduct["quantity"] . "個 (代表)";
