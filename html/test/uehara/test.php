0d871f74ef056c5167c7580a63c9ac570ca3f9a7
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-21 06:05:28 +0000

    blank

--- html/test/uehara/test.php
+++ html/test/uehara/test.php
@@ -32,22 +32,21 @@ function sfGetFileTree($dir) {
 function sfGetFileTree($dir) {
 	global $arrTree;
 	if(file_exists($dir)) {
-		// ディレクトリかチェック
-		if (is_dir($dir)) {
-			if ($handle = opendir("$dir")) {
-				$cnt = 0;
-				while (false !== ($item = readdir($handle))) {
-					if ($item != "." && $item != "..") {
-						if (is_dir("$dir/$item")) {
-							$arrTree[]['file_name'] = "$dir/$item<br/>";
-							sfGetFileTree("$dir/$item");
-						}
+		if ($handle = opendir("$dir")) {
+			$cnt = 0;
+			while (false !== ($item = readdir($handle))) {
+				if ($item != "." && $item != "..") {
+					// ディレクトリかチェック
+					if (is_dir("$dir/$item")) {
+						$arrTree[]['file_name'] = "$dir/$item<br/>";
+						// 下層ディレクトリ取得の為、再帰的に呼び出す
+						sfGetFileTree("$dir/$item");
 					}
-					$cnt++;
 				}
+				$cnt++;
 			}
-			closedir($handle);
 		}
+		closedir($handle);
 	}
 }
 
