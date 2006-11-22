4640fdea5a4a380f82ca440e893259cdc2a03ef7
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-22 14:30:05 +0000

    blank

--- html/test/uehara/js/tree.js
+++ html/test/uehara/js/tree.js
@@ -56,7 +56,7 @@ function fnTreeView(view_id, arrTree) {
 
 		if(arrTree[i][4]) {
 			// 開き状態を保持
-			arrTreeStatus.push();
+			arrTreeStatus.push(arrTree[i][2]);
 			display = 'block';
 		} else {
 			display = 'none';
