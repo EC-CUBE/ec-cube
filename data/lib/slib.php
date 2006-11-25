efae6ef5170d87ba178f521956d40ae20aee2669
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-25 05:22:34 +0000

    blank

--- data/lib/slib.php
+++ data/lib/slib.php
@@ -800,7 +800,7 @@ function sfGetCSVList($array) {
 	sfprintr($array);
 	if (count($array) > 0) {
 		foreach($array as $key => $val) {
-			$line = ereg_replace("\n", ",$", $line);
+			$line = ereg_replace("\n", "\\n", $line);
 			$line .= "\"".$val."\",";
 		}
 		$line = ereg_replace(",$", "\n", $line);
