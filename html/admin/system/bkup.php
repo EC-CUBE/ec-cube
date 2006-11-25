ce7956efa11e8dffccd1d13988bb4d60e3e854ef
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-25 06:29:48 +0000

    blank

--- html/admin/system/bkup.php
+++ html/admin/system/bkup.php
@@ -92,7 +92,7 @@ case 'download' :
 	$filename = $_POST['list_name'] . ".tar.gz";
 	$dl_file = $objPage->bkup_dir.$_POST['list_name'];
 	
-	header("Content-Disposition: attachment;filename=" .$filename. "\""); 
+	header("Content-Disposition: attachment;filename=" .$_POST['list_name']. "\""); 
 	header("Content-Length: " .filesize($dl_file)); 
 	header("Content-Type: application/octet-stream; name=" .$filename. "\""); 
 	readfile ($dl_file);
