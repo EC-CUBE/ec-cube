9acb332740a32e093ae7044fa49c6c3eacb1b8aa
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-26 12:34:47 +0000

    blank

--- html/test/uehara/js/tree.js
+++ html/test/uehara/js/tree.js
@@ -10,7 +10,7 @@ var arrTreeStatus = new Array();
 // ツリー表示
 function fnTreeView(view_id, arrTree) {
 
-	//tree += '<form name="tree_form">';
+	tree += '<form name="tree_form">';
 	for(i = 0; i < arrTree.length; i++) {
 		
 		id = arrTree[i][0];
@@ -64,13 +64,18 @@ function fnTreeView(view_id, arrTree) {
 		}
 		arrFileSplit = arrTree[i][2].split("/");
 		file_name = arrFileSplit[arrFileSplit.length-1];
+		style="display:'+ display +'";
+		tree += '<div style="display:none">';
+		tree += '<input type="button" name="tree_test"'+ i +'">';
+		tree += '</div>';
+
 		tree += '<input type="image" src="'+ defalt_img +'" border="0" name="tree_img'+ i +'" id="tree_img'+ i +'" onclick="fnTreeMenu(\'tree'+ i +'\',\''+ arrTree[i][1] +'\',\'tree_img'+ i +'\',\''+ arrTree[i][2] +'\')">'+ file_name +'<br/>';
 		tree += '<div id="tree'+ i +'" style="display:'+ display +'">';
 	
 	}
-	//tree += '</form>';
+	tree += '</form>';
 	fnDrow(view_id, tree);
-	//document.tree_form.tree_img1.focus();	
+	document.tree_form.tree_test2.focus();	
 }
 
 // Tree状態をhiddenにセット
