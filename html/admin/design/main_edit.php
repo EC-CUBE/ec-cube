1c638c287ebddd483bdedeccd0408c67d39317c2
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-26 11:17:57 +0000

    blank

--- html/admin/design/main_edit.php
+++ html/admin/design/main_edit.php
@@ -127,7 +127,7 @@ if ($_POST['mode'] == 'preview') {
 	
 	$_SESSION['preview'] = "ON";
 	
-	header("location: " . URL_DIR . "preview/index.php");
+	//header("location: " . URL_DIR . "preview/index.php");
 }
 
 // データ登録処理
