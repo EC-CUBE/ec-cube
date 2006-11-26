8516e255046d3d46e81979910e9d92928e93c450
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-26 11:01:51 +0000

    blank

--- html/test/uehara/js/tree.js
+++ html/test/uehara/js/tree.js
@@ -68,7 +68,7 @@ function fnTreeView(view_id, arrTree) {
 	}
 	tree += '</form>';
 	fnDrow(view_id, tree);
-	document.getElementById(tree_img10).focus();	
+	document.tree_form.element[0].focus();	
 }
 
 // Tree状態をhiddenにセット
