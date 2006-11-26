cf3db63f5bb20cb395c96703bc92cde85a4fe9ff
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-26 12:46:04 +0000

    blank

--- html/test/uehara/js/tree.js
+++ html/test/uehara/js/tree.js
@@ -65,7 +65,7 @@ function fnTreeView(view_id, arrTree) {
 		arrFileSplit = arrTree[i][2].split("/");
 		file_name = arrFileSplit[arrFileSplit.length-1];
 		tree += '<div style="display:none">';
-		tree += '<input type="button" name="tree_test"'+ i +'">';
+		tree += '<input type="button" name="tree_test'+ i +'">';
 		tree += '</div>';
 
 		tree += '<input type="image" src="'+ defalt_img +'" border="0" name="tree_img'+ i +'" id="tree_img'+ i +'" onclick="fnTreeMenu(\'tree'+ i +'\',\''+ arrTree[i][1] +'\',\'tree_img'+ i +'\',\''+ arrTree[i][2] +'\')">'+ file_name +'<br/>';
