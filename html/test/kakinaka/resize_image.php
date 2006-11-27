98e182d9711c53e0111e34a3e95a480a65d1bb25
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-27 02:17:55 +0000

    blank

--- html/test/kakinaka/resize_image.php
+++ html/test/kakinaka/resize_image.php
@@ -6,7 +6,7 @@ $objView = new SC_UserView("./templates/");
 $objImage = new SC_Image(IMAGE_TEMP_DIR);
 
 
-$file = IMAGE_TEMP_DIR . "kaki.gif";
+$file = IMAGE_TEMP_DIR . "kaki.png";
 
 sfprintr(pathinfo($file));
 $path = $objImage->saveResizeImage($file, 0.5);
