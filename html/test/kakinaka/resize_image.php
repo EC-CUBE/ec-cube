62fed1d635f8a84090be9717ea25aa2fb226a8c4
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-27 01:53:22 +0000

    *** empty log message ***

---
+++ html/test/kakinaka/resize_image.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once("../../require.php");
+
+$objView = new SC_UserView("./templates/");
+$objImage = new SC_Image();
+
+
+$file = "/html/upload/temp_image/kaki.jpg";
+$path = $objImage->saveResizeImage($file, 1);
+
+sfprintr($path);
+
+?>
