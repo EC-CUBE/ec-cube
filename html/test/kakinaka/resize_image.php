7fa60330b37b9ab90912e850cc0dcde647905e0f
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-27 10:11:35 +0000

    blank

--- html/test/kakinaka/resize_image.php
+++ html/test/kakinaka/resize_image.php
@@ -9,7 +9,7 @@ $objImage = new SC_Image(IMAGE_TEMP_DIR);
 $file = IMAGE_TEMP_DIR . "kaki.jpg";
 
 //sfprintr(pathinfo($file));
-$path = $objImage->saveResizeImage($file, 100, 1000, true);
+$path = $objImage->saveResizeImage($file, 1000, 1000, true);
 
 //sfprintr($path);
 
