f0c65e508e602f31cd8b01ac5a986f2b9c51b00e
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-27 13:49:34 +0000

    blank

--- html/test/uehara/js/tree.js
+++ html/test/uehara/js/tree.js
@@ -1,7 +1,7 @@
 var IMG_FOLDER_CLOSE   = "./img/folder_close.gif";				// フォルダクローズ時画像
 var IMG_FOLDER_OPEN    = "./img/folder_open.gif";				// フォルダオープン時画像
 var IMG_PLUS           = "./img/plus.gif";						// プラスボタン
-var IMG_MINUS          = "./img/folder_open_m.gif";				// マイナスボタン
+var IMG_MINUS          = "./img/minus.gif";						// マイナスボタン
 
 var tree = "";
 var count = 0;
