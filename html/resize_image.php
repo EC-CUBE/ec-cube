de6a5d765fb8f933fadc27f6d8251db7b2fe396a
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-27 10:24:26 +0000

    blank

--- html/resize_image.php
+++ html/resize_image.php
@@ -6,7 +6,9 @@ require_once($include_dir . "/../data/class/SC_Image.php");
 $objImage = new SC_Image(IMAGE_TEMP_DIR);
 
 $file = $_GET["image"];
+if(file_exists($file)){
+	$objImage->saveResizeImage($file, $_GET["width"], $_GET["height"], true);
+}
 
-$objImage->saveResizeImage($file, 100, 100, true);
 
 ?>
