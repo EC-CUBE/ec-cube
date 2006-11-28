9ce10f9d153ceccffa29c374fcafe358a12c3954
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-28 10:28:35 +0000

    blank

--- html/test/kakinaka/index.php
+++ html/test/kakinaka/index.php
@@ -5,9 +5,31 @@ require_once("../../require.php");
 $objView = new SC_UserView("./templates/");
 $objImage = new SC_Image(IMAGE_TEMP_DIR);
 
+//---- ページ表示用クラス
+class LC_Page {
+	function LC_Page() {
+		$this->tpl_mainpage = 'customer/release_set.tpl';
+		$this->tpl_mainno = 'release_set';
+		$this->tpl_subnavi = 'customer/subnavi.tpl';
+		$this->tpl_subno = 'release_set';
+		
+		global $arrRelease;
+		$this->arrRelease = $arrRelease;
+	}
+}
 
-$file = IMAGE_TEMP_DIR . "kaki.jpg";
+exit();
 
-$objView->display("index.tpl")
-
+//---- ページ表示用クラス
+class LC_Page {
+	function LC_Page() {
+		$this->tpl_mainpage = 'customer/release_set.tpl';
+		$this->tpl_mainno = 'release_set';
+		$this->tpl_subnavi = 'customer/subnavi.tpl';
+		$this->tpl_subno = 'release_set';
+		
+		global $arrRelease;
+		$this->arrRelease = $arrRelease;
+	}
+}
 ?>
