13bd46616e1d7c446691840d6cc15ede3c26b8bc
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-28 02:08:49 +0000

    blank

--- html/resize_image.php
+++ html/resize_image.php
@@ -9,9 +9,7 @@ $objThumb = new gdthumb();
 $file = $_GET["image"];
 if(file_exists($file)){
 	$objThumb->Main($file, $_GET["width"], $_GET["height"], "", true);
-//	$objImage->saveResizeImage($file, $_GET["width"], $_GET["height"], true);
 }else{
-//	$objImage->saveResizeImage(NO_IMAGE_DIR, $_GET["width"], $_GET["height"], true);
 	$objThumb->Main(NO_IMAGE_DIR, $_GET["width"], $_GET["height"], "", true);
 }
 
