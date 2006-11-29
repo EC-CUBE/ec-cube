02f79a1e31e6dec18801ce6ae28d4b7167a6f8f3
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-29 14:01:47 +0000

    blank

--- html/admin/contents/file_manager.php
+++ html/admin/contents/file_manager.php
@@ -122,6 +122,7 @@ $objPage->tpl_now_file = $now_dir;
 $objPage->arrErr = $arrErr;
 $objPage->arrParam = $_POST;
 $objPage->arrTree = sfGetFileTree($top_dir, $_POST['tree_status']);
+// ツリーを表示する divタグid, ツリー配列変数名, 現在ディレクトリ
 $objPage->$tpl_onload .= "fnTreeView('tree', arrTree, '$now_dir')";
 
 // 画面の表示
