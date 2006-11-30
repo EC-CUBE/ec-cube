3fe14b5a10cacf83f5c2ebfdd4c3c1718e3a685a
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-30 06:07:32 +0000

    blank

--- data/lib/slib.php
+++ data/lib/slib.php
@@ -1110,14 +1110,11 @@ function sfPreTax($price, $tax, $tax_rule, $digit = 1) {
 // 桁数を指定して四捨五入
 function sfRound($value, $digit = 2){
 	$adjust = 1;
-	sfprintr($value);
 	
 	// 整数且つ0出なければ桁数指定を行う
 	if(sfIsInt($digit) and $digit > 1){
 		$adjust = 10 * ($digit - 1);
-		$value = $value * $adjust;
 		$ret = round($value);
-	sfprintr($ret);
 		$ret = ($ret/$adjust);
 	sfprintr($ret);
 		sfRound($ret, $digit - 1);
