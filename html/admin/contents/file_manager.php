dfe6deeb7b63f9ae53ca64ae8e6f3e8173413b56
uehara <uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-30 00:54:40 +0000

    blank

--- html/admin/contents/file_manager.php
+++ html/admin/contents/file_manager.php
@@ -53,7 +53,7 @@ case 'view':
 		} else {
 			// javascriptで別窓表示(テンプレート側に渡す)
 			$file_url = ereg_replace(USER_PATH, "", $_POST['select_file']);
-			$objPage->tpl_javascript = "win02('./file_view.php?file=". $file_url ."', 'user_data', '600', '400');";
+			$objPage->tpl_onload = "win02('./file_view.php?file=". $file_url ."', 'user_data', '600', '400');";
 		}
 	}
 	break;
