82f1cbce94a7e1c6d5d3ae59874357f3da9d5471
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-30 04:47:39 +0000

    blank

--- html/admin/products/product.php
+++ html/admin/products/product.php
@@ -86,6 +86,7 @@ case 'copy' :
 			$arrSaveFile = $objUpFile->save_file;
 			
 			foreach($arrSaveFile as $key => $val){
+				sfprintr($arrKeyID[$val]);
 				lfMakeScaleImage($arrKeyID[$val], $arrKeyID[$val], true); 
 			}
 		}
