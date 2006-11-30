bacccf176cff7ebc9fd26fa69d8645eff14d84f2
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-30 02:01:33 +0000

    blank

--- data/lib/gdthumb.php
+++ data/lib/gdthumb.php
@@ -213,6 +213,7 @@ class gdthumb {
     
 			// png·Á¼°    
 			case "3": 
+					sfprintr(imagecolorstotal(imageCreateFromGIF($path)));
 
 				$src_im = imageCreateFromPNG($path);
 				
@@ -226,8 +227,6 @@ class gdthumb {
 				} else {				
 					$dst_im = $imagecreate($re_size[0], $re_size[1]);
 					imagecopyresized($dst_im,$src_im, 0, 0, 0, 0, $re_size[0], $re_size[1], $size[0], $size[1]);
-					
-					sfprintr(imagecolorstotal($dst_im));
 					
 					imagetruecolortopalette($dst_im, false, imagecolorstotal($src_im));
 				}
