44caf889b21db2bf9640b765f2893a964d4bb607
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-30 05:50:50 +0000

    blank

--- data/lib/slib.php
+++ data/lib/slib.php
@@ -1108,7 +1108,7 @@ function sfPreTax($price, $tax, $tax_rule, $digit = 1) {
 }
 
 // 桁数を指定して四捨五入
-function sfRound($value, $digit = 2){
+function sfRound($value, $digit = 1){
 	$adjust = 1;
 	
 	// 整数且つ0出なければ桁数指定を行う
