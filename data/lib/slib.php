e33ab3b20dceb0160b7293bf740b332a8c57cb0f
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-11-30 07:22:00 +0000

    blank

--- data/lib/slib.php
+++ data/lib/slib.php
@@ -1115,6 +1115,8 @@ function sfRound($value, $pow = 0){
 
 	// 整数且つ0出なければ桁数指定を行う
 	if(sfIsInt($adjust) and $pow > 0){
+		
+		sfprintr(floor($value * $adjust)));
 		$ret = (floor($value * $adjust)/$adjust);
 		$ret = sfRound($ret, $pow - 1);
 		return $ret;
