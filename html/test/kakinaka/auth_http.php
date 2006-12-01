68a2e0bd265b4382f2f0e615f2beaaddcd74485b
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-12-01 06:49:24 +0000

    blank

--- html/test/kakinaka/auth_http.php
+++ html/test/kakinaka/auth_http.php
@@ -4,34 +4,29 @@ require_once("../../require.php");
 $include_dir = realpath(dirname( __FILE__));
 require_once($include_dir . "/pear/Auth_HTTP.php");
 
-define("DSN", "pgsql://kakinaka_db_user:password@kakinaka.ec-cube.net/kakinaka_db");
+//define("DSN", "pgsql://kakinaka_db_user:password@kakinaka.ec-cube.net/kakinaka_db");
+define("DSN", "mysql://user_db:password@210.18.212.165:3308/eccube_db");
 
 // データベース接続オプションの設定
-$AuthOptions = array(
-'dsn'=>DSN,
-'table'=>"dtb_member",                            // テーブル名 
-'usernamecol'=>"login_id",			// ユーザ名のコラム
-'passwordcol'=>"password",			// パスワードのコラム
-//'cryptType'=>"none",				// データベース中でのパスワードの暗号化形式
-'cryptType'=>"none",				// データベース中でのパスワードの暗号化形式
-'db_fields'=>"*",				// 他のコラムの取得を可能にする
+$arrDbConn = array(
+	'dsn'=>DSN,
+	'table'=>"dtb_member",              // テーブル名 
+	'usernamecol'=>"login_id",			// ユーザ名のカラム
+	'passwordcol'=>"password",			// パスワードのカラム
+	'cryptType'=>"none",				// データベース中でのパスワードの暗号化形式
+	'db_fields'=>"*",					// 他のカラムの取得を可能にする
 );
 
-$a = new Auth_HTTP("DB", $AuthOptions);
+$objAuthHttp = new Auth_HTTP("DB", $AuthOptions);
 
-$a->setRealm('user realm');			// 領域 (realm) 名
-$a->setCancelText('<h2>Error 401</h2>');        // 認証が失敗した際に表示されるメッセージ
-$a->start();					// 認証プロセスの開始
+$objAuthHttp->setRealm('user realm');				// 領域 (realm) 名
+$objAuthHttp->setCancelText('接続エラー'); 	   	// 認証が失敗した際に表示されるメッセージ
+$objAuthHttp->start();					// 認証プロセスの開始
 
-if($a->getAuth())				// 認証すべきユーザかどうかの確認 
+if($objAuthHttp->getAuth())				// 認証すべきユーザかどうかの確認 
 {
-	echo "Hello " . $a->username . " welcome to my secret page <BR>";
-	echo "Your details on file are: <BR>";
-	echo $a->getAuthData('name');		// データベースから他のデータを取得している。
-	echo $a->getAuthData('telephone');      // この例では、ユーザID (userid)、電話番号 (telephone)
-	echo $a->getAuthData('name');		// およびメールアドレス (email) を取得。
-};
-
-sfprintr($a);
+	echo "認証成功";
+	echo $objAuthHttp->getAuthData('name');		// およびメールアドレス (email) を取得。
+}
 
 ?>
