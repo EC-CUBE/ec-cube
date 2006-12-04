2d93f373ae44fb5325ac7e005bd89d694343ff6f
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-12-04 05:06:38 +0000

    blank

--- html/admin/system/bkup.php
+++ html/admin/system/bkup.php
@@ -193,7 +193,7 @@ function lfCreateBkupData($bkup_name){
 				// データをCSV形式に整える
 				$data = "";
 				foreach($arrData as $data_key => $data_val){
-					mb_convert_encoding($data_val, CHAR_CODE, CHAR_CODE);
+					mb_convert_encoding($arrData[$data_key], CHAR_CODE, CHAR_CODE);
 					$data .= sfGetCSVList($arrData[$data_key]);
 					
 					
