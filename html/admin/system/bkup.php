80837ee69944cf1d3020f3af5c43d816113358e6
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2006-12-04 05:17:47 +0000

    *** empty log message ***

--- html/admin/system/bkup.php
+++ html/admin/system/bkup.php
@@ -193,10 +193,8 @@ function lfCreateBkupData($bkup_name){
 				// データをCSV形式に整える
 				$data = "";
 				foreach($arrData as $data_key => $data_val){
-					//mb_convert_encoding($arrData[$data_key], CHAR_CODE, CHAR_CODE);
 					$data .= sfGetCSVList($arrData[$data_key]);
 				}
-
 				// CSV出力データ生成
 				$csv_data .= $val . "\n";
 				$csv_data .= $arrKyes . "\n";
