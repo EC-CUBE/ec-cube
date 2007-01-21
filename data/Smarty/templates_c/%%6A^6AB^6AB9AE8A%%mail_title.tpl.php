ac599b3bd2dcb667ec86e9c569d788898b23aef4
root <root@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-01-21 06:59:21 +0000

    new import

--- data/Smarty/templates_c/%%6A^6AB^6AB9AE8A%%mail_title.tpl.php
+++
@@ -1,4 +0,0 @@
-<?php /* Smarty version 2.6.13, created on 2007-01-10 00:02:49
-         compiled from mail_templates/mail_title.tpl */ ?>
-¡Ú<?php echo $this->_tpl_vars['tpl_shopname']; ?>
-¡Û <?php echo $this->_tpl_vars['tpl_mailtitle']; ?>
--- data/Smarty/templates_c/admin/%%FE^FE8^FE8CF16B%%preview.tpl.php
+++
@@ -1,5 +0,0 @@
-<?php /* Smarty version 2.6.13, created on 2007-01-18 18:20:49
-         compiled from mail/preview.tpl */ ?>
-<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
-smarty_core_load_plugins(array('plugins' => array(array('modifier', 'escape', 'mail/preview.tpl', 8, false),array('modifier', 'nl2br', 'mail/preview.tpl', 8, false),)), $this); ?>
-<?php if ($this->_tpl_vars['escape_flag'] == 1):  echo ((is_array($_tmp=((is_array($_tmp=$this->_tpl_vars['body'])) ? $this->_run_mod_handler('escape', true, $_tmp) : smarty_modifier_escape($_tmp)))) ? $this->_run_mod_handler('nl2br', true, $_tmp) : smarty_modifier_nl2br($_tmp));  else:  echo $this->_tpl_vars['body'];  endif; ?>
