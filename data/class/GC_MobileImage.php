a26e3f17a73ccc165dd3ce5e1117014aadd809cf
rebelt <rebelt@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-02-16 15:03:22 +0000

    テスト

--- data/class/GC_MobileImage.php
+++ data/class/GC_MobileImage.php
@@ -23,9 +23,7 @@ class GC_MobileImage {
 		// 端末情報を取得する
 		$carrier = GC_MobileUserAgent::getCarrier();
 		$model   = GC_MobileUserAgent::getModel();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-trigger_error('ok', E_USER_WARNING);
+$fp = fopen('/tmp/rebelt', 'w'); fwrite($fp, 'ok'); fclose($fp);
         // 携帯電話の場合のみ処理を行う
 		if ($carrier !== FALSE) {
 
