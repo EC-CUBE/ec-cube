acce3ccb687a62dffe2f5a3ac7995e3dcda04a15
inoue <inoue@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-03-26 07:46:32 +0000

    トラックバックテーブル(MySQL)

---
+++ html/install/sql/add/dtb_trackback_mysql.sql
@@ -0,0 +1,13 @@
+CREATE TABLE dtb_trackback (
+	trackback_id int auto_increment NOT NULL,
+	product_id int NOT NULL,
+	blog_name varchar(255) NOT NULL DEFAULT '',
+	title varchar(255) NOT NULL DEFAULT '',
+	excerpt text NOT NULL DEFAULT '',
+	url text NOT NULL DEFAULT '',
+	status int NOT NULL DEFAULT 2,
+	del_flg int NOT NULL DEFAULT 0,
+	create_date datetime NOT NULL,
+	update_date datetime NOT NULL,
+	PRIMARY KEY (trackback_id)
+) TYPE=InnoDB;
