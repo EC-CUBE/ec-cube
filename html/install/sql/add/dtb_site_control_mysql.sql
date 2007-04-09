ff882eb8b58c43ef2abb75a2f397b0e1f5e5f5b2
kakinaka <kakinaka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-04-09 04:42:13 +0000

    dtb_site_control を NULL許可に変更

--- html/install/sql/add/dtb_site_control_mysql.sql
+++ html/install/sql/add/dtb_site_control_mysql.sql
@@ -1,10 +1,10 @@
 CREATE TABLE dtb_site_control (
     control_id int auto_increment NOT NULL,
-    control_title varchar(255) NOT NULL DEFAULT '',
-    control_text text NOT NULL DEFAULT '',
+    control_title varchar(255) ,
+    control_text text ,
     control_flg int NOT NULL DEFAULT 2,
     del_flg int NOT NULL DEFAULT 0,
-    memo text NOT NULL DEFAULT '',
+    memo text ,
     create_date datetime NOT NULL,
     update_date datetime NOT NULL,
     PRIMARY KEY  (control_id)
