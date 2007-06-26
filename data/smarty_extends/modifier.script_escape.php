130a167babe04e5ec105313eef52e012b82e8a69
adati <adati@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-06-26 06:10:47 +0000

    XSS:置換候補に「javascript:」を追加

--- data/smarty_extends/modifier.script_escape.php
+++ data/smarty_extends/modifier.script_escape.php
@@ -7,10 +7,10 @@
  */
 function smarty_modifier_script_escape($value) {
     
-    if (is_array($value)) {return $value;}
+    if (is_array($value)) return $value;
     
-    $pattern = "/<script.*?>|<\/script>/";
-    $convert = "#####";
+    $pattern = "/<script.*?>|<\/script>|javascript:/";
+    $convert = "#script tag escaped#";
     
     if ( preg_match_all($pattern, $value, $matches) ) {
         return preg_replace($pattern, $convert, $value);
