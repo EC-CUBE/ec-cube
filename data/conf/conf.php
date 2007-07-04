7932457b724391477c581a454f4731d7ead51733
adati <adati@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-07-04 03:28:24 +0000

    [1.3.3正式版リリース準備]

--- data/conf/conf.php
+++ data/conf/conf.php
@@ -118,7 +118,7 @@ define("CREDIT_HTTP_ANALYZE_URL", CREDIT_HTTP_DOMAIN . CREDIT_HTTP_ANALYZE_PROGR
 define("CHAR_CODE", "EUC-JP");
 
 // EC-CUBEバージョン情報
-define("ECCUBE_VERSION", "1.3.2");
+define("ECCUBE_VERSION", "1.3.3");
 
 // 決済モジュール付与文言
 define("ECCUBE_PAYMENT", "EC-CUBE");
