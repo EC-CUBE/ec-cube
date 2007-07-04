2f065d5d98cb325bf6a764285d0dfa648a52b406
adati <adati@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-07-04 03:32:41 +0000

    [1.3.3正式版リリース準備]

--- data/conf/conf.php
+++ data/conf/conf.php
@@ -88,7 +88,7 @@ define("UPDATE_PATH", DATA_PATH . UPDATE_DIR);
 //--------------------------------------------------------------------------------------------------------
 /** アップデート管理用 **/
 // アップデート管理用ファイル格納場所　
-define("UPDATE_HTTP",  "http://www.lockon.co.jp/share/");
+define("UPDATE_HTTP", "http://www.lockon.co.jp/share/");
 
 // アップデート管理用CSV1行辺りの最大文字数
 define("UPDATE_CSV_LINE_MAX", 4096);
