5009fd4acd363bf0ced23e591f1f77a25d9e89c0
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-07-27 11:02:02 +0000

    LC_Page のクラス化対応

--- html/entry/complete.php
+++ html/entry/complete.php
@@ -4,37 +4,16 @@
  *
  * http://www.lockon.co.jp/
  */
+
+// {{{ requires
 require_once("../require.php");
+require_once(CLASS_PATH . "page_extends/entry/LC_Page_Entry_Complete_Ex.php");
 
-class LC_Page {
-	function LC_Page() {
-		$this->tpl_css = URL_DIR.'css/layout/entry/complete.css';	// メインCSSパス
-		
-		if(CUSTOMER_CONFIRM_MAIL == true) {
-			// 仮会員登録完了
-			$this->tpl_mainpage = 'entry/complete.tpl';			// メインテンプレート
-		} else {
-			// 本会員登録完了
-			$this->tpl_mainpage = 'regist/complete.tpl';		// メインテンプレート			
-		}
-		
-		$this->tpl_title .= '会員登録(完了ページ)';			//　ページタイトル
-	}
-}
+// }}}
+// {{{ generate page
 
-$objPage = new LC_Page();
-$objView = new SC_SiteView();
-$objCampaignSess = new SC_CampaignSession();
-
-// レイアウトデザインを取得
-$objPage = sfGetPageLayout($objPage, false, DEF_LAYOUT);
-
-// キャンペーンからの遷移がチェック
-$objPage->is_campaign = $objCampaignSess->getIsCampaign();
-$objPage->campaign_dir = $objCampaignSess->getCampaignDir();
-
-$objView->assignobj($objPage);
-// フレームを選択(キャンペーンページから遷移なら変更)
-$objCampaignSess->pageView($objView);
-//----------------------------------------------------------------------------------------------------------------------
+$objPage = new LC_Page_Entry_Complete_Ex();
+$objPage->init();
+$objPage->process();
+register_shutdown_function(array($objPage, "destroy"));
 ?>
--- html/entry/kiyaku.php
+++ html/entry/kiyaku.php
@@ -4,41 +4,16 @@
  *
  * http://www.lockon.co.jp/
  */
+
+// {{{ requires
 require_once("../require.php");
+require_once(CLASS_PATH . "page_extends/entry/LC_Page_Entry_Kiyaku_Ex.php");
 
-class LC_Page {
-	function LC_Page() {
-		$this->tpl_mainpage = 'entry/kiyaku.tpl';
-		$this->tpl_title="ご利用規約";
-	}
-}
+// }}}
+// {{{ generate page
 
-$objPage = new LC_Page();
-$objView = new SC_SiteView();
-$objCustomer = new SC_Customer();
-$objCampaignSess = new SC_CampaignSession();
-
-// レイアウトデザインを取得
-$objPage = sfGetPageLayout($objPage, false, DEF_LAYOUT);
-
-// 規約内容の取得
-$objQuery = new SC_Query();
-$objQuery->setorder("rank DESC");
-$arrRet = $objQuery->select("kiyaku_title, kiyaku_text", "dtb_kiyaku", "del_flg <> 1");
-
-$max = count($arrRet);
-$objPage->tpl_kiyaku_text = "";
-for ($i = 0; $i < $max; $i++) {
-	$objPage->tpl_kiyaku_text.=$arrRet[$i]['kiyaku_title'] . "\n\n"; 
-	$objPage->tpl_kiyaku_text.=$arrRet[$i]['kiyaku_text'] . "\n\n"; 
-}
-
-// キャンペーンからの遷移がチェック
-$objPage->is_campaign = $objCampaignSess->getIsCampaign();
-$objPage->campaign_dir = $objCampaignSess->getCampaignDir();
-
-$objView->assignobj($objPage);
-// フレームを選択(キャンペーンページから遷移なら変更)
-$objCampaignSess->pageView($objView);
-//--------------------------------------------------------------------------------------------------------------------------
+$objPage = new LC_Page_Entry_Kiyaku_Ex();
+$objPage->init();
+$objPage->process();
+register_shutdown_function(array($objPage, "destroy"));
 ?>
