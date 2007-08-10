162bee0145df9aeb62633c1dc74faef31d16788e
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-08-10 03:13:55 +0000

    $include_dir . HTML2DATA_DIR を CLASS_PATH に変更

--- html/require.php
+++ html/require.php
@@ -13,40 +13,40 @@ if (!defined("CLASS_PATH")) {
 }
 require_once($include_dir . HTML2DATA_DIR. "conf/conf.php");
 require_once($include_dir . HTML2DATA_DIR . "include/module.inc");
-require_once($include_dir . HTML2DATA_DIR . "class/util_extends/GC_Utils_Ex.php");
-require_once($include_dir . HTML2DATA_DIR . "class/util_extends/SC_Utils_Ex.php");
-require_once($include_dir . HTML2DATA_DIR . "class/db_extends/SC_DB_MasterData_Ex.php");
-require_once($include_dir . HTML2DATA_DIR . "class/db_extends/SC_DB_DBFactory_Ex.php");
+require_once(CLASS_PATH . "util_extends/GC_Utils_Ex.php");
+require_once(CLASS_PATH . "util_extends/SC_Utils_Ex.php");
+require_once(CLASS_PATH . "db_extends/SC_DB_MasterData_Ex.php");
+require_once(CLASS_PATH . "db_extends/SC_DB_DBFactory_Ex.php");
 //require_once($include_dir . HTML2DATA_DIR . "lib/glib.php");
 //require_once($include_dir . HTML2DATA_DIR . "lib/slib.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_View.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_DbConn.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_Session.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_Query.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_SelectSql.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_CheckError.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_PageNavi.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_Date.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_Image.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_UploadFile.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_SiteInfo.php");
-require_once($include_dir . HTML2DATA_DIR . "class/GC_SendMail.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_FormParam.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_CartSession.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_SiteSession.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_CampaignSession.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_Customer.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_Cookie.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_Page.php");
-require_once($include_dir . HTML2DATA_DIR . "class/SC_Pdf.php");
-require_once($include_dir . HTML2DATA_DIR . "class/GC_MobileUserAgent.php");
-require_once($include_dir . HTML2DATA_DIR . "class/GC_MobileEmoji.php");
-require_once($include_dir . HTML2DATA_DIR . "class/helper_extends/SC_Helper_PageLayout_Ex.php");
+require_once(CLASS_PATH . "SC_View.php");
+require_once(CLASS_PATH . "SC_DbConn.php");
+require_once(CLASS_PATH . "SC_Session.php");
+require_once(CLASS_PATH . "SC_Query.php");
+require_once(CLASS_PATH . "SC_SelectSql.php");
+require_once(CLASS_PATH . "SC_CheckError.php");
+require_once(CLASS_PATH . "SC_PageNavi.php");
+require_once(CLASS_PATH . "SC_Date.php");
+require_once(CLASS_PATH . "SC_Image.php");
+require_once(CLASS_PATH . "SC_UploadFile.php");
+require_once(CLASS_PATH . "SC_SiteInfo.php");
+require_once(CLASS_PATH . "GC_SendMail.php");
+require_once(CLASS_PATH . "SC_FormParam.php");
+require_once(CLASS_PATH . "SC_CartSession.php");
+require_once(CLASS_PATH . "SC_SiteSession.php");
+require_once(CLASS_PATH . "SC_CampaignSession.php");
+require_once(CLASS_PATH . "SC_Customer.php");
+require_once(CLASS_PATH . "SC_Cookie.php");
+require_once(CLASS_PATH . "SC_Page.php");
+require_once(CLASS_PATH . "SC_Pdf.php");
+require_once(CLASS_PATH . "GC_MobileUserAgent.php");
+require_once(CLASS_PATH . "GC_MobileEmoji.php");
+require_once(CLASS_PATH . "helper_extends/SC_Helper_PageLayout_Ex.php");
 require_once(CLASS_PATH . "helper_extends/SC_Helper_DB_Ex.php");
 //require_once($include_dir . HTML2DATA_DIR . "include/page_layout.inc");
 
 // アップデートで取得したPHPを読み出す
-SC_Utils::sfLoadUpdateModule();
+SC_Utils_Ex::sfLoadUpdateModule();
 
 // 携帯端末の場合は mobile 以下へリダイレクトする。
 if (GC_MobileUserAgent::isMobile()) {
