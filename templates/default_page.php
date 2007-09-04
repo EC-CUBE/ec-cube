f0cbb76da0c5a5f89f3703e8f54ed1ada066a4b5
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-04 01:42:08 +0000

    クラステンプレート追加

---
+++ templates/default_page.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Copyright(c) 2000-2007 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ */
+
+// {{{ requires
+require_once("../require.php");
+require_once(CLASS_PATH . "page_extends/XXX/LC_Page_XXX_Ex.php");
+
+// }}}
+// {{{ generate page
+
+$objPage = new LC_Page_XXX_Ex();
+$objPage->init();
+$objPage->process();
+register_shutdown_function(array($objPage, "destroy"));
+?>
---
+++ templates/page_class.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Copyright(c) 2000-2007 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ */
+
+// {{{ requires
+require_once(CLASS_PATH . "pages/LC_Page.php");
+
+/**
+ * XXX のページクラス.
+ *
+ * @package Page
+ * @author LOCKON CO.,LTD.
+ * @version $Id$
+ */
+class LC_Page_XXX extends LC_Page {
+
+    // }}}
+    // {{{ functions
+
+    /**
+     * Page を初期化する.
+     *
+     * @return void
+     */
+    function init() {
+        parent::init();
+    }
+
+    /**
+     * Page のプロセス.
+     *
+     * @return void
+     */
+    function process() {
+    }
+
+    /**
+     * デストラクタ.
+     *
+     * @return void
+     */
+    function destroy() {
+        parent::destroy();
+    }
+}
+/*
+ * Local variables:
+ * coding: utf-8
+ * End:
+ */
+?>
