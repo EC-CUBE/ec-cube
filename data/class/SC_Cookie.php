e595c1deb822d0b259da30839bc8b753f52e313d
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-05 05:15:21 +0000

    tab to space

--- data/class/SC_Cookie.php
+++ data/class/SC_Cookie.php
@@ -5,25 +5,25 @@
  * http://www.lockon.co.jp/
  */
 
-/*　日時表示用クラス */
+/* 日時表示用クラス */
 class SC_Cookie {
-	
-	var $expire;
-	
-	// コンストラクタ
-	function SC_Cookie($day = 365) {
-		// 有効期限
-		$this->expire = time() + ($day * 24 * 3600);
-	}
-	
-	// クッキー書き込み
-	function setCookie($key, $val) {
-		setcookie($key, $val, $this->expire, "/", DOMAIN_NAME);
-	}
-	
-	// クッキー取得
-	function getCookie($key) {
-		return $_COOKIE[$key];
-	}
+
+    var $expire;
+
+    // コンストラクタ
+    function SC_Cookie($day = 365) {
+        // 有効期限
+        $this->expire = time() + ($day * 24 * 3600);
+    }
+
+    // クッキー書き込み
+    function setCookie($key, $val) {
+        setcookie($key, $val, $this->expire, "/", DOMAIN_NAME);
+    }
+
+    // クッキー取得
+    function getCookie($key) {
+        return $_COOKIE[$key];
+    }
 }
 ?>
