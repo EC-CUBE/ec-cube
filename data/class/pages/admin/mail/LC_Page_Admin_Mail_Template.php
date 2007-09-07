04b7bef1ad10f39532434f5998f544a9cd0cbb40
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-07 07:25:31 +0000

    クラス化に伴う修正

---
+++ data/class/pages/admin/mail/LC_Page_Admin_Mail_Template.php
@@ -0,0 +1,88 @@
+<?php
+/*
+ * Copyright(c) 2000-2007 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ */
+
+// {{{ requires
+require_once(CLASS_PATH . "pages/LC_Page.php");
+
+/**
+ * メールテンプレート設定 のページクラス.
+ *
+ * @package Page
+ * @author LOCKON CO.,LTD.
+ * @version $Id$
+ */
+class LC_Page_Admin_Mail_Template extends LC_Page {
+
+    // }}}
+    // {{{ functions
+
+    /**
+     * Page を初期化する.
+     *
+     * @return void
+     */
+    function init() {
+        parent::init();
+        $this->tpl_mainpage = 'mail/template.tpl';
+		$this->tpl_mainno = 'mail';
+		$this->tpl_subnavi = 'mail/subnavi.tpl';
+		$this->tpl_subno = "template";
+		$this->tpl_subtitle = 'テンプレート設定';
+
+        $masterData = new SC_DB_MasterData_Ex();
+        $this->arrMagazineTypeAll = $masterData->getMasterData("mtb_magazine_type");
+    }
+
+    /**
+     * Page のプロセス.
+     *
+     * @return void
+     */
+    function process() {
+        $conn = new SC_DBConn();
+        $objView = new SC_AdminView();
+        $objSess = new SC_Session();
+
+        // 認証可否の判定
+        SC_Utils_Ex::sfIsSuccess($objSess);
+
+        if (!isset($_GET['mode'])) $_GET['mode'] = "";
+        if (!isset($_GET['id'])) $_GET['id'] = "";
+
+        if ( $_GET['mode'] == "delete" && SC_Utils_Ex::sfCheckNumLength($_GET['id'])===true ){
+
+            // メール担当の画像があれば削除しておく
+            $sql = "SELECT charge_image FROM dtb_mailmaga_template WHERE template_id = ?";
+            $result = $conn->getOne($sql, array($_GET["id"]));
+            if (strlen($result) > 0) {
+                @unlink(IMAGE_SAVE_DIR. $result);
+            }
+
+            // 登録削除
+            $sql = "UPDATE dtb_mailmaga_template SET del_flg = 1 WHERE template_id = ?";
+            $conn->query($sql, array($_GET['id']));
+            $this->reload();
+        }
+
+
+        $sql = "SELECT *, (substring(create_date, 1, 19)) as disp_date FROM dtb_mailmaga_template WHERE del_flg = 0 ORDER BY create_date DESC";
+        $this->list_data = $conn->getAll($sql);
+
+        $objView->assignobj($this);
+        $objView->display(MAIN_FRAME);
+    }
+
+    /**
+     * デストラクタ.
+     *
+     * @return void
+     */
+    function destroy() {
+        parent::destroy();
+    }
+}
+?>
--- html/admin/mail/template.php
+++ html/admin/mail/template.php
@@ -4,51 +4,16 @@
  *
  * http://www.lockon.co.jp/
  */
-require_once("../require.php");
 
-class LC_Page {
-	var $arrSession;
-	var $list_data;
-	var $arrMagazineType;
-	
-	function LC_Page() {
-		$this->tpl_mainpage = 'mail/template.tpl';
-		$this->tpl_mainno = 'mail';
-		$this->tpl_subnavi = 'mail/subnavi.tpl';
-		$this->tpl_subno = "template";
-		$this->tpl_subtitle = 'テンプレート設定';
-	}
-}
+// {{{ requires
+require_once("../../require.php");
+require_once(CLASS_PATH . "page_extends/admin/mail/LC_Page_Admin_Mail_Template_Ex.php");
 
-$conn = new SC_DBConn();
-$objPage = new LC_Page();
-$objView = new SC_AdminView();
-$objSess = new SC_Session();
+// }}}
+// {{{ generate page
 
-// 認証可否の判定
-sfIsSuccess($objSess);
-
-if ( $_GET['mode'] == "delete" && sfCheckNumLength($_GET['id'])===true ){
-
-	// メール担当の画像があれば削除しておく
-	$sql = "SELECT charge_image FROM dtb_mailmaga_template WHERE template_id = ?";
-	$result = $conn->getOne($sql, array($_GET["id"]));
-	if (strlen($result) > 0) {
-		@unlink(IMAGE_SAVE_DIR. $result);
-	}
-	
-	// 登録削除
-	$sql = "UPDATE dtb_mailmaga_template SET del_flg = 1 WHERE template_id = ?";
-	$conn->query($sql, array($_GET['id']));
-	sfReload();
-}
-
-
-$sql = "SELECT *, (substring(create_date, 1, 19)) as disp_date FROM dtb_mailmaga_template WHERE del_flg = 0 ORDER BY create_date DESC";
-$objPage->list_data = $list_data = $conn->getAll($sql);
-$objPage->arrMagazineType = $arrMagazineTypeAll;
-
-
-$objView->assignobj($objPage);
-$objView->display(MAIN_FRAME);
+$objPage = new LC_Page_Admin_Mail_Template_Ex();
+$objPage->init();
+$objPage->process();
+register_shutdown_function(array($objPage, "destroy"));
 ?>
