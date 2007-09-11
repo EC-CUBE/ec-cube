fcd2f22ab350d0d2bea47a7acb4c58d585d95555
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-11 01:38:47 +0000

    バッチクラス用の基底クラス追加

---
+++ data/class/batch/SC_Batch.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Copyright(c) 2000-2007 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ */
+
+/**
+ * バッチ処理用 の基底クラス.
+ *
+ * @package Page
+ * @author LOCKON CO.,LTD.
+ * @version $Id$
+ */
+class SC_Batch {
+
+    // }}}
+    // {{{ functions
+
+    /**
+     * バッチ処理を実行する
+     *
+     * @param mixed $argv コマンドライン引数
+     * @return mixed バッチの実行結果
+     */
+    function execute($argv = "") {}
+}
+?>
