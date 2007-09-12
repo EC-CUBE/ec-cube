fc05b3812955648e36e33b829e5d2b6e4c34579d
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-12 03:08:18 +0000

    クラス化に伴う修正

--- html/__default.php
+++ html/__default.php
@@ -1,21 +1,66 @@
 <?php
+// {{{ requires
 require_once("###require###");
+require_once(CLASS_PATH . "pages/LC_Page.php");
 
-class LC_Page {
-	function LC_Page() {
-		/** 必ず変更する **/
-		$this->tpl_css = URL_DIR.'css/layout/contact/index.css';	// メインCSSパス		
-	}
+/**
+ * ユーザーカスタマイズ用のページクラス
+ *
+ * 管理画面から自動生成される
+ *
+ * @package Page
+ */
+class LC_Page_User extends LC_Page {
+
+    // }}}
+    // {{{ functions
+
+    /**
+     * Page を初期化する.
+     *
+     * @return void
+     */
+    function init() {
+        parent::init();
+        // メインCSSパス
+        $this->tpl_css = URL_DIR.'css/layout/contact/index.css';
+    }
+
+    /**
+     * Page のプロセス.
+     *
+     * @return void
+     */
+    function process() {
+        $objView = new SC_SiteView();
+        $objLayout = new SC_Helper_PageLayout_Ex();
+
+        // レイアウトデザインを取得
+        $objLayout->sfGetPageLayout($this);
+
+        // 画面の表示
+        $objView->assignobj($this);
+        $objView->display(SITE_FRAME);
+    }
+
+    /**
+     * デストラクタ.
+     *
+     * @return void
+     */
+    function destroy() {
+        parent::destroy();
+    }
 }
 
-$objPage = new LC_Page();
-$objView = new SC_SiteView();
 
-// レイアウトデザインを取得
-$objPage = sfGetPageLayout($objPage);
+// }}}
+// {{{ generate page
 
-// 画面の表示
- $objView->assignobj($objPage);
- $objView->display(SITE_FRAME);
-//-----------------------------------------------------------------------------------------------------------------------------------
+$objPage = new LC_Page_User();
+$objPage->init();
+$objPage->process();
+register_shutdown_function(array($objPage, "destroy"));
+
+
 ?>
