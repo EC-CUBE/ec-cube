e34c983601f793443c98b4474a73617e26114fc2
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-14 06:54:43 +0000

    * php_ini.inc を conf.php に移植  * conf.php にて, mtb_constants.php の再生成ができるように対応

--- data/conf/conf.php
+++ data/conf/conf.php
@@ -11,32 +11,70 @@ $CONF_PHP_PATH = realpath( dirname( __FILE__) );
 require_once($CONF_PHP_PATH ."/../install.php");
 require_once($CONF_PHP_PATH ."/core.php" );
 
-//--------------------------------------------------------------------------------------------------------
-/** エラーレベル設定
-/*
- *	'E_ERROR'             => 大な実行時エラー。これは、メモリ確保に関する問題のように復帰で きないエラーを示します。スクリプトの実行は中断されます。
- *	'E_WARNING'           => 実行時の警告 (致命的なエラーではない)。スクリプトの実行は中断さ れません
- *	'E_PARSE'             => コンパイル時のパースエラー。パースエラーはパーサでのみ生成されま す。
- *	'E_NOTICE'            => 実行時の警告。エラーを発しうる状況に遭遇したことを示す。 ただし通常のスクリプト実行の場合にもこの警告を発することがありうる。
- *	'E_CORE_ERROR'        => PHPの初期始動時点での致命的なエラー。E_ERRORに 似ているがPHPのコアによって発行される点が違う。
- *	'E_CORE_WARNING'      => （致命的ではない）警告。PHPの初期始動時に発生する。 E_WARNINGに似ているがPHPのコアによって発行される 点が違う。
- *	'E_COMPILE_ERROR'     => コンパイル時の致命的なエラー。E_ERRORに 似ているがZendスクリプティングエンジンによって発行される点が違う。
- *	'E_COMPILE_WARNING'   => コンパイル時の警告（致命的ではない）。E_WARNINGに 似ているがZendスクリプティングエンジンによって発行される点が違う。
- *	'E_USER_ERROR'        => ユーザーによって発行されるエラーメッセージ。E_ERROR に似ているがPHPコード上でtrigger_error()関数を 使用した場合に発行される点が違う。
- *	'E_USER_WARNING'      => ユーザーによって発行される警告メッセージ。E_WARNING に似ているがPHPコード上でtrigger_error()関数を 使用した場合に発行される点が違う。
- *	'E_USER_NOTICE'       => ユーザーによって発行される注意メッセージ。E_NOTICEに に似ているがPHPコード上でtrigger_error()関数を 使用した場合に発行される点が違う。
- *	'E_ALL'               => サポートされる全てのエラーと警告。PHP < 6 では E_STRICT レベルのエラーは除く。
- *	'E_STRICT'            => ※PHP5からサポート 実行時の注意。コードの相互運用性や互換性を維持するために PHP がコードの変更を提案する。
- *	'E_RECOVERABLE_ERROR' => ※PHP5からサポート キャッチできる致命的なエラー。危険なエラーが発生したが、 エンジンが不安定な状態になるほどではないことを表す。 ユーザ定義のハンドラでエラーがキャッチされなかった場合 (set_error_handler() も参照ください) は、 E_ERROR として異常終了する。
+/**
+ * エラーレベル設定
+ *
+ * ・推奨値
+ *   開発時 - E_ALL
+ *   運用時 - E_ALL & ~E_NOTICE
  */
 //error_reporting(E_ALL & ~E_NOTICE);
 error_reporting(E_ALL);
 
-if (is_file($CONF_PHP_PATH . "/cache/mtb_constants.php")) {
-    require_once($CONF_PHP_PATH . "/cache/mtb_constants.php");
-} else {
-    // TODO インストーラで設定する
+// 定数を設定する
+defineConstants();
+
+/**
+ * マルチバイト文字列設定
+ *
+ * TODO SJIS-win や, eucJP-win への対応
+ */
+ini_set("mbstring.http_input", CHAR_CODE);
+ini_set("mbstring.http_output", CHAR_CODE);
+ini_set("auto_detect_line_endings", 1);
+ini_set("default_charset", CHAR_CODE);
+ini_set("mbstring.internal_encoding", CHAR_CODE);
+ini_set("mbstring.detect_order", "auto");
+ini_set("mbstring.substitute_character", "none");
+
+/**
+ * 定数を設定する.
+ *
+ * 注意: この関数を外部で使用することを推奨しません.
+ *
+ * mtb_constants.php を読み込んで定数を設定する.
+ * キャッシュディレクトリに存在しない場合は, インストーラからコピーする.
+ *
+ * @access private
+ * @return void
+ */
+function defineConstants() {
+    $CONF_PHP_PATH = realpath( dirname( __FILE__) );
+
+    $errorMessage = "data/conf/cache/mtb_constants.php が生成できません";
+    // 定数を設定
+    if (is_file($CONF_PHP_PATH . "/cache/mtb_constants.php")) {
+        require_once($CONF_PHP_PATH . "/cache/mtb_constants.php");
+
+    // キャッシュが無ければ, インストーラからコピー
+    } elseif (is_file($CONF_PHP_PATH
+                      . "/../" . DATA_DIR2HTML . "install/mtb_constants.php")) {
+
+        $mtb_constants = file_get_contents($CONF_PHP_PATH
+                                           . "/../" . DATA_DIR2HTML
+                                           . "install/mtb_constants.php");
+        $handle = fopen($CONF_PHP_PATH . "/cache/mtb_constants.php", "w");
+        if (!$handle) {
+            die($errorMessage);
+        }
+        if (fwrite($handle, $mtb_constants) === false) {
+            die($errorMessage);
+        }
+        fclose($handle);
+
+        require_once($CONF_PHP_PATH . "/cache/mtb_constants.php");
+    } else {
+        die("html/install/mtb_constants.php が存在しません.");
+    }
 }
-
-
 ?>
