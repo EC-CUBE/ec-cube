c88424bef411e7c96dafb4bcd38dc67d883759a7
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-19 07:22:02 +0000

    DATA_DIR2HTML 定数追加

--- html/define.php
+++ html/define.php
@@ -1,4 +1,16 @@
 <?php
-    /** HTMLディレクトリからのDATAディレクトリの相対パス */
-    define("HTML2DATA_DIR", "/../data/");
+/** HTMLディレクトリからのDATAディレクトリの相対パス */
+define("HTML2DATA_DIR", "/../data/");
+
+/** DATA ディレクトリから HTML ディレクトリの相対パス */
+define("DATA_DIR2HTML", "/../html/");
+
+/*
+ * Local variables:
+ * coding: utf-8
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
 ?>
