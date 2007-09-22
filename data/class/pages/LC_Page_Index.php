cb7c71578a9e7877a4880c339c4fa3e59889ea22
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-22 11:05:49 +0000

    $tpl_column_num 追加

--- data/class/pages/LC_Page_Index.php
+++ data/class/pages/LC_Page_Index.php
@@ -28,6 +28,7 @@ class LC_Page_Index extends LC_Page {
     function init() {
         parent::init();
         $this->tpl_mainpage =  TEMPLATE_DIR . "top.tpl";
+        $this->tpl_column_num = 3;
     }
 
     /**
