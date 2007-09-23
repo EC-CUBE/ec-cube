ed84d7c07cd06e8a634cda445b12f01201a8b0cc
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-23 12:52:22 +0000

    新規追加

---
+++ data/class/page_extends/mobile/LC_Page_Mobile_Redirect_Ex.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * Copyright(c) 2000-2007 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ */
+
+// {{{ requires
+require_once(CLASS_PATH . "pages/mobile/LC_Page_Mobile_Redirect.php");
+
+/**
+ * モバイルページ/リダイレクト のページクラス(拡張).
+ *
+ * LC_Page_Mobile_Redirect をカスタマイズする場合はこのクラスを編集する.
+ *
+ * @package Page
+ * @author LOCKON CO.,LTD.
+ * @version $Id$
+ */
+class LC_Page_Mobile_Redirect_Ex extends LC_Page_Mobile_Redirect {
+
+    // }}}
+    // {{{ functions
+
+    /**
+     * Page を初期化する.
+     *
+     * @return void
+     */
+    function init() {
+        parent::init();
+    }
+
+    /**
+     * Page のプロセス.
+     *
+     * @return void
+     */
+    function process() {
+        parent::process();
+    }
+
+    /**
+     * デストラクタ.
+     *
+     * @return void
+     */
+    function destroy() {
+        parent::destroy();
+    }
+}
+?>
