5751d76744ec01cd62d49c86b3e7a00c8dc4e528
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-26 12:25:51 +0000

    $this->tpl_column_num = 1 追加

--- data/class/pages/cart/LC_Page_Cart.php
+++ data/class/pages/cart/LC_Page_Cart.php
@@ -40,6 +40,7 @@ class LC_Page_Cart extends LC_Page {
         parent::init();
         $this->tpl_css = URL_DIR.'css/layout/cartin/index.css';
         $this->tpl_mainpage = 'cart/index.tpl';
+        $this->tpl_column_num = 1;
         $this->tpl_title = "カゴの中を見る";
     }
 
