5f045a5d4c3dbdce4289f2b5dca488be21a905cc
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-27 10:50:21 +0000

    $this->tpl_column_num = 1 追加

--- data/class/pages/mypage/LC_Page_Mypage_Delivery.php
+++ data/class/pages/mypage/LC_Page_Mypage_Delivery.php
@@ -35,6 +35,7 @@ class LC_Page_Mypage_Delivery extends LC_Page {
         $masterData = new SC_DB_MasterData_Ex();
         $this->arrPref= $masterData->getMasterData("mtb_pref",
                             array("pref_id", "pref_name", "rank"));
+        $this->tpl_column_num = 1;
         $this->allowClientCache();
     }
 
