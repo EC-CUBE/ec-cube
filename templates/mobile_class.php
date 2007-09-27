f7ba45e7a5f9011641e09404a18cf266bea22336
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-27 17:31:35 +0000

    新規追加

---
+++ templates/mobile_class.php
@@ -0,0 +1,17 @@
+<?php
+    /**
+     * モバイルページを初期化する.
+     *
+     * @return void
+     */
+    function mobileInit() {
+    }
+
+    /**
+     * Page のプロセス(モバイル).
+     *
+     * @return void
+     */
+    function mobileProcess() {
+    }
+?>
