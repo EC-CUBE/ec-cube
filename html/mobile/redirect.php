d2c17c3dd5a059e303070ade6516a6f0f44bd6a2
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-27 17:23:42 +0000

    コメント追加

--- html/mobile/redirect.php
+++ html/mobile/redirect.php
@@ -1,19 +1,20 @@
 <?php
 /**
- * 
+ *
  * Copyright(c) 2000-2007 LOCKON CO.,LTD. All Rights Reserved.
  *
  * http://www.lockon.co.jp/
- * 
+ *
  *
  * モバイルサイト/空メールの返信からの遷移
  */
 
+// FIXME
 define('SKIP_MOBILE_INIT', true);
 require_once 'require.php';
 
 if (isset($_GET['token'])) {
-	$next_url = gfFinishKaraMail($_GET['token']);
+	$next_url = GC_Utils_Ex::gfFinishKaraMail($_GET['token']);
 }
 
 if (isset($next_url) && $next_url !== false) {
