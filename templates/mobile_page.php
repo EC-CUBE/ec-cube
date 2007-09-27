6e7919ec8bf7d55e4adbd10b86d6deda185398c2
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-27 18:36:30 +0000

    CLASS_PATH 修正

--- templates/mobile_page.php
+++ templates/mobile_page.php
@@ -11,7 +11,7 @@
 
 // {{{ requires
 require_once("../require.php");
-require_once(CLASS_PATH . "page_extends/LC_Page_XXX_Ex.php");
+require_once(CLASS_PATH . "page_extends/XXX/LC_Page_XXX_Ex.php");
 
 // }}}
 // {{{ generate page
