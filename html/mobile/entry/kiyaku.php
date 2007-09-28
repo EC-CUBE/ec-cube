c6cf4d323d6c88b4def3667a51f1593b84760723
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-09-28 04:21:16 +0000

    モバイル対応

--- html/mobile/entry/kiyaku.php
+++ html/mobile/entry/kiyaku.php
@@ -1,50 +1,23 @@
 <?php
-/*
- * Copyright(c) 2000-2006 LOCKON CO.,LTD. All Rights Reserved.
+/**
+ *
+ * Copyright(c) 2000-2007 LOCKON CO.,LTD. All Rights Reserved.
  *
  * http://www.lockon.co.jp/
+ *
+ *
+ * モバイルサイト/会員規約
  */
+
+// {{{ requires
 require_once("../require.php");
+require_once(CLASS_PATH . "page_extends/entry/LC_Page_Entry_Kiyaku_Ex.php");
 
-class LC_Page {
-	function LC_Page() {
-		$this->tpl_mainpage = 'entry/kiyaku.tpl';
-		$this->tpl_title="ご利用規約";
-	}
-}
+// }}}
+// {{{ generate page
 
-$objPage = new LC_Page();
-$objView = new SC_MobileView();
-$objCustomer = new SC_Customer();
-
-$offset = isset($_REQUEST['offset']) ? $_REQUEST['offset'] : 0;
-$next = $offset;
-
-// レイアウトデザインを取得
-$objPage = sfGetPageLayout($objPage, false, DEF_LAYOUT);
-
-// 規約内容の取得
-$objQuery = new SC_Query();
-$count = $objQuery->count("dtb_kiyaku", "del_flg <> 1");
-$objQuery->setorder("rank DESC");
-$objQuery->setlimitoffset(1, $offset);
-$arrRet = $objQuery->select("kiyaku_title, kiyaku_text", "dtb_kiyaku", "del_flg <> 1");
-
-if($count > $offset + 1){
-	$next++;
-} else {
-	$next = -1;
-}
-
-$max = count($arrRet);
-$objPage->tpl_kiyaku_text = "";
-for ($i = 0; $i < $max; $i++) {
-	$objPage->tpl_kiyaku_text.=$arrRet[$i]['kiyaku_title'] . "\n\n"; 
-	$objPage->tpl_kiyaku_text.=$arrRet[$i]['kiyaku_text'] . "\n\n"; 
-}
-
-$objView->assign("offset", $next);
-$objView->assignobj($objPage);
-$objView->display(SITE_FRAME);
-//--------------------------------------------------------------------------------------------------------------------------
+$objPage = new LC_Page_Entry_Kiyaku_Ex();
+$objPage->mobileInit();
+$objPage->mobileProcess();
+register_shutdown_function(array($objPage, "destroy"));
 ?>
