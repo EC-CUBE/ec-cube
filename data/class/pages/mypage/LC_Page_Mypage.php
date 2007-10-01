8ed0b494d2cbcafd38a8c23672929c48f42da231
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-10-01 03:43:02 +0000

    クラス化対応

--- data/class/pages/mypage/LC_Page_Mypage.php
+++ data/class/pages/mypage/LC_Page_Mypage.php
@@ -102,6 +102,98 @@ class LC_Page_MyPage extends LC_Page {
     }
 
     /**
+     * モバイルページを初期化する.
+     *
+     * @return void
+     */
+    function mobileInit() {
+        $this->tpl_mainpage = 'mypage/index.tpl';
+        $this->tpl_title = 'MYページ/購入履歴一覧';
+        $this->allowClientCache();
+    }
+
+    /**
+     * Page のプロセス(モバイル).
+     *
+     * @return void
+     */
+    function mobileProcess() {
+        $objView = new SC_MobileView();
+        $objQuery = new SC_Query();
+        $objCustomer = new SC_Customer();
+        // クッキー管理クラス
+        $objCookie = new SC_Cookie(COOKIE_EXPIRE);
+        // パラメータ管理クラス
+        $objFormParam = new SC_FormParam();
+        // パラメータ情報の初期化
+        $this->lfInitParam($objFormParam);
+        // POST値の取得
+        $objFormParam->setParam($_POST);
+
+        // レイアウトデザインを取得
+        //$objPage = sfGetPageLayout($objPage, false, "mypage/index.php");
+
+        // 携帯端末IDが一致する会員が存在するかどうかをチェックする。
+        $this->tpl_valid_phone_id = $objCustomer->checkMobilePhoneId();
+
+        // ログイン処理
+        if($_POST['mode'] == 'login') {
+            $objFormParam->toLower('login_email');
+            $arrErr = $objFormParam->checkError();
+            $arrForm =  $objFormParam->getHashArray();
+
+            // クッキー保存判定
+            if ($arrForm['login_memory'] == "1" && $arrForm['login_email'] != "") {
+                $objCookie->setCookie('login_email', $_POST['login_email']);
+            } else {
+                $objCookie->setCookie('login_email', '');
+            }
+
+            if (count($arrErr) == 0){
+                if($objCustomer->getCustomerDataFromMobilePhoneIdPass($arrForm['login_pass']) ||
+                   $objCustomer->getCustomerDataFromEmailPass($arrForm['login_pass'], $arrForm['login_email'], true)) {
+                    // ログインが成功した場合は携帯端末IDを保存する。
+                    $objCustomer->updateMobilePhoneId();
+
+                    // 携帯のメールアドレスをコピーする。
+                    $objCustomer->updateEmailMobile();
+
+                    // 携帯のメールアドレスが登録されていない場合
+                    if (!$objCustomer->hasValue('email_mobile')) {
+                        header('Location: ' . SC_Helper_DB_Ex::gfAddSessionId('../entry/email_mobile.php'));
+                        exit;
+                    }
+                } else {
+                    $objQuery = new SC_Query;
+                    $where = "email = ? AND status = 1 AND del_flg = 0";
+                    $ret = $objQuery->count("dtb_customer", $where, array($arrForm['login_email']));
+
+                    if($ret > 0) {
+                        SC_Utils_Ex::sfDispSiteError(TEMP_LOGIN_ERROR, "", false, "", true);
+                    } else {
+                        SC_Utils_Ex::sfDispSiteError(SITE_LOGIN_ERROR, "", false, "", true);
+                    }
+                }
+            }
+        }
+
+        // ログインチェック
+        if(!$objCustomer->isLoginSuccess()) {
+            $this->tpl_mainpage = 'mypage/login.tpl';
+            $objView->assignArray($objFormParam->getHashArray());
+            $objView->assignArray(array("arrErr" => $arrErr));
+        }else {
+            //マイページトップ顧客情報表示用
+            $this->CustomerName1 = $objCustomer->getvalue('name01');
+            $this->CustomerName2 = $objCustomer->getvalue('name02');
+        }
+
+        $objView->assignobj($this);				//$objpage内の全てのテンプレート変数をsmartyに格納
+        $objView->display(SITE_FRAME);				//パスとテンプレート変数の呼び出し、実行
+
+    }
+
+    /**
      * デストラクタ.
      *
      * @return void
@@ -118,5 +210,14 @@ class LC_Page_MyPage extends LC_Page {
         $objErr->dofunc(array("パスワード", "login_password", PASSWORD_LEN2), array("EXIST_CHECK","ALNUM_CHECK"));
         return $objErr->arrErr;
     }
+
+    /* パラメータ情報の初期化 */
+    function lfInitParam(&$objFormParam) {
+
+        $objFormParam->addParam("記憶する", "login_memory", INT_LEN, "n", array("MAX_LENGTH_CHECK", "NUM_CHECK"));
+        $objFormParam->addParam("メールアドレス", "login_email", STEXT_LEN, "KVa", array("EXIST_CHECK", "MAX_LENGTH_CHECK"));
+        $objFormParam->addParam("パスワード", "login_pass", STEXT_LEN, "KVa", array("EXIST_CHECK", "MAX_LENGTH_CHECK"));
+    }
+
 }
 ?>
