13c1d738a5bfb26be62ef7f65938fa8e084ab5b7
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-10-05 10:29:04 +0000

    不要な変数削除

--- data/class/pages/LC_Page_Index.php
+++ data/class/pages/LC_Page_Index.php
@@ -27,7 +27,6 @@ class LC_Page_Index extends LC_Page {
      */
     function init() {
         parent::init();
-        $this->tpl_mainpage =  TEMPLATE_DIR . "top.tpl";
         $this->tpl_column_num = 3;
     }
 
