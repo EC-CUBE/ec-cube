22cb357677eba7c00880c6eec9c3320aff6867e0
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-10-06 13:07:11 +0000

    更新取り消し

--- data/install.php
+++ data/install.php
@@ -1,19 +1,2 @@
 <?php
-    define ('ECCUBE_INSTALL', 'ON');
-    define ('HTML_PATH', 'C:\mydir\pdt\workspace\feature-module-update/html/');
-    define ('SITE_URL', 'http://update.ec-cube.net/');
-    define ('SSL_URL', 'http://update.ec-cube.net/');
-    define ('URL_DIR', '/');
-    define ('DOMAIN_NAME', '');
-    define ('DB_TYPE', 'pgsql');
-    define ('DB_USER', 'nanasess');
-    define ('DB_PASSWORD', 'nanasess');
-    define ('DB_SERVER', '192.168.0.70');
-    define ('DB_NAME', 'naka_db');
-    define ('DB_PORT', '');
-    define ('DATA_PATH', 'C:\mydir\pdt\workspace\feature-module-update/data/');
-    define ('MOBILE_HTML_PATH', HTML_PATH . 'mobile/');
-    define ('MOBILE_SITE_URL', SITE_URL . 'mobile/');
-    define ('MOBILE_SSL_URL', SSL_URL . 'mobile/');
-    define ('MOBILE_URL_DIR', URL_DIR . 'mobile/');
 ?>
