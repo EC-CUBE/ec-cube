9d102c25a5db678cda023cf831ecdaf3f404bc9e
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-10-16 01:45:19 +0000

    未定義変数の修正

--- data/class/SC_View.php
+++ data/class/SC_View.php
@@ -62,7 +62,9 @@ class SC_View {
                 // 都道府県名を変換
                 $masterData = new SC_DB_MasterData_Ex();
                 $arrPref = $masterData->getMasterData("mtb_pref", array("pref_id", "pref_name", "rank"));
-                $arrInfo['arrSiteInfo']['pref'] = $arrPref[$arrInfo['arrSiteInfo']['pref']];
+                $arrInfo['arrSiteInfo']['pref'] =
+                    isset($arrPref[$arrInfo['arrSiteInfo']['pref']])
+                    ? $arrPref[$arrInfo['arrSiteInfo']['pref']] : "";
 
                  // サイト情報を割り当てる
                 foreach ($arrInfo as $key => $value){
