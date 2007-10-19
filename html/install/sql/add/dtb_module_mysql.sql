59114e115a4c2372ddd8bef61407dc5c8c202d3a
adachi <adachi@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-10-19 03:08:19 +0000

    dtb_module変更

--- html/install/sql/add/dtb_module_mysql.sql
+++ html/install/sql/add/dtb_module_mysql.sql
@@ -1,20 +1,8 @@
 CREATE TABLE dtb_module (
     module_id int NOT NULL UNIQUE,
     module_name text NOT NULL,
-    now_version text,
-    latest_version text NOT NULL,
-    module_explain text,
-    main_php text NOT NULL,
-    extern_php text NOT NULL,
-    install_sql text,
-    uninstall_sql text,
-    other_files text,
+    auto_update_flg smallint NOT NULL DEFAULT 0,
     del_flg smallint NOT NULL DEFAULT 0,
     create_date datetime NOT NULL ,
-    update_date datetime,
-    release_date datetime NOT NULL,
-    sub_data text,
-    module_x int,
-    module_y int,
-    eccube_version text
+    update_date datetime NOT NULL
 ) TYPE=InnoDB;
