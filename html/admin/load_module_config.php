c303f109ba33ae3b505b904ad8f48e4a30e66bee
adachi <adachi@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-10-31 03:42:38 +0000

    テーブル名修正

--- html/admin/load_module_config.php
+++ html/admin/load_module_config.php
@@ -38,7 +38,7 @@ if(!empty($module_id) && is_numeric($module_id)) {
     GC_Utils::gfPrintLog("loading module ====> module_id = " . $module_id);
 
     $objQuery = new SC_Query();
-    $arrRet = $objQuery->select("module_name", "dtb_module2", "module_id = ?", array($module_id));
+    $arrRet = $objQuery->select("module_name", "dtb_module", "module_id = ?", array($module_id));
 
     if (isset($arrRet[0]['module_name'])) {
         $config_path = MODULE_PATH . $arrRet[0]['module_name'] . '/config.php';
