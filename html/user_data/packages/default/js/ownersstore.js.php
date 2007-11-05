72a7e3d1adefa15a2829d2e66c8e27960e4cb734
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-11-05 12:31:27 +0000

    ユーザパッケージの移動

---
+++ html/user_data/packages/default/js/ownersstore.js.php
@@ -0,0 +1,169 @@
+<?php
+require_once '../../../../require.php';
+ob_start();
+?>
+/*
+ * Thickbox 3.1 - One Box To Rule Them All.
+ * By Cody Lindley (http://www.codylindley.com)
+ * Copyright (c) 2007 cody lindley
+ * Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
+*/
+
+/*
+ * ownersstore.js
+ *
+ * オーナーズストア通信用ライブラリ.
+ * CSSやjavascriptのオーバーレイ処理はThickboxのものを使っています.
+ *
+*/
+
+(function() {
+// オーナーズストア通信スクリプトのパス
+var upgrade_url = '[%URL_DIR%]upgrade/index.php';
+
+// ロード中メッセージ(配信サーバと通信中です…)
+var loading_message = '\u30B5\u30FC\u30D0\u3068\u901A\u4FE1\u4E2D\u3067\u3059';
+
+// ロード中画像の先読み
+var loading_img = new Image();
+loading_img.src = '[%URL_DIR%]user_data/templates/default/img/ajax/loading.gif';
+
+var OwnersStore = function() {}
+OwnersStore.prototype = {
+    // detect Mac and Firefox use.
+    detectMacFF: function() {
+        var ua = navigator.userAgent.toLowerCase();
+        if (ua.indexOf('mac') != -1 && ua.indexOf('firefox') != -1) {
+            return true;
+        }
+    },
+    // remove ajax window
+    remove: function() {
+        $("#TB_window").fadeOut(
+            "fast",
+            function(){
+                $('#TB_window,#TB_overlay,#TB_HideSelect').trigger("unload").unbind().remove();
+            }
+        );
+        $("#TB_load").remove();
+        //if IE 6
+        if (typeof document.body.style.maxHeight == "undefined") {
+            $("body", "html").css({height: "auto", width: "auto"});
+            $("html").css("overflow", "");
+        }
+        return false;
+    },
+    // show loading page
+    show_loading: function() {
+        //if IE 6
+        if (typeof document.body.style.maxHeight === "undefined") {
+            $("body","html").css({height: "100%", width: "100%"});
+            $("html").css("overflow","hidden");
+            //iframe to hide select elements in ie6
+            if (document.getElementById("TB_HideSelect") === null) {
+                $("body").append("<iframe id='TB_HideSelect'></iframe><div id='TB_overlay'></div><div id='TB_window'></div>");
+                $("#TB_overlay").click(this.remove);
+            }
+        //all others
+        } else {
+            if(document.getElementById("TB_overlay") === null){
+                $("body").append("<div id='TB_overlay'></div><div id='TB_window'></div>");
+                $("#TB_overlay").click(this.remove);
+            }
+        }
+
+        if(this.detectMacFF()){
+            //use png overlay so hide flash
+            $("#TB_overlay").addClass("TB_overlayMacFFBGHack");
+        } else {
+            //use background and opacity
+            $("#TB_overlay").addClass("TB_overlayBG");
+        }
+
+        //add and show loader to the page
+        $("body").append(
+              "<div id='TB_load'>"
+            + "  <p style='color:#ffffff'>" + loading_message + "</p>"
+            + "  <img src='" + loading_img.src + "' />"
+            + "</div>"
+        );
+        $('#TB_load').show();
+    },
+    // show results
+    show_result: function(resp, status) {
+        var title    = resp.status;
+        var contents = resp.body;
+        var errcode  = resp.errcode || '';
+
+        var TB_WIDTH = 400;
+        var TB_HEIGHT = 300;
+        var ajaxContentW = TB_WIDTH - 20;
+        var ajaxContentH = TB_HEIGHT - 45;
+
+        if ($("#TB_window").css("display") != "block"){
+            $("#TB_window").append(
+                "<div id='TB_title'>"
+              + "  <div id='TB_ajaxWindowTitle'></div>"
+              + "  <div id='TB_closeAjaxWindow'><a href='#' id='TB_closeWindowButton' onclick='OwnersStore.remove();'>close</a></div>"
+              + "</div>"
+              + "<div id='TB_ajaxContent' style='width:" + ajaxContentW + "px;height:" + ajaxContentH + "px'>"
+              + "</div>"
+            );
+         //this means the window is already up, we are just loading new content via ajax
+        } else {
+            $("#TB_ajaxContent")[0].style.width = ajaxContentW +"px";
+            $("#TB_ajaxContent")[0].style.height = ajaxContentH +"px";
+            $("#TB_ajaxContent")[0].scrollTop = 0;
+
+        }
+
+        $("#TB_load").remove();
+        $("#TB_window").css({marginLeft: '-' + parseInt((TB_WIDTH / 2),10) + 'px', width: TB_WIDTH + 'px'});
+
+        // take away IE6
+        if (!(jQuery.browser.msie && jQuery.browser.version < 7)) {
+            $("#TB_window").css({marginTop: '-' + parseInt((TB_HEIGHT / 2),10) + 'px'});
+        }
+
+        $("#TB_ajaxWindowTitle").html(title);
+        $("#TB_ajaxContent").html(contents);
+        $("#TB_window").css({display:"block"});
+    },
+
+    // exexute install or update
+    download: function(product_id) {
+        this.show_loading();
+        $.post(
+            upgrade_url,
+            {mode: 'download', product_id: product_id},
+            this.show_result,
+            'json'
+        )
+    },
+    // get products list
+    products_list: function() {
+        this.show_loading();
+        var show = this.show_result;
+        var remove = this.remove;
+        $().ajaxError(this.show_result);
+        $.post(
+           upgrade_url,
+            {mode: 'products_list'},
+            function(resp, status) {
+                if (resp.status == 'ERROR') {
+                    show(resp, status);
+                } else {
+                    remove();
+                    $('#ownersstore_products_list').html(resp.body);
+                }
+            },
+            'json'
+        )
+    }
+}
+window.OwnersStore = new OwnersStore();
+})();
+<?php
+header('Content-Type: application/x-javascript');
+echo str_replace('[%URL_DIR%]', URL_DIR, ob_get_clean());
+?>
