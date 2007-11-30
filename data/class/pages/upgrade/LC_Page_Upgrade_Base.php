a7780af3da15a1fc4166efe22a84dd59c0267ab4
adachi <adachi@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-11-30 12:12:38 +0000

    大文字小文字の修正

--- data/class/pages/upgrade/LC_Page_Upgrade_Base.php
+++ data/class/pages/upgrade/LC_Page_Upgrade_Base.php
@@ -1,7 +1,7 @@
 <?php
 // {{{ requires
 require_once CLASS_PATH . 'pages/LC_Page.php';
-require_once CLASS_PATH . 'pages/upgrade/helper/LC_Upgrade_Helper_LOG.php';
+require_once CLASS_PATH . 'pages/upgrade/helper/LC_Upgrade_Helper_Log.php';
 require_once CLASS_PATH . 'pages/upgrade/helper/LC_Upgrade_Helper_Json.php';
 require_once DATA_PATH . 'module/Request.php';
 
