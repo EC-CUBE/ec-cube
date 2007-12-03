81db69cfa9d2542ec96591175558dbe69e9128a7
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-12-03 15:17:59 +0000

    patch_downloadログ出力用

--- html/install/sql/insert_data.sql
+++ html/install/sql/insert_data.sql
@@ -1069,3 +1069,5 @@ INSERT INTO mtb_constants VALUES ('OSTORE_E_C_INVALID_PARAM', '"2007"', 519, '�
 INSERT INTO mtb_constants VALUES ('OSTORE_E_C_AUTOUP_DISABLE', '"2008"', 520, 'オーナーズストア通信エラーコード');
 INSERT INTO mtb_constants VALUES ('OSTORE_E_C_PERMISSION', '"2009"', 521, 'オーナーズストア通信エラーコード');
 INSERT INTO mtb_constants VALUES ('OSTORE_E_C_BATCH_ERR', '"2010"', 522, 'オーナーズストア通信エラーコード');
+
+INSERT INTO dtb_module (module_id,module_code,module_name,update_date)values(0,0,'patch',now());
