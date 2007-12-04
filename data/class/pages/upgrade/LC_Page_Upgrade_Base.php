e42ea9a4a7455b454387f6780a300ad762403b4c
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-12-04 05:21:16 +0000

    IP許可のログを出力

--- data/class/pages/upgrade/LC_Page_Upgrade_Base.php
+++ data/class/pages/upgrade/LC_Page_Upgrade_Base.php
@@ -14,15 +14,16 @@ require_once DATA_PATH . 'module/Request.php';
  */
 class LC_Page_Upgrade_Base extends LC_Page {
     function isValidIP() {
+    	$objLog  = new LC_Upgrade_Helper_Log;
         $masterData = new SC_DB_MasterData();
         $arrOstoreIPs = $masterData->getMasterData("mtb_ownersstore_ips");
 
         if (isset($_SERVER['REMOTE_ADDR'])
         && in_array($_SERVER['REMOTE_ADDR'], $arrOstoreIPs)) {
-
+        	$objLog->log('* ip ok ' . $_SERVER['REMOTE_ADDR']);
             return true;
         }
-
+        $objLog->log('* refused ip ' . $_SERVER['REMOTE_ADDR']);
         return false;
     }
 
