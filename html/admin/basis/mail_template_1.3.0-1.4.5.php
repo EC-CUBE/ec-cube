ad155d38523a5954991c137b9015787321bfa7f1
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2007-12-04 00:42:39 +0000

    メールテンプレートバージョンアップ用

---
+++ html/admin/basis/mail_template_1.3.0-1.4.5.php
@@ -0,0 +1,41 @@
+<?php
+/*
+ * Copyright(c) 2000-2007 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ */
+require_once("../../require.php");
+
+$message = "";
+
+// カラム作成
+if(sfColumnExists("dtb_mailtemplate", "send_type", "int2", DEFAULT_DSN, true)) {
+    $message.= "send_type ok<br />\n";
+}
+
+// カラム作成
+if(sfColumnExists("dtb_mailtemplate", "body", "text", DEFAULT_DSN, true)) {
+    $message.= "body ok<br />\n";
+}
+
+// カラム作成
+if(sfColumnExists("dtb_mailtemplate", "template_name", "text", DEFAULT_DSN, true)) {
+    $message.= "template_name ok<br />\n";
+}
+
+$objQuery = new SC_Query();
+
+$arrVal['send_type'] = '1';
+$arrVal['template_name'] = '受注完了テンプレート(PCサイト用)';
+
+$objQuery->update("dtb_mailtemplate", $arrVal, "template_id = ?", array('1'));
+
+$arrVal['send_type'] = '2';
+$arrVal['template_name'] = '受注完了テンプレート(携帯サイト用)';
+
+$objQuery->update("dtb_mailtemplate", $arrVal, "template_id = ?", array('2'));
+
+$message.= "データを更新しました。<br />\n";
+print($message);
+
+?>
