f22774cb7f635a6eacc167c69f93ef90ffb69545
naka <naka@42d9b83e-2207-4a45-8b47-68c1da84f352>
2008-01-14 13:51:34 +0000

    load_payment_moduleを2.0対応に修正

--- html/shopping/load_payment_module.php
+++ html/shopping/load_payment_module.php
@@ -27,20 +27,22 @@ $objCartSess = new SC_CartSession();
 $objQuery = new SC_Query();
 
 // 前のページで正しく登録手続きが行われた記録があるか判定
-sfIsPrePage($objSiteSess);
+SC_Utils::sfIsPrePage($objSiteSess);
 
 // アクセスの正当性の判定
-$uniqid = sfCheckNormalAccess($objSiteSess, $objCartSess);
+$uniqid = SC_Utils::sfCheckNormalAccess($objSiteSess, $objCartSess);
 
 $payment_id = $_SESSION["payment_id"];
 
 // 支払いIDが無い場合にはエラー
 if($payment_id == ""){
-	sfDispSiteError(PAGE_ERROR, "", true);
+	SC_Utils::sfDispSiteError(PAGE_ERROR, "", true);
 }
 
 // 決済情報を取得する
-if(sfColumnExists("dtb_payment", "memo01")){
+$objDB = new SC_Helper_DB_Ex();
+
+if($objDB->sfColumnExists("dtb_payment", "memo01")){
 	$sql = "SELECT module_path, memo01, memo02, memo03, memo04, memo05, memo06, memo07, memo08, memo09, memo10 FROM dtb_payment WHERE payment_id = ?";
 	$arrPayment = $objQuery->getall($sql, array($payment_id));
 }
@@ -51,7 +53,7 @@ if(count($arrPayment) > 0) {
 		require_once($path);
 		exit;
 	} else {
-		sfDispSiteError(FREE_ERROR_MSG, "", true, "モジュールファイルの取得に失敗しました。<br />この手続きは無効となりました。");
+		SC_Utils::sfDispSiteError(FREE_ERROR_MSG, "", true, "モジュールファイルの取得に失敗しました。<br />この手続きは無効となりました。");
 	}
 }
 
