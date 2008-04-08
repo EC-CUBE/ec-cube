566c91a9609ec037ee191263c6cf78dfb8ccb6e2
satou <satou@42d9b83e-2207-4a45-8b47-68c1da84f352>
2008-04-08 07:35:08 +0000

    version up (1.5.0)

--- data/conf/conf.php
+++ data/conf/conf.php
@@ -125,7 +125,7 @@ define("CREDIT_HTTP_ANALYZE_URL", CREDIT_HTTP_DOMAIN . CREDIT_HTTP_ANALYZE_PROGR
 define("CHAR_CODE", "EUC-JP");
 
 // EC-CUBEバージョン情報
-define("ECCUBE_VERSION", "1.4.6");
+define("ECCUBE_VERSION", "1.5.0-beta");
 
 // 決済モジュール付与文言
 define("ECCUBE_PAYMENT", "EC-CUBE");
