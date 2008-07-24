70df78e0890cd9c971e00f04ff246d7a6e6330a1
adachi <adachi@42d9b83e-2207-4a45-8b47-68c1da84f352>
2008-07-24 07:50:21 +0000

    install.php巻き戻し

--- data/install.php
+++ data/install.php
@@ -1,19 +1,2 @@
 <?php
-    define ('ECCUBE_INSTALL', 'ON');
-    define ('HTML_PATH', 'C:/mydir/pdt/workspace/version-2/html/');
-    define ('SITE_URL', 'http://version2.local/');
-    define ('SSL_URL', 'http://version2.local/');
-    define ('URL_DIR', '/');
-    define ('DOMAIN_NAME', '');
-    define ('DB_TYPE', 'pgsql');
-    define ('DB_USER', 'naka_db_user');
-    define ('DB_PASSWORD', 'password');
-    define ('DB_SERVER', 'svn.ec-cube.net');
-    define ('DB_NAME', 'naka2_db');
-    define ('DB_PORT', '');
-    define ('DATA_PATH', 'C:/mydir/pdt/workspace/version-2/data/');
-    define ('MOBILE_HTML_PATH', HTML_PATH . 'mobile/');
-    define ('MOBILE_SITE_URL', SITE_URL . 'mobile/');
-    define ('MOBILE_SSL_URL', SSL_URL . 'mobile/');
-    define ('MOBILE_URL_DIR', URL_DIR . 'mobile/');
 ?>
