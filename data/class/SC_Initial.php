d0b0b7d660bd9e07cb23970c2548fd85e680f0e9
nakanishi <nakanishi@42d9b83e-2207-4a45-8b47-68c1da84f352>
2008-08-18 01:09:38 +0000

    バージョン番号修正

--- data/class/SC_Initial.php
+++ data/class/SC_Initial.php
@@ -37,7 +37,7 @@ class SC_Initial {
     function SC_Initial() {
 
         /** EC-CUBEのバージョン */
-        define('ECCUBE_VERSION', "2.2.0-beta");
+        define('ECCUBE_VERSION', "2.3.0-rc1");
     }
 
     // }}}
