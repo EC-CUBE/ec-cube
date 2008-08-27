dfda95fac67f6618a1b7ab7d5da8420b3983809d
kishida <kishida@42d9b83e-2207-4a45-8b47-68c1da84f352>
2008-08-27 04:38:43 +0000

    require_once("../../require.php");をrequire_once("../require.php");に変更する

--- html/campaign/index.php
+++ html/campaign/index.php
@@ -22,7 +22,7 @@
  */
 
 // {{{ requires
-require_once("../../require.php");
+require_once("../require.php");
 require_once(CLASS_EX_PATH . "page_extends/campaign/LC_Page_Campaign_Ex.php");
 
 // }}}
