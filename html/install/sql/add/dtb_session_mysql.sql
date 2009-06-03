45148fe048eabb706f9dfc90ec985c2b32addb4b
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2009-06-03 13:39:58 +0000

    dtb_session にプライマリキーを付与。   * 正式版の r17774 の改訂バージョン

--- html/install/sql/add/dtb_session_mysql.sql
+++ html/install/sql/add/dtb_session_mysql.sql
@@ -1,6 +1,7 @@
 CREATE TABLE dtb_session (
-    sess_id text NOT NULL,
+    sess_id varchar(50) NOT NULL,
     sess_data text,
     create_date datetime NOT NULL,
-    update_date datetime NOT NULL
+    update_date datetime NOT NULL,
+    PRIMARY KEY (sess_id)
 ) TYPE=InnoDB;
--- html/install/sql/add/dtb_session_pgsql.sql
+++ html/install/sql/add/dtb_session_pgsql.sql
@@ -2,5 +2,6 @@ CREATE TABLE dtb_session (
     sess_id text NOT NULL,
     sess_data text,
     create_date timestamp NOT NULL,
-    update_date timestamp NOT NULL
+    update_date timestamp NOT NULL,
+    PRIMARY KEY (sess_id)
 );
