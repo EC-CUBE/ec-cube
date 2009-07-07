c5df23fd404b10383bbd9b9e703cd588b0daf340
kajiwara <kajiwara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2009-07-07 10:20:24 +0000

    バージョンを2.4.1に変更

--- data/class/SC_Initial.php
+++ data/class/SC_Initial.php
@@ -37,7 +37,7 @@ class SC_Initial {
     function SC_Initial() {
 
         /** EC-CUBEのバージョン */
-        define('ECCUBE_VERSION', "2.4.0 RC2");
+        define('ECCUBE_VERSION', "2.4.1");
     }
 
     // }}}
