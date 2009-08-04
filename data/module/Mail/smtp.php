fc4401ce5576e7f1c97def7db7b464eb86958783
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2009-08-04 04:02:59 +0000

    r18252 で無関係なファイルが含まれたのを戻す。

--- data/module/Mail/smtp.php
+++ data/module/Mail/smtp.php
@@ -77,19 +77,19 @@ class Mail_smtp extends Mail {
      *
      * @var mixed
      */
-    var $auth = true;
+    var $auth = false;
 
     /**
      * The username to use if the SMTP server requires authentication.
      * @var string
      */
-    var $username = 'eccube_smtp';
+    var $username = '';
 
     /**
      * The password to use if the SMTP server requires authentication.
      * @var string
      */
-    var $password = 'ecpw09';
+    var $password = '';
 
     /**
      * Hostname or domain that will be sent to the remote SMTP server in the
