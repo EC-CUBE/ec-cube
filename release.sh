d973ffd51e4344aa6ea9b2c90d2dffe74ad7e76b
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2009-11-14 06:50:09 +0000

    r18296 対応漏れ。

--- release.sh
+++ release.sh
@@ -39,6 +39,7 @@ chmod -R 777 $WRK_DIR/data/Smarty
 chmod -R 777 $WRK_DIR/data/class
 chmod -R 777 $WRK_DIR/data/logs
 chmod -R 777 $WRK_DIR/html/cp
+chmod -R 777 $WRK_DIR/data/upload
 
 echo "complession files..."
 
