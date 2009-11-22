66b0d486335739d7edc3c2a7e9c4e2436101726c
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2009-11-22 15:00:00 +0000

    一部のみデフォルト値を設定するのは誤りと思われるので、外した。   * 参考: http://www.php.net/manual/ja/oop4.constructor.php

--- data/class/graph/SC_GraphBase.php
+++ data/class/graph/SC_GraphBase.php
@@ -181,7 +181,7 @@ class SC_GraphBase {
     var $ARR_GRID_COLOR;
 
     // コンストラクタ
-    function SC_GraphBase($bgw = BG_WIDTH, $bgh = BG_HEIGHT, $left, $top) {
+    function SC_GraphBase($bgw, $bgh, $left, $top) {
         $this->init();
         // 画像作成
         $this->bgw = $bgw;
