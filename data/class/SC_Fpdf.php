17058db0481dfb10d3593158f7885536646e307c
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-01-01 14:49:16 +0000

    * merge r18405     * 取得元: version-2_4 【取得元のログメッセージ】   * #551　受注管理の帳票出力機能で機種依存文字が表示されない　不具合を修正

--- data/class/SC_Fpdf.php
+++ data/class/SC_Fpdf.php
@@ -295,7 +295,7 @@ class SC_Fpdf {
 
     // PDF_Japanese::Text へのパーサー
     function lfText($x, $y, $text, $size, $style = '') {
-        $text = mb_convert_encoding($text, "SJIS", CHAR_CODE);
+        $text = mb_convert_encoding($text, "SJIS-win", CHAR_CODE);
 
         $this->pdf->SetFont('SJIS', $style, $size);
         $this->pdf->Text($x, $y, $text);
