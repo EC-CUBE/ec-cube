33f9d299ea4eb94a5021b2a86e529045879a6b8a
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-01-01 14:46:26 +0000

    * merge r18391,r18392     * 取得元: version-2_4   * ソース整形 【取得元のログメッセージ】   * #525 お届け先を指定せずに注文完了できる 不具合を修正

--- data/class/pages/shopping/LC_Page_Shopping_Payment.php
+++ data/class/pages/shopping/LC_Page_Shopping_Payment.php
@@ -95,6 +95,14 @@ class LC_Page_Shopping_Payment extends LC_Page {
             $this->tpl_back_url = URL_SHOP_TOP . "?from=nonmember";
         }
 
+        // 一時受注テーブルの読込
+        $arrOrderTemp = $objDb->sfGetOrderTemp($uniqid);
+        //不正遷移チェック（正常に受注情報が格納されていない場合は一旦カート画面まで戻す）
+        if (!$arrOrderTemp) {
+            $this->sendRedirect($this->getLocation(URL_CART_TOP));
+            exit;
+        }
+
         // カート内商品の集計処理を行う
         $objDb->sfTotalCart($this, $objCartSess);
         if (strlen($this->tpl_message) >= 1) {
@@ -198,6 +206,14 @@ class LC_Page_Shopping_Payment extends LC_Page {
         if($this->objCustomer->isLoginSuccess(true)) {
             $this->tpl_login = '1';
             $this->tpl_user_point = $this->objCustomer->getValue('point');
+        }
+
+        // 一時受注テーブルの読込
+        $arrOrderTemp = $objDb->sfGetOrderTemp($uniqid);
+        //不正遷移チェック（正常に受注情報が格納されていない場合は一旦カート画面まで戻す）
+        if (!$arrOrderTemp) {
+            $this->sendRedirect($this->getLocation(MOBILE_URL_CART_TOP));
+            exit;
         }
 
         // 金額の取得 (購入途中で売り切れた場合にはこの関数内にてその商品の数量が０になる)
