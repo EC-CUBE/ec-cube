8b50e5e4878e4a04305949571fadf253840b95ba
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-02-02 10:34:24 +0000

    data/install.php の内容を削除

--- data/install.php
+++ data/install.php
@@ -1,17 +1,2 @@
 <?php
-    define ('ECCUBE_INSTALL', 'ON');
-    define ('SITE_URL', 'http://k-1.comu-ver2.ec-cube.net/');
-    define ('SSL_URL', 'http://k-1.comu-ver2.ec-cube.net/');
-    define ('URL_DIR', '/');
-    define ('DOMAIN_NAME', '');
-    define ('DB_TYPE', 'pgsql');
-    define ('DB_USER', 'nanasess');
-    define ('DB_PASSWORD', 'nanasess');
-    define ('DB_SERVER', '127.0.0.1');
-    define ('DB_NAME', 'eccube_db_comu_ver2');
-    define ('DB_PORT', '');
-    define ('MOBILE_HTML_PATH', HTML_PATH . 'mobile/');
-    define ('MOBILE_SITE_URL', SITE_URL . 'mobile/');
-    define ('MOBILE_SSL_URL', SSL_URL . 'mobile/');
-    define ('MOBILE_URL_DIR', URL_DIR . 'mobile/');
 ?>
