691cd6bd1a87c97f8a74f90510b47d3965508539
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-05-11 15:28:24 +0000

    #628(未使用処理・定義の削除)

--- html/define.php
+++ html/define.php
@@ -2,9 +2,6 @@
 /** HTMLディレクトリからのDATAディレクトリの相対パス */
 define("HTML2DATA_DIR", "../data/");
 
-/** DATA ディレクトリから HTML ディレクトリの相対パス(未使用) */
-define("DATA_DIR2HTML", "../html/");
-
 /**
  * DIR_INDEX_FILE にアクセスするときにファイル名を使用するか
  *
