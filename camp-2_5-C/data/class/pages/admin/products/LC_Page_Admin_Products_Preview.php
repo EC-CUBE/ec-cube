77286ef58bd0940870296537e7385e8b539aada8
kishik <kishik@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-11-06 20:40:47 +0000

    プレビュー機能のデバッグ

--- camp-2_5-C/data/class/pages/admin/products/LC_Page_Admin_Products_Preview.php
+++ camp-2_5-C/data/class/pages/admin/products/LC_Page_Admin_Products_Preview.php
@@ -78,9 +78,7 @@ class LC_Page_Admin_Products_Preview extends LC_Page {
      */
     function process() {
         // プロダクトIDの正当性チェック
-        $product_id = $this->lfCheckProductId();
-        var_dump('HERE');
-        exit;
+        // $product_id = $this->lfCheckProductId();
 
         $objView = new SC_SiteView(strlen($_POST['mode']) == 0);
         $objCustomer = new SC_Customer();
