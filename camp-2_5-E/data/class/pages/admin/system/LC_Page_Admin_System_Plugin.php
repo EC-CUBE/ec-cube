5e49e8e0caf65a22cfe7f83fc471fe95f9c27cee
miningbrownie <miningbrownie@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-11-06 17:51:34 +0000

    auto commit by watch

--- camp-2_5-E/data/class/pages/admin/system/LC_Page_Admin_System_Plugin.php
+++ camp-2_5-E/data/class/pages/admin/system/LC_Page_Admin_System_Plugin.php
@@ -79,7 +79,8 @@ class LC_Page_Admin_System_Plugin extends LC_Page_Admin {
                 $arrPlugins = $objQuery->get("dtb_plugin", "plugin_id", "plugin_name = ?",array($objForm->getValue('plugin_name')));
                 if(count($arrPlugins) == 0){
                     //新規インストール
-                    
+                    $data = array();
+                    $objQuery->insert("dtb_plugin", $data);
                 }else{
                     //再インストール
                     
