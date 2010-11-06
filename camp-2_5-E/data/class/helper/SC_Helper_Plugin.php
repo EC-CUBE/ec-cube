ba3e52bbfddb59c8bcc2a12cc21b814859bb6fc5
nakanishi <nakanishi@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-11-06 12:28:18 +0000

    loadとprocessの動作を見直した。

--- camp-2_5-E/data/class/helper/SC_Helper_Plugin.php
+++ camp-2_5-E/data/class/helper/SC_Helper_Plugin.php
@@ -1,7 +1,5 @@
 <?php
-/**
- *
- */
+
 class SC_Helper_Plugin{
 
     /**
@@ -10,27 +8,48 @@ class SC_Helper_Plugin{
      */
     public static function load(LC_Page $lcpage){
         //データベースからクラス名を読み込む
-        $objQuery =& SC_Query::getSingletonInstance();
-        $arrRet = $objQuery->select('*', 'dtb_plugin');
+        $objQuery = new SC_Query();
+        $col = "*";
+        $table = "dtb_plugin";
+        $where = "enable = 1 AND del_flg = 0";
+        $arrRet = $objQuery->select($col, $table, $where);
+        $arrEnablePlugins = array();
+        $class_name = get_class($lcpage);
 
         // 実行されたぺーじ
         // 現在のページで使用するプラグインが存在するかどうかを検証する
         foreach ($arrRet as $key => $value){
             // プラグインを稼働させるクラス名のリストを取得する
             // プラグインのディレクトリ内の設定ファイルを参照する
-            require_once DATA_PATH.'plugin/'.$value['class_name'].'/conf.php';
-            if(in_array($value['class_name'], $arrPluginExecutePages)){
-                require_once DATA_PATH.'plugin/'.$value['class_name'].$value['class_name'].'.php';
+            require_once DATA_PATH.'plugin/'.$value['class_name'].'/config.php';
+            if( in_array($class_name,$arrPluginPageList) == true ){
+                require_once DATA_PATH.'plugin/'.$value['class_name'].'/'.$value['class_name'].'.php';
+                $arrPluginList[] = $value['class_name'];
             }
         }
-
+        return $arrPluginList;
     }
+
+     /* 読み込んだプラグインの実行用メソッド
+     *
+     */
+    public static function process(LC_Page $lcpage){
+        //プラグインの名前を判別してページ内で有効なプラグインがあれば実行する
+        $arrPluginList = SC_Helper_Plugin::load($lcpage);
+        if(count($arrPluginList) > 0){
+            foreach ($arrPluginList as $key => $value){
+            $instance = new $value;
+            $instance->process($lcpage);
+            }
+        }
+        return $lcpage;
+  }
 
     /**
      * 稼働中のプラグインを取得する。
      */
     public static function getEnablePlugin(){
-        $objQuery =& SC_Query::getSingletonInstance();
+        $objQuery = new SC_Query();
         $col = '*';
         $table = 'dtb_plugin';
         $where = 'enable = 1 AND del_flg = 0';
@@ -42,7 +61,7 @@ class SC_Helper_Plugin{
      * インストールされているプラグインを取得する。
      */
     public static function getAllPlugin(){
-        $objQuery =& SC_Query::getSingletonInstance();
+        $objQuery = new SC_Query();
         $col = '*';
         $table = 'dtb_plugin';
         $where = 'del_flg = 0';
@@ -50,14 +69,5 @@ class SC_Helper_Plugin{
         return $arrRet;
     }
 
-
-    
-    
-    /* プラグイン
-     *
-     */
-    public static function process(LC_Page $lcpage,SC_View $view){       
-        //プラグインの名前を判別してページ内で有効なプラグインがあれば実行する
-        $view;
-  }
 }
+
