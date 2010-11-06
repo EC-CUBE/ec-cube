cfa2ba731b5a3dbb26c696281e8892461cd507f8
miningbrownie <miningbrownie@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-11-06 11:32:35 +0000

    commit by watch

--- camp-2_5-E/data/class/SC_Display.php
+++ camp-2_5-E/data/class/SC_Display.php
@@ -1,22 +1,22 @@
 <?php
 class SC_Display{
-    
+
     var $response;
-    
+
     function SC_Display(){
         require_once(CLASS_EX_PATH."/SC_Response_Ex.php");
         $this->response = new SC_Response_Ex();
     }
-    
-    
+
+
     // TODO このメソッドは、レスポンスを返すためのメソッドです。名前を絶対に変えましょう。
     /**
-     * 
-     * @param $page LC_Page
-     */
+    *
+    * @param $page LC_Page
+    */
     function hoge(LC_Page $page){
-      $this->assign($page);
-      
+        $this->assign($page);
+
     }
 
     /**
@@ -25,17 +25,19 @@ class SC_Display{
      * SC_Display::SMARTPOHNE = スマホ = 2
      * SC_Display::PC = PC = 4
      * ※PHP4の為にconstは使っていません。 1がガラケーで、2がスマホで4がPCです。
-     * @return 
+     * @return
      */
     function detectAgent(){
-      $_SERVER['User-Agent']
+        $nu = new Net_UserAgent_Mobile();
         
-        
-    }   
-    
-    function assign(LC_Page $page){
-        
+        $_SERVER['User-Agent']
+
+
     }
 
-    
+    function assign(LC_Page $page){
+
+    }
+
+
 }
