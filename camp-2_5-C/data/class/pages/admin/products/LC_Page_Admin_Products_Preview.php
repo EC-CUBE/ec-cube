70d7233c60f26b0d12a81410e96d2a1a4a12fb7b
kishik <kishik@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-11-06 20:49:12 +0000

    プレビュー機能のデバッグ

--- camp-2_5-C/data/class/pages/admin/products/LC_Page_Admin_Products_Preview.php
+++ camp-2_5-C/data/class/pages/admin/products/LC_Page_Admin_Products_Preview.php
@@ -264,11 +264,10 @@ class LC_Page_Admin_Products_Preview extends LC_Page {
             $this->arrTrackback = $this->lfGetTrackbackData($product_id);
         }
         $this->trackback_url = TRACKBACK_TO_URL . $product_id;
-        //関連商品情報表示
-        $this->arrRecommend = $this->lfPreGetRecommendProducts($product_id);
-        
         var_dump('HERE');
         exit;
+        //関連商品情報表示
+        $this->arrRecommend = $this->lfPreGetRecommendProducts($product_id);
 
         $this->lfConvertParam();
 
