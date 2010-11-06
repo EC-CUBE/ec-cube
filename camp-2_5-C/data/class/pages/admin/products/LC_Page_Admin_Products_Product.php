3d525a29e7b3b1e2299788576df5ad93d8cc9ad5
konishi <konishi@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-11-06 17:49:29 +0000

    表示テスト

--- camp-2_5-C/data/class/pages/admin/products/LC_Page_Admin_Products_Product.php
+++ camp-2_5-C/data/class/pages/admin/products/LC_Page_Admin_Products_Product.php
@@ -237,8 +237,8 @@ class LC_Page_Admin_Products_Product extends LC_Page {
             	$objCartSess->addPreview($this->arrForm);
             	$_SESSION[preview] = $this->arrForm;
             	print_r($_SESSION);
-                $this->tpl_javascript = "window.open('/html/admin/products/product_preview.php','test','100','100');";
-                print_r($this->tpl_javascript);
+                $this->tpl_onload = "window.open('URL_DIR . admin/products/product_preview.php','test','100','100');";
+                print_r($this->onload);
                 $this->lfProductPage();
             
             	break;
