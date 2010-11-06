b80e5829f9f6f3dee7dc2c216219dfd7fa825e32
miningbrownie <miningbrownie@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-11-06 18:12:03 +0000

    auto commit by watch

--- camp-2_5-E/data/plugin/testPlugin1/TestPlugin1.php
+++ camp-2_5-E/data/plugin/testPlugin1/TestPlugin1.php
@@ -50,8 +50,13 @@ class TestPlugin1 extends SC_Plugin_Ex {
         }else{
             $objQuery->update('dtb_plugin',$data,'plugin_id = ?',array($arrPlugins[0]['plugin_id']));
         }
+        $create = <<< SQL
+CREATE TABLE dtb_plugin_test_table1 
+
+
+SQL;
         
-        $objQuery->query("CREATE TABLE ");
+        $objQuery->query("");
 
     }
 
