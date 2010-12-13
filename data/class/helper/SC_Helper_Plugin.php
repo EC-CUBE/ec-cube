107c5521c0588dc45688512fa72e05e7a21b6338
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-12-13 19:56:30 +0000

    * #854(PHP4対応)

--- data/class/helper/SC_Helper_Plugin.php
+++ data/class/helper/SC_Helper_Plugin.php
@@ -1,12 +1,39 @@
 <?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2010 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
 
+/**
+ * プラグインのヘルパークラス.
+ *
+ * @package Helper
+ * @version $Id$
+ */
 class SC_Helper_Plugin{
 
     /**
      * enableかどうかを判別する
      * インスタンス化
      */
-    function load(LC_Page &$lcpage){
+    function load(&$lcpage){
         //データベースからクラス名を読み込む
         $objQuery = new SC_Query();
         $col = "*";
@@ -33,7 +60,7 @@ class SC_Helper_Plugin{
         return $arrPluginList;
     }
 
-    function preProcess(LC_Page &$lcpage){
+    function preProcess(&$lcpage){
         //プラグインの名前を判別してページ内で有効なプラグインがあれば実行する
         $arrPluginList = SC_Helper_Plugin::load($lcpage);
        if(count($arrPluginList) > 0){
@@ -48,7 +75,7 @@ class SC_Helper_Plugin{
     /* 読み込んだプラグインの実行用メソッド
      *
      */
-    function process(LC_Page &$lcpage){
+    function process(&$lcpage){
         //プラグインの名前を判別してページ内で有効なプラグインがあれば実行する
         $arrPluginList = SC_Helper_Plugin::load($lcpage);
         if(count($arrPluginList) > 0){
