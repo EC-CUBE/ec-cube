f9a061223668533cacaae041a6d11f444e5c783f
system_friend <system_friend@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-12-21 06:21:39 +0000

    #872 (サイト情報が無条件に送信されている) 「サイト情報について」ページで「EC-CUBEのシステム向上及び、デバッグのため以下の情報のご提供をお願いいたします」欄に「はい」と回答した場合のみサイト情報が送信される様に修正

--- html/install/index.php
+++ html/install/index.php
@@ -283,26 +283,28 @@ case 'complete':
     $GLOBAL_ERR = "";
     $objPage = lfDispComplete($objPage);
 
-    // サイト情報を送信
-    $req = new HTTP_Request("http://www.ec-cube.net/mall/use_site.php");
-    $req->setMethod(HTTP_REQUEST_METHOD_POST);
+    if(isset($_POST['send_info']) && $_POST['send_info'] === 'true'){
+        // サイト情報を送信
+        $req = new HTTP_Request("http://www.ec-cube.net/mall/use_site.php");
+        $req->setMethod(HTTP_REQUEST_METHOD_POST);
 
-    $arrSendData = array();
-    foreach($_POST as $key => $val){
-        if (ereg("^senddata_*", $key)){
-            $arrSendDataTmp = array(str_replace("senddata_", "", $key) => $val);
-            $arrSendData = array_merge($arrSendData, $arrSendDataTmp);
+        $arrSendData = array();
+        foreach($_POST as $key => $val){
+            if (ereg("^senddata_*", $key)){
+                $arrSendDataTmp = array(str_replace("senddata_", "", $key) => $val);
+                $arrSendData = array_merge($arrSendData, $arrSendDataTmp);
+            }
         }
-    }
 
-    $req->addPostDataArray($arrSendData);
+        $req->addPostDataArray($arrSendData);
 
-    if (!PEAR::isError($req->sendRequest())) {
-        $response1 = $req->getResponseBody();
-    } else {
-        $response1 = "";
+        if (!PEAR::isError($req->sendRequest())) {
+            $response1 = $req->getResponseBody();
+        } else {
+            $response1 = "";
+        }
+        $req->clearPostData();
     }
-    $req->clearPostData();
 
     break;
 case 'return_step0':
