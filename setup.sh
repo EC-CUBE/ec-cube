bd544f07645d83e10347e06e93cfb7677c23e1b0
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2010-12-25 07:51:33 +0000

    #821(setup.sh Linux で不具合) #875(setup.sh ファイルに実行権限を与えている)

--- setup.sh
+++ setup.sh
@@ -12,21 +12,21 @@ rm -rf $WRK_DIR/release.sh
 rm -rf $WRK_DIR/html/test
 rm -rf $WRK_DIR/data/downloads/module/*
 rm -rf $WRK_DIR/data/downloads/module2/*
-find $WRK_DIR -name "dummy" -delete
-find $WRK_DIR -name "info.php" -delete
+find $WRK_DIR -name "dummy" -print0 | xargs -0 rm -rf
+find $WRK_DIR -name "info.php" -print0 | xargs -0 rm -rf
 find $WRK_DIR -name ".svn" -type d -print0 | xargs -0 rm -rf
 # find $WRK_DIR -iname "*.bak" -delete
 
 echo "set permissions..."
-chmod 666 $WRK_DIR/data/install.php
-chmod -R 777 $WRK_DIR/html/install/temp
-chmod -R 777 $WRK_DIR/html/user_data
-chmod -R 777 $WRK_DIR/html/upload
-chmod -R 777 $WRK_DIR/data/cache
-chmod -R 777 $WRK_DIR/data/downloads
-chmod -R 777 $WRK_DIR/data/Smarty
-chmod -R 777 $WRK_DIR/data/class
-chmod -R 777 $WRK_DIR/data/logs
-chmod -R 777 $WRK_DIR/data/upload
+chmod a+w $WRK_DIR/data/install.php
+chmod -R a+w $WRK_DIR/html/install/temp
+chmod -R a+w $WRK_DIR/html/user_data
+chmod -R a+w $WRK_DIR/html/upload
+chmod -R a+w $WRK_DIR/data/cache
+chmod -R a+w $WRK_DIR/data/downloads
+chmod -R a+w $WRK_DIR/data/Smarty
+chmod -R a+w $WRK_DIR/data/class
+chmod -R a+w $WRK_DIR/data/logs
+chmod -R a+w $WRK_DIR/data/upload
 
 echo "finished."
