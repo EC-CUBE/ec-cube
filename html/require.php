459144469472e07bb74ad6bf0d33e78cafa6127f
fukuda <fukuda@42d9b83e-2207-4a45-8b47-68c1da84f352>
2011-01-02 06:20:54 +0000

    #880(mobile/sphoneディレクトリを削除)に対応。まずmobileのみ意図通りの動作になるように一部コミット

--- html/require.php
+++ html/require.php
@@ -29,9 +29,17 @@ require_once HTML_REALDIR . 'handle_error.php';
 define('FRONT_FUNCTION_PC_SITE', true);
 require_once HTML_REALDIR . HTML2DATA_DIR . 'require_base.php';
 
-// スマートフォンの場合、リダイレクト
-SC_SmartphoneUserAgent::sfAutoRedirectSmartphoneSite();
-
-// 絵文字変換 (除去) フィルターを組み込む。
-ob_start(array('SC_MobileEmoji', 'handler'));
+if ( Net_UserAgent_Mobile::isMobile() === true ){
+    $objMobile = new SC_Helper_Mobile_Ex();
+    $objMobile->sfMobileInit();
+    ob_start();
+    
+} else {
+	
+	// スマートフォンの場合、リダイレクト
+	SC_SmartphoneUserAgent::sfAutoRedirectSmartphoneSite();
+	
+	// 絵文字変換 (除去) フィルターを組み込む。
+	ob_start(array('SC_MobileEmoji', 'handler'));
+}
 ?>
