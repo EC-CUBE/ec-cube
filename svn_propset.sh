78c0d616d99f004a9d4ed4e2d0a648baf397e93c
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2011-02-09 04:54:27 +0000

    module 以下は除外するように修正

--- svn_propset.sh
+++ svn_propset.sh
@@ -10,8 +10,8 @@ svn:mime-type
 svn:eol-style
 EOF
 
-svn propset -R 'svn:keywords' 'Id' ./
+find . \( -name module -o -name .svn \) -prune -o -type f -print | xargs svn propset 'svn:keywords' 'Id'
 svn propset -R 'svn:executable' *.sh
 svn propset -R 'svn:eol-style' 'LF' ./
 find . -name '*.php' | xargs svn propset -R 'svn:mime-type' 'text/x-httpd-php; charset=UTF-8'
-find . -name '*.tpl' -o -purne 'module' | xargs svn propset -R 'svn:mime-type' 'text/x-smarty-template; charset=UTF-8'
+find . -name '*.tpl' | xargs svn propset -R 'svn:mime-type' 'text/x-smarty-template; charset=UTF-8'
