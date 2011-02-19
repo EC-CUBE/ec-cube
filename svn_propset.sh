270be64cd8750f02857161f9fc10253169c07561
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2011-02-19 16:51:00 +0000

    r20116 で発生したファイル破損を予防する試み

--- svn_propset.sh
+++ svn_propset.sh
@@ -15,3 +15,7 @@ svn propset -R 'svn:executable' *.sh
 svn propset -R 'svn:eol-style' 'LF' ./
 find . -name '*.php' | xargs svn propset -R 'svn:mime-type' 'text/x-httpd-php; charset=UTF-8'
 find . -name '*.tpl' | xargs svn propset -R 'svn:mime-type' 'text/x-smarty-template; charset=UTF-8'
+find . -name '*.gif' | xargs svn propset -R 'svn:mime-type' 'image/gif'
+find . -name '*.jpg' | xargs svn propset -R 'svn:mime-type' 'image/jpeg'
+find . -name '*.png' | xargs svn propset -R 'svn:mime-type' 'image/png'
+find . -name '*.pdf' | xargs svn propset -R 'svn:mime-type' 'application/pdf'
