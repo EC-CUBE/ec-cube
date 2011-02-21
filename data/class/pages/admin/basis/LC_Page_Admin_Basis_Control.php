a53bce5e0cfe02a49f37e23e2e669dcfcfdcab24
coelacanth <coelacanth@42d9b83e-2207-4a45-8b47-68c1da84f352>
2011-02-21 09:35:54 +0000

    #964 リファクタリング 宣言を除き, 引数や返り値が無く, すべて内部のメンバ変数で処理するような関数は極力作成しない

--- data/class/pages/admin/basis/LC_Page_Admin_Basis_Control.php
+++ data/class/pages/admin/basis/LC_Page_Admin_Basis_Control.php
@@ -74,22 +74,19 @@ class LC_Page_Admin_Basis_Control extends LC_Page_Admin {
         // 認証可否の判定
         SC_Utils_Ex::sfIsSuccess($objSess);
 
-        // パラメータ管理クラス
-        $this->objFormParam = new SC_FormParam();
-        // パラメータ情報の初期化
-        $this->lfInitParam();
-        // POST値の取得
-        $this->objFormParam->setParam($_POST);
+        $objFormParam = new SC_FormParam();
+        $this->lfInitParam($objFormParam);
+        $objFormParam->setParam($_POST);
 
         switch($this->getMode()) {
             case 'edit':
                 // 入力値の変換
-                $this->objFormParam->convParam();
+                $objFormParam->convParam();
 
                 // エラーチェック
                 $this->arrErr = $this->lfCheckError();
                 if(count($this->arrErr) == 0) {
-                    $this->lfSiteControlData($_POST['control_id']);
+                    $this->lfSiteControlData($_POST['control_id'], $objFormParam->getHashArray());
                     // javascript実行
                     $this->tpl_onload = "alert('更新が完了しました。');";
                 }
@@ -139,30 +136,29 @@ class LC_Page_Admin_Basis_Control extends LC_Page_Admin {
     }
 
     /* パラメータ情報の初期化 */
-    function lfInitParam() {
-        $this->objFormParam->addParam("設定状況", "control_flg", INT_LEN, "n", array("EXIST_CHECK", "NUM_CHECK", "MAX_LENGTH_CHECK"));
+    function lfInitParam(&$objFormParam) {
+        $objFormParam->addParam("設定状況", "control_flg", INT_LEN, "n", array("EXIST_CHECK", "NUM_CHECK", "MAX_LENGTH_CHECK"));
     }
 
     /* 入力内容のチェック */
-    function lfCheckError() {
+    function lfCheckError(&$objFormParam) {
         // 入力データを渡す。
-        $arrRet =  $this->objFormParam->getHashArray();
+        $arrRet =  $objFormParam->getHashArray();
         $objErr = new SC_CheckError($arrRet);
-        $objErr->arrErr = $this->objFormParam->checkError();
+        $objErr->arrErr = $objFormParam->checkError();
 
         return $objErr->arrErr;
     }
 
     /* DBへデータを登録する */
-    function lfSiteControlData($control_id = "") {
+    function lfSiteControlData($control_id = "", $post) {
         $objQuery =& SC_Query::getSingletonInstance();
-        $sqlval = $this->objFormParam->getHashArray();
+        $sqlval = $post;
         $sqlval['update_date'] = 'Now()';
 
         // 新規登録
         if($control_id == "") {
             // INSERTの実行
-            //$sqlval['creator_id'] = $_SESSION['member_id'];
             $sqlval['create_date'] = 'Now()';
             $objQuery->nextVal("dtb_site_control_control_id");
             $objQuery->insert("dtb_site_control", $sqlval);
