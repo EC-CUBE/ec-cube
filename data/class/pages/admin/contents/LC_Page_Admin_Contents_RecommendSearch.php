b1f0d8e8749ba57c06dc44fdfc196f52172609b6
shutta <shutta@42d9b83e-2207-4a45-8b47-68c1da84f352>
2011-02-24 16:22:15 +0000

    refs #1054 コンテンツ管理:おすすめ商品管理の商品検索ポップアップのページ送り処理を修正。

--- data/class/pages/admin/contents/LC_Page_Admin_Contents_RecommendSearch.php
+++ data/class/pages/admin/contents/LC_Page_Admin_Contents_RecommendSearch.php
@@ -90,7 +90,7 @@ class LC_Page_Admin_Contents_RecommendSearch extends LC_Page_Admin_Ex {
                 $page_max = SC_Utils_Ex::sfGetSearchPageMax($arrPost['search_page_max']);
 
                 // ページ送りの取得
-                $objNavi = new SC_PageNavi($arrPost['search_pageno'], $linemax, $page_max, "fnNaviSearchOnlyPage", NAVI_PMAX);
+                $objNavi = new SC_PageNavi($arrPost['search_pageno'], $this->tpl_linemax, $page_max, "fnNaviSearchOnlyPage", NAVI_PMAX);
                 $this->tpl_strnavi = $objNavi->strnavi;      // 表示文字列
                 $startno = $objNavi->start_row;
 
@@ -126,6 +126,7 @@ class LC_Page_Admin_Contents_RecommendSearch extends LC_Page_Admin_Ex {
         $objFormParam->addParam("商品ID", "search_name", LTEXT_LEN, "KVa", array( "MAX_LENGTH_CHECK"));
         $objFormParam->addParam("商品ID", "search_category_id", INT_LEN, "n", array( "MAX_LENGTH_CHECK","NUM_CHECK"));
         $objFormParam->addParam("商品コード", "search_product_code", LTEXT_LEN, "KVa", array( "MAX_LENGTH_CHECK"));
+        $objFormParam->addParam("ページ番号", "search_pageno", INT_LEN, "n", array( "MAX_LENGTH_CHECK","NUM_CHECK"));
     }
 
     /**
