37087fcbae7bb26708598fae2ca1b9afc47d3788
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2011-03-02 06:19:05 +0000

    #1082([共通]URLチェックが正しくチェックされない（URL_CHECK）)

--- data/class/SC_CheckError.php
+++ data/class/SC_CheckError.php
@@ -582,7 +582,7 @@ class SC_CheckError {
          if(isset($this->arrErr[$value[1]])) {
             return;
         }
-        if( strlen($this->arrParam[$value[1]]) > 0 && !preg_match( "/^https?://+($|[a-zA-Z0-9_~=:&\?\.\/-])+$/i", $this->arrParam[$value[1]] ) ) {
+         if( strlen($this->arrParam[$value[1]]) > 0 && !preg_match("@^https?://+($|[a-zA-Z0-9_~=:&\?\.\/-])+$@i", $this->arrParam[$value[1]] ) ) {
             $this->arrErr[$value[1]] = "※ " . $value[0] . "を正しく入力してください。<br />";
         }
     }
