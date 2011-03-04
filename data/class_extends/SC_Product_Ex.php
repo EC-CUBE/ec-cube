dedd14e25bfe86348f85f6e7f61a0de8a0b6a99a
shutta <shutta@42d9b83e-2207-4a45-8b47-68c1da84f352>
2011-03-04 14:00:25 +0000

    r20501 で間違って変更してしまったのを修正。

--- data/class_extends/SC_Product_Ex.php
+++ data/class_extends/SC_Product_Ex.php
@@ -21,9 +21,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-require_once(CLASS_REALDIR . "SC_FormParam.php");
+require_once(CLASS_REALDIR . "SC_Product.php");
 
-class SC_FormParam_Ex extends SC_FormParam {
+class SC_Product_Ex extends SC_Product {
 }
 
 ?>
