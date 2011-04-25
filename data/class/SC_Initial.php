a1ec5fd37707a394825e0e65ba16663ea429f464
nanasess <nanasess@42d9b83e-2207-4a45-8b47-68c1da84f352>
2011-04-25 05:34:18 +0000

    2.11.1-RELEASE

--- data/class/SC_Initial.php
+++ data/class/SC_Initial.php
@@ -37,7 +37,7 @@ class SC_Initial {
     function SC_Initial() {
 
         /** EC-CUBEのバージョン */
-        define('ECCUBE_VERSION', "2.11.1-dev");
+        define('ECCUBE_VERSION', "2.11.1");
     }
 
     // }}}
