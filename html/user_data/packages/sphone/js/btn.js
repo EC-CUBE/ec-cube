0a42dfad0307e9b8e81498f0a405e7b5d6024ec0
468 <468@42d9b83e-2207-4a45-8b47-68c1da84f352>
2011-07-27 05:03:39 +0000

    #1413 スマートフォン版Javascript(JQuery、JQueryMobile、JQueryLightBox、JQueryFlickslideライブラリ含む)ファイル追加

---
+++ html/user_data/packages/sphone/js/btn.js
@@ -0,0 +1,27 @@
+
+		
+		function smartRollover() {
+	if(document.getElementsByTagName) {
+		var images = document.getElementsByTagName("img");
+
+		for(var i=0; i < images.length; i++) {
+			if(images[i].getAttribute("src").match("_off."))
+			{
+				images[i].onmouseover = function() {
+					this.setAttribute("src", this.getAttribute("src").replace("_off.", "_on."));
+				}
+				images[i].onmouseout = function() {
+					this.setAttribute("src", this.getAttribute("src").replace("_on.", "_off."));
+				}
+			}
+		}
+	}
+}
+
+if(window.addEventListener) {
+	window.addEventListener("load", smartRollover, false);
+}
+else if(window.attachEvent) {
+	window.attachEvent("onload", smartRollover);
+}
+
---
+++ html/user_data/packages/sphone/js/config.js
@@ -0,0 +1,3 @@
+$(document).bind("mobileinit", function(){
+    $.mobile.ajaxFormsEnabled = false;
+});
