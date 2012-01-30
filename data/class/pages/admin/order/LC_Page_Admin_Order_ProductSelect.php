bc6e72d88ecfbced35e219328efe371f67dbcec6
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2012-01-30 00:36:15 +0000

    #1619 (受注管理＞受注登録 商品コード検索で条件外の商品が抽出される)

--- data/class/pages/admin/order/LC_Page_Admin_Order_ProductSelect.php
+++ data/class/pages/admin/order/LC_Page_Admin_Order_ProductSelect.php
@@ -221,7 +221,7 @@ class LC_Page_Admin_Order_ProductSelect extends LC_Page_Admin_Ex {
                     }
                     break;
                 case 'search_product_code':
-                    $where .=    " AND alldtl.product_id IN (SELECT product_id FROM dtb_products_class WHERE product_code LIKE ? GROUP BY product_id)";
+                    $where .=    " AND alldtl.product_id IN (SELECT product_id FROM dtb_products_class WHERE product_code LIKE ? AND del_flg = 0 GROUP BY product_id)";
                     $bind[] = '%'.$val.'%';
                     break;
 
