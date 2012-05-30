e0147daf1aac5b377a45a199f96fa232e85a88bb
nakanishi <nakanishi@42d9b83e-2207-4a45-8b47-68c1da84f352>
2012-05-30 07:37:01 +0000

    #1831 Copyright Update

--- test/createEcCubeCustomerData-v25.php
+++ test/createEcCubeCustomerData-v25.php
@@ -3,7 +3,7 @@
 /*
  * EC-CUBE 動作検証用会員データ生成スクリプト
  *
- * Copyright(c) 2000-2011 LOCKON CO.,LTD. All Rights Reserved.
+ * Copyright(c) 2000-2012 LOCKON CO.,LTD. All Rights Reserved.
  *
  * http://www.lockon.co.jp/
  *
--- test/createEcCubeData-v25.php
+++ test/createEcCubeData-v25.php
@@ -3,7 +3,7 @@
 /*
  * EC-CUBE データ生成スクリプト
  *
- * Copyright(c) 2000-2011 LOCKON CO.,LTD. All Rights Reserved.
+ * Copyright(c) 2000-2012 LOCKON CO.,LTD. All Rights Reserved.
  *
  * http://www.lockon.co.jp/
  *
