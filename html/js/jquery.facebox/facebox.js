ba29c08fdbb434c24a26fc4a963465c0387dff70
pineray <pineray@42d9b83e-2207-4a45-8b47-68c1da84f352>
2012-06-21 05:03:03 +0000

    facebox の画像パス指定が間違っているためエラーログが頻発していたのを修正.

--- html/js/jquery.facebox/facebox.js
+++ html/js/jquery.facebox/facebox.js
@@ -85,15 +85,15 @@
     settings: {
       opacity      : 0.2,
       overlay      : true,
-      loadingImage : '/facebox/loading.gif',
-      closeImage   : '/facebox/closelabel.png',
+      loadingImage : '/js/jquery.facebox/loading.gif',
+      closeImage   : '/js/jquery.facebox/closelabel.png',
       imageTypes   : [ 'png', 'jpg', 'jpeg', 'gif' ],
       faceboxHtml  : '\
     <div id="facebox" style="display:none;"> \
       <div class="popup"> \
         <div class="content"> \
         </div> \
-        <a href="#" class="close"><img src="/facebox/closelabel.png" title="close" class="close_image" /></a> \
+        <a href="#" class="close"><img src="/js/jquery.facebox/closelabel.png" title="close" class="close_image" /></a> \
       </div> \
     </div>'
     },
