99c6a5b93f6ab9f8242be5ad4607460a37e98870
h_yoshimoto <h_yoshimoto@42d9b83e-2207-4a45-8b47-68c1da84f352>
2012-06-29 03:59:04 +0000

    #1726 文字コード変換処理をSJIS-winに変更

--- data/class/pages/products/LC_Page_Products_List.php
+++ data/class/pages/products/LC_Page_Products_List.php
@@ -125,7 +125,7 @@ class LC_Page_Products_List extends LC_Page_Ex {
         $urlParam           = "category_id={$this->arrSearchData['category_id']}&pageno=#page#";
         // モバイルの場合に検索条件をURLの引数に追加
         if (SC_Display_Ex::detectDevice() === DEVICE_TYPE_MOBILE) {
-            $searchNameUrl = urlencode(mb_convert_encoding($this->arrSearchData['name'], 'SJIS', 'UTF-8'));
+            $searchNameUrl = urlencode(mb_convert_encoding($this->arrSearchData['name'], 'SJIS-win', 'UTF-8'));
             $urlParam .= "&mode={$this->mode}&name={$searchNameUrl}&orderby={$this->orderby}";
         }
         $this->objNavi      = new SC_PageNavi_Ex($this->tpl_pageno, $this->tpl_linemax, $this->disp_number, 'fnNaviPage', NAVI_PMAX, $urlParam, SC_Display_Ex::detectDevice() !== DEVICE_TYPE_MOBILE);
