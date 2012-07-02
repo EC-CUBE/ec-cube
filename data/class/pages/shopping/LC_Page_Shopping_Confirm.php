f5d5529817342b6d1edd39547a9cb569897e9013
pineray <pineray@42d9b83e-2207-4a45-8b47-68c1da84f352>
2012-07-02 12:24:12 +0000

    SC_Helper_Mail クラスのオブジェクトを作成する際に Extends を利用するように変更.

--- data/class/pages/shopping/LC_Page_Shopping_Confirm.php
+++ data/class/pages/shopping/LC_Page_Shopping_Confirm.php
@@ -76,7 +76,7 @@ class LC_Page_Shopping_Confirm extends LC_Page_Ex {
         $objCustomer = new SC_Customer_Ex();
         $objQuery =& SC_Query_Ex::getSingletonInstance();
         $objPurchase = new SC_Helper_Purchase_Ex();
-        $objHelperMail = new SC_Helper_Mail();
+        $objHelperMail = new SC_Helper_Mail_Ex();
         $objHelperMail->setPage($this);
 
         $this->is_multiple = $objPurchase->isMultiple();
