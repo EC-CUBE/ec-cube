ac567b86b0068ee0909743c6ffcebdf3e4a6c883
habu <habu@42d9b83e-2207-4a45-8b47-68c1da84f352>
2012-08-21 01:47:11 +0000

    #1922 月度集計の際に、下部の表に１日分しかデータが表示されない

--- data/class/pages/admin/total/LC_Page_Admin_Total.php
+++ data/class/pages/admin/total/LC_Page_Admin_Total.php
@@ -273,6 +273,7 @@ class LC_Page_Admin_Total extends LC_Page_Admin_Ex {
     function lfSetStartEndDate(&$objFormParam) {
 
         $arrRet = $objFormParam->getHashArray();
+        $out_flg = 0;
 
         foreach ($arrRet as $key => $val) {
             if ($val == '') {
@@ -289,9 +290,14 @@ class LC_Page_Admin_Total extends LC_Page_Admin_Ex {
                     list($sdate, $edate) = SC_Utils_Ex::sfTermMonth($objFormParam->getValue('search_startyear_m'),
                                                                     $objFormParam->getValue('search_startmonth_m'),
                                                                     CLOSE_DAY);
+                    $out_flg = 1;
                     break;
                 default:
                     break;
+            }
+            // 月度集計の場合に、集計期間が本日日付で上書きされてしまうのを回避するため
+            if ( ($objFormParam->getValue('form') == 1) && ( $out_flg == 1 ) ) {
+                break;
             }
         }
 
