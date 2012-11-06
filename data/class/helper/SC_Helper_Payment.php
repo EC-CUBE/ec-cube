23937ca10eee0896b545e4e41ffdde8b889532ba
pineray <pineray@42d9b83e-2207-4a45-8b47-68c1da84f352>
2012-11-06 03:03:56 +0000

    .#1960 (page クラスからビジネスロジックを分離)
    支払方法の管理ページクラスにあるロジックをヘルパークラスへ移動.

--- data/class/helper/SC_Helper_Payment.php
+++ data/class/helper/SC_Helper_Payment.php
@@ -31,6 +31,93 @@
 class SC_Helper_Payment
 {
     /**
+     * 支払方法の情報を取得.
+     * 
+     * @param integer $payment_id 支払方法ID
+     * @return array
+     */
+    public function get($payment_id) {
+        $objQuery =& SC_Query_Ex::getSingletonInstance();
+        $where = 'payment_id = ?';
+        $arrRet = $objQuery->select('*', 'dtb_payment', $where, array($payment_id));
+        return $arrRet[0];
+    }
+
+    /**
+     * 支払方法一覧の取得.
+     *
+     * @return array
+     */
+    public function getList() {
+        $objQuery =& SC_Query_Ex::getSingletonInstance();
+        $col = 'payment_id, payment_method, charge, rule_max, upper_rule, note, fix, charge_flg';
+        $where = 'del_flg = 0';
+        $table = 'dtb_payment';
+        $objQuery->setOrder('rank DESC');
+        $arrRet = $objQuery->select($col, $table, $where);
+        return $arrRet;
+    }
+
+    /**
+     * 支払方法の登録.
+     * 
+     * @param array $sqlval
+     * @return void
+     */
+    public function save($sqlval) {
+        $payment_id = $sqlval['payment_id'];
+
+        $objQuery =& SC_Query_Ex::getSingletonInstance();
+        // 新規登録
+        if ($payment_id == '') {
+            // INSERTの実行
+            $sqlval['rank'] = $objQuery->max('rank', 'dtb_payment') + 1;
+            $sqlval['create_date'] = 'CURRENT_TIMESTAMP';
+            $sqlval['payment_id'] = $objQuery->nextVal('dtb_payment_payment_id');
+            $objQuery->insert('dtb_payment', $sqlval);
+        // 既存編集
+        } else {
+            unset($sqlval['creator_id']);
+            $where = 'payment_id = ?';
+            $objQuery->update('dtb_payment', $sqlval, $where, array($payment_id));
+        }
+    }
+
+    /**
+     * 支払方法の削除.
+     * 
+     * @param integer $payment_id 支払方法ID
+     * @return void
+     */
+    public function delete($payment_id) {
+        $objDb = new SC_Helper_DB_Ex();
+        // ランク付きレコードの削除
+        $objDb->sfDeleteRankRecord('dtb_payment', 'payment_id', $payment_id);
+    }
+
+    /**
+     * 支払方法の表示順をひとつ上げる.
+     * 
+     * @param integer $payment_id 支払方法ID
+     * @return void
+     */
+    public function rankUp($payment_id) {
+        $objDb = new SC_Helper_DB_Ex();
+        $objDb->sfRankUp('dtb_payment', 'payment_id', $payment_id);
+    }
+
+    /**
+     * 支払方法の表示順をひとつ下げる.
+     * 
+     * @param integer $payment_id 支払方法ID
+     * @return void
+     */
+    public function rankDown($payment_id) {
+        $objDb = new SC_Helper_DB_Ex();
+        $objDb->sfRankDown('dtb_payment', 'payment_id', $payment_id);
+    }
+
+    /**
      * 決済モジュールを使用するかどうか.
      *
      * dtb_payment.memo03 に値が入っている場合は決済モジュールと見なす.
--- data/class/pages/admin/basis/LC_Page_Admin_Basis_Payment.php
+++ data/class/pages/admin/basis/LC_Page_Admin_Basis_Payment.php
@@ -67,17 +67,15 @@ class LC_Page_Admin_Basis_Payment extends LC_Page_Admin_Ex {
      */
     function action() {
 
-        $objDb = new SC_Helper_DB_Ex();
-
-        $mode = $this->getMode();
+        $objPayment = new SC_Helper_Payment_Ex();
 
         if (!empty($_POST)) {
             $objFormParam = new SC_FormParam_Ex();
-            $objFormParam->addParam('配送業者ID', 'payment_id', INT_LEN, 'n', array('NUM_CHECK', 'MAX_LENGTH_CHECK'));
+            $objFormParam->addParam('支払方法ID', 'payment_id', INT_LEN, 'n', array('NUM_CHECK', 'MAX_LENGTH_CHECK'));
             $objFormParam->setParam($_POST);
             $objFormParam->convParam();
 
-            $arrErr = $objFormParam->checkError();
+            $this->arrErr = $objFormParam->checkError();
             if (!empty($this->arrErr['payment_id'])) {
                 trigger_error('', E_USER_ERROR);
                 return;
@@ -88,26 +86,25 @@ class LC_Page_Admin_Basis_Payment extends LC_Page_Admin_Ex {
         switch ($this->getMode()) {
             case 'delete':
                 // ランク付きレコードの削除
-                $objDb->sfDeleteRankRecord('dtb_payment', 'payment_id', $post['payment_id']);
+                $objPayment->delete($post['payment_id']);
 
                 // 再表示
                 $this->objDisplay->reload();
                 break;
             case 'up':
-                $objDb->sfRankUp('dtb_payment', 'payment_id', $post['payment_id']);
+                $objPayment->rankUp($post['payment_id']);
 
                 // 再表示
                 $this->objDisplay->reload();
                 break;
             case 'down':
-                $objDb->sfRankDown('dtb_payment', 'payment_id', $post['payment_id']);
+                $objPayment->rankDown($post['payment_id']);
 
                 // 再表示
                 $this->objDisplay->reload();
                 break;
         }
-        $this->arrDelivList = $objDb->sfGetIDValueList('dtb_deliv', 'deliv_id', 'service_name');
-        $this->arrPaymentListFree = $this->lfGetPaymentList();
+        $this->arrPaymentListFree = $objPayment->getList();
 
     }
 
@@ -118,18 +115,5 @@ class LC_Page_Admin_Basis_Payment extends LC_Page_Admin_Ex {
      */
     function destroy() {
         parent::destroy();
-    }
-
-    /**
-     * 支払方法一覧の取得.
-     */
-    function lfGetPaymentList() {
-        $objQuery =& SC_Query_Ex::getSingletonInstance();
-        $col = 'payment_id, payment_method, charge, rule_max, upper_rule, note, fix, charge_flg';
-        $where = 'del_flg = 0';
-        $table = 'dtb_payment';
-        $objQuery->setOrder('rank DESC');
-        $arrRet = $objQuery->select($col, $table, $where);
-        return $arrRet;
     }
 }
