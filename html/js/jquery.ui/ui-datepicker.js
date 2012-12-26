64b5b94f5e6182cf96054274e79b2d71eff486ff
yomoro <yomoro@42d9b83e-2207-4a45-8b47-68c1da84f352>
2012-12-26 10:11:39 +0000

    #1987 データの削除を可能にする。

--- html/js/jquery.ui/ui-datepicker.js
+++ html/js/jquery.ui/ui-datepicker.js
@@ -2,13 +2,10 @@ $(function(){
 	$.datepicker.setDefaults( $.datepicker.regional[ "ja" ] );
 	$( "#datepicker" ).datepicker({
 	beforeShowDay: function(date) {
-		//Sunday
 		if(date.getDay() == 0) {
-			return [true,"date-sunday"];
-		//Saturday   
+			return [true,"date-sunday"]; 
 		} else if(date.getDay() == 6){
 			return [true,"date-saturday"];
-		//day
 		} else {
 			return [true];
 		}
@@ -19,6 +16,28 @@ $(function(){
 		$("*[name=year]").val(dates[0]);
 		$("*[name=month]").val(dates[1]);
 		$("*[name=day]").val(dates[2]);
-	}
+	},
+	showButtonPanel: true,
+	beforeShow: showAdditionalButton,       
+	onChangeMonthYear: showAdditionalButton
 	});
 });
+
+
+var showAdditionalButton = function (input) {
+	setTimeout(function () {
+		var buttonPane = $(input)
+				 .datepicker("widget")
+				 .find(".ui-datepicker-buttonpane");
+		var btn = $('<button class="ui-datepicker-current ui-state-default ui-priority-secondary ui-corner-all" type="button">クリア</button>');
+		btn
+				.unbind("click")
+				.bind("click", function () {
+					$.datepicker._clearDate(input);
+					$("*[name=year]").val("");
+					$("*[name=month]").val("");
+					$("*[name=day]").val("");
+				});
+		btn.appendTo(buttonPane);
+	}, 1);
+};
