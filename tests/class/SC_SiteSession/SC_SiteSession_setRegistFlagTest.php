de8cee4a975809a228c8d03d396f2bc762710b68
poego <poego@42d9b83e-2207-4a45-8b47-68c1da84f352>
2013-01-03 11:55:53 +0000

    #2001 SC_SiteSessionのテストケース作成

---
+++ tests/class/SC_SiteSession/SC_SiteSession_setRegistFlagTest.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2012 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+$HOME = realpath(dirname(__FILE__)) . "/../../..";
+require_once($HOME . "/tests/class/Common_TestCase.php");
+
+class SC_Session_setRegistFlagTest extends Common_TestCase {
+
+    protected function setUp() {
+        parent::setUp();
+        $this->objSiteSession = new SC_SiteSession_Ex();
+    }
+
+    protected function tearDown() {
+        parent::tearDown();
+    }
+
+    /////////////////////////////////////////
+
+    public function testSetRegistFlag_TRUEがセットされる() {
+        $this->objSiteSession->setRegistFlag();
+        $this->assertTrue($_SESSION['site']['regist_success'],'登録成功フラグ');
+    }
+}
