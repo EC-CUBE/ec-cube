fc394f5f3677a6b6fbded305c3093222081efef0
shift_hiroko.tamagawa <shift_hiroko.tamagawa@42d9b83e-2207-4a45-8b47-68c1da84f352>
2013-01-05 08:42:42 +0000

    #1977 #1978 単体テストのファイルにコピーライトを追記、定数が邪魔をしてテストできない箇所をいったんコメントアウト

--- tests/class/helper/SC_Helper_Purchase/SC_Helper_Purchase_TestBase.php
+++ tests/class/helper/SC_Helper_Purchase/SC_Helper_Purchase_TestBase.php
@@ -2,8 +2,33 @@
 
 $HOME = realpath(dirname(__FILE__)) . "/../../../..";
 require_once($HOME . "/tests/class/Common_TestCase.php");
-/**
+/*
+ * This file is part of EC-CUBE
  *
+ * Copyright(c) 2000-2012 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+/**
+ * SC_Helper_Purchaseのテストの基底クラス.
+ *
+ *
+ * @author Hiroko Tamagawa
+ * @version $Id$
  */
 class SC_Helper_Purchase_TestBase extends Common_TestCase {
 
@@ -124,7 +149,7 @@ class SC_Helper_Purchase_TestBase extends Common_TestCase {
           'update_date' => 'CURRENT_TIMESTAMP',
           'product_class_id' => '1001',
           'product_id' => '1001',
-          'product_type_id' => '1001',
+          'product_type_id' => '1',
           'product_code' => 'code1001',
           'classcategory_id1' => '1001',
           'classcategory_id2' => '1002',
@@ -137,7 +162,7 @@ class SC_Helper_Purchase_TestBase extends Common_TestCase {
           'update_date' => 'CURRENT_TIMESTAMP',
           'product_class_id' => '1002',
           'product_id' => '1002',
-          'product_type_id' => '1002',
+          'product_type_id' => '2',
           'price02' => '2500',
           'creator_id' => '1',
           'del_flg' => '0'
