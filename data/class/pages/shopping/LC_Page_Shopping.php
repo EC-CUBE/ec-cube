ac75b0df2db9ff8769c444a5e6a5ad446e4c8f9e
Seasoft <Seasoft@42d9b83e-2207-4a45-8b47-68c1da84f352>
2013-01-27 10:44:52 +0000

    #2067 (LC_Page_Shopping 非会員で画面に戻った場合、お届け先が入力復帰しない)

--- data/class/pages/shopping/LC_Page_Shopping.php
+++ data/class/pages/shopping/LC_Page_Shopping.php
@@ -480,7 +480,11 @@ class LC_Page_Shopping extends LC_Page_Ex {
         if (count($arrShippingTemp) > 1) {
             $objFormParam->setParam($arrShippingTemp[1]);
         } else {
-            $objFormParam->setParam($arrShippingTemp[0]);
+            if (empty($arrShippingTemp[1])) {
+                $objFormParam->setParam($arrShippingTemp[0]);
+            } else {
+                $objFormParam->setParam($arrShippingTemp[1]);
+            }
         }
         $objFormParam->setValue('order_email02', $arrOrderTemp['order_email']);
         $objFormParam->setDBDate($arrOrderTemp['order_birth']);
