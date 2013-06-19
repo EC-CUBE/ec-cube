25741dd6f7c973cf7071998ac56a8064e0cc2871
m_uehara <m_uehara@42d9b83e-2207-4a45-8b47-68c1da84f352>
2013-06-19 07:59:50 +0000

    #2266 入力チェック修正

--- data/class/pages/LC_Page_ResizeImage.php
+++ data/class/pages/LC_Page_ResizeImage.php
@@ -105,10 +105,8 @@ class LC_Page_ResizeImage extends LC_Page_Ex {
      * @return boolean 正常な形式:true 不正な形式:false
      */
     function lfCheckFileName() {
-        //$pattern = '|^[0-9]+_[0-9a-z]+\.[a-z]{3}$|';
-        $pattern = '|\./|';
         $file    = trim($_GET['image']);
-        if (preg_match_all($pattern, $file, $matches)) {
+        if (!preg_match("/^[[:alnum:]_\.-]+$/i", $file)) {
             return false;
         } else {
             return true;
