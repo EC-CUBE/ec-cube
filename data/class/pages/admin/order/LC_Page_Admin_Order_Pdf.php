19cc41611b174244a59ea40489d263ccae252e1a
undertree <undertree@42d9b83e-2207-4a45-8b47-68c1da84f352>
2013-06-23 23:46:44 +0000

    #2130 帳票出力の帳票タイトルが必須入力となっている

--- data/class/pages/admin/order/LC_Page_Admin_Order_Pdf.php
+++ data/class/pages/admin/order/LC_Page_Admin_Order_Pdf.php
@@ -140,7 +140,6 @@ class LC_Page_Admin_Order_Pdf extends LC_Page_Admin_Ex {
                 $arrForm['order_id'][] = $val;
             }
         }
-
         return $arrForm;
     }
 
@@ -154,8 +153,10 @@ class LC_Page_Admin_Order_Pdf extends LC_Page_Admin_Ex {
         $arrErr = $this->lfCheckError($objFormParam);
         $arrRet = $objFormParam->getHashArray();
 
-        $this->arrForm = $arrRet;
-        // エラー入力なし
+		 //タイトルが未記入の場合、デフォルトのタイトルを表示
+		 if($arrRet['title'] == "") $arrRet['title'] = 'お買い上げ明細書(納品書)';
+
+		// エラー入力なし
         if (count($arrErr) == 0) {
             $objFpdf = new SC_Fpdf_Ex($arrRet['download'], $arrRet['title']);
             foreach ($arrRet['order_id'] AS $key => $val) {
@@ -164,7 +165,7 @@ class LC_Page_Admin_Order_Pdf extends LC_Page_Admin_Ex {
                 $objFpdf->setData($arrPdfData);
             }
             $objFpdf->createPdf();
-            return true;
+	    	return true;
         } else {
             return $arrErr;
         }
@@ -191,7 +192,7 @@ class LC_Page_Admin_Order_Pdf extends LC_Page_Admin_Ex {
         $objFormParam->addParam('発行日', 'day', INT_LEN, 'n', array('EXIST_CHECK', 'MAX_LENGTH_CHECK', 'NUM_CHECK'));
         $objFormParam->addParam('帳票の種類', 'type', INT_LEN, 'n', array('EXIST_CHECK', 'MAX_LENGTH_CHECK', 'NUM_CHECK'));
         $objFormParam->addParam('ダウンロード方法', 'download', INT_LEN, 'n', array('EXIST_CHECK', 'MAX_LENGTH_CHECK', 'NUM_CHECK'));
-        $objFormParam->addParam('帳票タイトル', 'title', STEXT_LEN, 'KVa', array('EXIST_CHECK', 'MAX_LENGTH_CHECK'));
+        $objFormParam->addParam('帳票タイトル', 'title', STEXT_LEN, 'KVa', array('MAX_LENGTH_CHECK'));
         $objFormParam->addParam('帳票メッセージ1行目', 'msg1', STEXT_LEN*3/5, 'KVa', array('MAX_LENGTH_CHECK'));
         $objFormParam->addParam('帳票メッセージ2行目', 'msg2', STEXT_LEN*3/5, 'KVa', array('MAX_LENGTH_CHECK'));
         $objFormParam->addParam('帳票メッセージ3行目', 'msg3', STEXT_LEN*3/5, 'KVa', array('MAX_LENGTH_CHECK'));
