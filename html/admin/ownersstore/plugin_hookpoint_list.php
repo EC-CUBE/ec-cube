340d4b59a99138b6250ae06c1af159c698254db3
kimoto <kimoto@42d9b83e-2207-4a45-8b47-68c1da84f352>
2013-12-12 02:03:39 +0000

    #2480 destroy削除漏れ

--- html/admin/ownersstore/plugin_hookpoint_list.php
+++ html/admin/ownersstore/plugin_hookpoint_list.php
@@ -29,6 +29,5 @@ require_once CLASS_EX_REALDIR . 'page_extends/admin/ownersstore/LC_Page_Admin_Ow
 // {{{ generate page
 
 $objPage = new LC_Page_Admin_OwnersStore_PluginHookPointList_Ex();
-register_shutdown_function(array($objPage, 'destroy'));
 $objPage->init();
 $objPage->process();
