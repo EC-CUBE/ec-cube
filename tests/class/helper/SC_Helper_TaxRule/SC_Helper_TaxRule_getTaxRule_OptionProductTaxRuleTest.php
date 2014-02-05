bc55db7bd01fd08fcac9f4bfac429bd266caa761
kimoto <kimoto@42d9b83e-2207-4a45-8b47-68c1da84f352>
2014-02-05 06:17:37 +0000

    #2503 不具合を確認する為のテストを追加

---
+++ tests/class/helper/SC_Helper_TaxRule/SC_Helper_TaxRule_getTaxRule_OptionProductTaxRuleTest.php
@@ -0,0 +1,60 @@
+<?php
+
+$HOME = realpath(dirname(__FILE__)) . "/../../../..";
+// 商品別税率機能無効
+define('OPTION_PRODUCT_TAX_RULE', 1);
+require_once($HOME . "/tests/class/helper/SC_Helper_TaxRule/SC_Helper_TaxRule_TestBase.php");
+
+class SC_Helper_TaxRule_getTaxRule_OptionProductTaxRuleTest extends SC_Helper_TaxRule_TestBase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->objTaxRule = new SC_Helper_TaxRule_Ex();
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /////////////////////////////////////////
+
+    /**
+     * @test
+     */
+    public function 定数が正しく設定されているかのテスト()
+    {
+        $this->expected = 1;
+        $this->actual = constant('OPTION_PRODUCT_TAX_RULE');
+        $this->verify();
+    }
+
+    /**
+     * @test
+     */
+    /*
+    public function 引数がからの場合税率設定で設定かつ適用日時内の最新の値が返される()
+    {
+        $this->setUpTax();
+        $this->expected = array(
+            'apply_date' => '2014-01-01 00:00:00',
+            'tax_rate' => '5',
+            'product_id' => '0',
+            'product_class_id' => '0',
+            'del_flg' => '0'
+        );
+
+        $return = $this->objTaxRule->getTaxRule();
+        $this->actual = array(
+            'apply_date' => $return['apply_date'],
+            'tax_rate' => $return['tax_rate'],
+            'product_id' => $return['product_id'],
+            'product_class_id' => $return['product_class_id'],
+            'del_flg' => $return['del_flg']
+        );
+
+        $this->verify();
+    }*/
+}
