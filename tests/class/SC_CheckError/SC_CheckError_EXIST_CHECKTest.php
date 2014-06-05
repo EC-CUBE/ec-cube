fe52c38a371ea11c8e12d0c5e65bc1057ec80fac
shutta <shutta@42d9b83e-2207-4a45-8b47-68c1da84f352>
2014-06-05 13:55:33 +0000

    #2509 [共通クラス] SC_CheckError SC_CheckError_EXISTS_CHECKTestの調整。 命名規則に則した関数名に修正。 テストケースに、false、floatの0.0、普通の文字列の場合を追加。 あと細かな調整。

--- tests/class/SC_CheckError/SC_CheckError_EXIST_CHECKTest.php
+++ tests/class/SC_CheckError/SC_CheckError_EXIST_CHECKTest.php
@@ -30,6 +30,7 @@ class SC_CheckError_EXIST_CHECKTest extends Common_TestCase
     protected function setUp()
     {
         parent::setUp();
+        $this->target_func = 'EXIST_CHECK';
     }
 
     protected function tearDown()
@@ -39,99 +40,144 @@ class SC_CheckError_EXIST_CHECKTest extends Common_TestCase
 
     /////////////////////////////////////////
 
-    public function testEXIST_CHECK_formが空()
+    public function testEXIST_CHECK_formが空文字の場合_エラー()
     {
+        $disp_name = $this->target_func;
         $arrForm = array('form' => '');
         $objErr = new SC_CheckError_Ex($arrForm);
-        $objErr->doFunc(array('EXIST_CHECK', 'form') ,array('EXIST_CHECK'));
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
 
-        $this->expected = '※ EXIST_CHECKが入力されていません。<br />';
+        $this->expected = "※ {$disp_name}が入力されていません。<br />";
         $this->actual = $objErr->arrErr['form'];
         $this->verify('');
     }
 
-    public function testEXIST_CHECK_formがNULL()
+    public function testEXIST_CHECK_formがnullの場合_エラー()
     {
-        $arrForm = array('form' => NULL);
+        $disp_name = $this->target_func;
+        $arrForm = array('form' => null);
         $objErr = new SC_CheckError_Ex($arrForm);
-        $objErr->doFunc(array('EXIST_CHECK', 'form') ,array('EXIST_CHECK'));
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
 
-        $this->expected = '※ EXIST_CHECKが入力されていません。<br />';
+        $this->expected = "※ {$disp_name}が入力されていません。<br />";
         $this->actual = $objErr->arrErr['form'];
         $this->verify('');
     }
 
-    public function testEXIST_CHECK_formがint0()
+    public function testEXIST_CHECK_formがfalseの場合_エラー()
     {
+        $disp_name = $this->target_func;
+        $arrForm = array('form' => false);
+        $objErr = new SC_CheckError_Ex($arrForm);
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
+
+        $this->expected = "※ {$disp_name}が入力されていません。<br />";
+        $this->actual = $objErr->arrErr['form'];
+        $this->verify('');
+    }
+
+    public function testEXIST_CHECK_formがint0の場合_エラーではない()
+    {
+        $disp_name = $this->target_func;
         $arrForm = array('form' => 0);
         $objErr = new SC_CheckError_Ex($arrForm);
-        $objErr->doFunc(array('EXIST_CHECK', 'form') ,array('EXIST_CHECK'));
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
 
         $this->expected = '';
         $this->actual = $objErr->arrErr['form'];
         $this->verify('');
     }
 
-    public function testEXIST_CHECK_formがstring0()
+    public function testEXIST_CHECK_formがfloat0の場合_エラーではない()
     {
+        $disp_name = $this->target_func;
+        $arrForm = array('form' => 0.0);
+        $objErr = new SC_CheckError_Ex($arrForm);
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
+
+        $this->expected = '';
+        $this->actual = $objErr->arrErr['form'];
+        $this->verify('');
+    }
+
+    public function testEXIST_CHECK_formがstring0の場合_エラーではない()
+    {
+        $disp_name = $this->target_func;
         $arrForm = array('form' => '0');
         $objErr = new SC_CheckError_Ex($arrForm);
-        $objErr->doFunc(array('EXIST_CHECK', 'form') ,array('EXIST_CHECK'));
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
 
         $this->expected = '';
         $this->actual = $objErr->arrErr['form'];
         $this->verify('');
     }
 
-    public function testEXIST_CHECK_formが空の配列()
+    public function testEXIST_CHECK_formが普通の文字列の場合_エラーではない()
     {
+        $disp_name = $this->target_func;
+        $arrForm = array('form' => '普通のテスト文字列');
+        $objErr = new SC_CheckError_Ex($arrForm);
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
+
+        $this->expected = '';
+        $this->actual = $objErr->arrErr['form'];
+        $this->verify('');
+    }
+
+    public function testEXIST_CHECK_formが空の配列の場合_エラー()
+    {
+        $disp_name = $this->target_func;
         $arrForm = array('form' => array());
         $objErr = new SC_CheckError_Ex($arrForm);
-        $objErr->doFunc(array('EXIST_CHECK', 'form') ,array('EXIST_CHECK'));
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
 
-        $this->expected = '※ EXIST_CHECKが選択されていません。<br />';
+        $this->expected = "※ {$disp_name}が選択されていません。<br />";
         $this->actual = $objErr->arrErr['form'];
         $this->verify('');
     }
 
-    public function testEXIST_CHECK_formが空文字の配列()
+    public function testEXIST_CHECK_formが空文字の配列の場合_エラーではない()
     {
+        $disp_name = $this->target_func;
         $arrForm = array('form' => array(''));
         $objErr = new SC_CheckError_Ex($arrForm);
-        $objErr->doFunc(array('EXIST_CHECK', 'form') ,array('EXIST_CHECK'));
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
 
         $this->expected = '';
         $this->actual = $objErr->arrErr['form'];
         $this->verify('');
     }
 
-    public function testEXIST_CHECK_formが0しか含まない配列()
+    public function testEXIST_CHECK_formが0しか含まない配列の場合_エラーではない()
     {
+        $disp_name = $this->target_func;
         $arrForm = array('form' => array(0));
         $objErr = new SC_CheckError_Ex($arrForm);
-        $objErr->doFunc(array('EXIST_CHECK', 'form') ,array('EXIST_CHECK'));
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
 
         $this->expected = '';
         $this->actual = $objErr->arrErr['form'];
         $this->verify('');
     }
 
-    public function testEXIST_CHECK_formが配列()
+    public function testEXIST_CHECK_formが配列の場合_エラーではない()
     {
-        $arrForm = array('form' => array(1,2,3));
+        $disp_name = $this->target_func;
+        $arrForm = array('form' => array(1, 2, 3));
         $objErr = new SC_CheckError_Ex($arrForm);
-        $objErr->doFunc(array('EXIST_CHECK', 'form') ,array('EXIST_CHECK'));
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
 
         $this->expected = '';
         $this->actual = $objErr->arrErr['form'];
         $this->verify('');
     }
 
-    public function testEXIST_CHECK_formが連想配列()
+    public function testEXIST_CHECK_formが連想配列の場合_エラーではない()
     {
+        $disp_name = $this->target_func;
         $arrForm = array('form' => array(0=> 'A', 1 => 'B', 2 => 'C'));
         $objErr = new SC_CheckError_Ex($arrForm);
-        $objErr->doFunc(array('EXIST_CHECK', 'form') ,array('EXIST_CHECK'));
+        $objErr->doFunc(array($disp_name, 'form'), array($this->target_func));
 
         $this->expected = '';
         $this->actual = $objErr->arrErr['form'];
