b0f070890e84873cb337b8938c28c1a92adc73fd
sunat134 <sunat134@42d9b83e-2207-4a45-8b47-68c1da84f352>
2014-08-26 06:54:04 +0000

    #2217 会員検索ポップアップにてバリデーションエラー時に非ポップアップフレームになっていたのを修正

--- data/class/pages/admin/customer/LC_Page_Admin_Customer_SearchCustomer.php
+++ data/class/pages/admin/customer/LC_Page_Admin_Customer_SearchCustomer.php
@@ -73,18 +73,16 @@ class LC_Page_Admin_Customer_SearchCustomer extends LC_Page_Admin_Ex
         $this->arrForm = $objFormParam->getFormParamList();
         // 入力パラメーターチェック
         $this->arrErr = $this->lfCheckError($objFormParam);
-        if (!SC_Utils_Ex::isBlank($this->arrErr)) {
-            return;
-        }
-
-        // POSTのモードがsearchなら会員検索開始
-        switch ($this->getMode()) {
-            case 'search':
-                list($this->tpl_linemax, $this->arrCustomer, $this->objNavi) = $this->lfDoSearch($objFormParam->getHashArray());
-                $this->tpl_strnavi = $this->objNavi->strnavi;
-                break;
-            default:
-                break;
+        if (SC_Utils_Ex::isBlank($this->arrErr)) {
+            // POSTのモードがsearchなら会員検索開始
+            switch ($this->getMode()) {
+                case 'search':
+                    list($this->tpl_linemax, $this->arrCustomer, $this->objNavi) = $this->lfDoSearch($objFormParam->getHashArray());
+                    $this->tpl_strnavi = $this->objNavi->strnavi;
+                    break;
+                default:
+                    break;
+            }
         }
         $this->setTemplate($this->tpl_mainpage);
     }
