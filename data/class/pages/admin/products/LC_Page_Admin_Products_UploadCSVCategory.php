cafaacfd600d67ae012794c14b07f061067ab2d1
shinichi_takahashi <shinichi_takahashi@42d9b83e-2207-4a45-8b47-68c1da84f352>
2014-10-28 07:27:46 +0000

    #2028 r23498をリバート

--- data/class/pages/admin/products/LC_Page_Admin_Products_UploadCSVCategory.php
+++ data/class/pages/admin/products/LC_Page_Admin_Products_UploadCSVCategory.php
@@ -510,16 +510,6 @@ class LC_Page_Admin_Products_UploadCSVCategory extends LC_Page_Admin_Ex
             if ($level >= LEVEL_MAX) {
                 $arrErr['parent_category_id'] = '※ ' . LEVEL_MAX . '階層以上の登録はできません。';
             }
-            // 親カテゴリー論理チェック
-            if (array_search('category_id', $this->arrFormKeyList) !== FALSE
-                and $item['category_id'] != '') {
-                $arrTrail = $objCategory->getTreeTrail($item['parent_category_id'], true);
-                foreach ($arrTrail as $trailId) {
-                    if ($trailId == $item['category_id']) {
-                        $arrErr['parent_category_id'] = '※ 再帰的な親カテゴリーの指定はできません。';
-                    }
-                }
-            }
         }
 
 
