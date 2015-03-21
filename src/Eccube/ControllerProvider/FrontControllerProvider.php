57b8e36355227d8c641a16d49817e77899fb1064
Tao Sasaki <tao@xross-cube.com>
2015-03-21 18:06:39 +0000

    プライバシーポリシー ページ追加

--- src/Eccube/ControllerProvider/FrontControllerProvider.php
+++ src/Eccube/ControllerProvider/FrontControllerProvider.php
@@ -60,11 +60,11 @@ class FrontControllerProvider implements ControllerProviderInterface
         $controllers->match('/frontparts/login_check.php', '\\Eccube\\Page\\FrontParts\\LoginCheck')->bind('frontparts_login_check');
 
         // guide
-        $controllers->match('/guide/', '\\Eccube\\Page\\Guide\\Index')->bind('guide');
+        $controllers->match('/guide/', '\\Eccube\\Controller\\Guide\\Controller::Index')->bind('guide');
         $controllers->match('/guide/about.php', '\\Eccube\\Page\\Guide\\About')->bind('guide_about');
         $controllers->match('/guide/charge.php', '\\Eccube\\Page\\Guide\\Charge')->bind('guide_charge');
         $controllers->match('/guide/kiyaku.php', '\\Eccube\\Page\\Guide\\Kiyaku')->bind('guide_kiyaku');
-        $controllers->match('/guide/privacy.php', '\\Eccube\\Page\\Guide\\Privacy')->bind('guide_privacy');
+        $controllers->match('/guide/privacy/', '\\Eccube\\Controller\\Guide\\Controller::Index')->bind('guide_privacy');
         $controllers->match('/guide/usage.php', '\\Eccube\\Page\\Guide\\Usage')->bind('guide_usage');
 
         // mypage
