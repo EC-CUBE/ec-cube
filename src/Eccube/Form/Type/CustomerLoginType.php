95fa5710ad1774d676e152917bb75f0632991964
TSURU <tsuyoshi.tsurushima@gmail.com>
2015-03-23 03:19:04 +0900

    リファクタリング

--- src/Eccube/Form/Type/CustomerLoginType.php
+++ src/Eccube/Form/Type/CustomerLoginType.php
@@ -5,30 +5,37 @@ namespace Eccube\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class CustomerLoginType extends AbstractType
 {
+    public $session;
 
-    public function getName()
+    public function __construct(SessionInterface $session)
     {
-        return 'customer_login';
+        $this->session = $session;
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('login_email', 'text', array(
-            'max_length' => 50,
+            'attr' => array(
+                'max_length' => 50,
+            ),
             'constraints' => array(
                 new Assert\NotBlank(),
-            )
+            ),
+            'data' => $this->session->get('_security.last_username'),
         ));
         $builder->add('login_memory', 'checkbox', array(
             'required' => false,
             'label' => 'メールアドレスをコンピューターに記憶させる',
         ));
         $builder->add('login_pass', 'password', array(
-            'max_length' => 50,
+            'attr' => array(
+                'max_length' => 50,
+            ),
             'constraints' => array(
                 new Assert\NotBlank(),
             ),
@@ -40,6 +47,11 @@ class CustomerLoginType extends AbstractType
         $resolver->setDefaults(array(
             'csrf_protection' => false,
         ));
+    }
+
+    public function getName()
+    {
+        return 'customer_login';
     }
 
 }
