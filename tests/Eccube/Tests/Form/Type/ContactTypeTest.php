33546ffb6b10bdbfdb1960e2e341284c5ee44069
TSURU <tsuyoshi.tsurushima@gmail.com>
2015-03-23 09:14:46 +0900

    Testを修正

--- tests/Eccube/Tests/Form/Type/ContactTypeTest.php
+++ tests/Eccube/Tests/Form/Type/ContactTypeTest.php
@@ -6,46 +6,45 @@ use Symfony\Component\Form\Test\TypeTestCase;
 
 class ContactTypeTest extends TypeTestCase
 {
-	// デフォルト値（正常系）を設定
-	private $formData = array(
-            'name01' => 'たかはし',
-            'name02' => 'しんいち',
-            'kana01' => 'タカハシ',
-            'kana02' => 'シンイチ',
-            'zip01' => '530',
-            'zip02' => '0001',
-            // カスタムフィールドタイプ
-            'pref' => array(
-            	'pref' => 5
-            ),
-            'addr01' => '北区',
-            'addr02' => '梅田',
-            'tel01' => '012',
-            'tel02' => '345',
-            'tel03' => '6789',
-            'email' => 'shinichi_takahashi@lockon.co.jp',
-            'contents' => 'お問い合わせ内容テスト',
-        );
 
-    public function setUp() {
+    // デフォルト値（正常系）を設定
+    private $formData = array(
+        'name01' => 'たかはし',
+        'name02' => 'しんいち',
+        'kana01' => 'タカハシ',
+        'kana02' => 'シンイチ',
+        'zip01' => '530',
+        'zip02' => '0001',
+        'pref' => 5,
+        'addr01' => '北区',
+        'addr02' => '梅田',
+        'tel01' => '012',
+        'tel02' => '345',
+        'tel03' => '6789',
+        'email' => 'shinichi_takahashi@lockon.co.jp',
+        'contents' => 'お問い合わせ内容テスト',
+    );
+
+    public function setUp()
+    {
         parent::setUp();
-        
+
         $this->app = new \Eccube\Application;
 
         // CSRF tokenを無効にしてFormを作成
         $this->form = $this->app['form.factory']
-              ->createBuilder('contact', null, array(
+                ->createBuilder('contact', null, array(
                     'csrf_protection' => false,
                 ))
-              ->getForm();
+                ->getForm();
     }
 
-	public function testValidData()
-	{
+    public function testValidData()
+    {
         $this->form->submit($this->formData);
 
         $this->assertTrue($this->form->isValid());
-	}
+    }
 
     public function testInvalidNam01_NotBlank()
     {
@@ -79,14 +78,6 @@ class ContactTypeTest extends TypeTestCase
         $this->assertFalse($this->form->isValid());
     }
 
-    public function testInvalidZip01_NotBlank()
-    {
-        $this->formData['zip01'] = '';
-        $this->form->submit($this->formData);
-
-        $this->assertFalse($this->form->isValid());
-    }
-
     public function testInvalidZip01_LengthMin()
     {
         $this->formData['zip01'] = '1';
@@ -98,14 +89,6 @@ class ContactTypeTest extends TypeTestCase
     public function testInvalidZip01_LengthMax()
     {
         $this->formData['zip01'] = '1234';
-        $this->form->submit($this->formData);
-
-        $this->assertFalse($this->form->isValid());
-    }
-
-    public function testInvalidZip02_NotBlank()
-    {
-        $this->formData['zip02'] = '';
         $this->form->submit($this->formData);
 
         $this->assertFalse($this->form->isValid());
@@ -127,41 +110,9 @@ class ContactTypeTest extends TypeTestCase
         $this->assertFalse($this->form->isValid());
     }
 
-    public function testInvalidPref_NotBlank()
-    {
-        $this->formData['pref'] = '';
-        $this->form->submit($this->formData);
-
-        $this->assertFalse($this->form->isValid());
-    }
-
     public function testInvalidPref_Invalid()
     {
         $this->formData['pref']['pref'] = '100';
-        $this->form->submit($this->formData);
-
-        $this->assertFalse($this->form->isValid());
-    }
-
-    public function testInvalidAddr01_NotBlank()
-    {
-        $this->formData['addr01'] = '';
-        $this->form->submit($this->formData);
-
-        $this->assertFalse($this->form->isValid());
-    }
-
-    public function testInvalidAddr02_NotBlank()
-    {
-        $this->formData['addr02'] = '';
-        $this->form->submit($this->formData);
-
-        $this->assertFalse($this->form->isValid());
-    }
-
-    public function testInvalidTel01_NotBlank()
-    {
-        $this->formData['tel01'] = '';
         $this->form->submit($this->formData);
 
         $this->assertFalse($this->form->isValid());
@@ -183,14 +134,6 @@ class ContactTypeTest extends TypeTestCase
         $this->assertFalse($this->form->isValid());
     }
 
-    public function testInvalidTel02_NotBlank()
-    {
-        $this->formData['tel02'] = '';
-        $this->form->submit($this->formData);
-
-        $this->assertFalse($this->form->isValid());
-    }
-
     public function testInvalidTel02_LengthMin()
     {
         $this->formData['tel02'] = '1';
@@ -202,14 +145,6 @@ class ContactTypeTest extends TypeTestCase
     public function testInvalidTel02_LengthMax()
     {
         $this->formData['tel02'] = '12345';
-        $this->form->submit($this->formData);
-
-        $this->assertFalse($this->form->isValid());
-    }
-
-    public function testInvalidTel03_NotBlank()
-    {
-        $this->formData['tel03'] = '';
         $this->form->submit($this->formData);
 
         $this->assertFalse($this->form->isValid());
