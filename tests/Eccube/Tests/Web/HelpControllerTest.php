8f72648fccbb47ce6019e3dd4603d589d0f90fa9
のぶ <nobuhiko.kimoto@gmail.com>
2015-03-23 13:23:28 +0900

    routingTestを分割

---
+++ tests/Eccube/Tests/Web/HelpControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Eccube\Tests\Web;
+
+use Silex\WebTestCase;
+use Eccube\Application;
+
+class HelpControllerTest extends WebTestCase
+{
+    public $app;
+
+    public function createApplication()
+    {
+        $app = new Application();
+        $this->app = $app;
+
+        $app['debug'] = true;
+        $app['session.test'] = true;
+        $app['exception_handler']->disable();
+
+        return $app;
+    }
+
+    // 特定商取引法のテスト
+    public function testRoutingHelpTradelaw()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/help/tradelaw/');
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+}
