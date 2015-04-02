3e8a7e663a35bf5be1e11418e417dc5f6bbd3129
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2015-04-02 20:21:57 +0900

    購入確認コントローラのイメージ

--- src/Eccube/Controller/ShoppingController.php
+++ src/Eccube/Controller/ShoppingController.php
@@ -9,6 +9,24 @@ class ShoppingController extends AbstractController
 {
     public function index(Application $app, Request $request)
     {
+        $cart = $app['eccube.service.cart'];
+        
+        // カートに変更がある場合はエラーにする
+        if (!$cart->isLocked()) {
+            $app->abort("カートが変更されました");
+        }
+        /** @var Eccube\Service\Order */
+        $order = $app['eccube.service.order'];
+
+        /** @var Eccube\Entity\OrderTmp */
+        $preOrder = $order->findPreOrder($cart->getPreOrderId()); // 一時受注テーブルから復旧
+        if (is_null($preOrder)) {
+            // 一時受注テーブルが無いときは、カートから商品を取り出し、一時受注データを生成する(dtb_order_tmp)
+            $preOrder = $order->createPreOrder($cart->getProducts());
+            // カートへ一時受注IDをセット
+            $cart->setPreOrderId($preOrder->getOrderId());
+        }
+        
         $title = "購入確認 | レジ";
         
         return $app['twig']->render(
