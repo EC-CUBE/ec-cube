51657b3744c74e82ec6fa443f08d649821430964
fukuran <fukuran@42d9b83e-2207-4a45-8b47-68c1da84f352>
2015-04-09 23:24:54 +0900

    ポイント管理の修正 * 登録メッセージの出力 * main_frameへの埋め込み

--- src/Eccube/Controller/Admin/Basis/PointController.php
+++ src/Eccube/Controller/Admin/Basis/PointController.php
@@ -8,15 +8,14 @@ use Eccube\Controller\AbstractController;
 class PointController extends AbstractController
 {
     private $main_title;
-
-    private $title;
+    private $sub_title;
 
     public $form;
 
     public function __construct()
     {
         $this->main_title = '基本情報管理';
-        $this->title = 'ポイント設定';
+        $this->sub_title = 'ポイント設定';
     }
 
     public function Index(Application $app)
@@ -32,13 +31,14 @@ class PointController extends AbstractController
             if ($form->isValid()) {
                 $app['orm.em']->persist($baseInfo);
                 $app['orm.em']->flush();
+                $app['session']->getFlashBag()->add('point.complete', 'admin.register.complete');
                 return $app->redirect($app['url_generator']->generate('admin_basis_point'));
             }
         }
 
         return $app['twig']->render('Admin/Basis/point.twig', array(
-            'main_title' => $this->main_title,
-            'title'      => $this->title,
+            'tpl_maintitle' => $this->main_title,
+            'tpl_subtitle'  => $this->sub_title,
             'form'       => $form->createView(),
         ));
     }
