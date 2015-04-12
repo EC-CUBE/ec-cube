005f5ae6bd2f872216c2217650e07b948e17a486
POego <poego@eumyang.net>
2015-04-12 20:09:05 +0900

    会員登録のFormを簡易実装

--- src/Eccube/Form/Type/CustomerType.php
+++ src/Eccube/Form/Type/CustomerType.php
@@ -12,7 +12,7 @@ class CustomerType extends AbstractType
 {
     public $app;
 
-    public function __construct (\Eccube\Application $app)
+    public function __construct(\Eccube\Application $app)
     {
         $this->app = $app;
     }
@@ -22,55 +22,98 @@ class CustomerType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-            $app = $this->app;
+        $app = $this->app;
 
-            $builder
-                ->add('name01', 'text', array('constraints' => array(
-                    new Assert\NotBlank(),
-                )))
-                ->add('name02', 'text', array('constraints' => array(
-                    new Assert\NotBlank(),
-                )))
-                ->add('kana01', 'text', array('constraints' => array(
-                    new Assert\NotBlank(),
-                )))
-                ->add('kana02', 'text', array('constraints' => array(
-                    new Assert\NotBlank(),
-                )))
-                ->add('zip', 'zip', array(
-                    'required' => false,
-                ))
-                ->add('address', 'address', array(
-                    'help' => 'form.contact.address.help',
-                    'required' => false,
-                ))
-                ->add('tel', 'tel', array(
-                    'required' => false,
-                ))
-                ->add('fax', 'tel', array(
-                    'required' => false,
-                ))
-                ->add('email', 'email', array('constraints' => array(
+        $builder
+            ->add('name', 'name', array(
+                'options' => array(
+                    'attr' => array(
+                        'maxlength' => 50,
+                    ),
+                    'constraints' => array(
+                        new Assert\NotBlank(),
+                    ),
+                ),
+            ))
+            ->add('kana', 'name', array(
+                'options' => array(
+                    'attr' => array(
+                        'maxlength' => 50,
+                    ),
+                    'constraints' => array(
+                        new Assert\NotBlank(),
+                    ),
+                ),
+            ))
+            ->add('company_name', 'text', array(
+                'required' => false,
+                'constraints' => array(
+                    new Assert\Length(array(
+                        'max' => 50,
+                    ))
+                ),
+            ))
+            ->add('zip', 'zip', array(
+                'required' => false,
+            ))
+            ->add('address', 'address', array(
+                'help' => 'form.contact.address.help',
+                'required' => false,
+            ))
+            ->add('tel', 'tel', array(
+                'required' => false,
+            ))
+            ->add('fax', 'tel', array(
+                'required' => false,
+            ))
+            ->add('email', 'email', array(
+                'constraints' => array(
                     new Assert\NotBlank(),
                     new Assert\Email(),
-                    new Assert\Callback(function($email, ExecutionContextInterface $context) use ($app) {
+                    new Assert\Callback(function ($email, ExecutionContextInterface $context) use ($app) {
                         $customers = $app['orm.em']
                             ->getRepository('Eccube\\Entity\\Customer')
                             ->findBy(array(
-                                'email' => $email,
-                                'del_flg' => 0,
-                            )
-                        );
+                                    'email' => $email,
+                                    'del_flg' => 0,
+                                )
+                            );
                         if (count($customers) > 0) {
                             $context->addViolation('入力されたメールアドレスは既に使用されています。');
                         }
                     }),
-                 )))
-                ->add('sex', 'sex', array(
-                    'required' => false,
-                ))
-                ->add('password', 'repeated')
-            ;
+                )
+            ))
+            ->add('sex', 'sex', array(
+                'required' => false,
+            ))
+            ->add('job', 'job', array(
+                'required' => false,
+            ))
+            ->add('birth', 'date', array(
+                'required' => 'false',
+                'input' => 'timestamp',
+                'widget' => 'choice',
+                'format' => 'yyyy-MM-dd',
+                'years' => range(1950,date('Y')),
+                'empty_value' => array('year' => '----', 'month' => '--', 'day' => '--'),
+            ))
+            ->add('password', 'repeated')
+            ->add('reminder', 'reminder', array(
+            'required' => 'true',
+            ))
+            ->add('reminder_answer', 'text', array(
+                'required' => 'true',
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                    new Assert\Length(array(
+                        'max' => 50
+                    ))
+                )
+            ))
+            ->add('mailmaga_flg', 'mailmagazinetype', array(
+                'required' => 'false',
+            ));
     }
 
     /**
@@ -80,5 +123,5 @@ class CustomerType extends AbstractType
     {
         return 'customer';
     }
-      
+
 }
