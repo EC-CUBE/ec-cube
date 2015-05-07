a13cc8ddfee65b8fd9b17a22dc71d719fe0ec61e
shinichi-takahashi <shinichi_takahashi@lockon.co.jp>
2015-05-07 17:19:46 +0900

    create login block

--- html/index.php
+++ html/index.php
@@ -2,5 +2,5 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Eccube\Application();
+$app = new Eccube\Application(array('env'=>'dev'));
 $app->run();
--- src/Eccube/Controller/Block/LoginController.php
+++ src/Eccube/Controller/Block/LoginController.php
@@ -3,11 +3,41 @@
 namespace Eccube\Controller\Block;
 
 use Eccube\Application;
+use Symfony\Component\HttpFoundation\Request;
 
 class LoginController
 {
-    public function index(Application $app)
+    public function index(Application $app, Request $request)
     {
-        return '';
+        $email = $request->cookies->get('email');
+
+        /* @var $form \Symfony\Component\Form\FormInterface */
+        $form = $app['form.factory']
+            ->createNamedBuilder('', 'customer_login')
+            ->getForm();
+
+        $disableLogout = $this->isDisableLogoutPage($app);
+
+        return $app['view']->render('Block/login.twig', array(
+            'error' => $app['security.last_error']($request),
+            'disableLogout' => $disableLogout,
+            'email' => $email,
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function isDisableLogoutPage(Application $app)
+    {
+        $uri = str_replace($app['config']['root'], '', $app['request']->server->get('REDIRECT_URL'));
+        $disableLogout = $app['orm.em']->getRepository('Eccube\Entity\Master\DisableLogout')
+            ->findBy(array(
+                'name' => $uri,
+            ));
+        $disableLogout = false;
+        if (count($disableLogout) > 0) {
+            $disableLogout = true;
+        }
+
+        return $disableLogout;
     }
 }
