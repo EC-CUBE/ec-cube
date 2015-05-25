9adedad993afe12a16e8218684e77f8711f99283
shinichi-takahashi <shinichi_takahashi@lockon.co.jp>
2015-05-25 13:29:18 +0900

    create mail test

---
+++ tests/Eccube/Tests/Form/Type/MailTypeTest.php
@@ -0,0 +1,118 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+namespace Eccube\Tests\Form\Type;
+
+
+class MailTypeTest extends \Eccube\Tests\Form\Type\AbstractTypeTestCase
+{
+
+    /** @var \Eccube\Application */
+    protected $app;
+
+    /** @var \Symfony\Component\Form\FormInterface */
+    protected $form;
+
+    /** @var array デフォルト値（正常系）を設定 */
+    protected $formData = array(
+        'template' => 1,
+        'subject' => 'subject',
+        'header' => 'header',
+        'footer' => 'footer',
+    );
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        // CSRF tokenを無効にしてFormを作成
+        $this->form = $this->app['form.factory']
+            ->createBuilder('mail', null, array(
+                'csrf_protection' => false,
+            ))
+            ->getForm();
+    }
+
+    public function testValidData()
+    {
+        $this->form->submit($this->formData);
+
+        $this->assertTrue($this->form->isValid());
+    }
+
+    public function testInvalidSubject_NotBlank()
+    {
+        $this->formData['subject'] = '';
+        $this->form->submit($this->formData);
+
+        $this->assertFalse($this->form->isValid());
+    }
+
+    public function testInvalidSubject_MaxLengthInvalid()
+    {
+        $subject = str_repeat('S', $this->app['config']['mtext_len']) . 'S';
+
+        $this->formData['subject'] = $subject;
+        $this->form->submit($this->formData);
+
+        $this->assertFalse($this->form->isValid());
+    }
+
+    public function testInvalidHeader_NotBlank()
+    {
+        $this->formData['header'] = '';
+        $this->form->submit($this->formData);
+
+        $this->assertFalse($this->form->isValid());
+    }
+
+    public function testInvalidHeader_MaxLengthValid()
+    {
+        $header = str_repeat('S', $this->app['config']['ltext_len']);
+
+        $this->formData['header'] = $header;
+        $this->form->submit($this->formData);
+
+        $this->assertTrue($this->form->isValid());
+    }
+
+    public function testInvalidFooter_NotBlank()
+    {
+        $this->formData['footer'] = '';
+        $this->form->submit($this->formData);
+
+        $this->assertFalse($this->form->isValid());
+    }
+
+    public function testInvalidFooter_MaxLengthValid()
+    {
+        $footer = str_repeat('S', $this->app['config']['ltext_len']);
+
+        $this->formData['footer'] = $footer;
+        $this->form->submit($this->formData);
+
+        $this->assertTrue($this->form->isValid());
+    }
+
+}
