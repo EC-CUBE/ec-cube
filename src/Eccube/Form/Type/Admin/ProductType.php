2d3aebf4366306988f6d2880b7c4b5ef9e07c235
shinichi-takahashi <shinichi_takahashi@lockon.co.jp>
2015-05-26 22:05:45 +0900

    [WIP]modify admin-product-register

--- src/Eccube/Form/Type/Admin/ProductType.php
+++ src/Eccube/Form/Type/Admin/ProductType.php
@@ -47,113 +47,117 @@ class ProductType extends AbstractType
         $app = $this->app;
 
         $builder
-            ->add('id', 'hidden', array(
-                'mapped' => false,
-            ))
+            // 基本情報
             ->add('name', 'text', array(
                 'label' => '商品名',
                 'constraints' => array(
                     new Assert\NotBlank(),
                 ),
             ))
-           ->add('Category', 'category', array(
+            ->add('product_type', 'product_type', array(
+                'label' => '商品種別',
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                ),
+                'mapped' => false,
+            ))
+            ->add('product_image', 'collection', array(
+                'label' => '商品画像',
+                'type' => 'file',
+                'allow_add' => true,
+                'allow_delete' => true,
+                'prototype' => true,
+                'mapped' => false,
+            ))
+            ->add('description_detail', 'textarea', array(
+                'label' => '商品説明(詳細)',
+            ))
+            ->add('description_list', 'textarea', array(
+                'label' => '商品説明(一覧)',
+            ))
+            ->add('price02', 'money', array(
+                'label' => '販売価格',
+                'currency' => 'JPY',
+                'precision' => 0,
+                'constraints' => array(
+                    new Assert\NotBlank(),
+                ),
+                'mapped' => false,
+            ))
+            ->add('price01', 'money', array(
+                'label' => '通常価格',
+                'currency' => 'JPY',
+                'precision' => 0,
+                'mapped' => false,
+            ))
+            ->add('stock', 'integer', array(
+                'label' => '在庫数',
+                'required' => false,
+                'mapped' => false,
+            ))
+            ->add('stock_unlimited', 'checkbox', array(
+                'label' => '無制限',
+                'value' => '1',
+                'required' => false,
+                'mapped' => false,
+            ))
+            ->add('Category', 'category', array(
                'label' => '商品カテゴリ',
                'constraints' => array(
                    new Assert\NotBlank(),
                ),
-               'multiple' => 'multiple',
+               'multiple' => true,
                'expanded' => true,
                'mapped' => false,
-           ))
+            ))
+
+            // 詳細な説明
+            ->add('code', 'text', array(
+                'label' => '商品コード',
+                'required' => false,
+                'mapped' => false,
+            ))
+            ->add('sale_limit', 'integer', array(
+                'label' => '販売制限数',
+                'mapped' => false,
+            ))
+            ->add('tag', 'text', array(
+                'label' => 'タグ',
+                'mapped' => false,
+            ))
+            ->add('search_word', 'textarea', array(
+                'label' => "検索ワード",
+                'required' => false,
+                'mapped' => false,
+            ))
+            ->add('DeliveryDate', 'delivery_date', array(
+                'label' => '発送日目安',
+                'empty_value' => '選択してください',
+                'required' => false,
+            ))
+            ->add('point_rate', 'integer', array(
+                'label' => 'ポイント(%)',
+                'mapped' => false,
+            ))
+
+            // サブ情報
+            ->add('free_area', 'textarea', array(
+                'label' => 'サブ情報',
+            ))
+
+            // 右ブロック
             ->add('status', 'disp', array(
                 'label' => '公開・非公開',
                 'constraints' => array(
                     new Assert\NotBlank(),
                 ),
             ))
-            ->add('ProductClasses', 'collection', array(
-                'type' => 'admin_product_class',
-                'options'  => array(
-                ),
-            ))
-
-//            ->add('ProductStatuses', 'status', array(
-//                'label' => '商品ステータス',
-//                'multiple'=> true,
-//                'constraints' => array(
-//                    new Assert\NotBlank(),
-//                ),
-//            ))
-            ->add('DeliveryDate', 'delivery_date', array(
-                'label' => '発送日目安',
-                'empty_value' => '選択してください',
-                'required' => false,
-            ))
-            ->add('Maker', 'maker', array(
-                'label' => 'メーカー',
-                'empty_value' => '選択してください',
-                'required' => false,
-            ))
-            ->add('comment1', 'text', array(
-                'label' => 'メーカーURL',
-                'required' => false,
-            ))
-            ->add('comment3', 'textarea', array(
-                'label' => "検索ワード",
-                'required' => false,
-            ))
             ->add('note', 'textarea', array(
-                'label' => '備考欄(SHOP専用)',
+                'label' => 'ショップ用メモ帳',
                 'required' => false,
-            ))
-            ->add('main_list_comment', 'textarea', array(
-                'label' => '一覧-メインコメント',
-                'constraints' => array(
-                    new Assert\NotBlank(),
-                ),
-            ))
-            ->add('main_comment', 'textarea', array(
-                'label' => '詳細-メインコメント(タグ許可)',
-                'constraints' => array(
-                    new Assert\NotBlank(),
-                ),
-            ))
-            ->add('main_list_image', 'file', array(
-                'label' => '一覧-メイン画像',
-                'required' => false,
-                'mapped' => false,
-            ))
-            ->add('main_image', 'file', array(
-                'label' => '詳細-メイン画像',
-                'required' => false,
-                'mapped' => false,
-            ))
-            ->add('main_large_image', 'file', array(
-                'label' => '詳細-メイン拡大画像',
-                'required' => false,
-                'mapped' => false,
             ))
         ;
-        for ($i = 1; $i <= $app['config']['productsub_max']; $i++) {
-            $builder
-                ->add("sub_title{$i}", 'text', array(
-                    'label' => "詳細-サブタイトル({$i})",
-                    'required' => false,
-                ))
-                ->add("sub_comment{$i}", 'textarea', array(
-                    'label' => "詳細-サブコメント({$i})",
-                    'required' => false,
-                ))
-                ->add("sub_image{$i}", 'text', array(
-                    'label' => "詳細-サブ画像({$i})",
-                    'required' => false,
-                ))
-                ->add("sub_large_image{$i}", 'text', array(
-                    'label' => "詳細-サブ拡大画像({$i})",
-                    'required' => false,
-                ))
-            ;
-        }
+
     }
 
     /**
@@ -161,9 +165,6 @@ class ProductType extends AbstractType
      */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
-        $resolver->setDefaults(array(
-            'data_class' => 'Eccube\Entity\Product',
-        ));
     }
 
     /**
