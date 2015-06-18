97b345d3651a07d05a58fbdb83f75b44e324f09d
shuhei tomita <ssh.tomita@gmail.com>
2015-06-18 12:07:39 +0900

    merge commit

--- src/Eccube/Plugin/AbstractPluginManager.php
+++ src/Eccube/Plugin/AbstractPluginManager.php
@@ -2,10 +2,26 @@
 
 namespace Eccube\Plugin;
 
+use Doctrine\DBAL\Migrations\Migration;
+use Doctrine\DBAL\Migrations\Configuration\Configuration;
+
 class AbstractPluginManager {
 
-    public function 
-    {
-    }
+    CONST MIGRATION_TABLE_PREFIX = 'migration_';
 
+    public function migrationSchema($app,$migrationFilePath,$pluginCode,$version=null)
+    {
+        $config = new Configuration($app['db']);
+        $config->setMigrationsNamespace('DoctrineMigrations');
+        $config->setMigrationsDirectory($migrationFilePath);
+        $config->registerMigrationsFromDirectory($migrationFilePath );
+        $config->setMigrationsTableName(self::MIGRATION_TABLE_PREFIX.$pluginCode);
+        $migration = new Migration($config);
+                                  // null 又は 'last' を渡すと最新バージョンまでマイグレートする
+                                  // 0か'first'を渡すと最初に戻る
+        #throw new \Exception("$version");
+        $migration->migrate($version, false); 
+
+
+    }
 }
