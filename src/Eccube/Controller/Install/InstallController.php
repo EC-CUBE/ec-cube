9f35f06edc2900a3c667481d3cee34b872a2a115
shinichi-takahashi <shinichi_takahashi@lockon.co.jp>
2015-07-07 18:28:09 +0900

    bug fix install

--- src/Eccube/Controller/Install/InstallController.php
+++ src/Eccube/Controller/Install/InstallController.php
@@ -384,7 +384,7 @@ class InstallController
         ));
 
         $sth = $this->PDO->prepare("INSERT INTO dtb_member (member_id, login_id, password, salt, work, del_flg, authority, creator_id, rank, update_date, create_date,name,department) VALUES (2, :login_id, :admin_pass , :salt , '1', '0', '0', '1', '1', current_timestamp, current_timestamp,'管理者','EC-CUBE SHOP');");
-        $sth->execute(array('login_id' => $this->session_data['login_id'], ':admin_pass' => $encodedPassword, ':salt' => $salt));
+        $sth->execute(array(':login_id' => $this->session_data['login_id'], ':admin_pass' => $encodedPassword, ':salt' => $salt));
 
         $this->PDO->commit();
 
