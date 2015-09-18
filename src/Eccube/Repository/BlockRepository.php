5aa5323db94a07380b9b52be43edaaa93ab9042e
shuhei tomita <ssh.tomita@gmail.com>
2015-09-18 15:23:29 +0900

    コメントを削除

--- src/Eccube/Repository/BlockRepository.php
+++ src/Eccube/Repository/BlockRepository.php
@@ -159,7 +159,6 @@ class BlockRepository extends EntityRepository
      */
     public function getWriteTemplatePath($isUser = false)
     {
-        //return ($isUser) ? $this->app['config']['user_block_realdir'] : $this->app['config']['block_realdir'];
         return $this->app['config']['block_realdir'];
     }
 
