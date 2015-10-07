e33af049a2a590240d2029f3f83766d45f1d4e7d
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2015-10-07 15:57:07 +0900

    EntityUtilTest で EccubeTestCase を継承するよう修正

--- tests/Eccube/Tests/Util/EntityUtilTest.php
+++ tests/Eccube/Tests/Util/EntityUtilTest.php
@@ -2,6 +2,7 @@
 
 namespace Eccube\Tests\Util;
 
+use Eccube\Tests\EccubeTestCase;
 use Eccube\Application;
 use Eccube\Entity\Product;
 use Eccube\Entity\ProductClass;
@@ -14,58 +15,33 @@ use Eccube\Util\EntityUtil;
  *
  * @author Kentaro Ohkouchi
  */
-class EntityUtilTest extends \PHPUnit_Framework_TestCase
+class EntityUtilTest extends EccubeTestCase
 {
-    private $actual;
-    private $expected;
-
     private $Product;
     private $ProductClass;
     private $memberId;
     private $productClassId;
 
-    protected static $app = null;
-
-    public static function setUpBeforeClass()
+    public function setUp()
     {
-        // TODO Abstract にしたい.
-        $app = new Application();
-        $app['debug'] = true;
-        $app->initialize();
-        $app->initPluginEventDispatcher();
-        $app['session.test'] = true;
-        $app['exception_handler']->disable();
+        parent::setUp();
 
-        $app->boot();
-        $softDeleteFilter = $app['orm.em']->getFilters()->getFilter('soft_delete');
+        $softDeleteFilter = $this->app['orm.em']->getFilters()->getFilter('soft_delete');
         $softDeleteFilter->setExcludes(array(
             'Eccube\Entity\ProductClass'
         ));
         // soft_delete filter を有効にする
-        $config = $app['orm.em']->getConfiguration();
+        $config = $this->app['orm.em']->getConfiguration();
         $config->addFilter("soft_delete", '\Eccube\Doctrine\Filter\SoftDeleteFilter');
-        $app['orm.em']->getFilters()->enable('soft_delete');
-
-        self::$app = $app;
-    }
-
-    public static function tearDownAfterClass()
-    {
-        self::$app = null;
-    }
-
-    public function setUp()
-    {
-
-        self::$app['orm.em']->getConnection()->beginTransaction();
+        $this->app['orm.em']->getFilters()->enable('soft_delete');
 
         // eccube_install.sh で追加される Member
-        $Member = self::$app['eccube.repository.member']->find(2);
+        $Member = $this->app['eccube.repository.member']->find(2);
 
         $Product = new Product();
         $ProductClass = new ProductClass();
-        $Disp = self::$app['eccube.repository.master.disp']->find(\Eccube\Entity\Master\Disp::DISPLAY_HIDE);
-        $ProductType = self::$app['eccube.repository.master.product_type']->find(self::$app['config']['product_type_normal']);
+        $Disp = $this->app['eccube.repository.master.disp']->find(\Eccube\Entity\Master\Disp::DISPLAY_HIDE);
+        $ProductType = $this->app['eccube.repository.master.product_type']->find($this->app['config']['product_type_normal']);
         $Product
             ->setName('test')
             ->setCreator($Member)
@@ -84,18 +60,13 @@ class EntityUtilTest extends \PHPUnit_Framework_TestCase
         $ProductClass->setProductStock($ProductStock);
         $ProductStock->setProductClass($ProductClass);
 
-        self::$app['orm.em']->persist($Product);
-        self::$app['orm.em']->persist($ProductClass);
-        self::$app['orm.em']->persist($ProductStock);
-        self::$app['orm.em']->flush();
+        $this->app['orm.em']->persist($Product);
+        $this->app['orm.em']->persist($ProductClass);
+        $this->app['orm.em']->persist($ProductStock);
+        $this->app['orm.em']->flush();
 
         $this->Product = $Product;
         $this->ProductClass = $ProductClass;
-    }
-
-    public function tearDown()
-    {
-        self::$app['orm.em']->getConnection()->rollback();
     }
 
     /**
@@ -109,7 +80,7 @@ class EntityUtilTest extends \PHPUnit_Framework_TestCase
     public function testIsEmpty()
     {
         // migration で追加されるサンプル商品
-        $Product = self::$app['eccube.repository.product']->find(1);
+        $Product = $this->app['eccube.repository.product']->find(1);
         // migration で追加されるダミーの Member. del_flg = 1 の状態で INSERT されている
         $Member = $Product->getCreator();
         /*
@@ -122,7 +93,7 @@ class EntityUtilTest extends \PHPUnit_Framework_TestCase
     public function testIsEmptyWithFalse()
     {
         // setUp() で追加したサンプル商品
-        $Product = self::$app['eccube.repository.product']->find($this->Product->getId());
+        $Product = $this->app['eccube.repository.product']->find($this->Product->getId());
         // eccube_install.sh で追加される Member
         $Member = $Product->getCreator();
         /*
@@ -135,7 +106,7 @@ class EntityUtilTest extends \PHPUnit_Framework_TestCase
     public function testIsNotEmpty()
     {
         // migration で追加されるサンプル商品
-        $Product = self::$app['eccube.repository.product']->find(1);
+        $Product = $this->app['eccube.repository.product']->find(1);
         // migration で追加されるダミーの Member
         $Member = $Product->getCreator();
         /*
