f63d43ade58b05a4d6c5d8fb4922e38edde72ad2
Kiyoshi Yamamura <kiyoshi.yamamura@cule.jp>
2015-10-14 18:23:56 +0900

    開発環境時でメールの誤送信防止機能が正常に動作していないため修正

--- html/index_dev.php
+++ html/index_dev.php
@@ -70,6 +70,7 @@ $app['config'] = $app->share(function () use ($conf) {
 
     return array_replace_recursive($conf, $confarray);
 });
+// config_dev.ymlにmailが設定されていた場合、config_dev.ymlの設定内容を反映
 $app['swiftmailer.options'] = $app['config']['mail'];
 
 // Mail
