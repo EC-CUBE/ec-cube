ead548fff57853dc86b388008434df9de55768a6
unknown <yasumasa_yoshinaga@PC-0107.lockon.local>
2015-10-16 10:56:06 +0900

    ポスグレで落ちる箇所を修正

--- src/Eccube/Repository/CustomerRepository.php
+++ src/Eccube/Repository/CustomerRepository.php
@@ -144,18 +144,16 @@ class CustomerRepository extends EntityRepository implements UserProviderInterfa
             ->andWhere('c.del_flg = 0');
 
         if (!empty($searchData['multi']) && $searchData['multi']) {
-            //スペース除去
-            $clean_key_multi = preg_replace('/\s+|[　]+/u', '',$searchData['multi']);
-            if (preg_match('/^\d+$/', $clean_key_multi)) {
+            if (preg_match('/^\d+$/', $searchData['multi'])) {
                 $qb
                     ->andWhere('c.id = :customer_id')
-                    ->setParameter('customer_id', $clean_key_multi);
+                    ->setParameter('customer_id', $searchData['multi']);
             } else {
                 $qb
                     ->andWhere('CONCAT(c.name01, c.name02) LIKE :name OR CONCAT(c.kana01, c.kana02) LIKE :kana OR c.email LIKE :email')
-                    ->setParameter('name', '%' . $clean_key_multi . '%')
-                    ->setParameter('kana', '%' . $clean_key_multi . '%')
-                    ->setParameter('email', '%' . $clean_key_multi . '%');
+                    ->setParameter('name', '%' . $searchData['multi'] . '%')
+                    ->setParameter('kana', '%' . $searchData['multi'] . '%')
+                    ->setParameter('email', '%' . $searchData['multi'] . '%');
             }
         }
 
--- src/Eccube/Repository/ProductRepository.php
+++ src/Eccube/Repository/ProductRepository.php
@@ -88,6 +88,7 @@ class ProductRepository extends EntityRepository
     public function getQueryBuilderBySearchData($searchData)
     {
         $qb = $this->createQueryBuilder('p')
+            ->select('p,pc')
             ->distinct('p.id')
             ->andWhere('p.Status = 1');
 
