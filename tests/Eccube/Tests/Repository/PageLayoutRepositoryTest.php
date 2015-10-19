0b766f7f3a4bbe3ca31e16fdbe56fc6c74f02cf3
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2015-10-19 17:25:37 +0900

    PageLayoutRepository のテストケースを追加

--- tests/Eccube/Tests/Repository/PageLayoutRepositoryTest.php
+++ tests/Eccube/Tests/Repository/PageLayoutRepositoryTest.php
@@ -26,6 +26,7 @@ namespace Eccube\Tests\Repository;
 use Eccube\Tests\EccubeTestCase;
 use Eccube\Application;
 use Eccube\Entity\Master\DeviceType;
+use org\bovigo\vfs\vfsStream;
 
 class PageLayoutRepositoryTest extends EccubeTestCase
 {
@@ -40,28 +41,151 @@ class PageLayoutRepositoryTest extends EccubeTestCase
 
     public function test_findOrCreate_pageIdNullisCreate()
     {
-        $expected = null;
+        $this->expected = null;
         $PageLayout = $this->app['eccube.repository.page_layout']
             ->findOrCreate(null, $this->DeviceType);
-        $actual = $PageLayout->getUrl();
+        $this->actual = $PageLayout->getUrl();
 
-        $this->assertSame($actual, $expected);
+        $this->verify();
     }
 
     public function test_findOrCreate_findTopPage()
     {
-        $expected = array(
+        $this->expected = array(
             'url' => 'homepage',
             'DeviceType' => DeviceType::DEVICE_TYPE_PC,
         );
 
         $PageLayout = $this->app['eccube.repository.page_layout']
             ->findOrCreate(1, $this->DeviceType);
-        $actual = array(
+        $this->actual = array(
             'url' => $PageLayout->getUrl(),
             'DeviceType' => $PageLayout->getDeviceType()->getId(),
         );
 
-        $this->assertSame($actual, $expected);
+        $this->verify();
+    }
+
+    public function testFindUnusedBlocks()
+    {
+        $Blocks = $this->app['eccube.repository.page_layout']
+            ->findUnusedBlocks($this->DeviceType, 1);
+
+        $this->expected = 0;
+        $this->actual = count($Blocks);
+        $this->verify();
+    }
+
+    public function testGet()
+    {
+        $PageLayout = $this->app['eccube.repository.page_layout']
+            ->get($this->DeviceType, 1);
+
+        $this->expected = 1;
+        $this->actual = $PageLayout->getId();
+        $this->verify();
+        $this->assertNotNull($PageLayout->getBlockPositions());
+        foreach ($PageLayout->getBlockPositions() as $BlockPosition) {
+            $this->assertNotNull($BlockPosition->getBlock()->getId());
+        }
+    }
+
+    public function testGetByUrl()
+    {
+        $PageLayout = $this->app['eccube.repository.page_layout']
+            ->getByUrl($this->DeviceType, 'homepage');
+
+        $this->expected = 1;
+        $this->actual = $PageLayout->getId();
+        $this->verify();
+        $this->assertNotNull($PageLayout->getBlockPositions());
+        foreach ($PageLayout->getBlockPositions() as $BlockPosition) {
+            $this->assertNotNull($BlockPosition->getBlock()->getId());
+        }
+    }
+
+    public function testGetPageList()
+    {
+        $PageLayouts = $this->app['eccube.repository.page_layout']
+            ->getPageList($this->DeviceType);
+        $All = $this->app['eccube.repository.page_layout']->findAll();
+
+        $this->expected = count($All) - 1;
+        $this->actual = count($PageLayouts);
+        $this->verify();
+    }
+
+    public function testGetWriteTemplatePath()
+    {
+        $this->expected = $this->app['config']['template_realdir'];
+        $this->actual = $this->app['eccube.repository.page_layout']->getWriteTemplatePath();
+        $this->verify();
+    }
+    public function testGetWriteTemplatePathWithUser()
+    {
+        $this->expected = $this->app['config']['user_data_realdir'];
+        $this->actual = $this->app['eccube.repository.page_layout']->getWriteTemplatePath(true);
+        $this->verify();
+    }
+
+    public function testGetReadTemplateFile()
+    {
+        $fileName = 'example_page';
+        $root = vfsStream::setup('rootDir');
+        vfsStream::newDirectory('default');
+
+        // 一旦別の変数に代入しないと, config 以下の値を書きかえることができない
+        $config = $this->app['config'];
+        $config['template_realdir'] = vfsStream::url('rootDir');
+        $config['template_default_realdir'] = vfsStream::url('rootDir/default');
+        $this->app['config'] = $config;
+
+        file_put_contents($this->app['config']['template_realdir'].'/'.$fileName.'.twig', 'test');
+
+        $data = $this->app['eccube.repository.page_layout']->getReadTemplateFile($fileName);
+        // XXX 実装上は, tpl_data しか使っていない. 配列を返す意味がない
+        $this->actual = $data['tpl_data'];
+        $this->expected = 'test';
+        $this->verify();
+    }
+
+    public function testGetReadTemplateFileWithDefault()
+    {
+        $fileName = 'example_page';
+        $root = vfsStream::setup('rootDir');
+        mkdir(vfsStream::url('rootDir').'/default', 0777, true);
+
+        // 一旦別の変数に代入しないと, config 以下の値を書きかえることができない
+        $config = $this->app['config'];
+        $config['template_realdir'] = vfsStream::url('rootDir');
+        $config['template_default_realdir'] = vfsStream::url('rootDir/default');
+        $this->app['config'] = $config;
+
+        file_put_contents($this->app['config']['template_default_realdir'].'/'.$fileName.'.twig', 'test');
+
+        $data = $this->app['eccube.repository.page_layout']->getReadTemplateFile($fileName);
+        // XXX 実装上は, tpl_data しか使っていない. 配列を返す意味がない
+        $this->actual = $data['tpl_data'];
+        $this->expected = 'test';
+        $this->verify();
+    }
+
+    public function testGetReadTemplateFileWithUser()
+    {
+        $fileName = 'example_page';
+        $root = vfsStream::setup('rootDir');
+
+        // 一旦別の変数に代入しないと, config 以下の値を書きかえることができない
+        $config = $this->app['config'];
+        $config['user_data_realdir'] = vfsStream::url('rootDir');
+        $this->app['config'] = $config;
+
+        file_put_contents($this->app['config']['user_data_realdir'].'/'.$fileName.'.twig', 'test');
+
+        $data = $this->app['eccube.repository.page_layout']->getReadTemplateFile($fileName, true);
+        // XXX 実装上は, tpl_data しか使っていない. 配列を返す意味がない
+        $this->actual = $data['tpl_data'];
+        $this->expected = 'test';
+        $this->verify();
     }
 }
