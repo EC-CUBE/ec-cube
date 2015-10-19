eedbd32cc7efe3a76aebd918feb3e5a1e8217592
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2015-10-19 16:45:39 +0900

    未使用のテストを削除
    - EccubeTestCase を継承するよう変更

--- tests/Eccube/Tests/Repository/PageLayoutRepositoryTest.php
+++ tests/Eccube/Tests/Repository/PageLayoutRepositoryTest.php
@@ -23,34 +23,26 @@
 
 namespace Eccube\Tests\Repository;
 
+use Eccube\Tests\EccubeTestCase;
 use Eccube\Application;
 use Eccube\Entity\Master\DeviceType;
 
-class PageLayoutRepositoryTest extends AbstractRepositoryTestCase
+class PageLayoutRepositoryTest extends EccubeTestCase
 {
+    protected $DeviceType;
 
-/* privateなMethodにしたのでテストは別途考える
-    public function test_getNewPageId()
+    public function setUp()
     {
-        $app = $this->createApplication();
-
-        $actual = $app['eccube.repository.page_layout']
-            ->getNewPageId($app['config']['device_type_pc']);
-        $expected = 29;
-        $this->assertSame($actual, $expected);
-
+        parent::setUp();
+        $this->DeviceType = $this->app['eccube.repository.master.device_type']
+            ->find(DeviceType::DEVICE_TYPE_PC);
     }
-*/
 
     public function test_findOrCreate_pageIdNullisCreate()
     {
-        $app = $this->createApplication();
-
         $expected = null;
-        $DeviceType = $app['eccube.repository.master.device_type']
-            ->find(DeviceType::DEVICE_TYPE_PC);
-        $PageLayout = $app['eccube.repository.page_layout']
-            ->findOrCreate(null, $DeviceType);
+        $PageLayout = $this->app['eccube.repository.page_layout']
+            ->findOrCreate(null, $this->DeviceType);
         $actual = $PageLayout->getUrl();
 
         $this->assertSame($actual, $expected);
@@ -58,90 +50,18 @@ class PageLayoutRepositoryTest extends AbstractRepositoryTestCase
 
     public function test_findOrCreate_findTopPage()
     {
-        $app = $this->createApplication();
-
         $expected = array(
             'url' => 'homepage',
             'DeviceType' => DeviceType::DEVICE_TYPE_PC,
         );
 
-        $DeviceType = $app['eccube.repository.master.device_type']
-            ->find(DeviceType::DEVICE_TYPE_PC);
-        $PageLayout = $app['eccube.repository.page_layout']
-            ->findOrCreate(1, $DeviceType);
+        $PageLayout = $this->app['eccube.repository.page_layout']
+            ->findOrCreate(1, $this->DeviceType);
         $actual = array(
             'url' => $PageLayout->getUrl(),
             'DeviceType' => $PageLayout->getDeviceType()->getId(),
         );
 
         $this->assertSame($actual, $expected);
-    }
-
-    /*public function test_findOrCreate_findMobileMyPage()
-    {
-        // 非対応予定のためスキップ
-        self::markTestSkipped();
-
-        $app = $this->createApplication();
-
-        $expected = array(
-            'url' => 'mypage/index.php',
-            'device_type_id' => $app['config']['device_type_mobile'],
-        );
-
-        $PageLayout = $app['eccube.repository.page_layout']
-            ->findOrCreate(6, $app['config']['device_type_mobile']);
-        $actual = array(
-            'url' => $PageLayout->getUrl(),
-            'device_type_id' => $PageLayout->getDeviceTypeId(),
-        );
-
-        $this->assertSame($actual, $expected);
-    }*/
-
-    public function test_findOrCreate_findSmartphoneProduct()
-    {
-        // 非対応予定のためスキップ
-        self::markTestSkipped();
-
-        $app = $this->createApplication();
-
-        $expected = array(
-            'url' => 'products/list.php',
-            'DeviceType' => DeviceType::DEVICE_TYPE_SP,
-        );
-
-        $PageLayout = $app['eccube.repository.page_layout']
-            ->findOrCreate(2, DeviceType::DEVICE_TYPE_SP);
-        $actual = array(
-            'url' => $PageLayout->getUrl(),
-            'DeviceType' => $PageLayout->getDeviceType()->getId(),
-        );
-
-        $this->assertSame($actual, $expected);
-    }
-
-    /* FIXME: CI環境で定数が整っていないのでコケるひとまずコメントアウト
-    public function test_getTemplateFile_DefaultTemplateFile_isValid()
-    {
-        $app = $this->createApplication();
-
-        $actual = $app['eccube.repository.page_layout']
-            ->getTemplateFile('mypage/change', 10);
-
-        $expected = array(
-            'file_name' =>'change.tpl',
-            'tpl_data' => file_get_contents($app['config']['template_realdir'] . 'mypage/change.tpl')
-        );
-
-        $this->assertSame($actual, $expected);
-    }
-    */
-
-    public function tearDown()
-    {
-        $app = $this->createApplication();
-        $app['orm.em']->getConnection()->close();
-        parent::tearDown();
     }
 }
