4848f2c83437b6c81ec2b60e153644b7bd741b97
Kiyoshi Yamamura <kiyoshi.yamamura@cule.jp>
2015-10-26 10:55:33 +0900

    page_layoutにデータ追加・修正、不要なurl宣言を削除

--- src/Eccube/Resource/doctrine/migration/Version20151023102323.php
+++ src/Eccube/Resource/doctrine/migration/Version20151023102323.php
@@ -4,6 +4,7 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
+use Eccube\Entity\PageLayout;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -17,32 +18,86 @@ class Version20151023102323 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
 
-        if ($this->connection->getDatabasePlatform()->getName() == "postgresql") {
-            // postfresqlの場合、pege_idを指 定する
-            $this->addSql("INSERT INTO dtb_page_layout (page_id, device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (nextval('dtb_page_layout_page_id_seq'), 10, '商品購入/お届け先の追加', 'shopping_shipping_edit', 'Shopping/shipping_edit', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'noindex') RETURNING page_id;");
-            $this->addSql("INSERT INTO dtb_page_layout (page_id, device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (nextval('dtb_page_layout_page_id_seq'), 10, '商品購入/お届け先の複数指定(お届け先の追加)', 'shopping_shipping_multiple_edit', 'Shopping/shipping_multiple_edit', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'noindex') RETURNING page_id;");
-            $this->addSql("INSERT INTO dtb_page_layout (page_id, device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (nextval('dtb_page_layout_page_id_seq'), 10, '商品購入/購入エラー', 'shopping_error', 'Shopping/shopping_error', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'noindex') RETURNING page_id;");
-            $this->addSql("INSERT INTO dtb_page_layout (page_id, device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (nextval('dtb_page_layout_page_id_seq'), 10, 'ご利用ガイド', 'help_guide', 'Help/guide', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL) RETURNING page_id;");
-            $this->addSql("INSERT INTO dtb_page_layout (page_id, device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (nextval('dtb_page_layout_page_id_seq'), 10, 'パスワード再発行(入力ページ)', 'forgot', 'Forgot/index', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL) RETURNING page_id;");
-            $this->addSql("INSERT INTO dtb_page_layout (page_id, device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (nextval('dtb_page_layout_page_id_seq'), 10, 'パスワード再発行(完了ページ)', 'forgot_complete', 'Forgot/complete', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL) RETURNING page_id;");
-            $this->addSql("INSERT INTO dtb_page_layout (page_id, device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (nextval('dtb_page_layout_page_id_seq'), 10, 'パスワード変更(完了ページ)', 'forgot_reset', 'Forgot/reset', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL) RETURNING page_id;");
-        } else {
-            // postgresql以外の場合、pege_idはauto increment項目なので指定しない
-            $this->addSql("INSERT INTO dtb_page_layout (device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (10, '商品購入/お届け先の追加', 'shopping_shipping_edit', 'Shopping/shipping_edit', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'noindex');");
-            $this->addSql("INSERT INTO dtb_page_layout (device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (10, '商品購入/お届け先の複数指定(お届け先の追加)', 'shopping_shipping_multiple_edit', 'Shopping/shipping_multiple_edit', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'noindex');");
-            $this->addSql("INSERT INTO dtb_page_layout (device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (10, '商品購入/購入エラー', 'shopping_error', 'Shopping/shopping_error', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'noindex');");
-            $this->addSql("INSERT INTO dtb_page_layout (device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (10, 'ご利用ガイド', 'help_guide', 'Help/guide', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);");
-            $this->addSql("INSERT INTO dtb_page_layout (device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (10, 'パスワード再発行(入力ページ)', 'forgot', 'Forgot/index', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);");
-            $this->addSql("INSERT INTO dtb_page_layout (device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (10, 'パスワード再発行(完了ページ)', 'forgot_complete', 'Forgot/complete', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);");
-            $this->addSql("INSERT INTO dtb_page_layout (device_type_id, page_name, url, file_name, edit_flg, author, description, keyword, update_url, create_date, update_date, meta_robots) VALUES (10, 'パスワード変更(完了ページ)', 'forgot_reset', 'Forgot/reset', 2, NULL, NULL, NULL, NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, NULL);");
-        }
 
+        // pageを追加
+        $app = new \Eccube\Application();
+        $app->initialize();
+        $app->boot();
+        $em = $app['orm.em'];
+
+        $DeviceType = $app['eccube.repository.master.device_type']->find(10);
+
+        $PageLayout = new PageLayout();
+        $PageLayout->setDeviceType($DeviceType);
+        $PageLayout->setName( '商品購入/お届け先の追加');
+        $PageLayout->setUrl('shopping_shipping_edit');
+        $PageLayout->setFileName('Shopping/shipping_edit');
+        $PageLayout->setEditFlg(2);
+        $PageLayout->setMetaRobots('noindex');
+        $em->persist($PageLayout);
+
+        $PageLayout = new PageLayout();
+        $PageLayout->setDeviceType($DeviceType);
+        $PageLayout->setName( '商品購入/お届け先の複数指定(お届け先の追加)');
+        $PageLayout->setUrl('shopping_shipping_multiple_edit');
+        $PageLayout->setFileName('Shopping/shipping_multiple_edit');
+        $PageLayout->setEditFlg(2);
+        $PageLayout->setMetaRobots('noindex');
+        $em->persist($PageLayout);
+
+        $PageLayout = new PageLayout();
+        $PageLayout->setDeviceType($DeviceType);
+        $PageLayout->setName( '商品購入/購入エラー');
+        $PageLayout->setUrl('shopping_error');
+        $PageLayout->setFileName('Shopping/shopping_error');
+        $PageLayout->setEditFlg(2);
+        $PageLayout->setMetaRobots('noindex');
+        $em->persist($PageLayout);
+
+        $PageLayout = new PageLayout();
+        $PageLayout->setDeviceType($DeviceType);
+        $PageLayout->setName( 'ご利用ガイド');
+        $PageLayout->setUrl('help_guide');
+        $PageLayout->setFileName('Help/guide');
+        $PageLayout->setEditFlg(2);
+        $em->persist($PageLayout);
+
+        $PageLayout = new PageLayout();
+        $PageLayout->setDeviceType($DeviceType);
+        $PageLayout->setName( 'パスワード再発行(入力ページ)');
+        $PageLayout->setUrl('forgot');
+        $PageLayout->setFileName('Forgot/index');
+        $PageLayout->setEditFlg(2);
+        $em->persist($PageLayout);
+
+        $PageLayout = new PageLayout();
+        $PageLayout->setDeviceType($DeviceType);
+        $PageLayout->setName( 'パスワード再発行(完了ページ)');
+        $PageLayout->setUrl('forgot_complete');
+        $PageLayout->setFileName('Forgot/complete');
+        $PageLayout->setEditFlg(2);
+        $PageLayout->setMetaRobots('noindex');
+        $em->persist($PageLayout);
+
+        $PageLayout = new PageLayout();
+        $PageLayout->setDeviceType($DeviceType);
+        $PageLayout->setName( 'パスワード変更((完了ページ)');
+        $PageLayout->setUrl('forgot_reset');
+        $PageLayout->setFileName('Forgot/reset');
+        $PageLayout->setEditFlg(2);
+        $PageLayout->setMetaRobots('noindex');
+        $em->persist($PageLayout);
+
+        $em->flush();
+
+        // 文言、URLの修正
         $this->addSql("UPDATE dtb_page_layout  SET url = 'help_agreement', file_name = 'Help/agreement' WHERE file_name = 'Entry/kiyaku';");
         $this->addSql("UPDATE dtb_page_layout  SET url = 'shopping_shipping_multiple', file_name = 'Shopping/shipping_multiple' WHERE file_name = 'Shopping/multiple';");
         $this->addSql("UPDATE dtb_page_layout  SET page_name = '商品購入' WHERE page_name = '商品購入/ログイン';");
         $this->addSql("UPDATE dtb_page_layout  SET page_name = 'MYページ/お届け先一覧' WHERE page_name = 'MYページ/お届け先変更';");
         $this->addSql("UPDATE dtb_page_layout  SET file_name = 'Mypage/delivery_edit' WHERE page_name = 'MYページ/お届け先追加';");
 
+        // 不要なレコードを削除
         $this->addSql("DELETE from dtb_page_layout  WHERE file_name = 'Shopping/payment';");
         $this->addSql("DELETE from dtb_page_layout  WHERE file_name = 'Shopping/confirm';");
     }
