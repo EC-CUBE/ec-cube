531588658add9648d9ba9fbf396a72a926aa2827
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2015-10-26 19:55:06 +0900

    test追加

--- tests/Eccube/Tests/Repository/ProductRepositoryGetQueryBuilderBySearchDataAdminTest.php
+++ tests/Eccube/Tests/Repository/ProductRepositoryGetQueryBuilderBySearchDataAdminTest.php
@@ -235,4 +235,25 @@ class ProductRepositoryGetQueryBuilderBySearchDataAdminTest extends AbstractProd
         $this->actual = count($this->Results);
         $this->verify();
     }
+
+    public function testProductImage()
+    {
+        $this->searchData = array();
+
+        $this->scenario();
+
+        $Products = $this->Results;
+
+        foreach ($Products as $Product) {
+            $this->expected = array(0, 1, 2);
+            $this->actual = array();
+
+            $ProductImages = $Product->getProductImage();
+            foreach ($ProductImages as $ProductImage) {
+                $this->actual[] = $ProductImage->getRank();
+            }
+
+            $this->verify();
+        }
+    }
 }
