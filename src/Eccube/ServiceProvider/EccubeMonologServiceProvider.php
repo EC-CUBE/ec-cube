68e8274708435b7124f3c4deb586b8382c233adb
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2015-11-12 17:24:21 +0900

    site.log がローテーションされないのを修正(#1170)
    - EccubeMonologServiceProvider で Monolog の設定をするよう変更

---
+++ src/Eccube/ServiceProvider/EccubeMonologServiceProvider.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Eccube\ServiceProvider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\FingersCrossed\ErrorLevelActivationStrategy;
+use Monolog\Handler\FingersCrossedHandler;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+
+/**
+ * MonologServiceProvider for EC-CUBE.
+ *
+ * @author Kentaro Ohkouchi
+ */
+class EccubeMonologServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app->register(new \Silex\Provider\MonologServiceProvider());
+        $app['monolog.handler'] = function() use ($app) {
+            $levels = Logger::getLevels();
+            if ($app['debug']) {
+                $level = Logger::DEBUG;
+            } else {
+                $level = $app['config']['log']['log_level'];
+            }
+
+            $RotateHandler = new RotatingFileHandler($app['monolog.logfile'], $app['config']['log']['max_files'], $level);
+            $RotateHandler->setFilenameFormat(
+                $app['config']['log']['prefix'].'{date}'.$app['config']['log']['suffix'],
+                $app['config']['log']['format']
+            );
+            $RotateHandler->setFormatter(new LineFormatter(null, null, true));
+            $FingerCrossedHandler = new FingersCrossedHandler(
+                $RotateHandler,
+                new ErrorLevelActivationStrategy($levels[$app['config']['log']['action_level']])
+            );
+            return $FingerCrossedHandler;
+        };
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
