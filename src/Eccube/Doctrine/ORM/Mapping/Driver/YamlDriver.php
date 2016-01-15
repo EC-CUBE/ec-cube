dca5f4d46c2733abd2b7a4196b58d482876f07fb
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2016-01-15 20:19:14 +0900

    php7対応.

---
+++ src/Eccube/Doctrine/ORM/Mapping/Driver/YamlDriver.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace Eccube\Doctrine\ORM\Mapping\Driver;
+
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * The YamlDriver reads the mapping metadata from yaml schema files.
+ *
+ * YamlDriverのPHP7対応. Doctrine2.4で修正されれば不要.
+ *
+ * @see https://github.com/EC-CUBE/ec-cube/issues/1338
+ * @package Eccube\Doctrine\ORM\Mapping\Driver
+ */
+class YamlDriver extends \Doctrine\ORM\Mapping\Driver\YamlDriver
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function loadMappingFile($file)
+    {
+        return Yaml::parse(file_get_contents($file));
+    }
+}
