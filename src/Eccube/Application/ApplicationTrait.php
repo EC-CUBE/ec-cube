d8b686e1701ac2b33a649337a7df6bcc9fce76fa
Kiyoshi Yamamura <kiyoshi.yamamura@cule.jp>
2016-01-25 18:08:53 +0900

    軽微な不具合修正

--- src/Eccube/Application/ApplicationTrait.php
+++ src/Eccube/Application/ApplicationTrait.php
@@ -262,7 +262,7 @@ class ApplicationTrait extends \Silex\Application
      * @param string $view The view name
      * @param array $parameters An array of parameters to pass to the view
      *
-     * @return Response A Response instance
+     * @return string The rendered view
      */
     public function renderView($view, array $parameters = array())
     {
