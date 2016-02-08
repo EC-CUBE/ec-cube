8a801608208db0adc8d0386d42464e9b240602d6
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2016-02-08 17:25:56 +0900

    modify hook points

--- src/Eccube/Controller/Mypage/ChangeController.php
+++ src/Eccube/Controller/Mypage/ChangeController.php
@@ -26,18 +26,18 @@ namespace Eccube\Controller\Mypage;
 
 use Eccube\Application;
 use Eccube\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Eccube\Event\EccubeEvents;
 use Eccube\Event\EventArgs;
+use Symfony\Component\HttpFoundation\Request;
 
 class ChangeController extends AbstractController
 {
     /**
-     * Index
-     * 
+     * 会員情報編集画面.
+     *
      * @param Application $app
      * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
     public function index(Application $app, Request $request)
     {
@@ -51,26 +51,21 @@ class ChangeController extends AbstractController
         /* @var $builder \Symfony\Component\Form\FormBuilderInterface */
         $builder = $app['form.factory']->createBuilder('entry', $Customer);
 
-        /* @var $form \Symfony\Component\Form\FormInterface */
-        $form = $builder->getForm();
         $event = new EventArgs(
             array(
-                'form' => $form,
+                'builder' => $builder,
                 'customer' => $Customer
             ),
             $request
         );
-        $app['eccube.event.dispatcher']->dispatch(EccubeEvents::FRONT_CHANGE_INDEX_INITIALIZE, $event);
+        $app['eccube.event.dispatcher']->dispatch(EccubeEvents::FRONT_MYPAGE_CHANGE_INDEX_INITIALIZE, $event);
+
+        /* @var $form \Symfony\Component\Form\FormInterface */
+        $form = $builder->getForm();
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $event = new EventArgs(
-                array(
-                    'form' => $form,
-                    'customer' => $Customer
-                ),
-                $request
-            );
-            $app['eccube.event.dispatcher']->dispatch(EccubeEvents::FRONT_CHANGE_INDEX_COMPLETE, $event);
+
             if ($Customer->getPassword() === $app['config']['default_password']) {
                 $Customer->setPassword($previous_password);
             } else {
@@ -79,6 +74,15 @@ class ChangeController extends AbstractController
                 );
             }
             $app['orm.em']->flush();
+
+            $event = new EventArgs(
+                array(
+                    'form' => $form,
+                    'customer' => $Customer
+                ),
+                $request
+            );
+            $app['eccube.event.dispatcher']->dispatch(EccubeEvents::FRONT_MYPAGE_CHANGE_INDEX_COMPLETE, $event);
 
             return $app->redirect($app->url('mypage_change_complete'));
         }
@@ -91,7 +95,7 @@ class ChangeController extends AbstractController
     }
 
     /**
-     * Complete
+     * 会員情報編集完了画面.
      *
      * @param Application $app
      * @param Request $request
