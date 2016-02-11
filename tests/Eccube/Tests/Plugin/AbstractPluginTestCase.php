14228d550484b9187bd18c9d4028b175ee16bf20
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2016-02-11 10:58:35 +0900

    カートのテストを追加

---
+++ tests/Eccube/Tests/Plugin/AbstractPluginTestCase.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+namespace Eccube\Tests\Plugin;
+
+use Eccube\Tests\EccubeTestCase;
+use Symfony\Component\Filesystem\Filesystem;
+
+abstract class AbstractPluginTestCase extends EccubeTestCase
+{
+    protected $client;
+
+    // テスト用プラグイン.
+    protected $from = __DIR__.'/HookPoint';
+
+    // テスト用プラグイン設置先.
+    protected $to = __DIR__.'/../../../../app/Plugin/HookPoint';
+
+    public function setUp()
+    {
+        // テスト用プラグインを設置.
+        $fs = new Filesystem();
+        $fs->mirror($this->from, $this->to);
+
+        parent::setUp();
+        $this->client = $this->createClient();
+    }
+
+    public function tearDown()
+    {
+        // テスト用プラグインを削除.
+        $fs = new Filesystem();
+        $fs->remove($this->to);
+
+        parent::tearDown();
+        $this->client = null;
+    }
+
+    /**
+     * echo等で出力される内容の検証.
+     *
+     * @param array $expected
+     * @param string $message
+     */
+    public function verifyOutputString(array $expected, $message = '')
+    {
+        $expected = implode('', $expected);
+
+        $this->expectOutputString($expected, $message);
+    }
+}
