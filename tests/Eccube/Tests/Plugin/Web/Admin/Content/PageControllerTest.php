fc5c29331d318bab43761fdc6f68b96e2eea5ee6
ryo-endo <to.endo.dev@gmail.com>
2016-02-12 13:33:08 +0900

    管理画面：ページコントローラのテストケースを追加。

---
+++ tests/Eccube/Tests/Plugin/Web/Admin/Content/PageControllerTest.php
@@ -0,0 +1,77 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+
+namespace Eccube\Tests\Plugin\Web\Admin\Content;
+
+use Eccube\Event\EccubeEvents;
+use Eccube\Tests\Plugin\Web\Admin\AbstractAdminWebTestCase;
+
+class PageControllerTest extends AbstractAdminWebTestCase
+{
+
+    public function test_routing_AdminContentPage_index()
+    {
+        $this->client->request('GET', $this->app->url('admin_content_page'));
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+
+        $hookpoins = array(
+            EccubeEvents::ADMIN_CONTENT_PAGE_INDEX_COMPLETE,
+        );
+        $this->verifyOutputString($hookpoins);
+    }
+
+    public function test_routing_AdminContentPage_edit()
+    {
+        $this->client->request('GET',
+            $this->app->url(
+                'admin_content_page_edit',
+                array('id' => 1)
+            )
+        );
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+
+        $hookpoins = array(
+            EccubeEvents::ADMIN_CONTENT_PAGE_EDIT_INITIALIZE,
+        );
+        $this->verifyOutputString($hookpoins);
+    }
+
+    public function test_routing_AdminContentPage_delete()
+    {
+
+        $redirectUrl = $this->app->url('admin_content_page');
+
+        $this->client->request('DELETE',
+            $this->app->url(
+                'admin_content_page_delete',
+                array('id' => 1)
+            )
+        );
+
+        $actual = $this->client->getResponse()->isRedirect($redirectUrl);
+
+        $this->assertTrue($actual);
+    }
+
+}
