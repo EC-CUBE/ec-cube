b2ac6128c77d11fe7706b78565484a2bbb43dbb5
Hama <hama@daisy.link>
2016-09-30 13:00:16 +0900

    DQLでEXTRACTの構文チェック強化及びsqlite対応 #1794

--- src/Eccube/Application.php
+++ src/Eccube/Application.php
@@ -479,7 +479,7 @@ class Application extends ApplicationTrait
         $this->register(new \Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider(), array(
             'orm.proxies_dir' => __DIR__.'/../../app/cache/doctrine/proxies',
             'orm.em.options' => $options,
-            'orm.custom.functions.string' => array(
+            'orm.custom.functions.numeric' => array(
                 'EXTRACT' => 'Eccube\Doctrine\ORM\Query\Extract',
             ),
         ));
--- src/Eccube/Doctrine/ORM/Query/Extract.php
+++ src/Eccube/Doctrine/ORM/Query/Extract.php
@@ -29,13 +29,25 @@ use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 
 /**
- * OK:
- *  - EXTRACT(DATE FROM p.create_date)
- *  - EXTRACT(YEAR FROM TIMESTAMP '2016-09-29')
- *
- * NG:
- *  - EXTRACT(YEAR_MONTH FROM p.create_date) YEAR_MONTHはPostgreSQLで使えない
- *  - EXTRACT(MONTH FROM INTERVAL '3 MONTHS') INTERVALはMySQLで使えない
+ * EXTRACT (field FROM [type] source)
+ *  field:
+ *      単位指定子
+ *          - YEAR
+ *          - MONTH
+ *          - DAY
+ *          - HOUR
+ *          - MINUTE
+ *          - SECOND
+ *          - WEEK
+ *  type:
+ *      日付/時刻データ型
+ *      sourceが文字列の場合のみ必須
+ *          - TIMESTAMP
+ *          - DATE
+ *          - TIME
+ *  source:
+ *      日付/時刻データ型、もしくは日付/時刻を表す文字列
+ *      文字列の場合はtypeが必須
  *
  * @package Eccube\Doctrine\ORM\Query
  */
@@ -45,19 +57,45 @@ class Extract extends FunctionNode
     protected $type;
     protected $source;
 
+    protected $formats = array(
+        'YEAR'      => '%Y',
+        'MONTH'     => '%m',
+        'DAY'       => '%d',
+        'HOUR'      => '%H',
+        'MINUTE'    => '%M',
+        'SECOND'    => '%S',
+        'WEEK'      => '%W',
+    );
+
+    protected $dateTimeTypes = array(
+        'TIMESTAMP',
+        'DATE',
+        'TIME',
+    );
+
     public function parse(Parser $parser)
     {
         $lexer = $parser->getLexer();
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $upperField = strtoupper($lexer->lookahead['value']);
+        if ($lexer->lookahead['type'] !== Lexer::T_IDENTIFIER || !isset($this->formats[$upperField])) {
+            $parser->syntaxError(implode('/', array_keys($this->formats)));
+        }
+
         $parser->match(Lexer::T_IDENTIFIER);
-        $this->field = $lexer->token['value'];
+        $this->field = $upperField;
         $parser->match(Lexer::T_FROM);
 
         $next = $lexer->glimpse();
         if (isset($next['type']) && $next['type'] === Lexer::T_STRING) {
+            $upperType = strtoupper($lexer->lookahead['value']);
+            if ($lexer->lookahead['type'] !== Lexer::T_IDENTIFIER || !in_array($upperType, $this->dateTimeTypes, true)) {
+                $parser->syntaxError(implode('/', $this->dateTimeTypes));
+            }
             $parser->match(Lexer::T_IDENTIFIER);
-            $this->type = $lexer->token['value'];
+            $this->type = $upperType;
         }
 
         $this->source = $parser->ArithmeticPrimary();
@@ -66,6 +104,11 @@ class Extract extends FunctionNode
 
     public function getSql(SqlWalker $sqlWalker)
     {
-        return sprintf('EXTRACT(%s FROM %s %s)', $this->field, (string)$this->type, $this->source->dispatch($sqlWalker));
+        $driver = $sqlWalker->getConnection()->getDriver()->getName();
+        if ($driver == 'pdo_sqlite') {
+            return sprintf("CAST(STRFTIME('%s', %s) AS INTEGER)", $this->formats[$this->field], $this->source->dispatch($sqlWalker));
+        } else {
+            return sprintf('EXTRACT(%s FROM %s %s)', $this->field, (string)$this->type, $this->source->dispatch($sqlWalker));
+        }
     }
 }
