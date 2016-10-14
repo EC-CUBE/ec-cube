be22e0764ca8075ee8ecff7d99a485c86ab06859
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-10-14 16:47:25 +0900

    商品一覧の GROUP BY で不要なキーを対象としている(#1838)

--- src/Eccube/Repository/ProductRepository.php
+++ src/Eccube/Repository/ProductRepository.php
@@ -109,7 +109,7 @@ class ProductRepository extends EntityRepository
             //@see http://doctrine-orm.readthedocs.org/en/latest/reference/dql-doctrine-query-language.html
             $qb->addSelect('MIN(pc.price02) as HIDDEN price02_min');
             $qb->innerJoin('p.ProductClasses', 'pc');
-            $qb->groupBy('p');
+            $qb->groupBy('p.id');
             $qb->orderBy('price02_min', 'ASC');
             // 新着順
         } else if (!empty($searchData['orderby']) && $searchData['orderby']->getId() == '2') {
