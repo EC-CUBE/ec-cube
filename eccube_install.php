382403d820481407e62b38038d76fba2cab10eed
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-10-17 13:13:45 +0900

    オプション追加とリファクタリング
    - 以下のオプションを追加
      - --no-createdb
      - -v, --version
      - -V, --verbose
    - 未定義変数の削除
    - dtb_baseinfo.shop_name の修正

--- eccube_install.php
+++ eccube_install.php
@@ -20,8 +20,15 @@ $argv[2] = isset($argv[2]) ? $argv[2] : null;
 
 if (in_array('--help', $argv) || empty($argv[1])) {
     displayHelp($argv);
-    exit(1);
+    exit(0);
 }
+
+if (in_array('-v', $argv) || in_array('--version', $argv)) {
+    require __DIR__.'/src/Eccube/Common/Constant.php';
+    echo 'EC-CUBE '.Eccube\Common\Constant::VERSION.PHP_EOL;
+    exit(0);
+}
+
 out('EC-CUBE3 installer use database driver of ', null, false);
 
 $database_driver = 'pdo_sqlite';
@@ -40,6 +47,12 @@ switch($argv[1]) {
 }
 out($database_driver);
 
+initializeDefaultVariables($database_driver);
+
+if (in_array('-V', $argv) || in_array('--verbose', $argv)) {
+    displayEnvironmentVariables();
+}
+
 $database = getDatabaseConfig($database_driver);
 $connectionParams = $database['database'];
 
@@ -49,12 +62,15 @@ if ($argv[2] != 'none') {
 }
 
 createConfigFiles($database_driver);
-createDatabase($connectionParams);
+
+if (!in_array('--skip-createdb', $argv)) {
+    createDatabase($connectionParams);
+}
 
 $app = createApplication();
 initializeDatabase($app);
 out('EC-CUBE3 install finished successfully!', 'success');
-if (PHP_VERSION_ID >= 50400) {
+if (PHP_VERSION_ID >= 50400 && empty(getenv('ROOT_URLPATH'))) {
     out('PHP built-in web server to run applications, `php -S localhost:8080 -t html`', 'info');
     out('Open your browser and access the http://localhost:8080/', 'info');
 }
@@ -75,28 +91,95 @@ Arguments[2]:
 Specifying the "none" to skip the installation of Composer
 
 Options:
+-v, --version        print ec-cube version
+-V, --verbose        enable verbose output
+--skip-createdb      skip to create database
 --help               this help
 --ansi               force ANSI color output
 --no-ansi            disable ANSI color output
 
 Environment variables:
-ADMIN_USER=admin
-ADMIN_PASS=password
-ADMIN_MAIL=admin@example.com
-SHOP_NAME=EC-CUBE SHOP
-ADMIN_ROUTE=admin
-DBSERVER=127.0.0.1
-DBNAME=cube3_dev
-DBUSER=cube3_dev_user
-DBPASS=password
-DBPORT=<database port>
-MAIL_BACKEND=smtp
-MAIL_HOST=localhost
-MAIL_PORT=25
-MAIL_USER=<SMTP AUTH user>
-MAIL_PASS=<SMTP AUTH password>
 
 EOF;
+    foreach (getExampleVariables() as $name => $value) {
+        echo $name.'='.$value.PHP_EOL;
+    }
+}
+
+function initializeDefaultVariables($database_driver)
+{
+    switch ($database_driver) {
+        case 'pdo_pgsql':
+            putenv('ROOTUSER='.(getenv('ROOTUSER') ? getenv('ROOTUSER') : (getenv('DBUSER') ? getenv('DBUSER') : 'postgres')));
+            putenv('ROOTPASS='.(getenv('ROOTPASS') ? getenv('ROOTPASS') : (getenv('DBPASS') ? getenv('DBPASS') : 'password')));
+            putenv('DBSERVER='.(getenv('DBSERVER') ? getenv('DBSERVER') : 'localhost'));
+            putenv('DBNAME='.(getenv('DBNAME') ? getenv('DBNAME') : 'cube3_dev'));
+            putenv('DBUSER='.(getenv('DBUSER') ? getenv('DBUSER') : 'cube3_dev_user'));
+            putenv('DBPORT='.(getenv('DBPORT') ? getenv('DBPORT') : '5432'));
+            putenv('DBPASS='.(getenv('DBPASS') ? getenv('DBPASS') : 'password'));
+            break;
+        case 'pdo_mysql':
+            putenv('ROOTUSER='.(getenv('ROOTUSER') ? getenv('ROOTUSER') : (getenv('DBUSER') ? getenv('DBUSER') : 'root')));
+            if (getenv('TRAVIS')) {
+                putenv('DBPASS=');
+                putenv('ROOTPASS=');
+            } else {
+                putenv('DBPASS='.(getenv('DBPASS') ? getenv('DBPASS') : 'password'));
+                putenv('ROOTPASS='.(getenv('ROOTPASS') ? getenv('ROOTPASS') : (getenv('DBPASS') ? getenv('DBPASS') : 'password')));
+            }
+            putenv('DBSERVER='.(getenv('DBSERVER') ? getenv('DBSERVER') : 'localhost'));
+            putenv('DBNAME='.(getenv('DBNAME') ? getenv('DBNAME') : 'cube3_dev'));
+            putenv('DBUSER='.(getenv('DBUSER') ? getenv('DBUSER') : 'cube3_dev_user'));
+            putenv('DBPORT='.(getenv('DBPORT') ? getenv('DBPORT') : '3306'));
+            putenv('DBPASS='.(getenv('DBPASS') ? getenv('DBPASS') : 'password'));
+            break;
+        default:
+        case 'pdo_sqlite':
+            break;
+    }
+    putenv('SHOP_NAME='.(getenv('SHOP_NAME') ? getenv('SHOP_NAME') : 'EC-CUBE SHOP'));
+    putenv('ADMIN_MAIL='.(getenv('ADMIN_MAIL') ? getenv('ADMIN_MAIL') : 'admin@example.com'));
+    putenv('ADMIN_USER='.(getenv('ADMIN_USER') ? getenv('ADMIN_USER') : 'admin'));
+    putenv('ADMIN_PASS='.(getenv('ADMIN_PASS') ? getenv('ADMIN_PASS') : 'password'));
+    putenv('MAIL_BACKEND='.(getenv('MAIL_BACKEND') ? getenv('MAIL_BACKEND') : 'smtp'));
+    putenv('MAIL_HOST='.(getenv('MAIL_HOST') ? getenv('MAIL_HOST') : 'localhost'));
+    putenv('MAIL_PORT='.(getenv('MAIL_PORT') ? getenv('MAIL_PORT') : 25));
+    putenv('MAIL_USER='.(getenv('MAIL_USER') ? getenv('MAIL_USER') : null));
+    putenv('MAIL_PASS='.(getenv('MAIL_PASS') ? getenv('MAIL_PASS') : null));
+    putenv('ADMIN_ROUTE='.(getenv('ADMIN_ROUTE') ? getenv('ADMIN_ROUTE') : 'admin'));
+    putenv('ROOT_URLPATH='.(getenv('ROOT_URLPATH') ? getenv('ROOT_URLPATH') : null));
+}
+
+function getExampleVariables()
+{
+    return array(
+        'ADMIN_USER' => 'admin',
+        'ADMIN_MAIL' => 'admin@example.com',
+        'SHOP_NAME' => 'EC-CUBE SHOP',
+        'ADMIN_ROUTE' => 'admin',
+        'ROOT_URLPATH' => '<ec-cube install path>',
+        'DBSERVER' => '127.0.0.1',
+        'DBNAME' => 'cube3_dev',
+        'DBUSER' => 'cube3_dev_user',
+        'DBPASS' => 'password',
+        'DBPORT' => '<database port>',
+        'ROOTUSER' => 'root|postgres',
+        'ROOTPASS' => 'password',
+        'MAIL_BACKEND' => 'smtp',
+        'MAIL_HOST' => 'localhost',
+        'MAIL_PORT' => '25',
+        'MAIL_USER' => '<SMTP AUTH user>',
+        'MAIL_PASS' => '<SMTP AUTH password>'
+    );
+}
+
+
+function displayEnvironmentVariables()
+{
+    echo 'Environment variables:'.PHP_EOL;
+    foreach (array_keys(getExampleVariables()) as $name) {
+        echo $name.'='.getenv($name).PHP_EOL;
+    }
 }
 
 function composerSetup()
@@ -131,21 +214,17 @@ function createDatabase(array $connectionParams)
     switch ($connectionParams['driver']) {
         case 'pdo_pgsql':
             $connectionParams['dbname'] = 'postgres';
-            $connectionParams['user'] = (getenv('ROOTUSER') ? getenv('ROOTUSER') : (getenv('DBUSER') ? getenv('DBUSER') : 'postgres'));
-            $connectionParams['password'] = (getenv('ROOTPASS') ? getenv('ROOTPASS') : (getenv('DBPASS') ? getenv('DBPASS') : 'password'));
+            $connectionParams['user'] = getenv('ROOTUSER');
+            $connectionParams['password'] = getenv('ROOTPASS');
             break;
         case 'pdo_mysql':
             $connectionParams['dbname'] = 'mysql';
-            $connectionParams['user'] = (getenv('ROOTUSER') ? getenv('ROOTUSER') : (getenv('DBUSER') ? getenv('DBUSER') : 'root'));
-            if (getenv('TRAVIS')) {
-                $connectionParams['password'] = '';
-            } else {
-                $connectionParams['password'] = (getenv('ROOTPASS') ? getenv('ROOTPASS') : (getenv('DBPASS') ? getenv('DBPASS') : 'password'));
-            }
+            $connectionParams['user'] = getenv('ROOTUSER');
+            $connectionParams['password'] = getenv('ROOTPASS');
             break;
         default:
         case 'pdo_sqlite':
-            $connectionParams['dbname'] = $database['database']['path'];
+            $connectionParams['dbname'] = '';
             if (file_exists($dbname)) {
                 out('remove database to '.$dbname, 'info');
                 unlink($dbname);
@@ -218,8 +297,8 @@ function initializeDatabase(\Eccube\Application $app)
     $migration->migrate();
     out('Database migration successfully!', 'success');
 
-    $login_id = (getenv('ADMIN_USER') ? getenv('ADMIN_USER') : 'admin');
-    $login_password = (getenv('ADMIN_PASS') ? getenv('ADMIN_PASS') : 'password');
+    $login_id = getenv('ADMIN_USER');
+    $login_password = getenv('ADMIN_PASS');
     $passwordEncoder = new \Eccube\Security\Core\Encoder\PasswordEncoder($app['config']);
     $salt = \Eccube\Util\Str::random(32);
     $encodedPassword = $passwordEncoder->encodePassword($login_password, $salt);
@@ -236,13 +315,13 @@ function initializeDatabase(\Eccube\Application $app)
     );
     $stmt->closeCursor();
 
-    $shop_name = (getenv('SHOP_NAME') ? getenv('SHOP_NAME') : 'EC-CUBE SHOP');
-    $admin_mail = (getenv('ADMIN_MAIL') ? getenv('ADMIN_MAIL') : 'admin@example.com');
+    $shop_name = getenv('SHOP_NAME');
+    $admin_mail = getenv('ADMIN_MAIL');
     $sql = "INSERT INTO dtb_base_info (id, shop_name, email01, email02, email03, email04, update_date, option_product_tax_rule) VALUES (1, :shop_name, :admin_mail1, :admin_mail2, :admin_mail3, :admin_mail4, current_timestamp, 0)";
     $stmt = $pdo->prepare($sql);
     $stmt->execute(
         array(
-            ':shop_name' => $login_id,
+            ':shop_name' => $shop_name,
             ':admin_mail1' => $admin_mail,
             ':admin_mail2' => $admin_mail,
             ':admin_mail3' => $admin_mail,
@@ -273,7 +352,7 @@ function getConfig()
     $config = array (
         'auth_magic' => \Eccube\Util\Str::random(32),
         'password_hash_algos' => 'sha256',
-        'shop_name' => (getenv('SHOP_NAME') ? getenv('SHOP_NAME') : 'admin'),
+        'shop_name' => getenv('SHOP_NAME'),
         'force_ssl' => NULL,
         'admin_allow_host' =>
         array (
@@ -300,25 +379,14 @@ function getDatabaseConfig($database_driver)
             $database['database']['dbname'] = $database['database']['path'] = __DIR__.'/app/config/eccube/eccube.db';
 
             break;
-        case 'pdo_mysql':
-            $database['database']['host'] = (getenv('DBSERVER') ? getenv('DBSERVER') : 'localhost');
-
-            $database['database']['dbname'] = (getenv('DBNAME') ? getenv('DBNAME') : 'cube3_dev');
-            $database['database']['user'] = (getenv('DBUSER') ? getenv('DBUSER') : 'cube3_dev_user');
-            $database['database']['port'] = (getenv('DBPORT') ? getenv('DBPORT') : '3306');
-           if (getenv('TRAVIS')) {
-               $database['database']['password'] = '';
-           } else {
-               $database['database']['password'] = (getenv('DBPASS') ? getenv('DBPASS') : 'password');
-           }
-            break;
         case 'pdo_pgsql':
-            $database['database']['host'] = (getenv('DBSERVER') ? getenv('DBSERVER') : 'localhost');
-
-            $database['database']['dbname'] = (getenv('DBNAME') ? getenv('DBNAME') : 'cube3_dev');
-            $database['database']['user'] = (getenv('DBUSER') ? getenv('DBUSER') : 'cube3_dev_user');
-            $database['database']['password'] = (getenv('DBPASS') ? getenv('DBPASS') : 'password');
-            $database['database']['port'] = (getenv('DBPORT') ? getenv('DBPORT') : '5432');
+        case 'pdo_mysql':
+            $database['database']['host'] = getenv('DBSERVER');
+            $database['database']['dbname'] = getenv('DBNAME');
+            $database['database']['user'] = getenv('DBUSER');
+            $database['database']['port'] = getenv('DBPORT');
+            $database['database']['password'] = getenv('DBPASS');
+            $database['database']['port'] = getenv('DBPORT');
             break;
     }
     $database['database']['charset'] = 'utf8';
@@ -331,11 +399,11 @@ function getMailConfig()
     $mail = array (
         'mail' =>
         array (
-            'transport' => (getenv('MAIL_BACKEND') ? getenv('MAIL_BACKEND') : 'smtp'),
-            'host' => (getenv('MAIL_HOST') ? getenv('MAIL_HOST') : 'localhost'),
-            'port' => (getenv('MAIL_PORT') ? getenv('MAIL_PORT') : 25),
-            'username' => (getenv('MAIL_USER') ? getenv('MAIL_USER') : null),
-            'password' => (getenv('MAIL_PASS') ? getenv('MAIL_PASS') : null),
+            'transport' => getenv('MAIL_BACKEND'),
+            'host' => getenv('MAIL_HOST'),
+            'port' => getenv('MAIL_PORT'),
+            'username' => getenv('MAIL_USER'),
+            'password' => getenv('MAIL_PASS'),
             'encryption' => NULL,
             'auth_mode' => NULL,
             'charset_iso_2022_jp' => false,
@@ -347,16 +415,16 @@ function getMailConfig()
 function getPathConfig()
 {
     $root_dir = realpath(__DIR__);
+    $root_urlpath = getenv('ROOT_URLPATH');
     // TODO path.yml.dist から取得したい
-    // TODO root_urlpath を指定できるようにする
     $path = array (
-        'root' => '/',
-        'admin_dir' => (getenv('ADMIN_ROUTE') ? getenv('ADMIN_ROUTE') : 'admin'),
-        'tpl' => '/user_data/packages/default/',
-        'admin_tpl' => '/user_data/packages/admin/',
-        'image_path' => '/upload/save_image/',
+        'root' => $root_urlpath.'/',
+        'admin_dir' => getenv('ADMIN_ROUTE'),
+        'tpl' => $root_urlpath.'/user_data/packages/default/',
+        'admin_tpl' => $root_urlpath.'/user_data/packages/admin/',
+        'image_path' => $root_urlpath.'/upload/save_image/',
         'root_dir' => $root_dir,
-        'root_urlpath' => NULL,
+        'root_urlpath' => $root_urlpath,
         'template_code' => 'default',
         'admin_route' => 'admin',
         'user_data_route' => 'user_data',
@@ -378,12 +446,12 @@ function getPathConfig()
         'plugin_realdir' => $root_dir.'/app/Plugin',
         'plugin_temp_realdir' => $root_dir.'/app/cache/plugin',
         'plugin_html_realdir' => $root_dir.'/html/plugin',
-        'admin_urlpath' => '/template/admin',
-        'front_urlpath' => '/template/default',
-        'image_save_urlpath' => '/upload/save_image',
-        'image_temp_urlpath' => '/upload/temp_image',
-        'user_data_urlpath' => '/user_data',
-        'plugin_urlpath' => '/plugin',
+        'admin_urlpath' => $root_urlpath.'/template/admin',
+        'front_urlpath' => $root_urlpath.'/template/default',
+        'image_save_urlpath' => $root_urlpath.'/upload/save_image',
+        'image_temp_urlpath' => $root_urlpath.'/upload/temp_image',
+        'user_data_urlpath' => $root_urlpath.'/user_data',
+        'plugin_urlpath' => $root_urlpath.'/plugin',
     );
     return $path;
 }
