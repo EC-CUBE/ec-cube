bba1878d21973c9492fe8292ebbf08e4fe9c7bb3
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2016-10-27 09:53:42 +0900

    fix cache

--- src/Eccube/Util/Cache.php
+++ src/Eccube/Util/Cache.php
@@ -76,6 +76,15 @@ class Cache
             }
         }
 
+        if (function_exists('opcache_reset')) {
+            opcache_reset();
+        }
+
+        if (function_exists('apc_clear_cache')) {
+            apc_clear_cache('user');
+            apc_clear_cache();
+        }
+
         return true;
     }
 }
