833fd72528ea39e690eda61d85f60f7619dac522
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2016-11-08 15:58:59 +0900

    初期状態から変更なければrankを振り直すように修正

--- src/Eccube/Resource/doctrine/migration/Version20161108095350.php
+++ src/Eccube/Resource/doctrine/migration/Version20161108095350.php
@@ -20,6 +20,28 @@ class Version20161108095350 extends AbstractMigration
         $app = Application::getInstance();
         $repository = $app['orm.em']->getRepository('Eccube\Entity\Master\ProductListOrderBy');
 
+        $isDefault = true;
+        $default = array(
+            array(
+                'id' => 1,
+                'name' => '価格順',
+                'rank' => 0,
+            ),
+            array(
+                'id' => 2,
+                'name' => '新着順',
+                'rank' => 1,
+            ),
+        );
+
+        // mtb_product_list_orderbyが初期状態から変更がないかどうかの判定
+        $entities = $repository->findBy(array(), array('id' => 'ASC'));
+        foreach ($entities as $entity) {
+            if (!in_array($entity->toArray(), $default, true)) {
+                $isDefault = false;
+            }
+        }
+
         // 価格が高い順ソートの追加
         $ProductListOrderBy = $repository->find(100);
         if (is_null($ProductListOrderBy)) {
@@ -41,6 +63,22 @@ class Version20161108095350 extends AbstractMigration
             $ProductListOrderBy->setName('価格が低い順');
             $app['orm.em']->persist($ProductListOrderBy);
             $app['orm.em']->flush($ProductListOrderBy);
+        }
+
+        // mtb_product_list_orderbyが初期状態から変更がなければ、価格が低い順->価格が高い順->新着順の順にrankを振り直す
+        if ($isDefault) {
+            $entity = $repository->find(1);
+            $entity->setRank(0);
+            $app['orm.em']->flush($entity);
+
+            $entity = $repository->find(2);
+            $entity->setRank(2);
+            $app['orm.em']->flush($entity);
+
+            $entity = $repository->find(100);
+            dump($entity);
+            $entity->setRank(1);
+            $app['orm.em']->flush($entity);
         }
     }
 
