7a39f35f5946225d56789b63e70446c8b297f770
lammn <mnlam@lockon-vn.com>
2017-04-03 13:53:38 +0700

    Back commit

--- tests/Eccube/Tests/Web/Admin/Order/EditControllerTest.php
+++ tests/Eccube/Tests/Web/Admin/Order/EditControllerTest.php
@@ -369,7 +369,7 @@ class EditControllerTest extends AbstractEditControllerTestCase
             )
         );
 
-        $this->assertContains("受注情報を保存しました", $this->client->getResponse()->getContent());
+        $this->assertTrue($this->client->getResponse()->isRedirect($this->app->url('admin_order_edit', array('id' => $Order->getId()))));
         $EditedOrderafterEdit = $this->app['eccube.repository.order']->find($Order->getId());
 
         //確認する「トータル税金」
