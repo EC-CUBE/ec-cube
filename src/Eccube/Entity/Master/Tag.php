bebc9dcabeff8ded85f5327bcf46a2e193de0b69
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2017-04-19 15:45:40 +0900

    Entity\Master のクラスを汎化
    - AbstractMasterEntity を追加

--- src/Eccube/Entity/Master/Tag.php
+++ src/Eccube/Entity/Master/Tag.php
@@ -14,125 +14,14 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity(repositoryClass="Eccube\Repository\Master\TagRepository")
  * @ORM\Cache(usage="NONSTRICT_READ_WRITE")
  */
-class Tag extends \Eccube\Entity\AbstractEntity
+class Tag extends \Eccube\Entity\Master\AbstractMasterEntity
 {
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        return $this->getName();
-    }
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="id", type="smallint", options={"unsigned":true})
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="NONE")
-     */
-    private $id;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=255)
-     */
-    private $name;
-
-    /**
-     * @var int
-     *
-     * @ORM\Column(name="rank", type="smallint", options={"unsigned":true})
-     */
-    private $rank;
-
     /**
      * @var \Doctrine\Common\Collections\Collection
      *
      * @ORM\OneToMany(targetEntity="Eccube\Entity\ProductTag", mappedBy="Tag")
      */
-    private $ProductTag;
-
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->ProductTag = new \Doctrine\Common\Collections\ArrayCollection();
-    }
-
-    /**
-     * Set id.
-     *
-     * @param int $id
-     *
-     * @return Tag
-     */
-    public function setId($id)
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    /**
-     * Get id.
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set name.
-     *
-     * @param string $name
-     *
-     * @return Tag
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name.
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set rank.
-     *
-     * @param int $rank
-     *
-     * @return Tag
-     */
-    public function setRank($rank)
-    {
-        $this->rank = $rank;
-
-        return $this;
-    }
-
-    /**
-     * Get rank.
-     *
-     * @return int
-     */
-    public function getRank()
-    {
-        return $this->rank;
-    }
+    protected $ProductTag;
 
     /**
      * Add productTag.
