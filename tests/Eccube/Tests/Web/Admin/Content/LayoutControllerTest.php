eaf811377152096c09ec03422ea69e1bb715133a
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2017-04-28 17:16:18 +0900

    テストコードの修正

--- tests/Eccube/Tests/Web/Admin/Content/LayoutControllerTest.php
+++ tests/Eccube/Tests/Web/Admin/Content/LayoutControllerTest.php
@@ -18,21 +18,23 @@ class LayoutControllerTest extends AbstractAdminWebTestCase
         $crawler = $this->client->request(
             'POST',
             $this->app->url(
-                'admin_content_layout',
+                'admin_content_layout_edit',
                 array('id' => 1)
             ),
             array(
                 'form' => array(
-                    '_token' => 'dummy'
+                    '_token' => 'dummy',
+                    'name' => 'テストレイアウト',
+                    'DeviceType' => 10
                 ),
                 'name_1' => 'カゴの中',
-                'id_1' => 2,
+                'block_id_1' => 2,
                 'target_id_1' => 2,
-                'top_1' => 2,
+                'block_row_1' => 2,
                 'name_2' => '商品検索',
-                'id_2' => 3,
+                'block_id_2' => 3,
                 'target_id_2' => 3,
-                'top_2' => 3,
+                'block_row_2' => 3,
             )
         );
         $this->assertTrue($this->client->getResponse()->isRedirect(
@@ -42,6 +44,9 @@ class LayoutControllerTest extends AbstractAdminWebTestCase
 
     public function testIndexWithPostPreview()
     {
+        // FIXME プレビュー機能が実装されたら有効にする
+        $this->markTestIncomplete('Layout Preview is not implemented.');
+
         $crawler = $this->client->request(
             'POST',
             $this->app->url(
