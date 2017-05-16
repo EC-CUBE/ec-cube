442af8a0706101a818434ed3420e85f5e5d89694
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2017-05-16 15:37:12 +0900

    明細の追加に対応

--- src/Eccube/Form/Type/Admin/ShipmentItemType.php
+++ src/Eccube/Form/Type/Admin/ShipmentItemType.php
@@ -60,6 +60,10 @@ class ShipmentItemType extends AbstractType
                 'mapped' => false,
                 'data' => 1
             ))
+            ->add('id', HiddenType::class, array(
+                'required' => false,
+                'mapped' => false
+            ))
             ->add('price', MoneyType::class, array(
                 'currency' => 'JPY',
                 'scale' => 0,
@@ -145,69 +149,28 @@ class ShipmentItemType extends AbstractType
                 $data = $event->getData();
                 // 新規明細行の場合にセット.
                 if (isset($data['new'])) {
-                    switch ($data['order_item_type']) {
-                        case OrderItemType::DELIVERY_FEE:
-                            $data['product_name'] = '送料';
-                            $data['product_code'] = null;
-                            $data['class_name1'] = null;
-                            $data['class_name2'] = null;
-                            $data['class_category_name1'] = null;
-                            $data['class_category_name2'] = null;
-                            $data['price'] = 0;
-                            $data['quantity'] = 1;
-                            $data['tax_type'] = TaxType::TAXATION;
-                            $data['tax_display_type'] = TaxDisplayType::INCLUDED;
-                            break;
-                        case OrderItemType::CHARGE:
-                            $data['product_name'] = '手数料';
-                            $data['product_code'] = null;
-                            $data['class_name1'] = null;
-                            $data['class_name2'] = null;
-                            $data['class_category_name1'] = null;
-                            $data['class_category_name2'] = null;
-                            $data['price'] = 0;
-                            $data['quantity'] = 1;
-                            $data['tax_type'] = TaxType::TAXATION;
-                            $data['tax_display_type'] = TaxDisplayType::INCLUDED;
-                            break;
-                        case OrderItemType::DISCOUNT:
-                            $data['product_name'] = '値引き';
-                            $data['product_code'] = null;
-                            $data['class_name1'] = null;
-                            $data['class_name2'] = null;
-                            $data['class_category_name1'] = null;
-                            $data['class_category_name2'] = null;
-                            $data['price'] = -0;
-                            $data['quantity'] = 1;
-                            $data['tax_type'] = TaxType::NON_TAXABLE;
-                            $data['tax_display_type'] = TaxDisplayType::INCLUDED;
-                            break;
-                        case OrderItemType::PRODUCT:
-                        default:
-                            /** @var \Eccube\Entity\ProductClass $ProductClass */
-                            $ProductClass = $app['eccube.repository.product_class']
-                                ->find($data['ProductClass']);
-                            /** @var \Eccube\Entity\Product $Product */
-                            $Product = $ProductClass->getProduct();
+                    /** @var \Eccube\Entity\ShipmentItem $ShipmentItem */
+                    $ShipmentItem = $app['eccube.repository.shipment_item']
+                        ->find($data['id']);
+                    $data = array_merge($data, $ShipmentItem->toArray(['Order', 'Product', 'ProductClass', 'Shipping', 'TaxType', 'TaxDisplayType', 'OrderItemType']));
 
-                            $data['product_name'] = $Product->getName();
-                            $data['product_code'] = $ProductClass->getCode();
-                            $data['class_name1'] = $ProductClass->hasClassCategory1() ?
-                                $ProductClass->getClassCategory1()->getClassName() :
-                                null;
-                            $data['class_name2'] = $ProductClass->hasClassCategory2() ?
-                                $ProductClass->getClassCategory2()->getClassName() :
-                                null;
-                            $data['class_category_name1'] = $ProductClass->hasClassCategory1() ?
-                                $ProductClass->getClassCategory1()->getName() :
-                                null;
-                            $data['class_category_name2'] = $ProductClass->hasClassCategory2() ?
-                                $ProductClass->getClassCategory2()->getName() :
-                                null;
-                            $data['price'] = $ProductClass->getPrice02();
-                            $data['quantity'] = empty($data['quantity']) ? 1 : $data['quantity'];
-                            $data['tax_type'] = TaxType::TAXATION;
-                            $data['tax_display_type'] = TaxDisplayType::EXCLUDED;
+                    if (is_object($ShipmentItem->getOrder())) {
+                        $data['Order'] = $ShipmentItem->getOrder()->getId();
+                    }
+                    if (is_object($ShipmentItem->getProduct())) {
+                        $data['Product'] = $ShipmentItem->getProduct()->getId();
+                    }
+                    if (is_object($ShipmentItem->getProduct())) {
+                        $data['ProductClass'] = $ShipmentItem->getProductClass()->getId();
+                    }
+                    if (is_object($ShipmentItem->getTaxType())) {
+                        $data['tax_type'] = $ShipmentItem->getTaxType()->getId();
+                    }
+                    if (is_object($ShipmentItem->getTaxDisplayType())) {
+                        $data['tax_display_type'] = $ShipmentItem->getTaxDisplayType()->getId();
+                    }
+                    if (is_object($ShipmentItem->getOrderItemType())) {
+                        $data['order_item_type'] = $ShipmentItem->getOrderItemType()->getId();
                     }
                     $event->setData($data);
                 }
