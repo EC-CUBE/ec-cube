df48cc79de594a90952234a1c4e932eef2f52dec
hoand-vn <ndhoa@lockon-vn.com>
2017-07-18 10:46:21 +0700

    throw event for include template

--- src/Eccube/Twig/Extension/EccubeExtension.php
+++ src/Eccube/Twig/Extension/EccubeExtension.php
@@ -262,5 +262,16 @@ class EccubeExtension extends \Twig_Extension
     {
         return Str::timeAgo($date);
     }
-
+    
+    /**
+     * Name of this extension
+     *
+     * @return string
+     */
+    public function getTokenParsers()
+    {
+        return array(
+            new Twig_TokenParser_Include($this->app),
+        );
+    }
 }
---
+++ src/Eccube/Twig/Extension/Twig_TokenParser_Include.php
@@ -0,0 +1,91 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) 2000-2015 LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+namespace Eccube\Twig\Extension;
+
+use \Twig_Token;
+use \Twig_Node_Include;
+use Eccube\Event\TemplateEvent;
+use Monolog\Logger;
+
+class Twig_TokenParser_Include extends \Twig_TokenParser
+{
+    public function __construct($app)
+    {
+        $this->app = $app;
+    }
+    public function parse(Twig_Token $token)
+    {   
+        $expr = $this->parser->getExpressionParser()->parseExpression();
+
+        list($variables, $only, $ignoreMissing) = $this->parseArguments();
+        
+        // Begin create event for include
+        $view = $expr->getAttribute('value');
+        $source = $this->app['twig']->getLoader()->getSource($view);
+
+        $event = new TemplateEvent($view, $source);
+        $eventName = $view;
+        if ($this->app->isAdminRequest()) {
+            // 管理画面の場合、event名に「Admin/」を付ける
+            $eventName = 'Admin/' . $view;
+        }
+        $this->app['monolog']->debug('Template Event Name : ' . $eventName);
+
+        $this->app['eccube.event.dispatcher']->dispatch($eventName, $event);
+        // Begin create event for include
+
+        return new Twig_Node_Include($expr, $variables, $only, $ignoreMissing, $token->getLine(), $this->getTag());
+    }
+
+    protected function parseArguments()
+    {
+        $stream = $this->parser->getStream();
+
+        $ignoreMissing = false;
+        if ($stream->nextIf(Twig_Token::NAME_TYPE, 'ignore')) {
+            $stream->expect(Twig_Token::NAME_TYPE, 'missing');
+
+            $ignoreMissing = true;
+        }
+
+        $variables = null;
+        if ($stream->nextIf(Twig_Token::NAME_TYPE, 'with')) {
+            $variables = $this->parser->getExpressionParser()->parseExpression();
+        }
+
+        $only = false;
+        if ($stream->nextIf(Twig_Token::NAME_TYPE, 'only')) {
+            $only = true;
+        }
+
+        $stream->expect(Twig_Token::BLOCK_END_TYPE);
+
+        return array($variables, $only, $ignoreMissing);
+    }
+
+    public function getTag()
+    {
+        return 'include';
+    }
+}
