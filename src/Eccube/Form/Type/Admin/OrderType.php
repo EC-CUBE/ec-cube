0730e001f725605dc36410d724c892bbfba045e1
kiy0taka <kiy0taka333@gmail.com>
2017-08-15 16:58:18 +0900

    余計なuseを削除

--- src/Eccube/Form/Type/Admin/OrderType.php
+++ src/Eccube/Form/Type/Admin/OrderType.php
@@ -41,7 +41,6 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
