d53f78b3604e707be73dd17722ce4c098f6619f8
Chihiro Adachi <chihiro_adachi@lockon.co.jp>
2017-09-05 11:19:36 +0900

    TwigLintValidatorでtwig関数のチェックができなかった問題を修正

--- src/Eccube/Form/Validator/TwigLintValidator.php
+++ src/Eccube/Form/Validator/TwigLintValidator.php
@@ -10,6 +10,25 @@ use Twig\Source;
 
 class TwigLintValidator extends ConstraintValidator
 {
+    /**
+     * @var \Twig_Environment
+     */
+    protected $twig;
+
+    /**
+     * TwigLintValidator constructor.
+     *
+     * @param \Twig_Environment $twig
+     */
+    public function __construct(\Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @param mixed $value
+     * @param Constraint $constraint
+     */
     public function validate($value, Constraint $constraint)
     {
         // valueがnullの場合は "Template is not defined"のエラーが投げられるので, 空文字でチェックする.
@@ -17,17 +36,18 @@ class TwigLintValidator extends ConstraintValidator
             $value = '';
         }
 
+        $realLoader = $this->twig->getLoader();
         try {
-            $loader = new ArrayLoader(['' => $value]);
-            $twig = new \Twig_Environment($loader);
-            $nodeTree = $twig->parse($twig->tokenize(new Source($value, '')));
-            $twig->compile($nodeTree);
+            $temporaryLoader = new ArrayLoader(['' => $value]);
+            $this->twig->setLoader($temporaryLoader);
+            $nodeTree = $this->twig->parse($this->twig->tokenize(new Source($value, '')));
+            $this->twig->compile($nodeTree);
 
         } catch (Error $e) {
-
             $this->context->buildViolation($constraint->message)
                 ->setParameter('{{ error }}', $e->getMessage())
                 ->addViolation();
         }
+        $this->twig->setLoader($realLoader);
     }
 }
