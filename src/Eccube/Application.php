fa98d62274fc2b518800ba869c67bcc953247e01
Hideki Okajima <hideki_okajima@lockon.co.jp>
2017-10-13 16:09:41 +0900

    1. form_layout.twigからform_div_layout.twigへの切り替え 2. form_layout.twigの削除 3. 表示崩れの修正

--- src/Eccube/Application.php
+++ src/Eccube/Application.php
@@ -468,7 +468,7 @@ class Application extends \Silex\Application
     public function initRendering()
     {
         $this->register(new \Silex\Provider\TwigServiceProvider(), array(
-            'twig.form.templates' => array('Form/form_layout.twig'),
+            'twig.form.templates' => array('Form/form_div_layout.twig'),
         ));
         $this->extend('twig', function (\Twig_Environment $twig, \Silex\Application $app) {
             $twig->addExtension(new \Eccube\Twig\Extension\EccubeExtension($app));
