4296b25571d8e1034bcc9ded90e688dc9674022f
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2017-10-20 11:00:41 +0900

    コメントの誤りを修正

--- src/Eccube/Resource/config/session_handler.php
+++ src/Eccube/Resource/config/session_handler.php
@@ -12,7 +12,7 @@
     //'save_handler' => 'memcached',
     //'save_path' => '127.0.0.1:11211',
 
-    // memcachedの設定例
+    // redisの設定例
     //'save_handler' => 'redis',
     //'save_path' => '127.0.0.1:6379',
   ],
