c3431ff82036f27562982b8eb60c5411347b3b9f
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2017-12-19 18:15:47 +0900

    IPアドレス制限を行うサブスクライバを追加

---
+++ src/Eccube/EventSubscriber/IpAddrSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Eccube\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class IpAddrSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var array
+     */
+    protected $eccubeConfig;
+
+    public function __construct(array $eccubeConfig)
+    {
+        $this->eccubeConfig = $eccubeConfig;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $allowHosts = $this->eccubeConfig['admin_allow_hosts'];
+        if (count($allowHosts) > 0) {
+            if (array_search($event->getRequest()->getClientIp(), $allowHosts) === false) {
+                throw new AccessDeniedHttpException();
+            }
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.request' => ['onKernelRequest', 512],
+        ];
+    }
+}
