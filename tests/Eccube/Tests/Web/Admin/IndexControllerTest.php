56d4b9a03a89afe203704eefbc3caf42e784c64e
Le Phu Sang <lpsang@lockon-vn.com>
2018-01-19 10:37:36 +0700

    fix admin/login base on #2768,#2769,#2770

--- tests/Eccube/Tests/Web/Admin/IndexControllerTest.php
+++ tests/Eccube/Tests/Web/Admin/IndexControllerTest.php
@@ -23,27 +23,38 @@
 namespace Eccube\Tests\Web\Admin;
 
 use Eccube\Entity\Master\OrderStatus;
+use Eccube\Entity\Member;
+use Eccube\Repository\Master\OrderStatusRepository;
+use Eccube\Repository\OrderRepository;
 
 class IndexControllerTest extends AbstractAdminWebTestCase
 {
+    /** @var  Member */
     protected $Member;
+
+    /** @var OrderStatusRepository */
+    protected $orderStatusRepository;
+
+    /** @var OrderRepository */
+    protected $orderRepository;
 
     public function setUp()
     {
-        $this->markTestIncomplete(get_class($this).' は未実装です');
         parent::setUp();
         $this->Member = $this->createMember();
+        $this->orderStatusRepository = $this->container->get(OrderStatusRepository::class);
+        $this->orderRepository = $this->container->get(OrderRepository::class);
     }
 
     public function testRoutingAdminIndex()
     {
-        $crawler = $this->client->request('GET', $this->app['url_generator']->generate('admin_homepage'));
+        $this->client->request('GET', $this->generateUrl('admin_homepage'));
         $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
 
     public function testRoutingAdminChangePassword()
     {
-        $this->client->request('GET', $this->app['url_generator']->generate('admin_change_password'));
+        $this->client->request('GET', $this->generateUrl('admin_change_password'));
         $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
 
@@ -56,17 +67,17 @@ class IndexControllerTest extends AbstractAdminWebTestCase
         $Today = new \DateTime();
         $Yesterday = new \DateTime('-1 days');
 
-        $OrderNew = $this->app['eccube.repository.order_status']->find(OrderStatus::NEW);
-        $OrderPending = $this->app['eccube.repository.order_status']->find(OrderStatus::PENDING);
-        $OrderCancel = $this->app['eccube.repository.order_status']->find(OrderStatus::CANCEL);
-        $OrderProcessing = $this->app['eccube.repository.order_status']->find(OrderStatus::PROCESSING);
+        $OrderNew = $this->orderStatusRepository->find(OrderStatus::NEW);
+        $OrderPending = $this->orderStatusRepository->find(OrderStatus::PENDING);
+        $OrderCancel = $this->orderStatusRepository->find(OrderStatus::CANCEL);
+        $OrderProcessing = $this->orderStatusRepository->find(OrderStatus::PROCESSING);
 
         $todaysSales = 0;
         for ($i = 0; $i < 3; $i++) {
             $Order = $this->createOrder($Customer);
             $Order->setOrderStatus($OrderNew);
             $Order->setOrderDate($Today);
-            $this->app['orm.em']->flush();
+            $this->entityManager->flush();
             $todaysSales += $Order->getPaymentTotal();
         }
         $yesterdaysSales = 0;
@@ -74,7 +85,7 @@ class IndexControllerTest extends AbstractAdminWebTestCase
             $Order = $this->createOrder($Customer);
             $Order->setOrderStatus($OrderNew);
             $Order->setOrderDate($Yesterday);
-            $this->app['orm.em']->flush();
+            $this->entityManager->flush();
             $yesterdaysSales += $Order->getPaymentTotal();
         }
 
@@ -84,13 +95,13 @@ class IndexControllerTest extends AbstractAdminWebTestCase
                 $Order = $this->createOrder($Customer);
                 $Order->setOrderStatus($OrderStatus);
                 $Order->setOrderDate($OrderDate);
-                $this->app['orm.em']->flush();
+                $this->entityManager->flush();
             }
         }
 
         $crawler = $this->client->request(
             'GET',
-            $this->app->url('admin_homepage')
+            $this->generateUrl('admin_homepage')
         );
 
         $this->assertTrue($this->client->getResponse()->isSuccessful());
@@ -121,7 +132,7 @@ class IndexControllerTest extends AbstractAdminWebTestCase
         $endDate->setDate($Today->format('Y'), $Today->format('m'), $Today->format('t'));
         $endDate->setTime(23, 59, 59);
 
-        $qb = $this->app['eccube.repository.order']->createQueryBuilder('o');
+        $qb = $this->orderRepository->createQueryBuilder('o');
         $qb->andWhere($qb->expr()->notIn('o.OrderStatus',
                                          array(
                                              $OrderPending->getId(),
@@ -162,29 +173,29 @@ class IndexControllerTest extends AbstractAdminWebTestCase
         $client = $this->client;
 
         $form = $this->createChangePasswordFormData();
-        $crawler = $client->request(
+        $client->request(
             'POST',
-            $this->app->path('admin_change_password'),
+            $this->generateUrl('admin_change_password'),
             array('admin_change_password' => $form)
         );
 
-        $this->assertTrue($client->getResponse()->isRedirect($this->app->url('admin_change_password')));
+        $this->assertTrue($client->getResponse()->isRedirect($this->generateUrl('admin_change_password')));
 
         $Member = clone $this->Member;
-        $encoder = $this->app['security.encoder_factory']->getEncoder($this->Member);
+        $encoder = $this->container->get('security.encoder_factory')->getEncoder($this->Member);
         $this->expected = $encoder->encodePassword($form['change_password']['first'], $this->Member->getSalt());
         $this->actual = $this->Member->getPassword();
 
         // XXX 実行タイミングにより、稀にパスワード変更前のハッシュ値を参照する場合があるため、変更に成功した場合のみ assertion を実行する
-        $old_password = hash_hmac('sha256', 'password'.':'.$this->app['config']['auth_magic'], $this->Member->getSalt());
+        $old_password = hash_hmac('sha256', 'password'.':'.$this->eccubeConfig['auth_magic'], $this->Member->getSalt());
         if ($this->actual === $old_password) {
-            $this->markTestSkipped('Failed to change the password by HttpClient. Skip this test.');
+             $this->markTestSkipped('Failed to change the password by HttpClient. Skip this test.');
         }
 
         $this->verify(
             'パスワードのハッシュ値が異なります '.PHP_EOL
-            .' AUTH_MAGIC='.$this->app['config']['auth_magic'].PHP_EOL
-            .' HASH_Algos='.$this->app['config']['password_hash_algos'].PHP_EOL
+            .' AUTH_MAGIC='.$this->eccubeConfig['auth_magic'].PHP_EOL
+            .' HASH_Algos='.$this->eccubeConfig['password_hash_algos'].PHP_EOL
             .' Input Password='.$form['change_password']['first'].PHP_EOL
             .' Expected: salt='.$Member->getSalt().', raw password='.$Member->getPassword().PHP_EOL
             .' Actual: salt='.$this->Member->getSalt()
@@ -198,7 +209,7 @@ class IndexControllerTest extends AbstractAdminWebTestCase
 
         $client->request(
             'POST',
-            $this->app->path('admin_change_password'),
+            $this->generateUrl('admin_change_password'),
             array()
         );
         $this->assertTrue($client->getResponse()->isSuccessful());
