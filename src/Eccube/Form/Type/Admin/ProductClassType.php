55b0f8c381fd709d7ebc2ebaee633db65650993e
ndquocphong <ndquocphong@gmail.com>
2018-01-25 10:51:41 +0700

    Remove unused namespace

--- src/Eccube/Form/Type/Admin/ProductClassType.php
+++ src/Eccube/Form/Type/Admin/ProductClassType.php
@@ -21,10 +21,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
-
 namespace Eccube\Form\Type\Admin;
 
-use Eccube\Application;
 use Eccube\Form\DataTransformer;
 use Eccube\Form\Type\Master\DeliveryDurationType;
 use Eccube\Form\Type\Master\SaleTypeType;
