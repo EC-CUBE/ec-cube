3251b6f22cab9e7ea0856aef72fa7fcf5737a5bc
ndquocphong <ndquocphong@gmail.com>
2018-02-26 10:10:17 +0700

    Fix incorrect price on cart, caused by commit 751ed92cff07866488c919bd6deed43abb84387e

--- src/Eccube/Service/CartService.php
+++ src/Eccube/Service/CartService.php
@@ -221,7 +221,7 @@ class CartService
 
         $newItem = new CartItem();
         $newItem->setQuantity($quantity);
-        $newItem->setPrice($ProductClass->getPrice01IncTax());
+        $newItem->setPrice($ProductClass->getPrice02IncTax());
         $newItem->setProductClass($ProductClass);
 
         $allCartItems = $this->mergeAllCartItems([$newItem]);
