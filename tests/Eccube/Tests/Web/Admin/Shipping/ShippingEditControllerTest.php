929432e1edeb22695ced0afcf35fddcbf4c87501
lqdung <lqdung@lockon-vn.com>
2018-04-05 14:48:00 +0700

    Add info message to shipping edit page

--- tests/Eccube/Tests/Web/Admin/Shipping/ShippingEditControllerTest.php
+++ tests/Eccube/Tests/Web/Admin/Shipping/ShippingEditControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Eccube\Tests\Web\Admin\Shipping;
 
+use Eccube\Entity\Order;
 use Eccube\Tests\Web\Admin\AbstractAdminWebTestCase;
 
 class ShippingEditControllerTest extends AbstractAdminWebTestCase
@@ -13,5 +14,29 @@ class ShippingEditControllerTest extends AbstractAdminWebTestCase
             $this->generateUrl('admin_shipping_new')
         );
         $this->assertTrue($this->client->getResponse()->isSuccessful());
+    }
+
+    public function testShippingMessageNoticeWhenPost()
+    {
+        $Customer = $this->createCustomer();
+        /** @var Order $Order */
+        $Order = $this->createOrder($Customer);
+
+        $shippingId = $Order->getShippings()->first()->getId();
+
+        $crawler = $this->client->request(
+            'GET',
+            $this->generateUrl('admin_shipping_edit', ['id' => $shippingId])
+        );
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+
+        $form = $crawler->selectButton('出荷情報を登録')->form();
+
+        $this->client->submit($form);
+        $crawler = $this->client->followRedirect();
+        $info = $crawler->filter('#page_admin_shipping_edit > div.c-container > div.c-contentsArea > div.alert.alert-primary')->text();
+        $success = $crawler->filter('#page_admin_shipping_edit > div.c-container > div.c-contentsArea > div.alert.alert-success')->text();
+        $this->assertContains('出荷情報を登録しました。', $success);
+        $this->assertContains('配送に関わる情報が変更されました：送料の変更が必要な場合は、受注管理より手動で変更してください。', $info);
     }
 }
