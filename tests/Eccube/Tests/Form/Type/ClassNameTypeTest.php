0de8e7194728ed5b506b7a40a639f7828725b1d4
Mi-Hiroki <monsieur.hiroki@gmail.com>
2018-04-13 13:48:03 +0900

    [管理画面UI実装]管理画面全般/規格名・管理名の表示順を変更
    必須項目の修正とテストケースの修正

--- tests/Eccube/Tests/Form/Type/ClassNameTypeTest.php
+++ tests/Eccube/Tests/Form/Type/ClassNameTypeTest.php
@@ -33,7 +33,7 @@ class ClassNameTypeTest extends AbstractTypeTestCase
 
     /** @var array デフォルト値（正常系）を設定 */
     protected $formData = array(
-        'backend_name' => '形状'
+        'display_name' => '形状'
     );
 
     public function setUp()
@@ -57,7 +57,7 @@ class ClassNameTypeTest extends AbstractTypeTestCase
 
     public function testInvalidName_NotBlank()
     {
-        $this->formData['backend_name'] = '';
+        $this->formData['display_name'] = '';
         $this->form->submit($this->formData);
 
         $this->assertFalse($this->form->isValid());
@@ -65,7 +65,7 @@ class ClassNameTypeTest extends AbstractTypeTestCase
 
     public function testInvalidName_SptabCheck()
     {
-        $this->formData['backend_name'] = '     ';
+        $this->formData['display_name'] = '     ';
         $this->form->submit($this->formData);
 
         $this->assertFalse($this->form->isValid());
@@ -75,7 +75,7 @@ class ClassNameTypeTest extends AbstractTypeTestCase
     {
         $name = str_repeat('S', $this->eccubeConfig['eccube_stext_len']) . 'S';
 
-        $this->formData['backend_name'] = $name;
+        $this->formData['display_name'] = $name;
         $this->form->submit($this->formData);
 
         $this->assertFalse($this->form->isValid());
@@ -85,7 +85,7 @@ class ClassNameTypeTest extends AbstractTypeTestCase
     {
         $name = str_repeat('S', $this->eccubeConfig['eccube_stext_len']);
 
-        $this->formData['backend_name'] = $name;
+        $this->formData['display_name'] = $name;
         $this->form->submit($this->formData);
 
         $this->assertTrue($this->form->isValid());
