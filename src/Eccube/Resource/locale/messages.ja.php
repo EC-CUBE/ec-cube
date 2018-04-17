ef1c5dd5c49f4df59bff2b4825b1d320be5174d7
Mi-Hiroki <monsieur.hiroki@gmail.com>
2018-04-17 12:52:18 +0900

    管理画面 > 商品管理 > 規格管理画面のメッセージID化の対応

--- src/Eccube/Resource/locale/messages.ja.php
+++ src/Eccube/Resource/locale/messages.ja.php
@@ -1356,6 +1356,7 @@ return [
     'admin.product.class_name.441' => '名前を編集',
     'admin.product.class_name.442' => '項目の順番はドラッグ＆ドロップでも変更可能です。',
     'admin.product.class_name.443' => '新規作成',
+    'admin.product.class_name.444' => '管理名：',
     'admin.product.csv_category.title' => 'カテゴリCSV登録',
     'admin.product.csv_category.sub_title' => '商品管理',
     'admin.product.csv_category.csv_file_upload' => 'CSVファイルをアップロード',
