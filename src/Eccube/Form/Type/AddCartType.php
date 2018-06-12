6f4d7e65ffd5302ff5f20f451980023f2647e54d
Kiyoshi Yamamura <kiyoshi.yamamura@cule.jp>
2018-06-12 14:49:16 +0900

    商品一覧からカートに追加できるように対応

--- src/Eccube/Form/Type/AddCartType.php
+++ src/Eccube/Form/Type/AddCartType.php
@@ -78,7 +78,7 @@ class AddCartType extends AbstractType
                 'constraints' => [
                     new Assert\NotBlank(),
                     new Assert\Regex(['pattern' => '/^\d+$/']),
-                ]])
+                ], ])
             ->add(
                 $builder
                     ->create('ProductClass', HiddenType::class, [
@@ -124,7 +124,7 @@ class AddCartType extends AbstractType
                 }
             }
 
-            $builder->addEventListener(FormEvents::PRE_SUBMIT, function(FormEvent $event) use ($Product) {
+            $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) use ($Product) {
                 $data = $event->getData();
                 $form = $event->getForm();
                 if (isset($data['classcategory_id1']) && !is_null($Product->getClassName2())) {
@@ -138,7 +138,7 @@ class AddCartType extends AbstractType
                 }
             });
 
-            $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) {
+            $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
                 /** @var CartItem $CartItem */
                 $CartItem = $event->getData();
                 $ProductClass = $CartItem->getProductClass();
