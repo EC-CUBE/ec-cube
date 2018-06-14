a15c930dadc2a596a96e4ecbcb09cb6357beaef5
Kiyoshi Yamamura <kiyoshi.yamamura@cule.jp>
2018-06-14 17:38:56 +0900

    購入手続き画面でのリロード発生時に他の操作ができないように画面をオーバレイする対応

--- html/template/default/js/function.js
+++ html/template/default/js/function.js
@@ -1,8 +1,8 @@
-$(function () {
+$(function() {
 
     /////////// Drawer menu
 
-    $('.nav-trigger').on('click', function (event) {
+    $('.nav-trigger').on('click', function(event) {
         event.preventDefault();
         if ($('.drawer-open #drawer').size() == 0) {
             $('.overlay').addClass('is-visible');
@@ -16,7 +16,7 @@ $(function () {
     });
 
     //open cart form
-    $('.cart-trigger').on('click', function (event) {
+    $('.cart-trigger').on('click', function(event) {
         event.preventDefault();
         toggleCart();
         $('#wrapper').removeClass('drawer-open');
@@ -24,13 +24,13 @@ $(function () {
     });
 
     //close lateral menu on mobile
-    $('.overlay').on('swiperight', function () {
+    $('.overlay').on('swiperight', function() {
         if ($('#wrapper').hasClass('drawer-open')) {
             $('#wrapper').removeClass('drawer-open');
             $('.overlay').removeClass('is-visible');
         }
     });
-    $('.overlay').on('click', function () {
+    $('.overlay').on('click', function() {
         $('#wrapper').removeClass('drawer-open');
         toggleCart('close')
         $('.overlay').removeClass('is-visible');
@@ -53,7 +53,7 @@ $(function () {
 
 
     /////////// category accordion
-    $("#category li .toggle").on('click', function () {
+    $("#category li .toggle").on('click', function() {
         var togglepanel = $(this).parent('a').next('ul');
         if (togglepanel.css("display") == "none") {
             $(this).parent('a').addClass("active");
@@ -66,7 +66,7 @@ $(function () {
     });
 
     /////////// アコーディオン
-    $(".accordion dl dt").on('click', function () {
+    $(".accordion dl dt").on('click', function() {
         if ($(this).parent('dl').children('dd').css('display') == 'none') {
             $(this).addClass('active');
             $(this).parent('dl').children('dd').slideDown(300);
@@ -78,7 +78,7 @@ $(function () {
     });
 
     /////////// スムーススクロール
-    $('a.anchor').on('click', function () {
+    $('a.anchor').on('click', function() {
         var speed = 400;//スクロール速度 単位：ミリ秒
         var href = $(this).attr("href");
         var destination = $(href == "#" || href == "" ? 'html' : href);
@@ -88,14 +88,14 @@ $(function () {
     });
 
     /////////// dropdownの中をクリックしても閉じないようにする
-    $(".dropdown-menu").click(function (e) {
+    $(".dropdown-menu").click(function(e) {
         e.stopPropagation();
     });
 
     /////////// 追従サイドバー + ページトップフェードイン
 
     // スクロールした時に以下の処理
-    $(window).on("scroll", function () {
+    $(window).on("scroll", function() {
         // ページトップフェードイン
         if ($(this).scrollTop() > 300) {
             $('.pagetop').fadeIn();
@@ -140,27 +140,48 @@ $(function () {
         $(this).attr('disabled', 'disabled');
         var $form = $(this).parents('form');
         // マスク表示させるためsetTimeoutを使って処理を遅らせる
-        setTimeout(function(){
+        setTimeout(function() {
             $form.submit();
         }, 0);
         return false;
     });
 
+    // イベント実行時のオーバーレイ処理
+    // classに「load-overlay」が記述されていると画面がオーバーレイされる
+    $('.load-overlay').on({
+        click: function() {
+            loadingOverlay();
+        },
+        change: function() {
+            loadingOverlay();
+        }
+    });
+
+    function loadingOverlay() {
+        $.LoadingOverlay('show', {
+            background: 'rgba(255, 255, 255, 0.4)',
+            image: ''
+        });
+
+        setTimeout(function() {
+            $.LoadingOverlay('hide');
+        }, 10000);
+    }
 });
 
 
 /////////// ロールオーバー
-$.fn.rollover = function () {
-    return this.each(function () {
+$.fn.rollover = function() {
+    return this.each(function() {
         var src = $(this).attr('src');
         if (src.match('_on.')) return;
         var src_on = src.replace(/^(.+)(\.[a-z]+)$/, "$1_on$2");
         $('').attr('src', src_on);
         $(this).hover(
-            function () {
+            function() {
                 $(this).attr('src', src_on);
             },
-            function () {
+            function() {
                 $(this).attr('src', src);
             }
         );
@@ -168,7 +189,7 @@ $.fn.rollover = function () {
 };
 
 // 画像をロールオーバーする箇所(imgタグ)を指定
-$(function () {
+$(function() {
     $('.rollover').rollover();
 });
 
@@ -179,34 +200,34 @@ $(function () {
  * http://brm.io/jquery-match-height/
  * License: MIT
  */
-(function (c) {
-    var n = -1, f = -1, g = function (a) {
+(function(c) {
+    var n = -1, f = -1, g = function(a) {
         return parseFloat(a) || 0
-    }, r = function (a) {
+    }, r = function(a) {
         var b = null, d = [];
-        c(a).each(function () {
+        c(a).each(function() {
             var a = c(this), k = a.offset().top - g(a.css("margin-top")), l = 0 < d.length ? d[d.length - 1] : null;
             null === l ? d.push(a) : 1 >= Math.floor(Math.abs(b - k)) ? d[d.length - 1] = l.add(a) : d.push(a);
             b = k
         });
         return d
-    }, p = function (a) {
+    }, p = function(a) {
         var b = {byRow: !0, property: "height", target: null, remove: !1};
-        if ("object" === typeof a)return c.extend(b, a);
+        if ("object" === typeof a) return c.extend(b, a);
         "boolean" === typeof a ? b.byRow = a : "remove" === a && (b.remove = !0);
         return b
     }, b = c.fn.matchHeight =
-        function (a) {
+        function(a) {
             a = p(a);
             if (a.remove) {
                 var e = this;
                 this.css(a.property, "");
-                c.each(b._groups, function (a, b) {
+                c.each(b._groups, function(a, b) {
                     b.elements = b.elements.not(e)
                 });
                 return this
             }
-            if (1 >= this.length && !a.target)return this;
+            if (1 >= this.length && !a.target) return this;
             b._groups.push({elements: this, options: a});
             b._apply(this, a);
             return this
@@ -216,14 +237,14 @@ $(function () {
     b._maintainScroll = !1;
     b._beforeUpdate = null;
     b._afterUpdate = null;
-    b._apply = function (a, e) {
+    b._apply = function(a, e) {
         var d = p(e), h = c(a), k = [h], l = c(window).scrollTop(), f = c("html").outerHeight(!0), m = h.parents().filter(":hidden");
-        m.each(function () {
+        m.each(function() {
             var a = c(this);
             a.data("style-cache", a.attr("style"))
         });
         m.css("display", "block");
-        d.byRow && !d.target && (h.each(function () {
+        d.byRow && !d.target && (h.each(function() {
             var a = c(this), b = "inline-block" === a.css("display") ? "inline-block" : "block";
             a.data("style-cache", a.attr("style"));
             a.css({
@@ -236,19 +257,19 @@ $(function () {
                 "border-bottom-width": "0",
                 height: "100px"
             })
-        }), k = r(h), h.each(function () {
+        }), k = r(h), h.each(function() {
             var a = c(this);
             a.attr("style", a.data("style-cache") || "")
         }));
-        c.each(k, function (a, b) {
+        c.each(k, function(a, b) {
             var e = c(b), f = 0;
-            if (d.target)f =
+            if (d.target) f =
                 d.target.outerHeight(!1); else {
                 if (d.byRow && 1 >= e.length) {
                     e.css(d.property, "");
                     return
                 }
-                e.each(function () {
+                e.each(function() {
                     var a = c(this), b = {display: "inline-block" === a.css("display") ? "inline-block" : "block"};
                     b[d.property] = "";
                     a.css(b);
@@ -256,59 +277,59 @@ $(function () {
                     a.css("display", "")
                 })
             }
-            e.each(function () {
+            e.each(function() {
                 var a = c(this), b = 0;
                 d.target && a.is(d.target) || ("border-box" !== a.css("box-sizing") && (b += g(a.css("border-top-width")) + g(a.css("border-bottom-width")), b += g(a.css("padding-top")) + g(a.css("padding-bottom"))), a.css(d.property,
                     f - b))
             })
         });
-        m.each(function () {
+        m.each(function() {
             var a = c(this);
             a.attr("style", a.data("style-cache") || null)
         });
         b._maintainScroll && c(window).scrollTop(l / f * c("html").outerHeight(!0));
         return this
     };
-    b._applyDataApi = function () {
+    b._applyDataApi = function() {
         var a = {};
-        c("[data-match-height], [data-mh]").each(function () {
+        c("[data-match-height], [data-mh]").each(function() {
             var b = c(this), d = b.attr("data-mh") || b.attr("data-match-height");
             a[d] = d in a ? a[d].add(b) : b
         });
-        c.each(a, function () {
+        c.each(a, function() {
             this.matchHeight(!0)
         })
     };
-    var q = function (a) {
+    var q = function(a) {
         b._beforeUpdate && b._beforeUpdate(a, b._groups);
-        c.each(b._groups, function () {
+        c.each(b._groups, function() {
             b._apply(this.elements,
                 this.options)
         });
         b._afterUpdate && b._afterUpdate(a, b._groups)
     };
-    b._update = function (a, e) {
+    b._update = function(a, e) {
         if (e && "resize" === e.type) {
             var d = c(window).width();
-            if (d === n)return;
+            if (d === n) return;
             n = d
         }
-        a ? -1 === f && (f = setTimeout(function () {
+        a ? -1 === f && (f = setTimeout(function() {
             q(e);
             f = -1
         }, b._throttle)) : q(e)
     };
     c(b._applyDataApi);
-    c(window).bind("load", function (a) {
+    c(window).bind("load", function(a) {
         b._update(!1, a)
     });
-    c(window).bind("resize orientationchange", function (a) {
+    c(window).bind("resize orientationchange", function(a) {
         b._update(!0, a)
     })
 })(jQuery);
 
 // 高さ揃えの要素を指定
-$(function () {
+$(function() {
 
     $('.pickup_item').matchHeight({
         byRow: true,
@@ -334,8 +355,8 @@ $(function () {
 // data-confirm : falseを定義すると確認ダイアログを出さない。デフォルトはダイアログを出す
 // data-message : 確認ダイアログを出す際のメッセージをデフォルトから変更する
 //
-$(function () {
-    var createForm = function (action, data) {
+$(function() {
+    var createForm = function(action, data) {
         var $form = $('<form action="' + action + '" method="post"></form>');
         for (input in data) {
             if (data.hasOwnProperty(input)) {
@@ -345,7 +366,7 @@ $(function () {
         return $form;
     };
 
-    $('a[token-for-anchor]').click(function (e) {
+    $('a[token-for-anchor]').click(function(e) {
         e.preventDefault();
         var $this = $(this);
         var data = $this.data();
