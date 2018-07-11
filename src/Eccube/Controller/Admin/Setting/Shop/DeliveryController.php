461639311654a5fc2ed21ca94503b8d46f63a1fb
oywc410 <ouyo@sundaysys.co.jp>
2018-07-11 14:13:46 +0900

    【配送方法登録】デフォルトの商品種別修正

--- src/Eccube/Controller/Admin/Setting/Shop/DeliveryController.php
+++ src/Eccube/Controller/Admin/Setting/Shop/DeliveryController.php
@@ -116,7 +116,7 @@ class DeliveryController extends AbstractController
     public function edit(Request $request, $id = null)
     {
         if (is_null($id)) {
-            $SaleType = $this->saleTypeRepository->findOneBy([]);
+            $SaleType = $this->saleTypeRepository->findOneBy([], ['sort_no' => 'ASC']);
             $Delivery = $this->deliveryRepository->findOneBy([], ['sort_no' => 'DESC']);
 
             $sortNo = 1;
