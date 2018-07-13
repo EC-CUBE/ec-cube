0ed23e101d02c8a2d237c6158a03979de9769440
ryo-endo <ryo_endo@lockon.co.jp>
2018-07-13 18:10:12 +0900

    data-toggleでtooltipとmodalが競合する問題に対応するためjsを修正。

--- html/template/admin/assets/js/script.js
+++ html/template/admin/assets/js/script.js
@@ -17,7 +17,7 @@ mainNavArea();
 //Bootstrap ツールチップ
 var toolTip = function(){
     $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-tooltip="tooltip"]').tooltip()
     })
 };
 
