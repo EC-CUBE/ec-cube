662b581307ca77cecaaa2620fb5e2ac31126ae8a
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-07-23 16:49:49 +0900

    shipping_dateは更新不可のため修正

--- tests/Eccube/Tests/Web/Admin/Shipping/ShippingEditControllerTest.php
+++ tests/Eccube/Tests/Web/Admin/Shipping/ShippingEditControllerTest.php
@@ -89,6 +89,7 @@ class ShippingEditControllerTest extends AbstractAdminWebTestCase
 
     public function testEditAddShippingDate()
     {
+        $this->markTestSkipped('出荷日は更新不可のためスキップ');
         $this->client->enableProfiler();
 
         $Order = $this->createOrder($this->createCustomer());
@@ -123,6 +124,8 @@ class ShippingEditControllerTest extends AbstractAdminWebTestCase
 
     public function testEditAddShippingDateWithNotifyMail()
     {
+        $this->markTestSkipped('出荷日は更新不可のためスキップ');
+
         $this->client->enableProfiler();
 
         $Order = $this->createOrder($this->createCustomer());
