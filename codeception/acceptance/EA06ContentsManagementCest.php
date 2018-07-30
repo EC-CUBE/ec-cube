59f09c533f9bd1849b61028d198493096da3ea05
sai_tanaka <tanaka@sai-net.jp>
2018-07-30 22:25:09 +0900

    #70 【レイアウト管理】ブロック名をリンクを削除する(とび先がないので)  テストケースの修正

--- codeception/acceptance/EA06ContentsManagementCest.php
+++ codeception/acceptance/EA06ContentsManagementCest.php
@@ -153,9 +153,7 @@ class EA06ContentsManagementCest
 
         $I->see('登録が完了しました。', LayoutEditPage::$登録完了メッセージ);
         $I->amOnPage('/user_data/'.$page);
-        /*
         $I->see('新着情報', '.ec-news');
-        */
 
         LayoutManagePage::go($I)->レイアウト編集('下層ページ用レイアウト');
         LayoutEditPage::at($I)
