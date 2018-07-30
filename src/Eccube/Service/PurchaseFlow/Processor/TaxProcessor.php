478fa9c6dac4f3fb2b33256ba5b0f7bcf6c12d7b
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-07-30 12:38:49 +0900

    TaxProcessorで税区分・税表示区分を制御するように修正

--- src/Eccube/Service/PurchaseFlow/Processor/TaxProcessor.php
+++ src/Eccube/Service/PurchaseFlow/Processor/TaxProcessor.php
@@ -13,7 +13,9 @@
 
 namespace Eccube\Service\PurchaseFlow\Processor;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Eccube\Entity\ItemInterface;
+use Eccube\Entity\Master\OrderItemType;
 use Eccube\Entity\Master\TaxDisplayType;
 use Eccube\Entity\Master\TaxType;
 use Eccube\Entity\OrderItem;
@@ -24,6 +26,11 @@ use Eccube\Service\TaxRuleService;
 
 class TaxProcessor implements ItemPreprocessor
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
     /**
      * @var TaxRuleRepository
      */
@@ -40,8 +47,12 @@ class TaxProcessor implements ItemPreprocessor
      * @param TaxRuleRepository $taxRuleRepository
      * @param TaxRuleService $taxRuleService
      */
-    public function __construct(TaxRuleRepository $taxRuleRepository, TaxRuleService $taxRuleService)
-    {
+    public function __construct(
+        EntityManagerInterface $entityManager,
+        TaxRuleRepository $taxRuleRepository,
+        TaxRuleService $taxRuleService
+    ) {
+        $this->entityManager = $entityManager;
         $this->taxRuleRepository = $taxRuleRepository;
         $this->taxRuleService = $taxRuleService;
     }
@@ -54,6 +65,15 @@ class TaxProcessor implements ItemPreprocessor
     {
         if (!$item instanceof OrderItem) {
             return;
+        }
+
+        // 明細種別に応じて税区分, 税表示区分を設定する,
+        $OrderItemType = $item->getOrderItemType();
+        if (!$item->getTaxType()) {
+            $item->setTaxType($this->getTaxType($OrderItemType));
+        }
+        if (!$item->getTaxDisplayType()) {
+            $item->setTaxDisplayType($this->getTaxDisplayType($OrderItemType));
         }
 
         // 税区分: 非課税, 不課税
@@ -75,15 +95,79 @@ class TaxProcessor implements ItemPreprocessor
         // 税込表示の場合は, priceが税込金額のため割り戻す.
         if ($item->getTaxDisplayType()->getId() == TaxDisplayType::INCLUDED) {
             $tax = $this->taxRuleService->calcTaxIncluded(
-                $item->getPrice(), $TaxRule->getTaxRate(), $TaxRule->getRoundingType()->getId(), $TaxRule->getTaxAdjust());
+                $item->getPrice(), $TaxRule->getTaxRate(), $TaxRule->getRoundingType()->getId(),
+                $TaxRule->getTaxAdjust());
         } else {
             $tax = $this->taxRuleService->calcTax(
-                $item->getPrice(), $TaxRule->getTaxRate(), $TaxRule->getRoundingType()->getId(), $TaxRule->getTaxAdjust());
+                $item->getPrice(), $TaxRule->getTaxRate(), $TaxRule->getRoundingType()->getId(),
+                $TaxRule->getTaxAdjust());
         }
 
         $item->setTax($tax);
         $item->setTaxRate($TaxRule->getTaxRate());
         $item->setRoundingType($TaxRule->getRoundingType());
         $item->setTaxRuleId($TaxRule->getId());
+    }
+
+    /**
+     * 税区分を取得する.
+     *
+     * - 商品: 課税
+     * - 送料: 課税
+     * - 値引き: 課税
+     * - 手数料: 課税
+     * - ポイント値引き: 不課税
+     *
+     * @param $OrderItemType
+     *
+     * @return TaxType
+     */
+    protected function getTaxType($OrderItemType)
+    {
+        if ($OrderItemType instanceof OrderItemType) {
+            $OrderItemType = $OrderItemType->getId();
+        }
+
+        switch ($OrderItemType) {
+            case OrderItemType::PRODUCT:
+            case OrderItemType::DELIVERY_FEE:
+            case OrderItemType::DISCOUNT:
+            case OrderItemType::CHARGE:
+            default:
+                return $this->entityManager->find(TaxType::class, TaxType::TAXATION);
+        }
+    }
+
+    /**
+     * 税表示区分を取得する.
+     *
+     * - 商品: 税抜
+     * - 送料: 税込
+     * - 値引き: 税抜
+     * - 手数料: 税込
+     * - ポイント値引き: 税込
+     *
+     * @param $OrderItemType
+     *
+     * @return TaxType
+     */
+    protected function getTaxDisplayType($OrderItemType)
+    {
+        if ($OrderItemType instanceof OrderItemType) {
+            $OrderItemType = $OrderItemType->getId();
+        }
+
+        switch ($OrderItemType) {
+            case OrderItemType::PRODUCT:
+                return $this->entityManager->find(TaxDisplayType::class, TaxDisplayType::EXCLUDED);
+            case OrderItemType::DELIVERY_FEE:
+                return $this->entityManager->find(TaxDisplayType::class, TaxDisplayType::INCLUDED);
+            case OrderItemType::DISCOUNT:
+                return $this->entityManager->find(TaxDisplayType::class, TaxDisplayType::EXCLUDED);
+            case OrderItemType::CHARGE:
+                return $this->entityManager->find(TaxDisplayType::class, TaxDisplayType::INCLUDED);
+            default:
+                return $this->entityManager->find(TaxDisplayType::class, TaxDisplayType::EXCLUDED);
+        }
     }
 }
