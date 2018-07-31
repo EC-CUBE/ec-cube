891af49d460f14342e35a43117c4b2e49d2d2e23
hideki_okajima <hideki_okajima@lockon.co.jp>
2018-07-31 20:37:31 +0900

    fix 配送編集画面 e2eテストが通るように修正

--- codeception/acceptance/EA09ShippingCest.php
+++ codeception/acceptance/EA09ShippingCest.php
@@ -113,7 +113,9 @@ class EA09ShippingCest
 
         $ShippingRegisterPage = ShippingEditPage::at($I);
         $ShippingRegisterPage
-            ->出荷先を追加();
+            ->出荷先を追加()
+            ->商品検索('パーコレーター')
+            ->商品検索結果_選択(1);
 
         /* 正常系 */
         $ShippingRegisterPage
