7bfc01ee7077f2ccd860d170755e8e0b85e9560e
tona-lc <nvtoan@lockon-vn.com>
2018-08-06 13:32:50 +0700

    Unit test

--- tests/Eccube/Tests/Service/PurchaseFlow/Processor/DeliveryFeeProcessorTest.php
+++ tests/Eccube/Tests/Service/PurchaseFlow/Processor/DeliveryFeeProcessorTest.php
@@ -16,12 +16,40 @@ namespace Eccube\Tests\Service\PurchaseFlow\Processor;
 use Eccube\Entity\Master\OrderItemType;
 use Eccube\Entity\Order;
 use Eccube\Entity\OrderItem;
+use Eccube\Entity\Product;
+use Eccube\Entity\ProductClass;
+use Eccube\Repository\BaseInfoRepository;
 use Eccube\Service\PurchaseFlow\Processor\DeliveryFeePreprocessor;
 use Eccube\Service\PurchaseFlow\PurchaseContext;
 use Eccube\Tests\EccubeTestCase;
+use Eccube\Tests\Fixture\Generator;
 
 class DeliveryFeeProcessorTest extends EccubeTestCase
 {
+    /** @var BaseInfoRepository */
+    protected $BaseInfoRepository;
+    /**
+     * @var Product
+     */
+    protected $Product;
+
+    /**
+     * @var ProductClass
+     */
+    protected $ProductClass;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->BaseInfoRepository = $this->container->get(BaseInfoRepository::class);
+        $this->Product = $this->createProduct('テスト商品', 1);
+        $this->ProductClass = $this->Product->getProductClasses()[0];
+    }
+
     public function testProcess()
     {
         $processor = $this->container->get(DeliveryFeePreprocessor::class);
@@ -63,6 +91,44 @@ class DeliveryFeeProcessorTest extends EccubeTestCase
         $processor->process($Order, new PurchaseContext());
 
         $DeliveryFeeList = $this->getDeliveryFees($Order);
+        self::assertCount(1, $DeliveryFeeList);
+        self::assertSame($DeliveryFee, array_shift($DeliveryFeeList));
+    }
+
+    public function testProcessWithDeliveryFeePerProduct()
+    {
+        $BaseInfo = $this->BaseInfoRepository->get();
+        $BaseInfo->setOptionProductDeliveryFee(true);
+        $this->entityManager->persist($BaseInfo);
+        $this->entityManager->flush($BaseInfo);
+
+        if ($this->BaseInfoRepository->get()->isOptionProductDeliveryFee()) {
+            $this->ProductClass->setDeliveryFee(10000);
+            $this->entityManager->persist($this->ProductClass);
+            $this->entityManager->flush($this->ProductClass);
+        }
+
+        $processor = $this->container->get(DeliveryFeePreprocessor::class);
+        $Order = $this->container->get(Generator::class)->createOrder($this->createCustomer(), [$this->ProductClass]);
+
+        /*
+         * @var OrderItem
+         */
+        foreach ($Order->getOrderItems() as $OrderItem) {
+            if ($OrderItem->isDeliveryFee()) {
+                $Order->getOrderItems()->removeElement($OrderItem);
+            }
+        }
+
+        $DeliveryFee = new OrderItem();
+        $OrderItemType = new OrderItemType();
+        $OrderItemType->setId(OrderItemType::DELIVERY_FEE);
+        $DeliveryFee->setOrderItemType($OrderItemType);
+        $Order->addItem($DeliveryFee);
+
+        $processor->process($Order, new PurchaseContext());
+        $DeliveryFeeList = $this->getDeliveryFees($Order);
+
         self::assertCount(1, $DeliveryFeeList);
         self::assertSame($DeliveryFee, array_shift($DeliveryFeeList));
     }
