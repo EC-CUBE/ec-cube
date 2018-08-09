ce6ab273b08abc97fc7d79352be350500082222f
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-08-09 09:44:23 +0900

    受注管理：受注登録

--- src/Eccube/Form/Type/Admin/ShippingType.php
+++ src/Eccube/Form/Type/Admin/ShippingType.php
@@ -303,7 +303,7 @@ class ShippingType extends AbstractType
                 if ($count < 1) {
                     // 画面下部にエラーメッセージを表示させる
                     $form = $event->getForm();
-                    $form['OrderItemsErrors']->addError(new FormError(trans('admin.order.edit.product.error')));
+                    $form['OrderItemsErrors']->addError(new FormError(trans('admin.order.product_item_not_found')));
                 }
             });
     }
