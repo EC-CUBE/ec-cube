2bdd37ec14027f345ff558f3eac730731e58cb25
hideki_okajima <hideki_okajima@lockon.co.jp>
2018-08-10 16:12:45 +0900

    fix メッセージID精査 コンテンツ管理 ブロック管理

--- src/Eccube/Controller/Admin/Content/BlockController.php
+++ src/Eccube/Controller/Admin/Content/BlockController.php
@@ -159,7 +159,7 @@ class BlockController extends AbstractController
             );
             $this->eventDispatcher->dispatch(EccubeEvents::ADMIN_CONTENT_BLOCK_EDIT_COMPLETE, $event);
 
-            $this->addSuccess('admin.register.complete', 'admin');
+            $this->addSuccess('admin.common.save_complete', 'admin');
 
             return $this->redirectToRoute('admin_content_block_edit', ['id' => $Block->getId()]);
         }
@@ -202,7 +202,7 @@ class BlockController extends AbstractController
             );
             $this->eventDispatcher->dispatch(EccubeEvents::ADMIN_CONTENT_BLOCK_DELETE_COMPLETE, $event);
 
-            $this->addSuccess('admin.delete.complete', 'admin');
+            $this->addSuccess('admin.common.delete_complete', 'admin');
 
             // twigキャッシュの削除
             $cacheDir = $this->getParameter('kernel.cache_dir').'/twig';
