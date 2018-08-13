defb1081e10d8a30ea12a993d20f762c0bd8b0ce
lqdung <lqdung@lockon-vn.com>
2018-08-13 16:46:26 +0700

    Add php path input on the authentication setting page.

---
+++ src/Eccube/Form/Type/Admin/AuthenticationType.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eccube\Form\Type\Admin;
+
+
+use Eccube\Common\EccubeConfig;
+use Eccube\Entity\BaseInfo;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class AuthenticationType extends AbstractType
+{
+    /**
+     * @var EccubeConfig
+     */
+    private $eccubeConfig;
+
+    /**
+     * AuthenticationType constructor.
+     * @param EccubeConfig $eccubeConfig
+     */
+    public function __construct(EccubeConfig $eccubeConfig)
+    {
+        $this->eccubeConfig = $eccubeConfig;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'authentication_key', TextType::class,
+            [
+                'label' => 'plugin.label.auth_key',
+                'required' => false,
+                'constraints' => [
+                    new Assert\Regex(['pattern' => '/^[0-9a-zA-Z]+$/']),
+                ],
+            ])
+            ->add('php_path', TextType::class,
+                [
+                    'label' => 'admin.store.authentication_setting.php_path.label',
+                    'required' => false,
+                    'constraints' => [
+                        new Assert\Length([
+                            'max' => $this->eccubeConfig->get('eccube_smtext_len'),
+                        ])
+                    ],
+                ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => BaseInfo::class
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'admin_authentication';
+    }
+}
