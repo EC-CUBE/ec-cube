d324da80f8ff2a2c5a7369f079d465e0bd436257
kengo-kamon <kamon@sai-net.jp>
2018-08-15 13:42:37 +0900

    Revert "コメントを追加"
    This reverts commit bbad905bf25d5401deafd5c4e0f44f4e1c4dac7a.

--- src/Eccube/Service/CartService.php
+++ src/Eccube/Service/CartService.php
@@ -204,7 +204,7 @@ class CartService
                     $Cart = $cart;
                     break;
                 }
-            }
+            } 
         } else {
             $Cart = current($Carts);
         }
@@ -502,13 +502,6 @@ class CartService
         return $cartKey;
     }
 
-    /**
-     * 商品情報を取得する
-     *
-     * @param  ProductClass|integer $ProductClass
-     *
-     * @return ProductClass|null
-     */
     protected function getProductClassById($ProductClass)
     {
         if (!$ProductClass instanceof ProductClass) {
