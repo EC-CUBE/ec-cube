3eabffc3c4c5a40cd60a02adc6a79594a8efc5d1
hideki_okajima <hideki_okajima@lockon.co.jp>
2018-08-16 16:50:16 +0900

    feat フロント画面 商品一覧のN+1問題対応

--- src/Eccube/Repository/ProductRepository.php
+++ src/Eccube/Repository/ProductRepository.php
@@ -65,12 +65,13 @@ class ProductRepository extends AbstractRepository
     public function findWithSortedClassCategories($productId)
     {
         $qb = $this->createQueryBuilder('p');
-        $qb->addSelect(['pc', 'cc1', 'cc2', 'pi', 'ps'])
+        $qb->addSelect(['pc', 'cc1', 'cc2', 'pi', 'ps', 'pt'])
             ->innerJoin('p.ProductClasses', 'pc')
             ->leftJoin('pc.ClassCategory1', 'cc1')
             ->leftJoin('pc.ClassCategory2', 'cc2')
             ->leftJoin('p.ProductImage', 'pi')
             ->innerJoin('pc.ProductStock', 'ps')
+            ->leftJoin('p.ProductTag', 'pt')
             ->where('p.id = :id')
             ->orderBy('cc1.sort_no', 'DESC')
             ->addOrderBy('cc2.sort_no', 'DESC');
