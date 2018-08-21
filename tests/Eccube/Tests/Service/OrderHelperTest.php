3a39ac3ea71d4a4753eba481d9b8b9f8c50a317b
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-08-21 19:11:25 +0900

    fix 不要なテストを削除

--- tests/Eccube/Tests/Service/OrderHelperTest.php
+++ tests/Eccube/Tests/Service/OrderHelperTest.php
@@ -21,41 +21,8 @@ use Eccube\Tests\EccubeTestCase;
 
 class OrderHelperTest extends EccubeTestCase
 {
-    /** @var OrderHelper */
-    private $helper;
-
-    public function setUp()
+    public function testNewInstance()
     {
-        parent::setUp();
-        $this->helper = $this->container->get(OrderHelper::class);
-    }
-
-    public function testConvertToCart_new_cart()
-    {
-        $Order = new Order();
-        self::assertInstanceOf(Cart::class, $this->helper->convertToCart($Order));
-    }
-
-    public function testConvertToCart()
-    {
-        $Product = $this->createProduct('test', 1);
-        $ProductClasses = $Product->getProductClasses()->toArray();
-        $Customer = $this->createCustomer();
-        $Order = $this->createOrderWithProductClasses($Customer, $ProductClasses);
-
-        /** @var OrderItem $OrderItem */
-        $OrderItem = $Order->getOrderItems()->get(0);
-
-        $Cart = $this->helper->convertToCart($Order);
-
-        $CartItems = $Cart->getCartItems();
-        self::assertCount(1, $CartItems);
-
-        $CartItem = $CartItems[0];
-        self::assertEquals($OrderItem->getProductClass(), $CartItem->getProductClass());
-        self::assertEquals($OrderItem->getPriceIncTax(), $CartItem->getPrice());
-        self::assertEquals($OrderItem->getQuantity(), $CartItem->getQuantity());
-
-        self::assertEquals($Order->getPreOrderId(), $Cart->getPreOrderId());
+        $this->assertInstanceOf(OrderHelper::class, $this->helper = $this->container->get(OrderHelper::class));
     }
 }
