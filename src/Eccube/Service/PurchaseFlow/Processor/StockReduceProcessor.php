bfc48c1fc51889446ee9f011c054e85ad6d4f06b
lqdung <lqdung@lockon-vn.com>
2018-08-24 11:26:38 +0700

    Remove pessimistic lock on the product stock table

--- src/Eccube/Service/PurchaseFlow/Processor/StockReduceProcessor.php
+++ src/Eccube/Service/PurchaseFlow/Processor/StockReduceProcessor.php
@@ -13,11 +13,8 @@
 
 namespace Eccube\Service\PurchaseFlow\Processor;
 
-use Doctrine\DBAL\LockMode;
 use Eccube\Entity\ItemHolderInterface;
 use Eccube\Entity\Order;
-use Eccube\Entity\ProductStock;
-use Eccube\Repository\ProductStockRepository;
 use Eccube\Service\PurchaseFlow\PurchaseContext;
 
 /**
@@ -25,21 +22,6 @@ use Eccube\Service\PurchaseFlow\PurchaseContext;
  */
 class StockReduceProcessor extends AbstractPurchaseProcessor
 {
-    /**
-     * @var ProductStockRepository
-     */
-    protected $productStockRepository;
-
-    /**
-     * StockReduceProcessor constructor.
-     *
-     * @param ProductStockRepository $productStockRepository
-     */
-    public function __construct(ProductStockRepository $productStockRepository)
-    {
-        $this->productStockRepository = $productStockRepository;
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -72,13 +54,7 @@ class StockReduceProcessor extends AbstractPurchaseProcessor
         foreach ($itemHolder->getProductOrderItems() as $item) {
             // 在庫が無制限かチェックし、制限ありなら在庫数をチェック
             if (!$item->getProductClass()->isStockUnlimited()) {
-                // 在庫チェックあり
-                // 在庫に対してロック(select ... for update)を実行
-                /* @var ProductStock $productStock */
-                $productStock = $this->productStockRepository->find(
-                    $item->getProductClass()->getProductStock()->getId(), LockMode::PESSIMISTIC_WRITE
-                );
-
+                $productStock = $item->getProductClass()->getProductStock();
                 $stock = $callback($productStock->getStock(), $item->getQuantity());
                 $productStock->setStock($stock);
                 $item->getProductClass()->setStock($stock);
