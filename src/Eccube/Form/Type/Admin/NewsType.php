4053aec0b6d44da08c9ff9a265722f942ab3bb48
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-08-26 11:24:25 +0900

    cs fix

--- src/Eccube/Form/Type/Admin/NewsType.php
+++ src/Eccube/Form/Type/Admin/NewsType.php
@@ -74,7 +74,7 @@ class NewsType extends AbstractType
             ->add('description', TextareaType::class, [
                 'required' => false,
                 'attr' => [
-                    'rows' => 8
+                    'rows' => 8,
                 ],
                 'constraints' => [
                     new Assert\Length(['max' => $this->eccubeConfig['eccube_ltext_len']]),
@@ -86,7 +86,6 @@ class NewsType extends AbstractType
                 'required' => true,
                 'expanded' => false,
             ]);
-
     }
 
     /**
