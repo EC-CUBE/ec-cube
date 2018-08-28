096cb37ea7acfcdfb3aae8e576b581e8c8e824b5
Kiyoshi Yamamura <kiyoshi.yamamura@cule.jp>
2018-08-28 12:38:37 +0900

    お届け先を複数指定した際、商品の在庫数が正常に減らない不具合修正

--- src/Eccube/Service/PurchaseFlow/Processor/StockReduceProcessor.php
+++ src/Eccube/Service/PurchaseFlow/Processor/StockReduceProcessor.php
@@ -84,6 +84,7 @@ class StockReduceProcessor extends AbstractPurchaseProcessor
         foreach ($itemHolder->getProductOrderItems() as $item) {
             // 在庫が無制限かチェックし、制限ありなら在庫を減らす
             if (!$item->getProductClass()->isStockUnlimited()) {
+                $productStock = $item->getProductClass()->getStock();
                 $stock = $callback($productStock->getStock(), $item->getQuantity());
                 $productStock->setStock($stock);
                 $item->getProductClass()->setStock($stock);
