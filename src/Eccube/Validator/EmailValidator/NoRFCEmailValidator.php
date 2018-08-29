ed844af66392843b64ffbf07f1201ac1073bbb56
Kiyoshi Yamamura <kiyoshi.yamamura@cule.jp>
2018-08-29 17:50:15 +0900

    メールアドレスチェックの修正

--- src/Eccube/Validator/EmailValidator/NoRFCEmailValidator.php
+++ src/Eccube/Validator/EmailValidator/NoRFCEmailValidator.php
@@ -26,26 +26,15 @@ class NoRFCEmailValidator extends EmailValidator
      */
     public function isValid($email, EmailValidation $emailValidation)
     {
-        $wsp = '[\x20\x09]';
-        $vchar = '[\x21-\x7e]';
-        $quoted_pair = "\\\\(?:$vchar|$wsp)";
-        $qtext = '[\x21\x23-\x5b\x5d-\x7e]';
-        $qcontent = "(?:$qtext|$quoted_pair)";
-        $quoted_string = "\"$qcontent*\"";
-        $atext = '[a-zA-Z0-9!#$%&\'*+\-\/\=?^_`{|}~]';
-        $dot_atom = "$atext+(?:[.]$atext+)*";
-        $domain = $dot_atom;
-        $dot_atom_loose = "$atext+(?:[.]|$atext)*";
-        $local_part_loose = "(?:$dot_atom_loose|$quoted_string)";
-        $addr_spec_loose = "{$local_part_loose}[@]$domain";
 
-        // 携帯メールアドレス用に、..や.@を許容する。
-        $regexp = "/\A{$addr_spec_loose}\z/";
-
-        if (preg_match($regexp, $email)) {
-            return true;
+        if (substr_count($email, '@') < 1) {
+            return false;
         }
 
-        return false;
+        if (!preg_match('/^.+\@\S+\.\S+$/', $email)) {
+            return false;
+        }
+
+        return true;
     }
 }
