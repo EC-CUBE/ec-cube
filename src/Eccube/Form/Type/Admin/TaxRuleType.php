09adfd81896b0ef9c03ef4df63b9fadcd7649160
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2018-09-05 16:42:05 +0900

    Fix validation to DateTimeType
    - Fix format
    - Fix minute to without leading zero
    - Add testcase

--- src/Eccube/Form/Type/Admin/TaxRuleType.php
+++ src/Eccube/Form/Type/Admin/TaxRuleType.php
@@ -61,7 +61,7 @@ class TaxRuleType extends AbstractType
             ->add('apply_date', DateTimeType::class, [
                 'date_widget' => 'choice',
                 'input' => 'datetime',
-                'format' => 'yyyy-MM-dd hh:mm',
+                'format' => 'yyyy-MM-dd HH:mm',
                 'years' => range(date('Y'), date('Y') + 10),
                 'placeholder' => [
                     'year' => '----', 'month' => '--', 'day' => '--',
