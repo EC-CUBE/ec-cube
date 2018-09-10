0a0546d49bf5565e1d8b30523bd350d9aeb87f74
hideki_okajima <hideki_okajima@lockon.co.jp>
2018-09-10 19:16:31 +0900

    feat 受注一覧画面の検索項目のUnitTestを追加

--- tests/Eccube/Tests/Repository/OrderRepositoryGetQueryBuilderBySearchDataAdminTest.php
+++ tests/Eccube/Tests/Repository/OrderRepositoryGetQueryBuilderBySearchDataAdminTest.php
@@ -21,6 +21,7 @@ use Eccube\Entity\Shipping;
 use Eccube\Repository\Master\OrderStatusRepository;
 use Eccube\Repository\Master\SexRepository;
 use Eccube\Repository\OrderRepository;
+use Eccube\Repository\PaymentRepository;
 use Eccube\Tests\EccubeTestCase;
 use Eccube\Util\StringUtil;
 
@@ -49,6 +50,8 @@ class OrderRepositoryGetQueryBuilderBySearchDataAdminTest extends EccubeTestCase
     protected $orderRepo;
     /** @var SexRepository */
     protected $sexRepo;
+    /** @var PaymentRepository */
+    protected $paymentRepo;
 
     public function setUp()
     {
@@ -56,6 +59,7 @@ class OrderRepositoryGetQueryBuilderBySearchDataAdminTest extends EccubeTestCase
         $this->createProduct();
 
         $this->orderStatusRepo = $this->container->get(OrderStatusRepository::class);
+        $this->paymentRepo = $this->container->get(PaymentRepository::class);
         $this->orderRepo = $this->container->get(OrderRepository::class);
         $this->sexRepo = $this->container->get(SexRepository::class);
         $this->Customer = $this->createCustomer();
@@ -486,6 +490,139 @@ class OrderRepositoryGetQueryBuilderBySearchDataAdminTest extends EccubeTestCase
         $this->expected = 2;
         $this->actual = count($this->Results);
         $this->verify();
+    }
+
+    /**
+     * @param array $searchPaymentNos
+     * @param int $expected
+     *
+     * @dataProvider dataPaymentProvider
+     */
+    public function testPayment(array $searchPaymentNos, int $expected)
+    {
+        // データの準備
+        $Payments = [];
+        for ($i = 1; $i < 4; $i++) {
+            $Payments[$i] = $this->paymentRepo->find($i);
+        }
+
+        // 支払い方法1, 2をそれぞれ設定
+        $this->Order1->setPayment($Payments[1]);
+        $this->Order2->setPayment($Payments[2]);
+
+        $this->entityManager->flush();
+
+        // Paymentの検索リストを作成
+        $Payments = array_filter($Payments, function ($Payment) use($searchPaymentNos){
+            return in_array($Payment->getId(), $searchPaymentNos);
+        });
+
+        // 検索
+        $this->searchData = [
+            'payment' => $Payments,
+        ];
+
+        $this->scenario();
+
+        $this->expected = $expected;
+        $this->actual = count($this->Results);
+        $this->verify();
+    }
+
+    /**
+     * Data for case check Payment.
+     *
+     * @return array
+     */
+    public function dataPaymentProvider()
+    {
+        return [
+            [[1], 1],
+            [[1,2], 2],
+            [[2,3], 1],
+            [[3], 0],
+        ];
+    }
+
+    public function testCompanyName()
+    {
+        $this->Order2->setCompanyName('ダミー会社名');
+        $this->entityManager->flush();
+
+        $this->searchData = [
+            'company_name' => 'ダミー会社名',
+        ];
+        $this->scenario();
+
+        $this->expected = 1;
+        $this->actual = count($this->Results);
+        $this->verify();
+    }
+
+    public function testOrderNo()
+    {
+        $this->Order2->setOrderNo('12345678abcd');
+        $this->entityManager->flush();
+
+        $this->searchData = [
+            'order_no' => '12345678abcd',
+        ];
+        $this->scenario();
+
+        $this->expected = 1;
+        $this->actual = count($this->Results);
+        $this->verify();
+    }
+
+    public function testTrackingNumber()
+    {
+        $this->Order2->getShippings()[0]->setTrackingNumber('1234abcdefgh');
+        $this->entityManager->flush();
+
+        $this->searchData = [
+            'tracking_number' => '1234abcdefgh',
+        ];
+        $this->scenario();
+
+        $this->expected = 1;
+        $this->actual = count($this->Results);
+        $this->verify();
+    }
+
+    /**
+     * @param array $checks
+     * @param int $expected
+     *
+     * @dataProvider dataShippingMailProvider
+     */
+    public function testShippingMail(array $checks, int $expected)
+    {
+        $this->Order2->getShippings()[0]->setMailSendDate(new \DateTime());
+        $this->entityManager->flush();
+
+        $this->searchData = [
+            'shipping_mail' => $checks,
+        ];
+        $this->scenario();
+
+        $this->expected = $expected;
+        $this->actual = count($this->Results);
+        $this->verify();
+    }
+
+    /**
+     * Data for case check shipping mail.
+     *
+     * @return array
+     */
+    public function dataShippingMailProvider()
+    {
+        return [
+            [[], 2],
+            [[Shipping::SHIPPING_MAIL_SENT], 1],
+            [[Shipping::SHIPPING_MAIL_UNSENT], 1],
+            [[Shipping::SHIPPING_MAIL_SENT, Shipping::SHIPPING_MAIL_UNSENT], 2],
+        ];
     }
 
     /**
