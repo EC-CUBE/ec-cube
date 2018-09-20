491a13c502beaefa7e3fdec45ac72dd211dc8bc0
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-09-20 13:38:16 +0900

    実行後のレスポンスに対してチェックするように修正

--- src/Eccube/Security/Http/Authentication/EccubeAuthenticationFailureHandler.php
+++ src/Eccube/Security/Http/Authentication/EccubeAuthenticationFailureHandler.php
@@ -14,51 +14,23 @@
 namespace Eccube\Security\Http\Authentication;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationFailureHandler;
-use Symfony\Component\Security\Http\ParameterBagUtils;
 
 class EccubeAuthenticationFailureHandler extends DefaultAuthenticationFailureHandler
 {
-
     /**
      * {@inheritdoc}
      */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        if ($failureUrl = ParameterBagUtils::getRequestParameterValue($request, $this->options['failure_path_parameter'])) {
-            if (preg_match('/^https?:\\\\/i', $failureUrl)) {
-                $this->options['failure_path'] = '/';
-            } else {
-                $this->options['failure_path'] = $failureUrl;
-            }
+        $response = parent::onAuthenticationFailure($request, $exception);
+        $location = $response->headers->get('location');
+        if (null !== $location && preg_match('/^https?:\\\\/i', $location)) {
+            $response->headers->set('location', $request->getUriForPath('/'));
         }
 
-        if (null === $this->options['failure_path']) {
-            $this->options['failure_path'] = $this->options['login_path'];
-        }
-
-        if ($this->options['failure_forward']) {
-            if (null !== $this->logger) {
-                $this->logger->debug('Authentication failure, forward triggered.', array('failure_path' => $this->options['failure_path']));
-            }
-
-            $subRequest = $this->httpUtils->createRequest($request, $this->options['failure_path']);
-            $subRequest->attributes->set(Security::AUTHENTICATION_ERROR, $exception);
-
-            return $this->httpKernel->handle($subRequest, HttpKernelInterface::SUB_REQUEST);
-        }
-
-        if (null !== $this->logger) {
-            $this->logger->debug('Authentication failure, redirect triggered.', array('failure_path' => $this->options['failure_path']));
-        }
-
-        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
-
-
-        return $this->httpUtils->createRedirectResponse($request, $this->options['failure_path']);
+        return $response;
     }
 
 }
--- src/Eccube/Security/Http/Authentication/EccubeAuthenticationSuccessHandler.php
+++ src/Eccube/Security/Http/Authentication/EccubeAuthenticationSuccessHandler.php
@@ -14,49 +14,22 @@
 namespace Eccube\Security\Http\Authentication;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationSuccessHandler;
-use Symfony\Component\Security\Http\ParameterBagUtils;
-use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class EccubeAuthenticationSuccessHandler extends DefaultAuthenticationSuccessHandler
 {
-    use TargetPathTrait;
-
     /**
-     * Builds the target URL according to the defined options.
-     *
-     * @return string
+     * {@inheritdoc}
      */
-    protected function determineTargetUrl(Request $request)
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
     {
-        if ($this->options['always_use_default_target_path']) {
-            return $this->options['default_target_path'];
+        $response = parent::onAuthenticationSuccess($request, $token);
+        $location = $response->headers->get('location');
+        if (null !== $location && preg_match('/^https?:\\\\/i', $location)) {
+            $response->headers->set('location', $request->getUriForPath('/'));
         }
 
-        if ($targetUrl = ParameterBagUtils::getRequestParameterValue($request, $this->options['target_path_parameter'])) {
-            if (preg_match('/^https?:\\\\/i', $targetUrl)) {
-                $targetUrl = '/';
-            }
-
-            return $targetUrl;
-        }
-
-        if (null !== $this->providerKey && $targetUrl = $this->getTargetPath($request->getSession(), $this->providerKey)) {
-            $this->removeTargetPath($request->getSession(), $this->providerKey);
-
-            return $targetUrl;
-        }
-
-        if ($this->options['use_referer'] && $targetUrl = $request->headers->get('Referer')) {
-            if (false !== $pos = strpos($targetUrl, '?')) {
-                $targetUrl = substr($targetUrl, 0, $pos);
-            }
-            if ($targetUrl && $targetUrl !== $this->httpUtils->generateUri($request, $this->options['login_path'])) {
-                return $targetUrl;
-            }
-        }
-
-        return $this->options['default_target_path'];
+        return $response;
     }
-
 }
