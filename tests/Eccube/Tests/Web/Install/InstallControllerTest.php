645c81b713d9b7b7ff68d9aec1977c6825d14097
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-02-07 14:33:43 +0900

    Backup dotenv file

--- tests/Eccube/Tests/Web/Install/InstallControllerTest.php
+++ tests/Eccube/Tests/Web/Install/InstallControllerTest.php
@@ -40,6 +40,16 @@ class InstallControllerTest extends AbstractWebTestCase
     protected $request;
 
     /**
+     * @var string
+     */
+    protected $envFile;
+
+    /**
+     * @var string
+     */
+    protected $envFileBackup;
+
+    /**
      * @var Session
      */
     protected $session;
@@ -48,9 +58,10 @@ class InstallControllerTest extends AbstractWebTestCase
     {
         parent::setUp();
 
-        $envFile = $this->container->getParameter('kernel.project_dir').'/.env';
-        if (file_exists($envFile)) {
-            unlink($envFile);
+        $this->envFile = $this->container->getParameter('kernel.project_dir').'/.env';
+        $this->envFileBackup = $this->envFile.'.'.date('YmdHis');
+        if (file_exists($this->envFile)) {
+            rename($this->envFile, $this->envFileBackup);
         }
 
         $formFactory = $this->container->get('form.factory');
@@ -68,6 +79,14 @@ class InstallControllerTest extends AbstractWebTestCase
         $propContainer->setValue($this->controller, $this->container);
 
         $this->request = $this->createMock(Request::class);
+    }
+
+    public function tearDown()
+    {
+        if (file_exists($this->envFileBackup)) {
+            rename($this->envFileBackup, $this->envFile);
+        }
+        parent::tearDown();
     }
 
     public function testIndex()
