0f5d276a7b0875cef849eb0eb7d8bfa1d0fb9882
hideki_okajima <hideki_okajima@lockon.co.jp>
2019-03-25 10:57:58 +0900

    fix コメントを修正

--- src/Eccube/Controller/Admin/Order/EditController.php
+++ src/Eccube/Controller/Admin/Order/EditController.php
@@ -658,7 +658,7 @@ class EditController extends AbstractController
 
     protected function newOrder(Application $app)
     {
-        // ランダムなpre_order_idを作成
+        // ユニークでランダムなpre_order_idを作成
         do {
             $preOrderId = sha1(Str::random(32));
             $Order = $app['eccube.repository.order']->findOneBy(array(
