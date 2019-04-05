ec698dcfafc51dcc72032e2751e1f2e3e3bedd59
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2019-04-05 10:46:17 +0900

    コピーライトの更新

--- html/template/admin/assets/js/layout_design.js
+++ html/template/admin/assets/js/layout_design.js
@@ -1,9 +1,9 @@
 /*
  * This file is part of EC-CUBE
  *
- * Copyright(c) 2000-2014 LOCKON CO.,LTD. All Rights Reserved.
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
  *
- * http://www.lockon.co.jp/
+ * http://www.ec-cube.co.jp/
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
