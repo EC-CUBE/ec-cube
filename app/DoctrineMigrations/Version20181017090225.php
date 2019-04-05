db6c14f3fdd33b9b9a0e4344bd103eae6130b19d
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2019-04-05 10:23:57 +0900

    コピーライト更新

--- app/DoctrineMigrations/Version20181017090225.php
+++ app/DoctrineMigrations/Version20181017090225.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 /*
  * This file is part of EC-CUBE
  *
- * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
  *
- * http://www.lockon.co.jp/
+ * http://www.ec-cube.co.jp/
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
--- html/template/admin/assets/js/file_manager.js
+++ html/template/admin/assets/js/file_manager.js
@@ -3,7 +3,7 @@
  *
  * Copyright(c) 2000-2014 LOCKON CO.,LTD. All Rights Reserved.
  *
- * http://www.lockon.co.jp/
+ * http://www.ec-cube.co.jp/
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
