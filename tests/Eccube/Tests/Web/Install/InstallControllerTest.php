05797a31d92adbc4dabd5a0b605b4ec61824d346
junpeko5 <junpeko5@gmail.com>
2019-06-20 23:52:41 +0900

    テストコードを、Webインストール時の実数チェックの正規表現と同様にする

--- tests/Eccube/Tests/Web/Install/InstallControllerTest.php
+++ tests/Eccube/Tests/Web/Install/InstallControllerTest.php
@@ -407,7 +407,7 @@ class InstallControllerTest extends AbstractWebTestCase
     public function testDatabaseVersion()
     {
         $version = $this->controller->getDatabaseVersion($this->entityManager);
-        $this->assertRegExp('/\A[\d\.]+/', $version);
+        $this->assertRegExp('/\A([\d+\.]+)/', $version);
     }
 
     public function testCreateAppData()
