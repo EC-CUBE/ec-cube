e01634e1d2a556a778ed5dc52815376fe34b68a8
junpeko5 <junpeko5@gmail.com>
2019-07-09 14:20:40 +0900

    商品CSVアップロード時、商品詳細の文字数が保存できる数を超えた場合にエラーメッセージを表示するように修正
    商品詳細の文字数が、マルチバイトで4000文字を超えた場合にエラーメッセージを表示させる

--- src/Eccube/Controller/Admin/Product/CsvImportController.php
+++ src/Eccube/Controller/Admin/Product/CsvImportController.php
@@ -287,7 +287,15 @@ class CsvImportController extends AbstractCsvImportController
 
                         if (isset($row[$headerByKey['description_detail']])) {
                             if (StringUtil::isNotBlank($row[$headerByKey['description_detail']])) {
-                                $Product->setDescriptionDetail(StringUtil::trimAll($row[$headerByKey['description_detail']]));
+                                if (mb_strlen($row[$headerByKey['description_detail']]) >= 4000) {
+                                    $message = trans('admin.common.csv_invalid_description_detail_upper_limit', ['%line%' => $line, '%name%' => $headerByKey['description_detail']]);
+                                    $this->addErrors($message);
+
+                                    return $this->renderWithError($form, $headers);
+
+                                } else {
+                                    $Product->setDescriptionDetail(StringUtil::trimAll($row[$headerByKey['description_detail']]));
+                                }
                             } else {
                                 $Product->setDescriptionDetail(null);
                             }
