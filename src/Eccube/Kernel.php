d4da9d3cb6a7a19a91148e4b94fff0e464b226d4
kurozumi <akira.kurozumi.jp@gmail.com>
2019-07-23 16:23:46 +0900

    開発環境用のservices.yamlをプラグインでも用意できるようにする
    開発環境用のプラグインを作りたい。

--- src/Eccube/Kernel.php
+++ src/Eccube/Kernel.php
@@ -126,6 +126,7 @@ class Kernel extends BaseKernel
         // プラグインのservices.phpをロードする.
         $dir = dirname(__DIR__).'/../app/Plugin/*/Resource/config';
         $loader->load($dir.'/services'.self::CONFIG_EXTS, 'glob');
+        $loader->load($dir.'/services_'.$this->environment.self::CONFIG_EXTS, 'glob');
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes)
