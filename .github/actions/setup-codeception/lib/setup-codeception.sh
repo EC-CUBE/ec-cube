7c3e6fc8312093c5a7007643e9762376be6fec8d
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-09-30 18:58:03 +0900

    Fix setup to chromedriver

--- .github/actions/setup-codeception/lib/setup-codeception.sh
+++ .github/actions/setup-codeception/lib/setup-codeception.sh
@@ -6,11 +6,6 @@ sudo apt-fast install -y build-essential debconf-utils screen google-chrome-stab
 sudo apt-fast install -y unzip xvfb autogen autoconf libtool pkg-config nasm libgconf-2-4
 wget -c -nc --retry-connrefused --tries=0 http://chromedriver.storage.googleapis.com/2.43/chromedriver_linux64.zip
 unzip -o -q chromedriver_linux64.zip
-export DISPLAY=:99
-./chromedriver --url-base=/wd/hub &
-echo ">>> Started chrome-driver"
-sudo Xvfb -ac :99 -screen 0 1280x1024x24 > /dev/null 2>&1 &
-echo ">>> Started xvfb"
 
 if [[ $1 == '--reset' ]]; then
     rm -rf app/Plugin/* html
@@ -25,9 +20,5 @@ if [[ $1 == '--reset' ]]; then
     bin/console doctrine:schema:drop --force --full-database --env=dev
     bin/console doctrine:schema:create --env=dev
     bin/console eccube:fixtures:load --env=dev
-    exit
 fi
 
-php -S localhost:8000 &
-
-exit 0
