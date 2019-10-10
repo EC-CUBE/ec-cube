dffbea8129fe7b1e353f9ab0e4dca9528d5dc1b4
Rights1995 <web@right-s.net>
2019-10-10 15:00:58 +0900

    商品画像アップロード時のリサイズ処理

--- src/Eccube/Controller/Admin/Product/ProductController.php
+++ src/Eccube/Controller/Admin/Product/ProductController.php
@@ -330,67 +330,62 @@ class ProductController extends AbstractController
                         throw new UnsupportedMediaTypeHttpException();
                     }
 
-						// 2019-10-07 Add Start
-						// 加工前の画像の情報を取得
-						list($origin_w, $origin_h, $type) = getimagesize($image);
+                    if ($this->eccubeConfig->get('eccube_product_image_resize')) {
 
-						// 加工前のファイルをフォーマット別に読み出す
-						switch ($type) {
-							case IMAGETYPE_JPEG:
-								$origin_image = imagecreatefromjpeg($image);
-								break;
+                        // 加工前の画像の情報を取得
+                        list($origin_w, $origin_h, $type) = getimagesize($image);
 
-							case IMAGETYPE_PNG:
-								$origin_image = imagecreatefrompng($image);
-								break;
+                        // 加工前のファイルをフォーマット別に読み出す
+                        switch ($type) {
+                            case IMAGETYPE_JPEG:
+                                $origin_image = imagecreatefromjpeg($image);
+                                break;
 
-							case IMAGETYPE_GIF:
-								$origin_image = imagecreatefromgif($image);
-								break;
+                            case IMAGETYPE_PNG:
+                                $origin_image = imagecreatefrompng($image);
+                                break;
 
-							default:
-								throw new RuntimeException('対応していないファイル形式です。: ', $type);
-						}
+                            case IMAGETYPE_GIF:
+                                $origin_image = imagecreatefromgif($image);
+                                break;
 
-						// 新しく描画するキャンバスを作成
-						$canvas = imagecreatetruecolor($origin_w, $origin_h);
-						imagecopyresampled($canvas, $origin_image, 0,0,0,0, $origin_w, $origin_h, $origin_w, $origin_h);
+                            default:
+                                throw new RuntimeException('対応していないファイル形式です。: ', $type);
+                        }
 
-						$filename = date('mdHis').uniqid('_').'.'.$extension;
-						$files[] = $filename;
+                        // 新しく描画するキャンバスを作成
+                        $canvas = imagecreatetruecolor($origin_w, $origin_h);
+                        imagecopyresampled($canvas, $origin_image, 0,0,0,0, $origin_w, $origin_h, $origin_w, $origin_h);
 
-						// 保存先を指定
-						$resize_path = $this->eccubeConfig['eccube_temp_image_dir'] . '/' . $filename;
+                        $filename = date('mdHis').uniqid('_').'.'.$extension;
+                        $files[] = $filename;
 
-						switch ($type) {
-							case IMAGETYPE_JPEG:
-								imagejpeg($canvas, $resize_path, 60);
-								break;
+                        // 保存先を指定
+                        $resize_path = $this->eccubeConfig['eccube_temp_image_dir'] . '/' . $filename;
 
-							case IMAGETYPE_PNG:
-								imagepng($canvas, $resize_path, 9);
-								break;
+                        switch ($type) {
+                            case IMAGETYPE_JPEG:
+                                imagejpeg($canvas, $resize_path, $this->eccubeConfig->get('eccube_product_image_jpg_quality'));
+                                break;
 
-							case IMAGETYPE_GIF:
-								imagegif($canvas, $resize_path);
-								break;
+                            case IMAGETYPE_PNG:
+                                imagepng($canvas, $resize_path, $this->eccubeConfig->get('eccube_product_image_png_quality'));
+                                break;
 
-						}
+                            case IMAGETYPE_GIF:
+                                imagegif($canvas, $resize_path);
+                                break;
+                        }
 
-						imagedestroy($origin_image);
-						imagedestroy($canvas);
-						// 2019-10-07 Add End
+                        imagedestroy($origin_image);
+                        imagedestroy($canvas);
+                    } else {
 
-						/* 2019-10-08 Comment Out ↓
-						// 拡張子
-						$extension = $image->getClientOriginalExtension();
-						if (!in_array(strtolower($extension), $allowExtensions)) {
-							throw new UnsupportedMediaTypeHttpException();
-						}
-						$filename = date('mdHis').uniqid('_').'.'.$extension;
-						$canvas->move($this->eccubeConfig['eccube_temp_image_dir'], $filename);
-						$files[] = $filename;
-						2019-10-08 Comment Out ↑ */
+                        $filename = date('mdHis').uniqid('_').'.'.$extension;
+                        $image->move($this->eccubeConfig['eccube_temp_image_dir'], $filename);
+                        $files[] = $filename;
+
+                    }
                 }
             }
         }
