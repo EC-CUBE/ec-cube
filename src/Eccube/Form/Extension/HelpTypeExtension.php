91f541b0c64611e23d72c2e5c77c366cb4d81fa6
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2019-12-05 17:56:29 +0900

    Revert to FormExtension

---
+++ src/Eccube/Form/Extension/HelpTypeExtension.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eccube\Form\Extension;
+
+use Eccube\Annotation\FormExtension;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @FormExtension
+ */
+class HelpTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->setAttribute('help', $options['help']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['help'] = $form->getConfig()->getAttribute('help');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'help' => null,
+        ]);
+    }
+
+    public function getExtendedType()
+    {
+        return FormType::class;
+    }
+
+    /**
+     * Return the class of the type being extended.
+     */
+    public static function getExtendedTypes(): iterable
+    {
+        // return FormType::class to modify (nearly) every field in the system
+        return [FormType::class];
+    }
+}
