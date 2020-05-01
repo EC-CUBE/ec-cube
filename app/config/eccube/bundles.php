37dc9852cc3afd1541d2090c537b68b1e7da4c55
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-05-01 13:21:48 +0900

    依存関係解消のために削除していたパッケージを追加
    - symfony/cache
    - codeception
    - doctrine-test-bundle

--- app/config/eccube/bundles.php
+++ app/config/eccube/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true, 'install' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
