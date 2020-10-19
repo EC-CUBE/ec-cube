5accbe92feab4afffe85d997882affd8ad6b6731
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2020-10-19 16:17:49 +0900

    scrutinizerの指摘を修正

--- src/Eccube/Service/OrderPdfService.php
+++ src/Eccube/Service/OrderPdfService.php
@@ -413,7 +413,7 @@ class OrderPdfService extends TcpdfFpdi
         $this->lfText(27, 51, $Shipping->getAddr02(), 10); //購入者住所2
 
         // 購入者氏名
-        if ($Shipping->getCompanyName()) {
+        if (null !== $Shipping->getCompanyName()) {
             // 会社名
             $text = $Shipping->getCompanyName();
             $this->lfText(27, 57, $text, 11);
