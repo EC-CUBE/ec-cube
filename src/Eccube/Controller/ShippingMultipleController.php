da8d122c1c6682f26dca4f5429aba72e10ccfaef
kurozumi <info@a-zumi.net>
2020-11-12 15:33:15 +0900

    購入手続き中でもPurchaseContextでユーザー情報を取得できるようにする

--- src/Eccube/Controller/ShippingMultipleController.php
+++ src/Eccube/Controller/ShippingMultipleController.php
@@ -343,7 +343,7 @@ class ShippingMultipleController extends AbstractShoppingController
                     }
                 }
 
-                $this->cartPurchaseFlow->validate($Cart, new PurchaseContext());
+                $this->cartPurchaseFlow->validate($Cart, new PurchaseContext($Cart, $this->getUser()));
                 $this->cartService->save();
             }
 
