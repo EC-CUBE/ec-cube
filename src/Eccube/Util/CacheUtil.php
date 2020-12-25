2b6613ea4733a403ec4c68e6d73c764c13675c06
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2020-12-25 17:51:45 +0900

    Fix debug toolber

--- src/Eccube/Util/CacheUtil.php
+++ src/Eccube/Util/CacheUtil.php
@@ -23,8 +23,10 @@ use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\CacheClearer\Psr6CacheClearer;
 use Symfony\Component\HttpKernel\Event\PostResponseEvent;
+use Symfony\Component\HttpKernel\Event\TerminateEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\KernelInterface;
+
 
 /**
  * キャッシュ関連のユーティリティクラス.
