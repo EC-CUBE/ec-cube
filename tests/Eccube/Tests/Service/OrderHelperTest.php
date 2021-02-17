f2bb59658a3a50bcb4ca3189c0efca665918b8e6
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2021-02-17 09:29:12 +0900

    テスト追加

--- tests/Eccube/Tests/Service/OrderHelperTest.php
+++ tests/Eccube/Tests/Service/OrderHelperTest.php
@@ -14,9 +14,12 @@
 namespace Eccube\Tests\Service;
 
 use Eccube\Entity\Customer;
+use Eccube\Entity\Delivery;
 use Eccube\Entity\Order;
+use Eccube\Entity\Payment;
 use Eccube\Service\OrderHelper;
 use Eccube\Tests\EccubeTestCase;
+use Eccube\Util\ReflectionUtil;
 
 class OrderHelperTest extends EccubeTestCase
 {
@@ -68,5 +71,33 @@ class OrderHelperTest extends EccubeTestCase
         $this->helper->updateCustomerInfo($Order, $Customer);
         self::assertNotNull($Order->getName01());
         self::assertSame($Order->getName01(), $Customer->getName01());
+    }
+
+    /**
+     * @see https://github.com/EC-CUBE/ec-cube/issues/4881
+     */
+    public function testSetDefaultPayment()
+    {
+        $paymentRepository = $this->entityManager->getRepository(Payment::class);
+        $Payments = $paymentRepository->findBy(['visible' => true], ['id' => 'asc']);
+        $Payments[0]->setRuleMax(100); // 除外
+        $Payments[1]->setRuleMax(null); $Payments[1]->setRuleMin(100); // 除外
+        $Payments[2]->setRuleMax(null); $Payments[2]->setRuleMin(0); // 対象
+        $Payments[3]->setRuleMax(null); $Payments[3]->setRuleMin(1); // 対象
+
+        $this->entityManager->flush();
+
+        $Customer = $this->createCustomer();
+        $Order = $this->createOrder($Customer);
+        $Order->setPayment(null);
+        $Order->setPaymentMethod(null);
+
+        $refObj = new \ReflectionObject($this->helper);
+        $refMethod = $refObj->getMethod('setDefaultPayment');
+        $refMethod->setAccessible(true);
+        $refMethod->invokeArgs($this->helper, [$Order]);
+
+        self::assertNotNull($Order->getPayment());
+        self::assertSame($Payments[2]->getId(), $Order->getPayment()->getId());
     }
 }
