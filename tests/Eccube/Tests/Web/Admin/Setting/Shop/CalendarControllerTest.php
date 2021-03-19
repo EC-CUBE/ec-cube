171e6a10336f6a84642ed697dfdc1e009fce8c7e
Yuko Kajihara <yuko_kajihara@ec-cube.co.jp>
2021-03-19 15:13:25 +0900

    管理画面の定休日カレンダー設定Webテスト実装完了

--- tests/Eccube/Tests/Web/Admin/Setting/Shop/CalendarControllerTest.php
+++ tests/Eccube/Tests/Web/Admin/Setting/Shop/CalendarControllerTest.php
@@ -13,18 +13,148 @@
 
 namespace Eccube\Tests\Web\Admin\Setting\Shop;
 
+use Eccube\Entity\Calendar;
+use Eccube\Repository\CalendarRepository;
 use Eccube\Tests\Web\Admin\AbstractAdminWebTestCase;
 
 class CalendarControllerTest extends AbstractAdminWebTestCase
 {
-    public function testCalendar()
+
+    /**
+     * @var CalendarRepository
+     */
+    protected $calendarRepository;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->calendarRepository = $this->container->get(CalendarRepository::class);
+    }
+
+    /**
+     * @return Calendar
+     */
+    public function createCalendar()
+    {
+        $TargetCalendar = new Calendar();
+        $TargetCalendar->setTitle('春分の日')
+            ->setHoliday(new \DateTime('2021-03-20 00:00:00'));
+        $this->entityManager->persist($TargetCalendar);
+        $this->entityManager->flush();
+
+        return $TargetCalendar;
+    }
+
+    public function testRouting()
     {
         $this->client->request(
             'GET',
-            $this->generateUrl('admin_setting_shop_tax')
+            $this->generateUrl('admin_setting_shop_calendar_new')
         );
         $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
 
-    // TODO テスト実装
+    public function testRoutingNew()
+    {
+        $this->client->request(
+            'GET',
+            $this->generateUrl('admin_setting_shop_calendar')
+        );
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+    }
+
+    public function testDeleteSuccess()
+    {
+        $Calendar = $this->createCalendar();
+        $id = $Calendar->getId();
+
+        $redirectUrl = $this->generateUrl('admin_setting_shop_calendar');
+
+        $this->client->request(
+            'DELETE',
+            $this->generateUrl('admin_setting_shop_calendar_delete', ['id' => $id])
+        );
+
+        $actual = $this->client->getResponse()->isRedirect($redirectUrl);
+        $this->assertSame(true, $actual);
+    }
+
+    public function testDeleteFail_NotFound()
+    {
+        $id = 99999;
+
+        $this->client->request(
+            'DELETE',
+            $this->generateUrl('admin_setting_shop_calendar_delete', ['id' => $id])
+        );
+        $this->assertSame(404, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testEditSuccess()
+    {
+        $Calendar = $this->createCalendar();
+        $id = $Calendar->getId();
+
+        $form = [
+            '_token' => 'dummy',
+            'title' => '昭和の日',
+            'holiday' => '2021-4-29',
+        ];
+
+        $this->client->request(
+            'POST',
+            $this->generateUrl('admin_setting_shop_calendar'),
+            [
+                'calendar' => $form,
+                'calendar_id' => "$id",
+                'mode' => 'edit_inline',
+            ]
+        );
+
+        $Calendar = $this->calendarRepository->find($id);
+        $this->expected = $form['title'];
+        $this->actual = $Calendar->getTitle();
+        $this->verify();
+
+        $this->expected = $form['holiday'];
+        $holiday = $Calendar->getHoliday();
+        $holiday->setTimezone(new \DateTimeZone('Asia/Tokyo'));
+        $this->actual = $holiday->format('Y-n-j');
+        $this->verify();
+    }
+
+    public function testNewSuccess()
+    {
+        $Calendar = $this->createCalendar();
+        $id = $Calendar->getId();
+        $form = [
+            '_token' => 'dummy',
+            'title' => '憲法記念日',
+            'holiday' => '2021-5-3',
+        ];
+
+        $this->client->request(
+            'POST',
+            $this->generateUrl('admin_setting_shop_calendar'),
+            [
+                'calendar' => $form,
+            ]
+        );
+
+        $Calendar = $this->calendarRepository->find($id + 1);
+
+        $this->expected = $form['title'];
+        $this->actual = $Calendar->getTitle();
+        $this->verify();
+
+        $this->expected = $form['holiday'];
+        $holiday = $Calendar->getHoliday();
+        $holiday->setTimezone(new \DateTimeZone('Asia/Tokyo'));
+        $this->actual = $holiday->format('Y-n-j');
+        $this->verify();
+    }
 }
