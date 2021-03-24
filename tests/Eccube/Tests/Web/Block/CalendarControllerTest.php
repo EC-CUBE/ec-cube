2cdfcce471eb5facc6ff32ab00cf8c276bf593dd
Yuko Kajihara <yuko_kajihara@ec-cube.co.jp>
2021-03-24 16:40:26 +0900

    Webテスト実装

--- tests/Eccube/Tests/Web/Block/CalendarControllerTest.php
+++ tests/Eccube/Tests/Web/Block/CalendarControllerTest.php
@@ -13,21 +13,74 @@
 
 namespace Eccube\Tests\Web\Block;
 
+use Carbon\Carbon;
+use Eccube\Entity\Calendar;
+use Eccube\Repository\CalendarRepository;
 use Eccube\Tests\Web\AbstractWebTestCase;
 
 class CalendarControllerTest extends AbstractWebTestCase
 {
-    public function testCalendar()
+    public function testRoutingCalendar()
     {
         $this->client->request('GET', '/block/calendar');
         $this->assertTrue($this->client->getResponse()->isSuccessful());
     }
 
-//    public function testTodayStile()
-//    {
-//        // TODO スタイルが当たってからテスト実装開始
-//        $crawler = $this->client->request('GET', $this->generateUrl('block_calendar'));
-//        $node = $crawler->filter('');
-//        $this->assertEquals('', $node->attr(''));
-//    }
+    public function testThisMonthTitle()
+    {
+        $crawler = $this->client->request('GET', $this->generateUrl('block_calendar'));
+        $this->expected = Carbon::now()->format('Y年n月');
+        $this->actual = $crawler->filter('#this-month-title')->text();
+        $this->verify();
+    }
+
+    public function testNextMonthTitle()
+    {
+        $crawler = $this->client->request('GET', $this->generateUrl('block_calendar'));
+        $this->expected = Carbon::now()->addMonth(1)->format('Y年n月');
+        $this->actual = $crawler->filter('#next-month-title')->text();
+        $this->verify();
+    }
+
+    public function testTodayAndHolidayStile()
+    {
+        $today = new \DateTime();
+        $Calendar = new Calendar();
+        $Calendar->setTitle('今日かつ定休日のパターン')
+            ->setHoliday($today);
+        $this->container->get(CalendarRepository::class);
+        $this->entityManager->persist($Calendar);
+        $this->entityManager->flush();
+
+        $crawler = $this->client->request('GET', $this->generateUrl('block_calendar'));
+        $this->expected = $today->format('j');
+        $this->actual = $crawler->filter('#today-and-holiday')->text();
+        $this->verify();
+    }
+
+    public function testHolidayStile()
+    {
+        $holiday = new \DateTime(Carbon::now()->format('Y-m').'-01');
+        $Calendar = new Calendar();
+        $Calendar->setTitle('今月の月初が定休日のパターン')
+            ->setHoliday($holiday);
+        $this->container->get(CalendarRepository::class);
+        $this->entityManager->persist($Calendar);
+        $this->entityManager->flush();
+
+        $crawler = $this->client->request('GET', $this->generateUrl('block_calendar'));
+        $this->expected = '1';
+        $this->actual = $crawler->filter('.ec-calendar__holiday')->text();
+        $this->verify();
+    }
+
+    public function testTodayStile()
+    {
+        $today = new \DateTime();
+
+        $crawler = $this->client->request('GET', $this->generateUrl('block_calendar'));
+        $this->expected = $today->format('j');
+        $this->actual = $crawler->filter('#today')->text();
+        $this->verify();
+    }
 }
