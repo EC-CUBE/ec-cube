3c5d927182079a078162d2ff4bcdd70575fcd7b7
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2021-04-12 17:13:29 +0900

    プラグインからのマイグレーションを追加

---
+++ app/DoctrineMigrations/Version20210412073123.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
+ *
+ * http://www.ec-cube.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Eccube\Entity\Master\LoginHistoryStatus;
+use Eccube\Util\StringUtil;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210412073123 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        if ($schema->hasTable('plg_admin_record_config')) {
+            $denyHosts = array_merge(
+                env('ECCUBE_ADMIN_DENY_HOSTS', []),
+                $this->connection->fetchArray('select admin_deny_hosts FROM plg_admin_record_config') ?: []
+            );
+
+            $denyHosts = \json_encode($denyHosts);
+
+            $envFile = __DIR__.'/../../.env';
+            $env = file_get_contents($envFile);
+
+            $env = StringUtil::replaceOrAddEnv($env, [
+                'ECCUBE_ADMIN_DENY_HOSTS' => "'{$denyHosts}'",
+            ]);
+
+            file_put_contents($envFile, $env);
+        }
+
+        if ($schema->hasTable('plg_admin_record')) {
+            $stmt = $this->connection->createQueryBuilder()
+                ->select('*')
+                ->from('plg_admin_record')
+                ->orderBy('id', 'ASC')
+                ->execute();
+
+            while ($row = $stmt->fetch()) {
+                $this->addSql(
+                    "INSERT INTO dtb_login_history (user_name, client_ip, create_date, update_date, login_history_status_id, member_id, discriminator_type) VALUES (?, ?, ?, ?, ?, ?, 'loginhistory')",
+                    [
+                        $row['user_name'],
+                        $row['client_ip'],
+                        $row['create_date'],
+                        $row['update_date'],
+                        $row['success_flg'] ? LoginHistoryStatus::SUCCESS : LoginHistoryStatus::FAILURE,
+                        $row['member_id'],
+                    ]
+                );
+            }
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+    }
+}
