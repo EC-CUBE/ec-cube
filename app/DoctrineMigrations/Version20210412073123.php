ac5b9b200868f6e48233e7c56e5a5dba2365daac
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2021-04-15 10:47:17 +0900

    downを実装

--- app/DoctrineMigrations/Version20210412073123.php
+++ app/DoctrineMigrations/Version20210412073123.php
@@ -70,6 +70,8 @@ final class Version20210412073123 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        if ($schema->hasTable('dtb_login_history')) {
+            $this->addSql('DROP TABLE dtb_login_history');
+        }
     }
 }
