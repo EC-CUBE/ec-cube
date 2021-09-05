723b6f9b561917ae0a81f535adc998c5c9ca7e87
kurozumi <info@a-zumi.net>
2021-09-05 11:31:44 +0900

    PurchaseFlowのProcessorやValidatorにpriorityを設定できるようにする

--- src/Eccube/DependencyInjection/Compiler/PurchaseFlowPass.php
+++ src/Eccube/DependencyInjection/Compiler/PurchaseFlowPass.php
@@ -18,12 +18,16 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Eccube\Annotation\CartFlow;
 use Eccube\Annotation\OrderFlow;
 use Eccube\Annotation\ShoppingFlow;
+use Eccube\Service\PurchaseFlow\PurchaseContext;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
 class PurchaseFlowPass implements CompilerPassInterface
 {
+    use PriorityTaggedServiceTrait;
+
     const ITEM_PREPROCESSOR_TAG = 'eccube.item.preprocessor';
     const ITEM_VALIDATOR_TAG = 'eccube.item.validator';
     const ITEM_HOLDER_PREPROCESSOR_TAG = 'eccube.item.holder.preprocessor';
@@ -34,26 +38,37 @@ class PurchaseFlowPass implements CompilerPassInterface
 
     public function process(ContainerBuilder $container)
     {
+        $flowTypes = [
+            PurchaseContext::CART_FLOW => $container->findDefinition('eccube.purchase.flow.cart'),
+            PurchaseContext::SHOPPING_FLOW => $container->findDefinition('eccube.purchase.flow.shopping'),
+            PurchaseContext::ORDER_FLOW => $container->findDefinition('eccube.purchase.flow.order')
+        ];
+
+        foreach ($this->getProcessorTags() as $tag => $methodName) {
+            foreach ($this->findAndSortTaggedServices($tag, $container) as $id) {
+                $definition = $container->findDefinition($id);
+                foreach ($definition->getTag($tag) as $attributes) {
+                    if (isset($attributes['flow_type'])) {
+                        foreach ($flowTypes as $flowType => $purchaseFlowDef) {
+                            if ($flowType === $attributes['flow_type']) {
+                                $purchaseFlowDef->addMethodCall($methodName, [new Reference($id)]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         $flowDefs = [
             CartFlow::class => $container->getDefinition('eccube.purchase.flow.cart'),
             ShoppingFlow::class => $container->getDefinition('eccube.purchase.flow.shopping'),
             OrderFlow::class => $container->getDefinition('eccube.purchase.flow.order'),
         ];
 
-        $processorTags = [
-            self::ITEM_PREPROCESSOR_TAG => 'addItemPreprocessor',
-            self::ITEM_VALIDATOR_TAG => 'addItemValidator',
-            self::ITEM_HOLDER_PREPROCESSOR_TAG => 'addItemHolderPreprocessor',
-            self::ITEM_HOLDER_VALIDATOR_TAG => 'addItemHolderValidator',
-            self::ITEM_HOLDER_POST_VALIDATOR_TAG => 'addItemHolderPostValidator',
-            self::DISCOUNT_PROCESSOR_TAG => 'addDiscountProcessor',
-            self::PURCHASE_PROCESSOR_TAG => 'addPurchaseProcessor',
-        ];
-
         AnnotationRegistry::registerAutoloadNamespace('Eccube\Annotation', __DIR__.'/../../../../src');
         $reader = new AnnotationReader();
 
-        foreach ($processorTags as $tag => $methodName) {
+        foreach ($this->getProcessorTags() as $tag => $methodName) {
             $ids = $container->findTaggedServiceIds($tag);
             foreach ($ids as $id => $tags) {
                 $def = $container->getDefinition($id);
@@ -66,5 +81,21 @@ class PurchaseFlowPass implements CompilerPassInterface
                 }
             }
         }
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getProcessorTags(): array
+    {
+        return [
+            self::ITEM_PREPROCESSOR_TAG => 'addItemPreprocessor',
+            self::ITEM_VALIDATOR_TAG => 'addItemValidator',
+            self::ITEM_HOLDER_PREPROCESSOR_TAG => 'addItemHolderPreprocessor',
+            self::ITEM_HOLDER_VALIDATOR_TAG => 'addItemHolderValidator',
+            self::ITEM_HOLDER_POST_VALIDATOR_TAG => 'addItemHolderPostValidator',
+            self::DISCOUNT_PROCESSOR_TAG => 'addDiscountProcessor',
+            self::PURCHASE_PROCESSOR_TAG => 'addPurchaseProcessor',
+        ];
     }
 }
