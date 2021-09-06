9db51c2b27f0041900403751e0116fdcc2d772bc
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2021-09-06 14:42:19 +0900

    Revert "Revert "管理画面 商品のタグでの絞り込み""
    This reverts commit 7233312d6c960bae66a873d3df38917b79a30a00.

--- src/Eccube/Form/Type/Admin/SearchProductType.php
+++ src/Eccube/Form/Type/Admin/SearchProductType.php
@@ -16,10 +16,13 @@ namespace Eccube\Form\Type\Admin;
 use Eccube\Entity\Category;
 use Eccube\Entity\Master\ProductStatus;
 use Eccube\Entity\ProductStock;
+use Eccube\Entity\Tag;
 use Eccube\Form\Type\Master\CategoryType as MasterCategoryType;
 use Eccube\Form\Type\Master\ProductStatusType;
 use Eccube\Repository\CategoryRepository;
 use Eccube\Repository\Master\ProductStatusRepository;
+use Eccube\Repository\TagRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
@@ -40,15 +43,25 @@ class SearchProductType extends AbstractType
     protected $categoryRepository;
 
     /**
+     * @var TagRepository
+     */
+    protected $tagRepository;
+
+    /**
      * SearchProductType constructor.
      *
      * @param ProductStatusRepository $productStatusRepository
      * @param CategoryRepository $categoryRepository
+     * @param TagRepository $tagRepository
      */
-    public function __construct(ProductStatusRepository $productStatusRepository, CategoryRepository $categoryRepository)
-    {
+    public function __construct(
+        ProductStatusRepository $productStatusRepository,
+        CategoryRepository $categoryRepository,
+        TagRepository $tagRepository
+    ) {
         $this->productStatusRepository = $productStatusRepository;
         $this->categoryRepository = $categoryRepository;
+        $this->tagRepository = $tagRepository;
     }
 
     /**
@@ -91,6 +104,15 @@ class SearchProductType extends AbstractType
                 ],
                 'expanded' => true,
                 'multiple' => true,
+            ])
+            ->add('tag_id', EntityType::class, [
+                'class' => Tag::class,
+                'label' => 'admin.product.tag',
+                'placeholder' => 'common.select__all_products',
+                'choice_label' => 'name',
+                'required' => false,
+                'multiple' => false,
+                'expanded' => false,
             ])
             ->add('create_date_start', DateType::class, [
                 'label' => 'admin.common.create_date__start',
