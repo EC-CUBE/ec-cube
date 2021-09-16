7e9cf8e50aee7e48c6a57ff294946015e83d79f1
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2021-09-16 13:54:35 +0900

    Fix inScopeOnly to false

--- zap/selenium/ci/TypeScript/test/front_guest/contact.test.ts
+++ zap/selenium/ci/TypeScript/test/front_guest/contact.test.ts
@@ -31,7 +31,7 @@ test('お問い合わせ - GET', async () => {
       , 10000).getText();
     expect(title).toBe('お問い合わせ');
 
-    const scanId = await zapClient.activeScan(url, false, true, null, 'GET', null, 1);
+    const scanId = await zapClient.activeScan(url, false, false, null, 'GET', null, 1);
 
     await intervalRepeater(async () => await zapClient.getActiveScanStatus(scanId), 5000);
 
@@ -69,7 +69,7 @@ test('お問い合わせ(入力ページ→確認ページ) - POST', async () =>
     await driver.findElement(By.xpath('//*[@id="page_contact"]/div[1]/div[2]/div/div/div[2]/div/form/div[2]/div/div/button')).click();
 
     const message = await zapClient.getLastMessage(url);
-    const scanId = await zapClient.activeScan(url, false, true, null, 'GET', message.requestBody, 1);
+    const scanId = await zapClient.activeScan(url, false, false, null, 'GET', message.requestBody, 1);
 
     await intervalRepeater(async () => await zapClient.getActiveScanStatus(scanId), 5000);
 
@@ -114,7 +114,7 @@ test('お問い合わせ(確認ページ→完了ページ) - POST', async () =>
     );
 
     const completeMessage = await zapClient.getLastMessage(url);
-    const scanId = await zapClient.activeScan(url, false, true, null, 'GET', completeMessage.requestBody, 1);
+    const scanId = await zapClient.activeScan(url, false, false, null, 'GET', completeMessage.requestBody, 1);
 
     await intervalRepeater(async () => await zapClient.getActiveScanStatus(scanId), 5000);
 
