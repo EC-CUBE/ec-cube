c00df4a4e5b0c471693aba43862745b2cedd6c5e
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2021-12-22 10:28:01 +0900

    失敗時にリトライしないよう修正

--- zap/selenium/ci/TypeScript/playwright.config.ts
+++ zap/selenium/ci/TypeScript/playwright.config.ts
@@ -22,8 +22,8 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
 
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* No retries */
+  retries: 0,
 
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
