dc6ea5d9b736f1983bc5cbe69619fb7ef304e2b1
Yousuke Hayashi <dev.belltzel@gmail.com>
2022-02-24 13:07:17 +0900

    支払方法一覧のテスト追加

---
+++ zap/selenium/ci/TypeScript/test/admin/setting_shop_payment.test.ts
@@ -0,0 +1,46 @@
+import { test, expect, chromium, Page } from '@playwright/test';
+import { intervalRepeater } from '../../utils/Progress';
+import { ZapClient, Mode, ContextType, Risk, HttpMessage } from '../../utils/ZapClient';
+const zapClient = new ZapClient('http://127.0.0.1:8090');
+
+const baseURL = 'https://ec-cube/admin';
+const url = baseURL + '/setting/shop/payment';
+
+test.describe.serial('支払方法一覧ページのテストをします', () => {
+  let page: Page;
+  test.beforeAll(async () => {
+    await zapClient.setMode(Mode.Protect);
+    await zapClient.newSession('/zap/wrk/sessions/admin_index', true);
+    await zapClient.importContext(ContextType.Admin);
+
+    if (!await zapClient.isForcedUserModeEnabled()) {
+      await zapClient.setForcedUserModeEnabled();
+      expect(await zapClient.isForcedUserModeEnabled()).toBeTruthy();
+    }
+    const browser = await chromium.launch();
+    page = await browser.newPage();
+    await page.goto(url);
+  });
+
+  test('支払方法一覧ページを表示します', async () => {
+    await expect(page).toHaveTitle(/支払方法一覧/);
+  });
+
+  test('タイトルを確認します', async () => {
+    await page.textContent('.c-pageTitle__title')
+      .then(title => expect(title).toContain('支払方法一覧'));
+  });
+
+  test.describe('テストを実行します[GET] @attack', () => {
+    let scanId: number;
+    test('アクティブスキャンを実行します', async () => {
+      scanId = await zapClient.activeScanAsUser(url, 2, 55, false, null, 'GET');
+      await intervalRepeater(async () => await zapClient.getActiveScanStatus(scanId), 5000, page);
+    });
+
+    test('結果を確認します', async () => {
+      await zapClient.getAlerts(url, 0, 1, Risk.High)
+        .then(alerts => expect(alerts).toEqual([]));
+    });
+  });
+});
