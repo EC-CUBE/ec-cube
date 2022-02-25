c3890c8478c6d8d0a1060f15eaba3c6da7a1bf65
h.matsuo <hiroshi_matsuo@ec-cube.co.jp>
2022-02-25 16:40:55 +0900

    modify test.beforeAll

--- zap/selenium/ci/TypeScript/test/admin/content_layout_delete.test.ts
+++ zap/selenium/ci/TypeScript/test/admin/content_layout_delete.test.ts
@@ -4,10 +4,12 @@ import { ZapClient, Mode, ContextType, Risk, HttpMessage } from '../../utils/Zap
 const zapClient = new ZapClient('http://127.0.0.1:8090');
 
 const baseURL = 'https://ec-cube/admin';
-const url = baseURL + '/content/layout';
+let url = null
 
 test.describe.serial('レイアウト管理>削除のテスト', () => {
   let page: Page;
+  let deleteButton = null
+
   test.beforeAll(async () => {
     await zapClient.setMode(Mode.Protect);
     await zapClient.newSession('/zap/wrk/sessions/admin_content_layout', true);
@@ -19,48 +21,46 @@ test.describe.serial('レイアウト管理>削除のテスト', () => {
     }
     const browser = await chromium.launch();
     page = await browser.newPage();
-    await page.goto(url);
-  });
 
-  test('レイアウト管理ページを表示します', async () => {
-    await expect(page).toHaveTitle(/レイアウト管理/);
-  });
-
-  test('レイアウト新規作成ページに遷移します', async () => {
-    await Promise.all([
-      page.waitForNavigation(),
-      page.click('a:has-text("新規作成")')
-    ])
-    await expect(page).toHaveURL(/content\/layout\/new$/)
-  });
-
-  test('レイアウトを新規作成します', async () => {
-    await page.fill('#admin_layout_name', 'dummy-layout')
+    // レイアウトの新規作成
+    const layoutNmae = Math.random().toString(36).slice(2)
+    await page.goto(`${baseURL}/content/layout/new`)
+    await page.fill('#admin_layout_name', layoutNmae)
     await Promise.all([
       page.waitForNavigation(),
       page.click('.c-conversionArea .btn-ec-conversion')
     ])
     await expect(page.locator('.c-contentsArea .alert')).toContainText('保存しました')
+
+    // 削除ボタンと削除URLの取得
+    await page.goto(`${baseURL}/content/layout`)
+    deleteButton = page.locator(`.c-contentsArea .card:has-text("${layoutNmae}")`).locator('button')
+    const layoutId = (await deleteButton.getAttribute('data-url')).replace(/^.*\/(\d+)\/.*$/, '$1')
+    url = baseURL + `/content/layout/${layoutId}/delete`;
   });
 
-  test('レイアウト管理ページに戻ります', async () => {
-    await page.goto(url);
-    await expect(page).toHaveTitle(/レイアウト管理/);
-  });
-
-  test('作成したレイアウトを削除します', async () => {
-    await page.locator('.c-contentsArea .card:has-text("dummy-layout")').locator('button').click()
+  test('レイアウトを削除します', async () => {
+    await deleteButton.click()
     await Promise.all([
       page.waitForNavigation(),
       page.click('.btn-ec-delete')
     ])
     await expect(page.locator('.c-contentsArea .alert')).toContainText('削除しました')
-});
+  });
 
-  test.describe('テストを実行します[DELETE] @attack', () => {
+  test.describe('テストを実行します[POST] @attack', () => {
+    let message: HttpMessage;
+
+    test('HttpMessage を取得します', async () => {
+      const messages = await zapClient.getMessages(url, await zapClient.getNumberOfMessages(url), 1);
+      message = messages.pop();
+      expect(message.requestHeader).toContain(`POST ${url}`)
+      expect(message.responseHeader).toContain('HTTP/1.1 302 Found');
+    });
+
     let scanId: number;
     test('アクティブスキャンを実行します', async () => {
-      scanId = await zapClient.activeScanAsUser(url, 2, 55, false, null, 'DELETE');
+      scanId = await zapClient.activeScanAsUser(url, 2, 55, false, null, 'POST', message.requestBody);
       await intervalRepeater(async () => await zapClient.getActiveScanStatus(scanId), 5000, page);
     });
 
