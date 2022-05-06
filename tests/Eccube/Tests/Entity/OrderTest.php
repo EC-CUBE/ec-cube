3a9a5a24dc3b0d747b98eb439450e947e4af1190
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-05-06 13:43:30 +0900

    assertArraySubset 廃止に伴う修正
    see https://github.com/sebastianbergmann/phpunit/issues/3494

--- tests/Eccube/Tests/Entity/OrderTest.php
+++ tests/Eccube/Tests/Entity/OrderTest.php
@@ -192,7 +192,7 @@ class OrderTest extends EccubeTestCase
     public function testGetTaxableTotalByTaxRate()
     {
         $Order = $this->createTestOrder();
-        self::assertArraySubset([10 => 220, 8 => 216], $Order->getTaxableTotalByTaxRate());
+        self::assertSame([10 => 220, 8 => 216], $Order->getTaxableTotalByTaxRate());
     }
 
     public function testGetTaxableDiscountItems()
