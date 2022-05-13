e05bf925d15116913c2a57cda58087e81600b9af
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-05-13 10:51:07 +0900

    OrderStateMachine の仕様変更対応

--- app/config/eccube/packages/order_state_machine.php
+++ app/config/eccube/packages/order_state_machine.php
@@ -19,13 +19,12 @@ $container->loadFromExtension('framework', [
         'order' => [
             'type' => 'state_machine',
             'marking_store' => [
-                // 'type' => 'single_state',
-                // 'arguments' => 'status',
+                'type' => 'single_state'
             ],
             'supports' => [
                 OrderStateMachineContext::class,
             ],
-            // 'initial_place' => (string) Status::NEW,
+            'initial_marking' => (string) Status::NEW,
             'places' => [
                 (string) Status::NEW,
                 (string) Status::CANCEL,
