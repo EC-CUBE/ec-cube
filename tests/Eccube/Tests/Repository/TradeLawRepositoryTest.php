4b57333213095d428ebde594a5c2612b3ec86e4b
ギリングハムピアース <gillingham@sai-net.jp>
2022-06-20 10:23:07 +0900

    UT修正。

--- tests/Eccube/Tests/Repository/TradeLawRepositoryTest.php
+++ tests/Eccube/Tests/Repository/TradeLawRepositoryTest.php
@@ -26,20 +26,20 @@ class TradeLawRepositoryTest extends EccubeTestCase
         $this->assertEquals(15, count($initialTradeLawRows));
 
         $notFoundNames = [
-            '販売業者', '代表責任者', '所在地', '電話番号', 'メールアドレス', 'URL', '商品代金以外の必要料金',
-            '引き渡し時期', 'お支払方法', '返品・交換について'
+            1 => '販売業者', 2 => '代表責任者',3 => '所在地',4 => '電話番号',5 => 'メールアドレス',6 => 'URL', 7 => '商品代金以外の必要料金',
+            8 => '引き渡し時期', 9 => 'お支払方法', 10 => '返品・交換について'
         ];
 
         $foundTimes = 1;
 
-        foreach($initialTradeLawRows as $initialTradeLawRow) {
+        foreach($initialTradeLawRows as $sortOrder => $initialTradeLawRow) {
             // Check that all fields are turned off initially.
             $this->assertEquals(false, $initialTradeLawRow->isDisplayOrderScreen());
-            if (in_array($initialTradeLawRow->getName(), $notFoundNames)) {
-                $foundTimes++;
-            }
+            $this->assertEquals($sortOrder, $initialTradeLawRow->getSortNo());
+            $this->assertContains($initialTradeLawRow->getName(), $notFoundNames);
+            $this->assertNull($initialTradeLawRow->getDescription());
+            $foundTimes++;
         }
-
         // Check that initial key values are found.
         $this->assertEquals(10, $foundTimes);
     }
