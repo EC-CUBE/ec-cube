e96fa4c0b2ef3eaa2d291a124ae13d50565ced65
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2022-06-27 17:24:17 +0900

    インデント調整

--- src/Eccube/Repository/OrderRepository.php
+++ src/Eccube/Repository/OrderRepository.php
@@ -128,8 +128,8 @@ class OrderRepository extends AbstractRepository
             }
             $qb
                 ->andWhere('o.id = :multi OR CONCAT(o.name01, o.name02) LIKE :likemulti OR '.
-                             'CONCAT(COALESCE(o.kana01,\'\'), COALESCE(o.kana02,\'\')) LIKE :likemulti OR o.company_name LIKE :company_name OR '.
-                            'o.order_no LIKE :likemulti OR o.email LIKE :likemulti OR o.phone_number LIKE :likemulti')
+                    "CONCAT(COALESCE(o.kana01, ''), COALESCE(o.kana02, '')) LIKE :likemulti OR o.company_name LIKE :company_name OR ".
+                    'o.order_no LIKE :likemulti OR o.email LIKE :likemulti OR o.phone_number LIKE :likemulti')
                 ->setParameter('multi', $multi)
                 ->setParameter('likemulti', '%'.$clean_key_multi.'%')
                 ->setParameter('company_name', '%'.$searchData['multi'].'%'); // 会社名はスペースを除去せず検索
