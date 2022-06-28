69282b863ba94ca1abf40224d4b01492f1b82a3c
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-06-28 16:14:00 +0900

    インストーラのプラグイン有効化画面を修正

--- html/template/install/assets/js/bundle.js
+++ html/template/install/assets/js/bundle.js
@@ -1,5 +1,6 @@
 const $ = require('jquery');
 global.$ = global.jQuery = $;
 
-require('bootstrap');
 require('bootstrap/dist/css/bootstrap.min.css');
+const bootstrap = require('bootstrap');
+global.bootstrap = bootstrap;
