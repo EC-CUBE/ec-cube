ad63c083c5d53c63f90f7c2d1e73417780d22b7a
ギリングハムピアース <gillingham@sai-net.jp>
2022-06-30 19:14:10 +0900

    import miss?

--- pgsql/eccubedb_localhost-2022_06_30_17_25_15-dump.sql
+++ pgsql/eccubedb_localhost-2022_06_30_17_25_15-dump.sql
@@ -145,14 +145,14 @@ CREATE TABLE public.dtb_base_info (
     option_product_delivery_fee boolean DEFAULT false NOT NULL,
     option_product_tax_rule boolean DEFAULT false NOT NULL,
     option_customer_activate boolean DEFAULT true NOT NULL,
-    invoice_registration_number character varying(255) DEFAULT NULL::character varying,
     option_remember_me boolean DEFAULT true NOT NULL,
     authentication_key character varying(255) DEFAULT NULL::character varying,
     php_path character varying(255) DEFAULT NULL::character varying,
     option_point boolean DEFAULT true NOT NULL,
     basic_point_rate numeric(10,0) DEFAULT '1'::numeric,
     point_conversion_rate numeric(10,0) DEFAULT '1'::numeric,
-    discriminator_type character varying(255) NOT NULL
+    discriminator_type character varying(255) NOT NULL,
+    invoice_registration_number character varying(255) DEFAULT NULL::character varying
 );
 
 
