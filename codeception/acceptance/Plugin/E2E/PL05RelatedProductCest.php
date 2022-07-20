e451c3dc4c48ef8fa5147e32d1a0d51328cb776d
ギリングハムピアース <gillingham@sai-net.jp>
2022-07-20 12:59:11 +0900

    商品連携プラグイン 「１００％完了」

--- codeception/acceptance/Plugin/E2E/PL05RelatedProductCest.php
+++ codeception/acceptance/Plugin/E2E/PL05RelatedProductCest.php
@@ -17,6 +17,7 @@ use AcceptanceTester;
 use Codeception\Util\Fixtures;
 use Codeception\Util\Locator;
 use Eccube\Entity\Product;
+use function PHPUnit\Framework\assertStringContainsString;
 
 /**
  * @group plugin
@@ -24,7 +25,8 @@ use Eccube\Entity\Product;
  */
 class PL05RelatedProductCest
 {
-    private $product;
+    private Product $product;
+    private Product $relatedProduct;
 
     public function _before(AcceptanceTester $I)
     {
@@ -95,14 +97,18 @@ class PL05RelatedProductCest
         $I->retry(7, 400);
         $Product = $this->registerBaseProductDetails();
         $RelatedProduct = $this->registerBaseProductDetails();
+        $this->product = $Product;
+        $this->relatedProduct = $RelatedProduct;
         $I->amOnPage(sprintf('/admin/product/product/%s/edit', $Product->getId()));
         $I->see('関連商品');
         $I->clickWithLeftButton('#RelatedProduct-search0');
-        $I->fillField('#admin_search_product_id', $RelatedProduct->getId());
+        $I->retryFillField('#admin_search_product_id', $RelatedProduct->getId());
+        $I->clickWithLeftButton('#RelatedProductSearchButton');
         $I->retrySee($RelatedProduct->getName());
-        $I->clickWithLeftButton(Locator::contains('//tr', $RelatedProduct->getName()). '//button');
+        $I->clickWithLeftButton(Locator::contains('//tr', $RelatedProduct->getName()).'//button');
         $I->retrySee($RelatedProduct->getName());
-        $I->clickWithLeftButton('.btn.btn-ec-conversion.px-5.ladda-button');
+        $I->wait(5);
+        $I->retryClickWithLeftButton('.btn.btn-ec-conversion.px-5.ladda-button');
         $I->see('保存しました');
         $I->see($RelatedProduct->getName());
         // フロント側
@@ -112,9 +118,103 @@ class PL05RelatedProductCest
         $I->seeInSource($RelatedProduct->getMainListImage());
     }
 
+    public function related_6(AcceptanceTester $I)
+    {
+        $I->retry(7, 400);
+        $Product = $this->registerBaseProductDetails();
+        $RelatedProduct = $this->registerBaseProductDetails();
+        $identityID = bin2hex(random_bytes(20));
+        $I->amOnPage(sprintf('/admin/product/product/%s/edit', $Product->getId()));
+        $I->see('関連商品');
+        $I->clickWithLeftButton('#RelatedProduct-search0');
+        $I->retryFillField('#admin_search_product_id', $RelatedProduct->getId());
+        $I->clickWithLeftButton('#RelatedProductSearchButton');
+        $I->retrySee($RelatedProduct->getName());
+        $I->clickWithLeftButton(Locator::contains('//tr', $RelatedProduct->getName()).'//button');
+        $I->retrySee($RelatedProduct->getName());
+        $I->wait(5);
+        $I->fillField('#admin_product_RelatedProducts_0_content', $identityID);
+        $I->retryClickWithLeftButton('.btn.btn-ec-conversion.px-5.ladda-button');
+        $I->see('保存しました');
+        $I->see($RelatedProduct->getName());
+        // フロント側
+        $I->amOnPage(sprintf('/products/detail/%s', $Product->getId()));
+        $I->see($Product->getName());
+        $I->see($RelatedProduct->getName());
+        $I->see($identityID);
+        $I->seeInSource($RelatedProduct->getMainListImage());
+    }
+
+    public function related_7(AcceptanceTester $I)
+    {
+        $I->retry(7, 400);
+        $Product = $this->registerBaseProductDetails();
+        $RelatedProducts = [];
+        $RelatedProductsDescriptionIDs = [];
+        for ($i = 0; $i < 6; $i++) {
+            $RelatedProducts[] = $this->registerBaseProductDetails();
+            $RelatedProductsDescriptionIDs[] = bin2hex(random_bytes(20));
+        }
+
+        $I->amOnPage(sprintf('/admin/product/product/%s/edit', $Product->getId()));
+        $I->see('関連商品');
+
+        foreach ($RelatedProducts as $key => $relatedProduct) {
+            $I->clickWithLeftButton('#RelatedProduct-search'.$key);
+            $I->retryFillField('#admin_search_product_id', $relatedProduct->getId());
+            $I->clickWithLeftButton('#RelatedProductSearchButton');
+            $I->retrySee($relatedProduct->getName());
+            $I->clickWithLeftButton(Locator::contains('//tr', $relatedProduct->getName()).'//button');
+            $I->retrySee($relatedProduct->getName());
+            $I->wait(5);
+            $I->fillField('#admin_product_RelatedProducts_'.$key.'_content', $RelatedProductsDescriptionIDs[$key]);
+        }
+        $I->retryClickWithLeftButton('.btn.btn-ec-conversion.px-5.ladda-button');
+        $I->see('保存しました');
+        foreach ($RelatedProducts as $key => $relatedProduct) {
+            $I->see($relatedProduct->getName());
+            $I->see($RelatedProductsDescriptionIDs[$key]);
+        }
+        // フロント側
+        $I->amOnPage(sprintf('/products/detail/%s', $Product->getId()));
+        $I->see($Product->getName());
+        foreach ($RelatedProducts as $key => $relatedProduct) {
+            $I->see($relatedProduct->getName());
+            $I->see($RelatedProductsDescriptionIDs[$key]);
+            $I->seeInSource($relatedProduct->getMainListImage());
+        }
+    }
+
+    public function related_8(AcceptanceTester $I)
+    {
+
+        $this->related_5($I);
+        $I->amOnPage(sprintf('/admin/product/product/%s/edit', $this->product->getId()));
+        $I->see('関連商品');
+        $I->see($this->relatedProduct->getName());
+        $I->clickWithLeftButton('#RelatedProduct-delete0');
+        $I->wait(1);
+        $I->clickWithLeftButton('.btn.btn-ec-conversion.px-5.ladda-button');
+        $I->dontSee($this->relatedProduct->getName());
+        // フロント側
+        $I->amOnPage(sprintf('/products/detail/%s', $this->product->getId()));
+        $I->dontSee($this->relatedProduct->getName());
+    }
+
+    public function related_9(AcceptanceTester $I)
+    {
+        $this->related_5($I);
+        $I->clickWithLeftButton('(//div[@id="RelatedProduct-product_area"]//a)[1]');
+        $I->wait(5);
+        assertStringContainsString(sprintf('/products/detail/%s', $this->relatedProduct->getId()), $I->grabFromCurrentUrl());
+        $I->see($this->relatedProduct->getName());
+        $I->seeInSource($this->relatedProduct->getMainListImage());
+        $I->see($this->relatedProduct->getDescriptionDetail());
+    }
 
     /**
      * @param AcceptanceTester $I
+     *
      * @return Product
      */
     private function registerBaseProductDetails()
