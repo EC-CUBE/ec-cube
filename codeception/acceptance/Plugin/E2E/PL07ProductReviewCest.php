5432dffae2638d02f0315e591afea9ccc0b2041e
ギリングハムピアース <gillingham@sai-net.jp>
2022-07-25 18:37:18 +0900

    商品レビュープラグイン：元の状態に戻す。

--- codeception/acceptance/Plugin/E2E/PL07ProductReviewCest.php
+++ codeception/acceptance/Plugin/E2E/PL07ProductReviewCest.php
@@ -25,7 +25,6 @@ use Page\Front\ProductDetailPage;
 use Page\Front\ProductReviewPage;
 use Plugin\ProductReview42\Entity\ProductReview;
 use Plugin\ProductReview42\Entity\ProductReviewStatus;
-use Plugin\E2E\Utility\ReviewData;
 
 /**
  * @group plugin
@@ -449,5 +448,28 @@ class PL07ProductReviewCest
                 ]
             ]
         ];
+    }
+}
+
+class ReviewData
+{
+    public string $reviewer_name;
+    public string $reviewer_url;
+    public string $title;
+    public string $comment;
+
+    /**
+     *
+     * @param $reviewer_name
+     * @param $reviewer_url
+     * @param $title
+     * @param $comment
+     */
+    public function __construct($reviewer_name, $reviewer_url, $title, $comment)
+    {
+        $this->reviewer_name = $reviewer_name;
+        $this->reviewer_url = $reviewer_url;
+        $this->title = $title;
+        $this->comment = $comment;
     }
 }
