24cd7437062512d5ea7ab900a811e50c0be9079e
ギリングハムピアース <gillingham@sai-net.jp>
2022-07-29 20:44:30 +0900

    Web API Plugin test.

--- codeception/acceptance/Plugin/E2E/PL08ApiCest.php
+++ codeception/acceptance/Plugin/E2E/PL08ApiCest.php
@@ -17,7 +17,9 @@ use AcceptanceTester;
 use Codeception\Util\Fixtures;
 use Codeception\Util\Locator;
 use Eccube\Entity\Customer;
+use Eccube\Entity\Master\OrderStatus;
 use Eccube\Entity\Order;
+use Eccube\Entity\ProductStock;
 use Page\Admin\ApiOauthEditPage;
 use Page\Admin\ApiOauthPage;
 use Page\Admin\ApiWebHookEditPage;
@@ -38,28 +40,29 @@ class PL08ApiCest
     }
 
     /**
-     * @skip
+     * ⓪ インストール
+     *
+     * @group install
      * @param AcceptanceTester $I
      * @return void
+     * @throws \Exception
      */
     public function web_api_01(AcceptanceTester $I)
     {
-
+        if ($I->seePluginIsInstalled('Web API', true)) {
+            $I->wantToUninstallPlugin('Web API');
+            $I->seePluginIsNotInstalled('Web API');
+        }
+        $I->wantToInstallPlugin('Web API');
+        $I->seePluginIsInstalled('Web API');
     }
 
-    public function web_api_02(AcceptanceTester $I)
-    {
-        $I->amOnPage('/admin/store/plugin');
-        $recommendPluginRow = Locator::contains('//tr', 'Web API');
-        $I->see('Web API', "//tr[contains(.,'Web API')]");
-        $I->see('有効', $recommendPluginRow);
-        $I->clickWithLeftButton("(" . $recommendPluginRow . "//i[@class='fa fa-pause fa-lg text-secondary'])[1]");
-        $I->see('「Web API」を無効にしました。');
-        $I->see('Web API', $recommendPluginRow);
-        $I->see('無効', $recommendPluginRow);
-    }
-
-    public function web_api_03(\AcceptanceTester $I)
+    /**
+     * @group install
+     * @param AcceptanceTester $I
+     * @return void
+     */
+    public function web_api_02(\AcceptanceTester $I)
     {
         $I->amOnPage('/admin/store/plugin');
         $recommendPluginRow = Locator::contains('//tr', 'Web API');
@@ -72,22 +75,18 @@ class PL08ApiCest
     }
 
     /**
-     * @skip
-     * @param \AcceptanceTester $I
-     * @return void
+     * @group main
+     * @param AcceptanceTester $I
+     * @param bool $fixedLocalhost
+     * @return API0AuthData
      */
-    public function web_api_04(\AcceptanceTester $I)
-    {
-
-    }
-
-    public function web_api_05(AcceptanceTester $I, bool $fixedLocalhost = false): API0AuthData
+    public function web_api_03(AcceptanceTester $I, bool $fixedLocalhost = false): API0AuthData
     {
         $I->retry(7, 400);
         $faker = \Faker\Factory::create('ja_JP');
         $url = $faker->unique()->url();
         if ($fixedLocalhost === true) {
-            $url = 'http://localhost:8080/callback';
+            $url = 'http://localhost:8012/anything';
         }
         $I->amOnPage('admin/api/oauth');
         $I->see('OAuth管理');
@@ -118,9 +117,15 @@ class PL08ApiCest
         return $API0AuthData;
     }
 
-    public function web_api_06(AcceptanceTester $I)
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return void
+     * @throws \Exception
+     */
+    public function web_api_04(AcceptanceTester $I)
     {
-        $API0AuthData = $this->web_api_05($I);
+        $API0AuthData = $this->web_api_03($I);
         $I->amOnPage('admin/api/oauth');
         $I->see('OAuth管理');
         $testRow = Locator::contains('//tr', $API0AuthData->redirectUri);
@@ -134,11 +139,19 @@ class PL08ApiCest
         $I->dontSeeInSource($API0AuthData->clientSecret);
     }
 
-    public function web_api_07(AcceptanceTester $I)
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return APIWebhookData
+     */
+    public function web_api_05(AcceptanceTester $I, string $url = '')
     {
         $I->retry(7, 400);
         $faker = \Faker\Factory::create('ja_JP');
-        $url = $faker->unique()->url();
+        if(empty($url))  {
+            $url = $faker->unique()->url();
+        }
+
         $secret = $faker->unique()->md5();
         $webHookData = new APIWebHookData($url, $secret);
 
@@ -163,9 +176,14 @@ class PL08ApiCest
         return $webHookData;
     }
 
-    public function web_api_08(AcceptanceTester $I)
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return void
+     */
+    public function web_api_06(AcceptanceTester $I)
     {
-        $webHookData = $this->web_api_07($I);
+        $webHookData = $this->web_api_05($I);
         $testRow = Locator::contains('//tr', $webHookData->url);
         $I->click("(" . $testRow . "//a[@class='btn btn-ec-actionIcon action-delete'])[1]");
         $I->retrySee('WebHookを削除します');
@@ -175,17 +193,23 @@ class PL08ApiCest
         $I->dontSee($webHookData->url);
     }
 
-    public function web_api_09(AcceptanceTester $I)
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return mixed
+     */
+    public function web_api_07(AcceptanceTester $I)
     {
         $I->retry(7, 400);
-        $apiAuthData = $this->web_api_05($I, true);
+        $apiAuthData = $this->web_api_03($I, true);
 
         $I->amOnPage(
             sprintf(
-                "/admin/authorize?response_type=code&client_id=%s&client_secret=%s&redirect_uri=%s&scope=read&state=test",
+                "/admin/authorize?response_type=code&client_id=%s&client_secret=%s&redirect_uri=%s&scope=%s&state=test",
                 $apiAuthData->clientID,
                 $apiAuthData->clientSecret,
-                $apiAuthData->redirectUri
+                $apiAuthData->redirectUri,
+                'read%20write'
             )
         );
 
@@ -208,13 +232,18 @@ class PL08ApiCest
         assertJson($tokens);
         $jsonTokens = json_decode($tokens, true);
         assertNotEmpty($jsonTokens['access_token']);
-        assertEquals($jsonTokens['token_type'], 'Bearer');
+        assertEquals('Bearer', $jsonTokens['token_type']);
         return $jsonTokens;
     }
 
-    public function web_api_10(AcceptanceTester $I)
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return void
+     */
+    public function web_api_08(AcceptanceTester $I)
     {
-        $tokenData = $this->web_api_09($I);
+        $tokenData = $this->web_api_07($I);
         $I->amOnPage('/admin/api/webhook');
 
         $I->haveHttpHeader('Authorization', 'Bearer ' . $tokenData['access_token']);
@@ -233,9 +262,14 @@ class PL08ApiCest
         ]);
     }
 
-    public function web_api_11(AcceptanceTester $I)
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return void
+     */
+    public function web_api_09(AcceptanceTester $I)
     {
-        $tokenData = $this->web_api_09($I);
+        $tokenData = $this->web_api_07($I);
         $I->amOnPage('/admin/api/webhook');
         $ordersGen = Fixtures::get('createOrders');
         $customer = Fixtures::get('createCustomer');
@@ -260,8 +294,14 @@ class PL08ApiCest
         ]);
     }
 
-    public function web_api_12(AcceptanceTester $I) {
-        $tokenData = $this->web_api_09($I);
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return void
+     */
+    public function web_api_10(AcceptanceTester $I)
+    {
+        $tokenData = $this->web_api_07($I);
         $I->amOnPage('/admin/api/webhook');
         $customerFixture = Fixtures::get('createCustomer');
         /**
@@ -284,6 +324,146 @@ class PL08ApiCest
                 ]
             ]
         ]);
+    }
+
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return void
+     */
+    public function web_api_11(AcceptanceTester $I)
+    {
+        $tokenData = $this->web_api_07($I);
+        $I->amOnPage('/admin/api/webhook');
+        /**
+         * @var ProductStock $productStock ;
+         */
+        $productStock = $I->grabEntitiesFromRepository(ProductStock::class, [
+                'id' => 2,
+            ]
+        )[0];
+
+        $getProductClassCode = $productStock->getProductClass()->getCode();
+
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $tokenData['access_token']);
+        $I->haveHttpHeader('HTTP_AUTHORIZATION', 'Bearer ' . $tokenData['access_token']);
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $response = $I->sendPost('/api', [
+            'query' => 'mutation ($code: String!, $stock: Int, $stock_unlimited: Boolean!) { updateProductStock(code: $code, stock: $stock, stock_unlimited: $stock_unlimited) { code, stock, stock_unlimited } }',
+            'variables' => [
+                'code' => $getProductClassCode,
+                'stock' => 10,
+                'stock_unlimited' => false,
+            ]
+        ]);
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'data' => [
+                'updateProductStock' => [
+                    'code' => $getProductClassCode,
+                    'stock' => 10,
+                    'stock_unlimited' => false
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return void
+     */
+    public function web_api_12(AcceptanceTester $I)
+    {
+        $tokenData = $this->web_api_07($I);
+        $I->amOnPage('/admin/api/webhook');
+        $ordersGen = Fixtures::get('createOrders');
+        $customer = Fixtures::get('createCustomer');
+        /**
+         * @var Order $targetOrder
+         */
+        $targetOrder = $ordersGen($customer(), 1, [], OrderStatus::IN_PROGRESS)[0];
+
+        assertEquals($targetOrder->getOrderStatus()->getId(), OrderStatus::IN_PROGRESS);
+
+        $I->haveHttpHeader('Authorization', 'Bearer ' . $tokenData['access_token']);
+        $I->haveHttpHeader('HTTP_AUTHORIZATION', 'Bearer ' . $tokenData['access_token']);
+        $I->haveHttpHeader('Content-Type', 'application/json');
+        $response = $I->sendPost('/api', [
+            'query' => 'mutation ($id: ID!, $shipping_date: DateTime, $shipping_delivery_name: String, $tracking_number: String, $note: String, $is_send_mail: Boolean) { updateShipped(id: $id, shipping_date: $shipping_date, shipping_delivery_name: $shipping_delivery_name, tracking_number: $tracking_number, note: $note, is_send_mail: $is_send_mail ) { id, shipping_date, shipping_delivery_name, tracking_number, note }}',
+            'variables' => [
+                'id' => $targetOrder->getId(),
+                'shipping_date' => '2016-01-02T03:04:05+09:00',
+                'stock_unlimited' => false,
+                'shipping_delivery_name' => 'ヤマト運輸',
+                'tracking_number' => '123456789',
+                'note' => 'test',
+                'is_send_mail' => true,
+            ]
+        ]);
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+
+        $I->seeResponseContainsJson([
+            'data' => [
+                'updateShipped' => [
+                    'id' => (string)$targetOrder->getId(),
+                    'shipping_date' => '2016-01-01T18:04:05+00:00',
+                    'shipping_delivery_name' => 'ヤマト運輸',
+                    'tracking_number' => '123456789',
+                    'note' => 'test',
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return void
+     */
+    public function web_api_13(AcceptanceTester $I)
+    {
+        // @todo: Creating a mock server and reading the events of a webhook will take time there I will temporarily disable this test.
+        assertEquals(true, true);
+    }
+
+    /**
+     * @group main
+     * @param AcceptanceTester $I
+     * @return void
+     */
+    public function web_api_14(AcceptanceTester $I)
+    {
+        $I->amOnPage('/admin/store/plugin');
+        $recommendPluginRow = Locator::contains('//tr', 'Web API');
+        $I->see('Web API', "//tr[contains(.,'Web API')]");
+        $I->see('有効', $recommendPluginRow);
+        $I->clickWithLeftButton("(" . $recommendPluginRow . "//i[@class='fa fa-pause fa-lg text-secondary'])[1]");
+        $I->see('「Web API」を無効にしました。');
+        $I->see('Web API', $recommendPluginRow);
+        $I->see('無効', $recommendPluginRow);
+    }
+
+    /**
+     * @group main
+     * @param \AcceptanceTester $I
+     * @return void
+     * @throws \Exception
+     */
+    public function web_api_15(\AcceptanceTester $I)
+    {
+        // 無効処理
+        $I->amOnPage('/admin/store/plugin');
+        $I->retry(10, 1000);
+        $I->wantToUninstallPlugin('Web API');
+        // プラグインの状態を確認する
+        $xpath = Locator::contains('tr', 'Web API');
+        $I->see('インストール', $xpath);
     }
 }
 
