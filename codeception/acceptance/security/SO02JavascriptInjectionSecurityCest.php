808acbf63e9410b9c6535a6a607391cda32c9638
ギリングハムピアース <gillingham@sai-net.jp>
2022-08-10 12:25:30 +0900

    Javascript fix.

--- codeception/acceptance/security/SO02JavascriptInjectionSecurityCest.php
+++ codeception/acceptance/security/SO02JavascriptInjectionSecurityCest.php
@@ -33,13 +33,18 @@ class SO02JavascriptInjectionSecurityCest {
         $I->fillField('#order_Shipping_address_addr02', '<script>alert(\'message\')</script>');
         $I->clickWithLeftButton(Locator::contains('button', '登録'));
         $I->see('保存しました');
+        $I->scrollTo(Locator::contains('a', 'メールを作成'));
+        $I->wait(5);
         $I->clickWithLeftButton(Locator::contains('a', 'メールを作成'));
         $I->retrySee('ページを移動します');
         $I->retryClickWithLeftButton(Locator::contains('a', '保存せずに移動'));
         $I->see('メール通知');
         $I->wait(10);
         $I->selectOption('#template-change', '注文受付メール');
-        $I->canSee('<script>alert("message");</script>');
+        // Wait for template loading.
+        $I->wait(5);
+        // Because of the Field plugin unable to 'see' contents, using rawSource instead.
+        $I->retrySeeInSource('＜script＞alert("message");＜/script＞');
         $I->clickWithLeftButton(Locator::contains('button', '送信内容を確認'));
         $I->retrySee('＜script＞alert("message");＜/script＞');
         $I->wait(10);
@@ -59,6 +64,8 @@ class SO02JavascriptInjectionSecurityCest {
         } catch (NoSuchAlertException $e) {
             $I->comment('Javascript injection via order email page is not detected');
         }
+        $I->executeJS('window.scrollTo(0, document.body.scrollHeight);');
+        $I->wait(5);
         $I->clickWithLeftButton(Locator::contains('a', '[EC-CUBE SHOP] ご注文ありがとうございます'));
         $I->retrySee('この度はご注文いただき誠にありがとうございます。');
         try {
