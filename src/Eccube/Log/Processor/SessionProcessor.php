bc02bc4038f52ae98bf9899e4182f9ac53d68c6a
kurozumi <info@a-zumi.net>
2022-08-25 15:07:33 +0900

    SessionProcessor修正

--- src/Eccube/Log/Processor/SessionProcessor.php
+++ src/Eccube/Log/Processor/SessionProcessor.php
@@ -13,30 +13,37 @@
 
 namespace Eccube\Log\Processor;
 
+use Symfony\Component\HttpFoundation\Exception\SessionNotFoundException;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class SessionProcessor
 {
     /**
-     * @var SessionInterface
+     * @var RequestStack
      */
-    protected $session;
+    protected $requestStack;
 
     public function __construct(RequestStack $requestStack)
     {
-        $this->session = $requestStack->getSession();
+        $this->requestStack = $requestStack;
     }
 
     public function __invoke(array $records)
     {
         $records['extra']['session_id'] = 'N/A';
 
-        if (!$this->session->isStarted()) {
+        try {
+            $session = $this->requestStack->getSession();
+        } catch (SessionNotFoundException $e) {
             return $records;
         }
 
-        $records['extra']['session_id'] = substr(sha1($this->session->getId()), 0, 8);
+        if (!$session->isStarted()) {
+            return $records;
+        }
+
+        $records['extra']['session_id'] = substr(sha1($session->getId()), 0, 8);
 
         return $records;
     }
