f35897699d6053797b5d212e41a5a806f7617c07
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2022-08-25 11:58:34 +0900

    ファイル名チェック修正

--- src/Eccube/Controller/Admin/Content/FileController.php
+++ src/Eccube/Controller/Admin/Content/FileController.php
@@ -466,6 +466,9 @@ class FileController extends AbstractController
      */
     protected function checkDir($targetDir, $topDir)
     {
+        if (strpos($targetDir, '..') !== false) {
+            return false;
+        }
         $targetDir = realpath($targetDir);
         $topDir = realpath($topDir);
 
