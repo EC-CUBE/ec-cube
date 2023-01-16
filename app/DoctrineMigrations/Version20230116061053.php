9a96a096fc4bc4c9e1e410069cfdf21f3ce0f5ac
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2023-01-16 16:52:56 +0900

    postgresでSQLエラーとなるため修正

--- app/DoctrineMigrations/Version20230116061053.php
+++ app/DoctrineMigrations/Version20230116061053.php
@@ -21,7 +21,7 @@ final class Version20230116061053 extends AbstractMigration
         }
         $exists = $this->connection->fetchOne("SELECT count(*) FROM dtb_csv WHERE csv_type_id = 1 AND entity_name = ? AND field_name = 'visible'", ['Eccube\\\\Entity\\\\ProductClass']);
         if ($exists == 0) {
-            $this->addSql("INSERT INTO dtb_csv (csv_type_id, creator_id, entity_name, field_name, disp_name, sort_no, enabled, create_date, update_date, discriminator_type) VALUES (1, null, 'Eccube\\\\Entity\\\\ProductClass', 'visible', '商品規格表示フラグ', 32, 0, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'csv')");
+            $this->addSql("INSERT INTO dtb_csv (csv_type_id, creator_id, entity_name, field_name, disp_name, sort_no, enabled, create_date, update_date, discriminator_type) VALUES (1, null, 'Eccube\\\\Entity\\\\ProductClass', 'visible', '商品規格表示フラグ', 32, false, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'csv')");
         }
     }
 
