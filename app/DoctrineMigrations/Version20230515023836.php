f47cab91c8b02e4d69ef3dfa6f8a049786b7646a
shinya <pemomomo@gmail.com>
2023-06-02 18:04:14 +0900

    Migration時の分岐処理を追加

--- app/DoctrineMigrations/Version20230515023836.php
+++ app/DoctrineMigrations/Version20230515023836.php
@@ -25,11 +25,12 @@ final class Version20230515023836 extends AbstractMigration
 {
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-
-        $this->addSql("
-            INSERT INTO dtb_mail_template (creator_id, name, file_name, mail_subject, create_date, update_date, discriminator_type)
-            VALUES (null, '会員情報変更通知メール', 'Mail/customer_change_notify.twig', '会員情報変更のお知らせ', '2017-03-07 10:14:52', '2017-03-07 10:14:52', 'mailtemplate');");
+        $exists = $this->connection->fetchOne("SELECT count(*) FROM dtb_mail_template WHERE file_name = 'Mail/customer_change_notify.twig'");
+        if ($exists == 0) {
+            $this->addSql("
+                INSERT INTO dtb_mail_template (creator_id, name, file_name, mail_subject, create_date, update_date, discriminator_type)
+                VALUES (null, '会員情報変更通知メール', 'Mail/customer_change_notify.twig', '会員情報変更のお知らせ', '2017-03-07 10:14:52', '2017-03-07 10:14:52', 'mailtemplate');");
+        }
     }
 
     public function down(Schema $schema): void
