14f1555150e261d7fc8703c8333623c08f7bb6f8
Piers Gillingham <gillingham@sai-net.jp>
2023-11-02 10:29:21 +0900

    LEVEL 6の修正。

--- src/Eccube/Service/PurchaseFlow/PurchaseContext.php
+++ src/Eccube/Service/PurchaseFlow/PurchaseContext.php
@@ -67,21 +67,35 @@ class PurchaseContext extends \SplObjectStorage
         return $this->user;
     }
 
+    /**
+     * @param string $flowType
+     *
+     * @return void
+     */
     public function setFlowType($flowType)
     {
         $this->flowType = $flowType;
     }
 
+    /**
+     * @return bool
+     */
     public function isOrderFlow()
     {
         return $this->flowType === self::ORDER_FLOW;
     }
 
+    /**
+     * @return bool
+     */
     public function isShoppingFlow()
     {
         return $this->flowType === self::SHOPPING_FLOW;
     }
 
+    /**
+     * @return bool
+     */
     public function isCartFlow()
     {
         return $this->flowType === self::CART_FLOW;
