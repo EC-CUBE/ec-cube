2f33f4b863b93badd367b9e27378ec4e02064870
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2023-12-19 11:04:26 +0900

    Fix "Input value contains a non-scalar value"

--- src/Eccube/Controller/Admin/Product/ProductController.php
+++ src/Eccube/Controller/Admin/Product/ProductController.php
@@ -631,8 +631,7 @@ class ProductController extends AbstractController
 
                 $this->entityManager->flush();
 
-                if (array_key_exists('product_image', $request->request->get('admin_product'))) {
-                    $product_image = $request->request->get('admin_product')['product_image'];
+                if ($product_image = $request->request->all()['admin_product']['product_image'] ?? []) {
                     foreach ($product_image as $sortNo => $filename) {
                         $ProductImage = $this->productImageRepository
                             ->findOneBy([
