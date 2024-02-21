3b34131e8bfb97942e4dae809cba20f6bda13dc5
kurozumi <info@a-zumi.net>
2024-02-21 20:42:09 +0900

    切り出されているプロセッサーを修正

--- src/Eccube/DependencyInjection/Compiler/PurchaseFlowPass.php
+++ src/Eccube/DependencyInjection/Compiler/PurchaseFlowPass.php
@@ -28,12 +28,12 @@ class PurchaseFlowPass implements CompilerPassInterface
 {
     use PriorityTaggedServiceTrait;
 
-    public const ITEM_PREPROCESSOR_TAG = 'eccube.item.preprocessor';
     public const ITEM_VALIDATOR_TAG = 'eccube.item.validator';
-    public const ITEM_HOLDER_PREPROCESSOR_TAG = 'eccube.item.holder.preprocessor';
     public const ITEM_HOLDER_VALIDATOR_TAG = 'eccube.item.holder.validator';
-    public const ITEM_HOLDER_POST_VALIDATOR_TAG = 'eccube.item.holder.post.validator';
+    public const ITEM_PREPROCESSOR_TAG = 'eccube.item.preprocessor';
+    public const ITEM_HOLDER_PREPROCESSOR_TAG = 'eccube.item.holder.preprocessor';
     public const DISCOUNT_PROCESSOR_TAG = 'eccube.discount.processor';
+    public const ITEM_HOLDER_POST_VALIDATOR_TAG = 'eccube.item.holder.post.validator';
     public const PURCHASE_PROCESSOR_TAG = 'eccube.purchase.processor';
 
     public function process(ContainerBuilder $container)
