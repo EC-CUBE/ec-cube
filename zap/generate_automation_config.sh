591dfb9baacbf187fa5346fb5f2dbec88cbca9e8
zeniya <hiroaki_zeniya@ec-cube.co.jp>
2026-02-25 18:49:04 +0900

    ZAP EOSL対応

--- zap/generate_automation_config.sh
+++ zap/generate_automation_config.sh
@@ -24,7 +24,7 @@ if [[ -z "${ZA_CONTEXT}" ]]; then
       action: add
       type: standalone
       name: forceuser
-      file: /zap/wrk/scripts/forceuser.groovy
+      file: /zap/scripts/forceuser.groovy
 
   - type: script
     parameters:
