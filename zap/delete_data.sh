65eafdc05cbb33b17acbbbf9d4bedbfd78f3ffe9
zeniya <hiroaki_zeniya@ec-cube.co.jp>
2026-03-10 17:20:55 +0900

    ZAP EOSL対応

--- zap/delete_data.sh
+++ zap/delete_data.sh
@@ -15,6 +15,9 @@ delete from dtb_payment where create_date between now() - interval '24:00' and n
 delete from dtb_block_position where layout_id in (select id from dtb_layout where create_date between now() - interval '24:00' and now() - interval '00:00:05');
 delete from dtb_block where create_date between now() - interval '24:00' and now() - interval '00:00:06' and id not in (select distinct block_id from dtb_block_position);
 delete from dtb_layout where create_date between now() - interval '24:00' and now() - interval '00:00:06';
+delete from dtb_category where create_date between now() - interval '24:00' and now() - interval '00:30:00';
+delete from dtb_class_category where class_name_id in (select id from dtb_class_name where create_date between now() - interval '24:00' and now() - interval '00:30:00');
+delete from dtb_class_name where create_date between now() - interval '24:00' and now() - interval '00:30:00';
 delete from dtb_member where create_date between now() - interval '24:00' and now() - interval '00:00:06' and id > ${MAX_MEMBER_ID};
 delete from dtb_template where create_date between now() - interval '24:00' and now() - interval '00:00:06';
 delete from dtb_tag where id > ${MAX_TAG_ID};";
